61a2f7902c19788a1794cb365c3a4368d1debd4c
Nikolay Vyahhi <vyahhi@gmail.com>
2011-02-22 18:08:13 +0300

    another Parser stackdump fix

--- assembler/src/parser.hpp
+++ assembler/src/parser.hpp
@@ -21,9 +21,9 @@ public:
 	bool eof();
 	void close();
 private:
-	gzFile fp1, fp2;
-	kseq_t *seq1, *seq2;
-	bool _eof_bit;
+	gzFile _fp1, _fp2;
+	kseq_t *_seq1, *_seq2;
+	bool _eof;
 	bool _opened; // ready for read
 	int _cnt;
 	void do_read(); // do actual read, it's one sequence ahead of read()
@@ -35,68 +35,71 @@ private:
 
 template <int size>
 MatePair<size> FASTQParser<size>::read() {
-	assert(this->_opened);
+	assert(_opened);
 	// invariant: actual read from file was already done by do_read();
 	// assert that they are mate pairs (have: name/1 and name/2)!
-	assert(strncmp(this->seq1->name.s, this->seq2->name.s, this->seq1->name.l - 1) == 0);
+	assert(strncmp(_seq1->name.s, _seq2->name.s, _seq1->name.l - 1) == 0);
 	// assume that all paired reads have the same length
-	assert(this->seq1->seq.l == this->seq2->seq.l);
+	assert(_seq1->seq.l == _seq2->seq.l);
 	// if there is 'N' in sequence, then throw out this mate read
-	for (char *si1 = this->seq1->seq.s, *si2 = this->seq2->seq.s; *si1 != 0; ++si1, ++si2) {
+	for (char *si1 = _seq1->seq.s, *si2 = _seq2->seq.s; *si1 != 0; ++si1, ++si2) {
 		if (*si1 == 'N' || *si2 == 'N') {
 			this->do_read();
 			return MatePair<size>::null;
 		}
 	}
 	// fill result
-	MatePair<size> res(this->seq1->seq.s, this->seq2->seq.s, this->_cnt++);
+	MatePair<size> res(_seq1->seq.s, _seq2->seq.s, _cnt++);
 	// make actual read for the next result
-	this->do_read();
+	do_read();
 	return res;
 }
 
 template <int size>
 FASTQParser<size>::FASTQParser() {
-	this->_opened = false;
+	_opened = false;
 }
 
 template <int size>
 void FASTQParser<size>::open(std::string filename1, std::string filename2) {
-	this->fp1 = gzopen(filename1.c_str(), "r"); // STEP 2: open the file handler
-	this->fp2 = gzopen(filename2.c_str(), "r"); // STEP 2: open the file handler
+	_fp1 = gzopen(filename1.c_str(), "r"); // STEP 2: open the file handler
+	_fp2 = gzopen(filename2.c_str(), "r"); // STEP 2: open the file handler
 	bool fail = false;
-	if (!this->fp1) { std::cerr << "File " << filename1 << " not found!" << std::endl; fail = true; }
-	if (!this->fp2) { std::cerr << "File " << filename2 << " not found!" << std::endl; fail = true; }
-	if (fail) { this->_eof_bit = true; this->_opened = true; return; } // behave like it's empty file
-	this->seq1 = kseq_init(fp1); // STEP 3: initialize seq
-	this->seq2 = kseq_init(fp2); // STEP 3: initialize seq
-	this->_eof_bit = false;
-	this->_cnt = 0;
-	this->do_read();
-	this->_opened = true;
+	if (!_fp1) { std::cerr << "File " << filename1 << " not found!" << std::endl; fail = true; }
+	if (!_fp2) { std::cerr << "File " << filename2 << " not found!" << std::endl; fail = true; }
+	if (fail) {
+		_opened = false;
+		return;
+	} // behave like it's empty file
+	_seq1 = kseq_init(_fp1); // STEP 3: initialize seq
+	_seq2 = kseq_init(_fp2); // STEP 3: initialize seq
+	_opened = true;
+	_eof = false;
+	_cnt = 0;
+	do_read();
 }
 
 template <int size>
 bool FASTQParser<size>::eof() {
-	return this->_eof_bit;
+	return !_opened || _eof;
 }
 
 template <int size>
 void FASTQParser<size>::close() {
-	if (!this->_opened) {
+	if (!_opened) {
 		return;
 	}
-	kseq_destroy(this->seq1); // STEP 5: destroy seq
-	kseq_destroy(this->seq2); // STEP 5: destroy seq
-	gzclose(this->fp1); // STEP 6: close the file handler
-	gzclose(this->fp2); // STEP 6: close the file handler
-	this->_opened = false;
+	kseq_destroy(_seq1); // STEP 5: destroy seq
+	kseq_destroy(_seq2); // STEP 5: destroy seq
+	gzclose(_fp1); // STEP 6: close the file handler
+	gzclose(_fp2); // STEP 6: close the file handler
+	_opened = false;
 }
 
 template <int size>
 void FASTQParser<size>::do_read() {
-	if (kseq_read(this->seq1) < 0 || kseq_read(this->seq2) < 0) { // STEP 4: read sequence
-		this->_eof_bit = true;
+	if (kseq_read(_seq1) < 0 || kseq_read(_seq2) < 0) { // STEP 4: read sequence
+		_eof = true;
 	}
 }
 #endif
