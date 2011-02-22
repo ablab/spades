bdbe69600b6e65a18551f5c727a10ccb4a0d5d3e
Nikolay Vyahhi <vyahhi@gmail.com>
2011-02-22 17:55:57 +0300

    Parser stackdump fix

--- assembler/src/parser.hpp
+++ assembler/src/parser.hpp
@@ -23,9 +23,9 @@ public:
 private:
 	gzFile fp1, fp2;
 	kseq_t *seq1, *seq2;
-	bool eof_bit;
-	bool opened; // ready for read
-	int cnt;
+	bool _eof_bit;
+	bool _opened; // ready for read
+	int _cnt;
 	void do_read(); // do actual read, it's one sequence ahead of read()
 };
 
@@ -35,7 +35,7 @@ private:
 
 template <int size>
 MatePair<size> FASTQParser<size>::read() {
-	assert(this->opened);
+	assert(this->_opened);
 	// invariant: actual read from file was already done by do_read();
 	// assert that they are mate pairs (have: name/1 and name/2)!
 	assert(strncmp(this->seq1->name.s, this->seq2->name.s, this->seq1->name.l - 1) == 0);
@@ -49,7 +49,7 @@ MatePair<size> FASTQParser<size>::read() {
 		}
 	}
 	// fill result
-	MatePair<size> res(this->seq1->seq.s, this->seq2->seq.s, this->cnt++);
+	MatePair<size> res(this->seq1->seq.s, this->seq2->seq.s, this->_cnt++);
 	// make actual read for the next result
 	this->do_read();
 	return res;
@@ -57,7 +57,7 @@ MatePair<size> FASTQParser<size>::read() {
 
 template <int size>
 FASTQParser<size>::FASTQParser() {
-	this->opened = false;
+	this->_opened = false;
 }
 
 template <int size>
@@ -67,33 +67,36 @@ void FASTQParser<size>::open(std::string filename1, std::string filename2) {
 	bool fail = false;
 	if (!this->fp1) { std::cerr << "File " << filename1 << " not found!" << std::endl; fail = true; }
 	if (!this->fp2) { std::cerr << "File " << filename2 << " not found!" << std::endl; fail = true; }
-	if (fail) { this->eof_bit = true; this->opened = true; return; } // behave like it's empty file
+	if (fail) { this->_eof_bit = true; this->_opened = true; return; } // behave like it's empty file
 	this->seq1 = kseq_init(fp1); // STEP 3: initialize seq
 	this->seq2 = kseq_init(fp2); // STEP 3: initialize seq
-	this->eof_bit = false;
-	this->cnt = 0;
+	this->_eof_bit = false;
+	this->_cnt = 0;
 	this->do_read();
-	this->opened = true;
+	this->_opened = true;
 }
 
 template <int size>
 bool FASTQParser<size>::eof() {
-	return this->eof_bit;
+	return this->_eof_bit;
 }
 
 template <int size>
 void FASTQParser<size>::close() {
+	if (!this->_opened) {
+		return;
+	}
 	kseq_destroy(this->seq1); // STEP 5: destroy seq
 	kseq_destroy(this->seq2); // STEP 5: destroy seq
 	gzclose(this->fp1); // STEP 6: close the file handler
 	gzclose(this->fp2); // STEP 6: close the file handler
-	this->opened = false;
+	this->_opened = false;
 }
 
 template <int size>
 void FASTQParser<size>::do_read() {
 	if (kseq_read(this->seq1) < 0 || kseq_read(this->seq2) < 0) { // STEP 4: read sequence
-		eof_bit = true;
+		this->_eof_bit = true;
 	}
 }
 #endif
