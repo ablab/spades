30a4cbfdc3e0e6bf371fba67ed6ae6247e90b729
Mikhail <misha@DARNLEY>
2011-02-22 21:51:11 +0300

    Basic graph infrastructure, parameters file

---
+++ assembler/src/abruijn/parameters.h
@@ -0,0 +1,23 @@
+/*
+ * parameters.h
+ *
+ *  Created on: 22.02.2011
+ *      Author: misha
+ */
+
+#ifndef PARAMETERS_H_
+#define PARAMETERS_H_
+
+#include "../seq.hpp"
+
+#define MPSIZE 100
+#define K 25
+
+typedef Seq<K> Kmer;
+typedef Seq<MPSIZE> Read;
+
+using namespace std;
+
+static pair<string,string> filenames = make_pair("./data/s_6_1.fastq.gz", "./data/s_6_2.fastq.gz");
+
+#endif /* PARAMETERS_H_ */
