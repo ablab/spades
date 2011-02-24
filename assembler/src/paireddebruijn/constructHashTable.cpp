a1ae1cdd7ac9350e7bf9888d97d5a469ecbd09bb
Dmitry Antipov <dmitrij.antipov@gmail.com>
2011-02-24 16:18:15 +0300

    fix

--- assembler/src/paireddebruijn/constructHashTable.cpp
+++ assembler/src/paireddebruijn/constructHashTable.cpp
@@ -1,69 +1,86 @@
 #include "common.h"
 
-
 using namespace std;
 
-typedef __gnu_cxx::hash_map<int, vector<int> > myMap;
+//typedef __gnu_cxx::hash_map<ll, vector<ll> > myMap;
+typedef map<ll, vector<ll> > myMap;
+
 myMap pairedTable;
 int k;
 int l;
 int read_length;
 
-inline int value(char a){
-	if (a == 'A') return 0;
-	else if (a == 'C') return 1;
-	else if (a == 'T') return 2;
-	else if (a == 'G') return 3;
+inline int value(char a) {
+	if (a == 'A')
+		return 0;
+	else if (a == 'C')
+		return 1;
+	else if (a == 'T')
+		return 2;
+	else if (a == 'G')
+		return 3;
 	else {
 		std::cerr << "oops!";
-		exit;
+		return -1;
 	}
 
 }
-int main(){
-	freopen("config.ini", "r", stdin);
-	scanf ("Upper k-mer size = %d",&k);
-	scanf ("Lower k-mer size = %d",&l);
-	freopen("reads.txt", "r", stdin);
+int main() {
+	//freopen("config.ini", "r", stdin);
+	//scanf ("Upper k-mer size = %d",&k);
+	//scanf ("Lower k-mer size = %d",&l);
+	freopen("./data/reads.txt", "r", stdin);
 	l = 31;
 	k = 25;
-	ll upper_cut = (((ll) 1) << (2 * l)) - 1;
+	ll upper_cut = (((ll) 1) << (2 * k)) - 1;
 
-	ll lower_cut = (((ll) 1) << (2 * k)) - 1;
+	ll lower_cut = (((ll) 1) << (2 * l)) - 1;
 	read_length = 100;
 	int shift = (l - k) / 2;
-	int maxn = 1000000;
-	forn(i, maxn) {
-
+	int maxn = 1 << 20;
+	int read_num = 0;
+	while (1) {
+		if (!(read_num & 1023))
+			cerr << "read:" << read_num << endl;
+		read_num++;
 		char r1[102];
 		char r2[102];
 		char rr1[102];
 		char rr2[102];
-		scanf("%s %s", rr1, rr2);
+		int n = scanf("%s %s", &rr1, &rr2);
+		if (n != 2)
+			break;
+		//cerr << n;
+		//cerr<< rr1;
+		//return 0;
 		forn(i, read_length) {
 			r1[i] = value(rr1[i]);
 			r2[i] = value(rr2[i]);
 		}
 		ll upper = 0;
 		ll lower = 0;
-		forn(j, l) {
-			upper = upper << 2;
-			upper += r1[j];
-		}
 		forn(j, k) {
-			lower = lower << 2;
-			lower += r1[j + shift];
+			upper = upper << 2;
+			upper += r1[j + shift];
 		}
+		forn(j, l) {
+			lower = lower << 2;
+			lower += r2[j];
+		}
+		ll upper_max = ((ll) 1) << 45;
 		forn(j, read_length - l) {
-			if (pairedTable.find(upper) != pairedTable.end())
-				pairedTable[upper].pb(lower);
-			else {
-				vector<ll> tmp;
-				tmp.pb(lower);
-				pairedTable.insert(make_pair(lower,tmp));
+			//if ((upper > 0) && (upper < upper_max))
+			if (1){
+				if (pairedTable.find(upper) != pairedTable.end())
+					pairedTable[upper].pb(lower);
+				else {
+					vector<ll> tmp;
+					tmp.pb(lower);
+					pairedTable.insert(make_pair(upper, tmp));
+				}
 			}
-			upper <<=2;
-			lower <<=2;
+			upper <<= 2;
+			lower <<= 2;
 
 			upper += r1[j + l - shift];
 			lower += r2[j + l];
@@ -72,12 +89,19 @@ int main(){
 			lower &= lower_cut;
 		}
 	}
-	for (myMap::iterator iter = pairedTable.begin(); iter !=pairedTable.end(); iter++ ) {
+	freopen("./data/reads.out", "w", stdout);
+	int j = 0;
+	for (myMap::iterator iter = pairedTable.begin(); iter != pairedTable.end(); iter++) {
 		pair<ll, vector<ll> > p = (*iter);
-		cerr << p.fi << endl;
-		forn(i, p.se.size()){
-			cerr<< p.se[i]<<" ";
+		//		cerr<<j<<endl;
+		cout << p.fi << endl;
+		//		cerr << p.fi << endl;
+		forn(i, p.se.size()) {
+			cout << p.se[i] << " ";
 		}
-
+		cout << endl << endl;
+		if (!(j & 1023))
+			cerr << j << endl;
+		j++;
 	}
 }
