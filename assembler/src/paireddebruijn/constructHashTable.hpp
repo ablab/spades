f898272ca5f009c984fb94482f36f9984a588a96
Anton <anton.bankevich@gmail.com>
2011-02-25 21:19:05 +0300

    The last header added at last`

---
+++ assembler/src/paireddebruijn/constructHashTable.hpp
@@ -0,0 +1,8 @@
+#ifndef CONSTRUCTHASHTABLE_HPP_
+#define CONSTRUCTHASHTABLE_HPP_
+#include "string"
+using namespace std;
+
+void readsToPairs(char *inputFile, char *outputFile);
+
+#endif /* CONSTRUCTHASHTABLE_HPP_ */
