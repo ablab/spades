7bed0c6efc141476cc5aa0e7657a9a48c337d32c
Sasha Kulikov <alexander.s.kulikov@gmail.com>
2011-02-25 19:15:12 +0300

    small graph drawing rewritten

--- assembler/src/abruijn/main.cpp
+++ assembler/src/abruijn/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "graph.h"
-#include "graphBuilder.h"
+//#include "graphBuilder.h"
+#include "toyexamples.h"
 
 int main() {
-	std::cout << "Hello, A Bruijn!";
-	CGraph graph = GraphBuilder().build();
+  std::cout << "Hello, A Bruijn!";
+  //CGraph graph = GraphBuilder().build();
+
+  ConstructDeBruijnGraph ( "ACTGTACGTAC", 4 );
+
 }
