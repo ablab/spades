53dce1071548b7b98a43bcd66ee58319a3704795
Anton <anton.bankevich@gmail.com>
2011-02-28 17:23:17 +0300

    Paired graph structure temporarily simplified

--- assembler/src/paireddebruijn/pairedGraph.hpp
+++ assembler/src/paireddebruijn/pairedGraph.hpp
@@ -1,5 +1,6 @@
-#include <vector>
-#include "hashTable.h"
+#include "vector"
+#include "../seq.hpp"
+//#include "hashTable.h"
 using namespace std;
 
 #ifndef CONDENSED_GRAPH_H_
@@ -9,60 +10,74 @@ namespace paired_assembler {
 
 typedef int Kmer;
 
-class Sequence {
-	char *_nucleotides;
-	short _length;
-public:
-	Sequence(char *nucleotides, short length) : _nucleotides(nucleotides), _length(length)
-	{}
-	char operator[](const int &index);
-};
+//class Sequence {
+//	char *_nucleotides;
+//	short _length;
+//public:
+//	Sequence(char *nucleotides, short length) : _nucleotides(nucleotides), _length(length)
+//	{}
+//	char operator[](const int &index);
+//};
 
 class Vertex;
 
-struct Arc {
-	short _coverage;
-	Vertex* _head;
-};
+//struct Arc {
+//	short _coverage;
+//	Vertex* _head;
+//};
 
 class Vertex {
-	int _coverage;
-	Sequence *_upper;
-	Sequence *_lower;
-	int _neighbours_count;
-	Arc* _neighbours;
-	short _delta_d;
-	Vertex *real_vertex;
+	//	int _coverage;
+	Sequence *upper;
+	Sequence *lower;
+	vector<Vertex*> neighbours;
+	//	Arc* _neighbours;
+	//	int _neighbours_count;
+	//	short _delta_d;
+	//	Vertex *real_vertex;
 public:
-	Vertex(int coverage, int length, Sequence *kmer, Sequence *pair, bool direction, int delta_d);
+	//	Vertex(int coverage, int length, Sequence *kmer, Sequence *pair, bool direction, int delta_d);
+	Vertex(Sequence *up, Sequence *low) {
+		upper = up;
+		lower = low;
+	}
 
-	int coverage() {return _coverage;};
+	void glue(Sequence *up, Sequence *low, int glueDepth);
+	//
+	//	int coverage() {return _coverage;};
+	//
+	//	int neighbours_count() {return _neighbours_count;};
+	//
+	//	int addEdge(Vertex *neighbour, short coverage);
+	//
+	//	vector<Vertex*> getEdges();
+	//
+	//	Kmer getKmer(int position);
 
-	int neighbours_count() {return _neighbours_count;};
-
-	int addEdge(Vertex *neighbour, short coverage);
-
-	void getEdges(Arc *begin, Arc *end);
-
-	Kmer getKmer(int position);
 };
 
 class Graph {
-	HashTable map;
-
-	int merge(Vertex *u, Vertex* v);
-
-	int split(Vertex *u, Vertex* v, short position);
+	//	HashTable map;
+	//
+	//	int merge(Vertex *u, Vertex* v);
+	//
+	//	int split(Vertex *u, Vertex* v, short position);
+	vector<Vertex *> vertices;
+	Vertex *addVertex(Sequence *upper, Sequence *lower) {
+		Vertex *newVertex = new Vertex(upper, lower);
+		vertices.push_back(newVertex);
+		return newVertex;
+	}
 };
 
-class GraphIterator {
-public:
-	GraphIterator(Graph *graph);
-
-	Vertex *nextVertex();
-
-	bool hasNext();
-};
+//class GraphIterator {
+//public:
+//	GraphIterator(Graph *graph);
+//
+//	Vertex *nextVertex();
+//
+//	bool hasNext();
+//};
 
 }
 #endif /* CONDENSED_GRAPH_H_ */
