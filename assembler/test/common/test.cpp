08a6230182f91744f3a4bf893bb91d32d274760a
Nikolay Vyahhi <vyahhi@gmail.com>
2011-03-02 22:33:20 +0300

    parser simplification, nucl/parser tests abstract assembler

--- assembler/test/common/test.cpp
+++ assembler/test/common/test.cpp
@@ -2,11 +2,15 @@
 #include "ide_listener.h"
 #include "cute_runner.h"
 #include "seqTest.hpp"
+#include "parserTest.hpp"
+#include "nuclTest.hpp"
 
 void runSuite() {
 	 cute::suite s;
 	 //TODO add your test here
 	 s += SeqSuite();
+	 s += ParserSuite();
+	 s += NuclSuite();
 	 cute::ide_listener lis;
 	 cute::makeRunner(lis)(s, "The Suite");
  }
