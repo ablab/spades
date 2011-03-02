b46d2669a006dacb0641842f2b33437ee8685e2f
Anton <anton.bankevich@gmail.com>
2011-03-02 20:14:52 +0300

    tests for paired graph

--- assembler/src/paireddebruijn/constructHashTable.hpp
+++ assembler/src/paireddebruijn/constructHashTable.hpp
@@ -1,8 +1,11 @@
 #ifndef CONSTRUCTHASHTABLE_HPP_
 #define CONSTRUCTHASHTABLE_HPP_
+#include "common.hpp"
 #include "string"
 using namespace std;
 
 void readsToPairs(string inputFile, string outputFile);
 int pairsToSequences(string inputFile, string outputFile);
+string decompress(ll a, int l);
+void codeRead(char *read, char *code);
 #endif /* CONSTRUCTHASHTABLE_HPP_ */
