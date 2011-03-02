a23ee81ff947b594b3679b2ba3a8cfa10bde5af4
Sergey Nurk <sergeynurk@gmail.com>
2011-03-02 15:12:01 +0300

    test structure added

---
+++ assembler/src/nucl.hpp
@@ -0,0 +1,14 @@
+/*
+ * nucl.hpp
+ *
+ *  Created on: 01.03.2011
+ *      Author: vyahhi
+ */
+
+#ifndef NUCL_HPP_
+#define NUCL_HPP_
+
+char complement(char c); // 0123 -> 3210
+char nucl(char c); // 0123 -> ACGT
+
+#endif /* NUCL_HPP_ */
