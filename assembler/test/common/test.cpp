571fb3be01ab39fee13e01ac7948f3a5a3cd881e
Nikolay Vyahhi <vyahhi@gmail.com>
2011-03-02 21:17:58 +0300

    graphVisualizer fixes

--- assembler/test/common/test.cpp
+++ assembler/test/common/test.cpp
@@ -4,11 +4,11 @@
 #include "seqTest.hpp"
 
 void runSuite() {
-         cute::suite s;
-         //TODO add your test here
-         s += SeqSuite();
-         cute::ide_listener lis;
-         cute::makeRunner(lis)(s, "The Suite");
+	 cute::suite s;
+	 //TODO add your test here
+	 s += SeqSuite();
+	 cute::ide_listener lis;
+	 cute::makeRunner(lis)(s, "The Suite");
  }
 
  int main() {
