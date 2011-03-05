6bd56928144bc0f992f31b8f5795d92e9873d11a
Sergey Nurk <sergeynurk@gmail.com>
2011-03-06 01:26:00 +0300

    commin

--- assembler/src/debruijn/assembler.cpp
+++ assembler/src/debruijn/assembler.cpp
@@ -6,25 +6,28 @@
  */
 
 #include "ireadstream.hpp"
+#include "condensedGraph.hpp"
 #include <cassert>
 #include <iostream>
 #include <cstdio>
 #include <vector>
 #include <ctime>
 #include <string>
+#include <stdio.h>
+
 using namespace std;
 
 // read size:
 #define R 100
 
 // k-mer size:
-#define K 11
+//#define K 11
 
 // input files:
-#define filename1 "./data/MG1655-K12_emul1.fasta.gz"
-#define filename2 "./data/MG1655-K12_emul2.fasta.gz"
-//#define filename1 "./data/s_6_1.fastq.gz"
-//#define filename2 "./data/s_6_2.fastq.gz"
+//#define filename1 "./data/MG1655-K12_emul1.fasta.gz"
+//#define filename2 "./data/MG1655-K12_emul2.fasta.gz"
+#define filename1 "./test/data/s_6_1.fastq.gz"
+#define filename2 "./test/data/s_6_2.fastq.gz"
 
 int main(int argc, char *argv[]) {
 	cerr << "Hello, I am assembler!" << endl;
@@ -44,6 +47,14 @@ int main(int argc, char *argv[]) {
 	for (int i = 0; i < v->size; ++i) {
 		for (int i = 0; )
 	}*/
+	condensed_graph::Graph g;
+	for (size_t i = 0; i < v->size(); ++i) {
+		g.ThreadRead(v->operator [](i)[0]);
+		g.ThreadRead(v->operator [](i)[1]);
+//		cout << v->operator [](i)[0].str() << endl;
+//		cout << v->operator [](i)[1].str() << endl;
+	}
+
 	cerr << "Current time: " << (time(NULL) - now) << " sec." << endl;
 
 
