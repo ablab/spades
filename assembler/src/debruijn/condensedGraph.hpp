8716b1c0af7593b8f589225c5032728f0d78d015
Sergey Nurk <sergeynurk@gmail.com>
2011-03-09 13:57:06 +0300

    constant fixes

--- assembler/src/debruijn/condensedGraph.hpp
+++ assembler/src/debruijn/condensedGraph.hpp
@@ -21,8 +21,8 @@
 using namespace std;
 
 namespace condensed_graph {
-#define K 11//5//25
-#define N 100//11//100
+#define K 5//11//5//25
+#define N 11//100//11//100
 #define HASH_SEED 1845724623
 
 typedef Seq<K> Kmer;
