d080fff614f8a66507f2e894957fc3f912d4c946
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2011-03-10 21:23:39 +0300

    graph building

--- assembler/src/abruijn/parameters.hpp
+++ assembler/src/abruijn/parameters.hpp
@@ -13,12 +13,10 @@ typedef Seq<MPSIZE> Read;
 
 using namespace std;
 
+#define CUT 600
 static pair<string,string> filenames = make_pair("./data/MG1655-K12_emul1.fasta.gz", "./data/MG1655-K12_emul2.fasta.gz");
 //static pair<string,string> filenames = make_pair("./data/s_6_1.fastq.gz", "./data/s_6_2.fastq.gz");
-//static ireadstream<MPSIZE, 2> irs(filenames.first.c_str(), filenames.second.c_str());
 
-static ireadstream<MPSIZE, 2> datastream() {
-	return ireadstream<MPSIZE, 2>(filenames.first.c_str(), filenames.second.c_str());
-}
+static ireadstream<MPSIZE, 2> irs(filenames.first.c_str(), filenames.second.c_str());
 
 #endif /* PARAMETERS_H_ */
