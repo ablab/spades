9dfe396853329f10d1dd26344e54eb0a5a163731
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2011-03-21 17:31:09 +0300

    Major refactoring

--- assembler/src/abruijn/parameters.hpp
+++ assembler/src/abruijn/parameters.hpp
@@ -1,6 +1,7 @@
 #ifndef PARAMETERS_H_
 #define PARAMETERS_H_
 
+#include <fstream>
 #include <sstream>
 #include "ireadstream.hpp"
 
@@ -8,22 +9,22 @@
 #define K 25
 
 // ===== Input data =====
-#define FILENAMES make_pair("./data/MG1655-K12_emul1.fasta.gz", "./data/MG1655-K12_emul2.fasta.gz")
+#define FILENAMES make_pair("./data/MG1655-K12_emul1.fastq.gz", "./data/MG1655-K12_emul2.fastq.gz")
 //#define FILENAMES make_pair("./data/s_6_1.fastq.gz", "./data/s_6_2.fastq.gz")
 // How many reads we process
-#define CUT 200000
+#define CUT 1000000
 // Mate-pair read size
 #define MPSIZE 100
 
 // ===== Visualization =====
 // Define OUTPUT_PAIRED if you want GraphViz to glue together complementary vertices
-//#define OUTPUT_PAIRED
+#define OUTPUT_PAIRED
 // How many nucleotides are put in the vertex label in GraphViz
 #define LABEL 5
 
 // ===== Output =====
-#define OUTPUT_FILE std::string("./output/") + LETTER + itoa(K) + "_" + itoa(CUT) + OUTPUT_FILE_SUFFIX
-#define LETTER inputFileNames.first[7]
+#define OUTPUT_FILE std::string("./output/") + OUTPUT_FILE_NAME
+#define OUTPUT_FILE_NAME inputFileNames.first[7] + toString(K) + "_" + toString(CUT) + OUTPUT_FILE_SUFFIX
 #ifdef OUTPUT_PAIRED
 	#define OUTPUT_FILE_SUFFIX ""
 #endif
@@ -39,13 +40,16 @@ typedef unsigned int hash_t;
 // ===== Debug parameters =====
 #define VERBOSE(n, message) if ((n & 8191) == 8191) INFO(n << message)
 
-static string itoa(int n) {
+template <typename T>
+string toString(T t) {
 	std::stringstream ss;
-	ss << n;
+	ss << t;
 	return ss.str();
 }
 
 static pair<std::string, std::string> inputFileNames = FILENAMES;
-static ireadstream<MPSIZE, 2> irs(inputFileNames.first.c_str(), inputFileNames.second.c_str());
+static ireadstream<MPSIZE, 2> inputStream(inputFileNames.first.c_str(), inputFileNames.second.c_str());
+static ofstream outputStream((OUTPUT_FILE + ".dot").c_str(), ios::out);
+
 
 #endif /* PARAMETERS_H_ */
