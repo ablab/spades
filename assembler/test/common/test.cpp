24be8a427b6034bac4d6aeb9c757cdef1e67139f
Anton <anton.bankevich@gmail.com>
2011-03-22 21:01:57 +0300

    bug fix in readGenerator

--- assembler/test/common/test.cpp
+++ assembler/test/common/test.cpp
@@ -11,6 +11,7 @@
 //#include "offlineGraphVisualizerTest.hpp"
 #include "similarTest.hpp"
 #include "cuckooTest.hpp"
+#include "readGeneratorTest.hpp"
 
 void runSuite() {
 	 cute::suite s;
@@ -25,6 +26,7 @@ void runSuite() {
 //	 s += offlineGraphVisualizerSuite();
 	 s += similarSuite();
 	 s += CuckooSuite();
+	 s += ReadGeneratorSuite();
 	 cute::ide_listener lis;
 	 cute::makeRunner(lis)(s, "The Suite");
  }
