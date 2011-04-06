a30f7092b597ec6eedb21b6020d5ff68729e85d1
Sergey Nurk <sergeynurk@gmail.com>
2011-04-06 20:19:22 +0400

    condensed graph works

--- assembler/src/debruijn/edge_graph_constructor.hpp
+++ assembler/src/debruijn/edge_graph_constructor.hpp
@@ -12,13 +12,15 @@
 
 namespace edge_graph {
 
-/*
+using de_bruijn::SimpleIndex;
+using de_bruijn::EdgeHashRenewer;
+
 template<size_t kmer_size_>
 class GraphConstructor {
 protected:
 	typedef Seq<kmer_size_> Kmer;
 	EdgeGraph *g_;
-	SimpleIndex<kmer_size_> *h_;
+	SimpleIndex<kmer_size_, Edge*> *h_;
 
 	pair<Vertex*, int> GetPosition(Kmer k) {
 		assert(h_->contains(k));
@@ -29,133 +31,132 @@ protected:
 		return h_->contains(k);
 	}
 
-	pair<Vertex*, int> GetPosMaybeMissing(Kmer k) {
-		if (!h_->contains(k)) {
-			g_->AddVertex(Sequence(k));
-		}
-		return h_->get(k);
-	}
+//	pair<Vertex*, int> GetPosMaybeMissing(Kmer k) {
+//		if (!h_->contains(k)) {
+//			g_->AddVertex(Sequence(k));
+//		}
+//		return h_->get(k);
+//	}
 
 	GraphConstructor() {
-		h_ = new SimpleIndex<kmer_size_> ();
-		g_ = new CondensedGraph(kmer_size_, new HashRenewer<kmer_size_> (h_));
+		h_ = new SimpleIndex<kmer_size_, Edge*> ();
+		g_ = new EdgeGraph(kmer_size_, new EdgeHashRenewer<kmer_size_, EdgeGraph> (h_));
 		//		DEBUG("HERE0");
 		//		GetPosMaybeMissing(Seq<5>("AAAAA"));
 	}
 
 public:
-	virtual void ConstructGraph(CondensedGraph* &g, SimpleIndex<kmer_size_>* &h) {
+	virtual void ConstructGraph(EdgeGraph* &g, SimpleIndex<kmer_size_, Edge*>* &h) {
 		g = g_;
 		h = h_;
 	}
 };
 
-template<size_t kmer_size_>
-class CondenseConstructor: public GraphConstructor<kmer_size_> {
-	typedef Seq<kmer_size_> Kmer;
-	typedef GraphConstructor<kmer_size_> super;
-	typedef DeBruijn<kmer_size_> debruijn;
-
-	DeBruijn<kmer_size_>& origin_;
-
-	bool StepLeftIfPossible(Kmer &kmer) {
-		if (origin_.PrevCount(kmer) == 1) {
-			Kmer prev_kmer = *(origin_.begin_prev(kmer));
-			DEBUG("Prev kmer " << prev_kmer);
-			DEBUG("Next Count of prev " << origin_.NextCount(prev_kmer));
-			assert(origin_.NextCount(prev_kmer) > 0);
-			if (origin_.NextCount(prev_kmer) == 1 && kmer != !prev_kmer) {
-				kmer = prev_kmer;
-				return true;
-			}
-		}
-		return false;
-	}
-
-	bool StepRightIfPossible(Kmer &kmer) {
-		if (origin_.NextCount(kmer) == 1) {
-			Kmer next_kmer = *(origin_.begin_next(kmer));
-			DEBUG("Next kmer " << next_kmer);
-			DEBUG("Prev Count of next " << origin_.PrevCount(next_kmer));
-			assert(origin_.PrevCount(next_kmer) > 0);
-			if (origin_.PrevCount(next_kmer) == 1 && kmer != !next_kmer) {
-				kmer = next_kmer;
-				return true;
-			}
-		}
-		return false;
-	}
-
-	Kmer GoLeft(Kmer kmer) {
-		DEBUG("Starting process for " << kmer);
-		Kmer initial_kmer = kmer;
-
-		DEBUG("Prev Count " << origin_.PrevCount(kmer));
-		//go left while can
-		while (StepLeftIfPossible(kmer) && kmer != initial_kmer) {
-			//todo comment
-		}
-		DEBUG("Stopped going left at " << kmer);
-		return kmer;
-	}
-
-	//go right, appending sequence
-	Sequence ConstructSeqGoingRight(Kmer kmer) {
-		SequenceBuilder s;
-		s.append(kmer);
-		Kmer initial_kmer = kmer;
-
-		DEBUG("Next Count " << origin_.NextCount(kmer));
-		while (StepRightIfPossible(kmer) && kmer != initial_kmer) {
-			//todo comment
-			s.append(kmer[kmer_size_ - 1]);
-		}
-		DEBUG("Stopped going right at " << kmer);
-		return s.BuildSequence();
-	}
-
-	Sequence ConstructSequenceWithKmer(Kmer kmer) {
-		return ConstructSeqGoingRight(GoLeft(kmer));
-	}
-
-	void MakeLinks() {
-		for (set<Vertex*>::iterator it = super::g_->vertices().begin(), end =
-				super::g_->vertices().end(); it != end; it++) {
-			Vertex* v = *it;
-			Kmer kmer = v->nucls().end<kmer_size_> ();
-
-			typename debruijn::neighbour_iterator n_it = origin_.begin_next(
-					kmer);
-			for (size_t i = 0, n = origin_.NextCount(kmer); i < n; ++i, ++n_it) {
-				pair<Vertex*, size_t> position = super::GetPosition(*n_it);
-				//				assert(position.second == 0);
-				super::g_->LinkVertices(v, position.first);
-			}
-			//todo now linking twice!!!
-		}
-	}
-
-public:
-	CondenseConstructor(DeBruijn<kmer_size_>& origin) :
-		origin_(origin) {
-
-	}
-	virtual void ConstructGraph(CondensedGraph* &g, SimpleIndex<kmer_size_>* &h) {
-
-		for (typename debruijn::kmer_iterator it = origin_.kmer_begin(), end =
-				origin_.kmer_end(); it != end; it++) {
-			Kmer kmer = *it;
-			if (!super::Contains(kmer)) {
-				super::g_->AddVertex(ConstructSequenceWithKmer(kmer));
-			}
-		}
-
-		MakeLinks();
-
-		super::ConstructGraph(g, h);
-	}
-};
-*/
+//template<size_t kmer_size_>
+//class CondenseConstructor: public GraphConstructor<kmer_size_> {
+//	typedef Seq<kmer_size_> Kmer;
+//	typedef GraphConstructor<kmer_size_> super;
+//	typedef DeBruijn<kmer_size_> debruijn;
+//
+//	DeBruijn<kmer_size_>& origin_;
+//
+//	bool StepLeftIfPossible(Kmer &kmer) {
+//		if (origin_.PrevCount(kmer) == 1) {
+//			Kmer prev_kmer = *(origin_.begin_prev(kmer));
+//			DEBUG("Prev kmer " << prev_kmer);
+//			DEBUG("Next Count of prev " << origin_.NextCount(prev_kmer));
+//			assert(origin_.NextCount(prev_kmer) > 0);
+//			if (origin_.NextCount(prev_kmer) == 1 && kmer != !prev_kmer) {
+//				kmer = prev_kmer;
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
+//
+//	bool StepRightIfPossible(Kmer &kmer) {
+//		if (origin_.NextCount(kmer) == 1) {
+//			Kmer next_kmer = *(origin_.begin_next(kmer));
+//			DEBUG("Next kmer " << next_kmer);
+//			DEBUG("Prev Count of next " << origin_.PrevCount(next_kmer));
+//			assert(origin_.PrevCount(next_kmer) > 0);
+//			if (origin_.PrevCount(next_kmer) == 1 && kmer != !next_kmer) {
+//				kmer = next_kmer;
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
+//
+//	Kmer GoLeft(Kmer kmer) {
+//		DEBUG("Starting process for " << kmer);
+//		Kmer initial_kmer = kmer;
+//
+//		DEBUG("Prev Count " << origin_.PrevCount(kmer));
+//		//go left while can
+//		while (StepLeftIfPossible(kmer) && kmer != initial_kmer) {
+//			//todo comment
+//		}
+//		DEBUG("Stopped going left at " << kmer);
+//		return kmer;
+//	}
+//
+//	//go right, appending sequence
+//	Sequence ConstructSeqGoingRight(Kmer kmer) {
+//		SequenceBuilder s;
+//		s.append(kmer);
+//		Kmer initial_kmer = kmer;
+//
+//		DEBUG("Next Count " << origin_.NextCount(kmer));
+//		while (StepRightIfPossible(kmer) && kmer != initial_kmer) {
+//			//todo comment
+//			s.append(kmer[kmer_size_ - 1]);
+//		}
+//		DEBUG("Stopped going right at " << kmer);
+//		return s.BuildSequence();
+//	}
+//
+//	Sequence ConstructSequenceWithKmer(Kmer kmer) {
+//		return ConstructSeqGoingRight(GoLeft(kmer));
+//	}
+//
+//	void MakeLinks() {
+//		for (set<Vertex*>::iterator it = super::g_->vertices().begin(), end =
+//				super::g_->vertices().end(); it != end; it++) {
+//			Vertex* v = *it;
+//			Kmer kmer = v->nucls().end<kmer_size_> ();
+//
+//			typename debruijn::neighbour_iterator n_it = origin_.begin_next(
+//					kmer);
+//			for (size_t i = 0, n = origin_.NextCount(kmer); i < n; ++i, ++n_it) {
+//				pair<Vertex*, size_t> position = super::GetPosition(*n_it);
+//				//				assert(position.second == 0);
+//				super::g_->LinkVertices(v, position.first);
+//			}
+//			//todo now linking twice!!!
+//		}
+//	}
+//
+//public:
+//	CondenseConstructor(DeBruijn<kmer_size_>& origin) :
+//		origin_(origin) {
+//
+//	}
+//	virtual void ConstructGraph(EdgeGraph* &g, SimpleIndex<kmer_size_, Edge*>* &h) {
+//
+//		for (typename debruijn::kmer_iterator it = origin_.kmer_begin(), end =
+//				origin_.kmer_end(); it != end; it++) {
+//			Kmer kmer = *it;
+//			if (!super::Contains(kmer)) {
+//				super::g_->AddVertex(ConstructSequenceWithKmer(kmer));
+//			}
+//		}
+//
+//		MakeLinks();
+//
+//		super::ConstructGraph(g, h);
+//	}
+//};
 
 }
 #endif /* EDGE_GRAPH_CONSTRUCTOR_HPP_ */
--- assembler/src/debruijn/utils.hpp
+++ assembler/src/debruijn/utils.hpp
@@ -72,12 +72,13 @@ public:
 		}
 	}
 
-
 };
 
-template <typename VertexId, typename EdgeId>
+template<class Graph>
 class GraphActionHandler {
 public:
+	typedef typename Graph::VertexId VertexId;
+	typedef typename Graph::EdgeId EdgeId;
 
 	virtual void HandleAdd(VertexId v) {
 	}
@@ -134,9 +135,10 @@ public:
 	}
 };
 
+template<size_t kmer_size_, class Graph>
+class EdgeHashRenewer: public GraphActionHandler<Graph> {
 
-template<size_t kmer_size_, typename Graph, typename VertexId, typename EdgeId>
-class EdgeHashRenewer: public GraphActionHandler<VertexId, EdgeId> {
+	typedef typename Graph::EdgeId EdgeId;
 
 	DataHashRenewer<kmer_size_, Graph, EdgeId> renewer_;
 
@@ -149,8 +151,154 @@ public:
 		renewer_.HandleAdd(e);
 	}
 
-	virtual void HandleDelete(EdgeId* e) {
+	virtual void HandleDelete(EdgeId e) {
 		renewer_.HandleDelete(e);
+	}
+};
+
+template<size_t kmer_size_, typename Graph>
+class VertexHashRenewer: public GraphActionHandler<Graph> {
+
+	typedef typename Graph::VertexId VertexId;
+
+	DataHashRenewer<kmer_size_, Graph, VertexId> renewer_;
+
+public:
+	VertexHashRenewer(const Graph& g, SimpleIndex<kmer_size_, VertexId> *index) :
+		renewer_(g, index) {
+	}
+
+	virtual void HandleAdd(VertexId e) {
+		renewer_.HandleAdd(e);
+	}
+
+	virtual void HandleDelete(VertexId e) {
+		renewer_.HandleDelete(e);
+	}
+};
+
+/**
+ * Stub base class for handling graph primitives during traversal.
+ */
+//template<typename VertexId, typename EdgeId>
+template<class Graph>
+class TraversalHandler {
+public:
+
+	typedef typename Graph::VertexId VertexId;
+	typedef typename Graph::EdgeId EdgeId;
+
+	virtual ~TraversalHandler();
+
+	virtual void VertexEntered(VertexId v) {
+	}
+//	virtual void VertexLeft(VertexId v) {
+//	}
+	virtual void EdgePassed(EdgeId e) {
+	}
+//	virtual void EdgeBacktracked(EdgeId e) {
+//	}
+};
+
+/**
+ * @brief Base class for condensed graph traversals.
+ */
+template<class Graph>
+class Traversal {
+public:
+	typedef typename Graph::VertexId VertexId;
+	typedef typename Graph::EdgeId EdgeId;
+
+	Traversal(Graph* g) :
+		g_(g) {
+	}
+
+	virtual ~Traversal(){}
+
+	virtual void Traverse(TraversalHandler<Graph>* h) = 0;
+
+protected:
+	Graph* g_;
+};
+
+template<class Graph>
+class DFS: public Traversal<Graph> {
+	typedef typename Graph::VertexId VertexId;
+	typedef typename Graph::EdgeId EdgeId;
+
+	set<VertexId> visited_;
+	void ProcessVertex(VertexId v, vector<VertexId>* stack,
+			TraversalHandler<Graph>* h);
+public:
+	DFS(Graph* g) :
+		Traversal<Graph> (g) {
+	}
+	virtual void Traverse(TraversalHandler<Graph>* h);
+};
+
+template<class Graph>
+void DFS<Graph>::ProcessVertex(
+		VertexId v, vector<VertexId>* stack,
+		TraversalHandler<Graph>* h) {
+	//todo how to get rid of this
+	typedef Traversal<Graph> super;
+//	typedef typename super::g_ g_;
+
+	if (visited_.count(v) == 0) {
+		h->VertexEntered(v);
+		visited_.insert(v);
+
+		vector<EdgeId> edges = super::g_->OutgoingEdges(v);
+		for (size_t i = 0; i < edges.size(); ++i) {
+			EdgeId e = edges[i];
+			h->EdgePassed(e);
+			stack.push_back(super::g_->EdgeEnd(e));
+		}
+	}
+}
+
+template<class Graph>
+void DFS<Graph>::Traverse(TraversalHandler<Graph>* h) {
+	//todo how to get rid of this
+	typedef Traversal<Graph> super;
+	typedef typename Graph::VertexIterator VertexIt;
+
+	for (VertexIt it = super::g_->begin(); it
+			!= super::g_->end(); it++) {
+		vector<VertexId> stack;
+		stack.push_back(*it);
+		while (!stack.empty()) {
+			VertexId v = stack[stack.size() - 1];
+			stack.pop_back();
+			ProcessVertex(v, &stack, h);
+		}
+	}
+}
+
+template <class Graph>
+class SimpleStatCounter: public TraversalHandler<Graph> {
+	size_t v_count_;
+	size_t e_count_;
+public:
+	typedef typename Graph::VertexId VertexId;
+	typedef typename Graph::EdgeId EdgeId;
+
+	SimpleStatCounter() :
+		v_count_(0), e_count_(0) {
+	}
+	virtual void VertexEntered(VertexId v) {
+		v_count_++;
+	}
+	virtual void EdgePassed(EdgeId e) {
+		e_count_++;
+	}
+
+	size_t v_count() const {
+		return v_count_;
+	}
+
+	size_t e_count() const {
+		return e_count_;
 	}
 };
 
