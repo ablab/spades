828a0a637106f97f9710a296e6c3ecfb8a74ba98
Dmitry Antipov <dmitrij.antipov@gmail.com>
2011-04-11 18:37:05 +0400

    fx

--- assembler/src/paireddebruijn/graphio.hpp
+++ assembler/src/paireddebruijn/graphio.hpp
@@ -15,14 +15,16 @@ void codeRead(char *read, char *code);
 
 
 inline bool nextReadPair(FILE* f, char * &read1, char * &read2) {
-	if (!fictiveSecondReads) return (fscanf(f, "%s %s", read1, read2) == 2);
-	else {
+	//if (!fictiveSecondReads
+   return (fscanf(f, "%s %s", read1, read2) == 2);
+/*	else {
 		if (fscanf(f, "%s %s", read1, read2) == 2){
 			forn(i,strlen(read2)) read2[i]='A';
 			return true;
 		}
 		return false;
 	}
+	*/
 }
 
 Sequence readGenome(istream &is);
