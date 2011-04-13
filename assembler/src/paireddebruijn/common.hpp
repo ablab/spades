4159d4d425dac9ac0f502004907853010508ae82
Dmitry Antipov <dmitrij.antipov@gmail.com>
2011-04-13 12:50:00 +0400

    fx

--- assembler/src/paireddebruijn/common.hpp
+++ assembler/src/paireddebruijn/common.hpp
@@ -39,8 +39,8 @@
 
 #define MAX_VERT_NUMBER 50000
 #define MAX_DEGREE 50
-#define coverage_cutoff 9
-#define lrange_cutoff 10
+#define coverage_cutoff 0
+#define range_cutoff 25
 #define suffix "_const_d"
 
 
