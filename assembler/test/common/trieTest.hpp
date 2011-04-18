3ea64b6d260c95cc1999ff4088ab9b4e0f1876f4
Mariya Fomkina <mariyafomkina@gmail.com>
2011-04-18 19:00:55 +0400

    Changed test for Trie.

--- assembler/test/common/trieTest.hpp
+++ assembler/test/common/trieTest.hpp
@@ -9,13 +9,13 @@ void TestTrie() {
   srand(42);
   trie<int, int> map; 
 	int a[20] = {10, 15, 20, 25, 30, 35, 40, 45, 10, 15, 
-							 100, 1, 2, 3, 4, 5, 255, 10, 255, 7};
+							 100, 1, 2, 3, 4, 5, 255, 10, 255, 0};
 	for (int i = 0; i < 20; ++i) {
 		map.insert(std::make_pair(a[i], 42));
 	}
 	ASSERT_EQUAL(map.size(), 16);
 	
-	map.clear(); 
+	map.clear();
   for (int i = 0; i < 100000; ++i) {
     int t = rand();
     map.insert(std::make_pair(t, 42));
