50d5d4c660dcca05318380b39e5119cbf667adcb
Sergey Nurk <sergeynurk@gmail.com>
2011-04-18 21:31:13 +0400

    path conditions checked, some refactorings

--- assembler/src/debruijn/bulge_remover.hpp
+++ assembler/src/debruijn/bulge_remover.hpp
@@ -17,10 +17,10 @@ public:
 	void RemoveBulges(Graph& g);
 
 	BulgeRemover(size_t max_length = 5 * K, double coverage_threshold = 1000.,
-			double coverage_gap = 1.1) :
+			double coverage_gap = 1.1, double delta_threshold = 4, double relative_delta = 0.1) :
 		max_length_(max_length), coverage_threshold_(coverage_threshold),
-				coverage_gap_(coverage_gap) {
-
+				coverage_gap_(coverage_gap), delta_threshold_(delta_threshold), relative_delta_(relative_delta) {
+		assert(coverage_gap_ > 1.);
 	}
 
 private:
@@ -28,8 +28,21 @@ private:
 	size_t max_length_;
 	double coverage_threshold_;
 	double coverage_gap_;
+	double delta_threshold_;
+	double relative_delta_;
 
 	bool PossibleBulgeEdge(const Graph& g, EdgeId e);
+
+	bool PathCondition(const Graph& g, EdgeId edge, const vector<EdgeId>& path);
+
+	bool BulgeCondition(const Graph& g, EdgeId edge, const vector<EdgeId>& path) {
+		return PathAvgCoverage(g, path) > coverage_gap_ * g.coverage(edge) && PathCondition(g, edge, path);
+		//		return path_and_coverage.second > coverage_gap * g.kplus_one_mer_coverage(edge);
+	}
+
+	double PathAvgCoverage(const Graph& g, const vector<EdgeId> path);
+
+	size_t PathLength(const Graph& g, const vector<EdgeId> path);
 
 	//returns reversed path!!!
 	pair<vector<EdgeId> , int> BestPath(const Graph& g, VertexId start,
@@ -38,7 +51,41 @@ private:
 
 template<class Graph>
 bool BulgeRemover<Graph>::PossibleBulgeEdge(const Graph& g, EdgeId e) {
-	return g.length(e) <= max_length_;
+	return g.length(e) <= max_length_ && g.coverage(e) < coverage_threshold_;
+}
+
+template<class Graph>
+size_t BulgeRemover<Graph>::PathLength(const Graph& g, const vector<EdgeId> path) {
+	size_t length = 0;
+	for (size_t i = 0; i < path.size(); ++i) {
+		length += g.length(path[i]);
+	}
+	return length;
+}
+
+template<class Graph>
+bool BulgeRemover<Graph>::PathCondition(const Graph& g, EdgeId edge, const vector<EdgeId>& path) {
+	for (size_t i = 0; i < path.size(); ++i)
+		if (edge == path[i] || edge == g.Complement(path[i]))
+			return false;
+	for (size_t i = 0; i < path.size(); ++i)
+		for (size_t j = i + 1; j < path.size(); ++j)
+			if (path[i] == path[j] || path[i] == g.Complement(path[j]))
+				return false;
+	return true;
+}
+
+template<class Graph>
+double BulgeRemover<Graph>::PathAvgCoverage(const Graph& g, const vector<EdgeId> path) {
+	double unnormalized_coverage = 0;
+	size_t path_length = 0;
+	for (size_t i = 0; i < path.size(); ++i) {
+		EdgeId edge = path[i];
+		size_t length = g.length(edge);
+		path_length += length;
+		unnormalized_coverage += g.coverage(edge) * length;
+	}
+	return unnormalized_coverage / path_length;
 }
 
 //todo move to some common place
@@ -58,12 +105,6 @@ template<class Graph>
 void BulgeRemover<Graph>::RemoveBulges(Graph& g) {
 	typedef de_bruijn::SmartEdgeIterator<Graph> EdgeIter;
 
-	//todo
-	size_t delta = 3;
-	double coverage_gap = 1.1;
-
-	assert(coverage_gap > 1.);
-
 	DEBUG("Bulge remove process started");
 
 	for (EdgeIter iterator = g.SmartEdgeBegin(), end = g.SmartEdgeEnd(); end
@@ -74,8 +115,7 @@ void BulgeRemover<Graph>::RemoveBulges(Graph& g) {
 						<< " and avg coverage " << g.coverage(edge));
 		DEBUG("Is possible bulge " << PossibleBulgeEdge(g, edge));
 		if (PossibleBulgeEdge(g, edge)) {
-			DEBUG(
-					"Processing edge " << g.EdgeNucls(edge) << " and coverage "
+			DEBUG("Processing edge " << g.EdgeNucls(edge) << " and coverage "
 							<< g.kplus_one_mer_coverage(edge));
 
 			VertexId start = g.EdgeStart(edge);
@@ -84,29 +124,25 @@ void BulgeRemover<Graph>::RemoveBulges(Graph& g) {
 			VertexId end = g.EdgeEnd(edge);
 			DEBUG("End " << g.VertexNucls(end));
 
-			DEBUG(
-					"Looking for best path between start and end shorter than "
-							<< g.length(edge) + delta);
+			size_t length_threshold = g.length(edge) + std::max(relative_delta_ * g.length(edge), delta_threshold_);
+			DEBUG("Looking for best path between start and end shorter than " << length_threshold);
 
 			pair<vector<EdgeId> , int> path_and_coverage = BestPath(g, start,
-					end, g.length(edge) + delta);
-			DEBUG(
-					"Best path with coverage " << path_and_coverage.second
-							<< " is " << PrintPath<Graph> (g,
-							path_and_coverage.first));
+					end, length_threshold);
+			const vector<EdgeId>& path = path_and_coverage.first;
+			double path_coverage = (double)path_and_coverage.second / PathLength(g, path);
+			DEBUG("Best path with coverage " << path_coverage
+							<< " is " << PrintPath<Graph> (g, path));
 
 			//if edge was returned, this condition will fail
-			DEBUG(
-					"Satisfies condition " << (path_and_coverage.second
-							> coverage_gap * g.kplus_one_mer_coverage(edge)));
-			if (path_and_coverage.second > coverage_gap
-					* g.kplus_one_mer_coverage(edge)) {
-				DEBUG(
-						"Deleting edge " << g.EdgeNucls(edge)
-								<< " and compressing ends");
+			if (BulgeCondition(g, edge, path)) {
+				DEBUG("Satisfied condition");
+				DEBUG("Deleting edge " << g.EdgeNucls(edge) << " and compressing ends");
 				g.DeleteEdge(edge);
 				g.CompressVertex(start);
 				g.CompressVertex(end);
+			} else {
+				DEBUG("Didn't satisfy condition");
 			}
 		}
 		DEBUG("-----------------------------------");
