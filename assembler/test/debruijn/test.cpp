79b982205378483c2ae184aaa90429b6ad3a31fa
Nikolay Vyahhi <vyahhi@gmail.com>
2011-05-03 14:44:56 +0400

    half-way refactoring (debruijnTest -> debruijn)

--- assembler/test/debruijn/test.cpp
+++ assembler/test/debruijn/test.cpp
@@ -33,27 +33,27 @@ void RunTestSuites() {
 	cute::makeRunner(lis)(s, "De Bruijn Project Test Suites");
 }
 
-void RunEdgeGraphTool() {
-	typedef StrobeReader<2, Read, ireadstream> ReadStream;
-	typedef PairedReader<ireadstream> PairedStream;
-	typedef RCReaderWrapper<PairedStream> RCStream;
-
-	//	const tr1::tuple<string, string, int> input = ;
-	const string reads[2] = {tr1::get<0>(INPUT), tr1::get<1>(INPUT)};
-	ReadStream reader(reads);
-	PairedStream pairStream(reader, tr1::get<2>(INPUT));
-	RCStream rcStream(pairStream);
-
-	ireadstream genome_stream(ECOLI_FILE);
-	Read genome;
-	genome_stream >> genome;
-	edge_graph::EdgeGraphTool(rcStream,  genome.getSequenceString().substr(0, tr1::get<2>(INPUT)));
-	reader.close();
-	genome_stream.close();
-}
+//void RunEdgeGraphTool() {
+//	typedef StrobeReader<2, Read, ireadstream> ReadStream;
+//	typedef PairedReader<ireadstream> PairedStream;
+//	typedef RCReaderWrapper<PairedStream> RCStream;
+//
+//	//	const tr1::tuple<string, string, int> input = ;
+//	const string reads[2] = {tr1::get<0>(INPUT), tr1::get<1>(INPUT)};
+//	ReadStream reader(reads);
+//	PairedStream pairStream(reader, tr1::get<2>(INPUT));
+//	RCStream rcStream(pairStream);
+//
+//	ireadstream genome_stream(ECOLI_FILE);
+//	Read genome;
+//	genome_stream >> genome;
+//	edge_graph::EdgeGraphTool(rcStream,  genome.getSequenceString().substr(0, tr1::get<2>(INPUT)));
+//	reader.close();
+//	genome_stream.close();
+//}
 
 int main() {
-//	RunTestSuites();
-	RunEdgeGraphTool();
+	RunTestSuites();
+//	RunEdgeGraphTool();
 	return 0;
 }
