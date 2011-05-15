b2296393515e0e1b3a2c8c86f4119b0b1be2ab4e
Mariya Fomkina <mariyafomkina@gmail.com>
2011-05-15 15:38:02 +0400

    Updated filter. Now it is only a filter, without any statistical features.

--- assembler/tools/filter/filter.cpp
+++ assembler/tools/filter/filter.cpp
@@ -6,7 +6,6 @@
 #include <tr1/unordered_map>
 #include <google/sparse_hash_map>
 #include "cuckoo.hpp"
-//#include "mem.hpp"
 
 #define K 31
 
@@ -22,62 +21,40 @@ typedef cuckoo<Seq<K>, size_t, Seq<K>::multiple_hash, Seq<K>::equal_to> hm5;
 int main(int argc, char** argv) {
   std::string filename = "";
   size_t L = 1; 
-  size_t m_num = 1;
-  bool stat_on = false;
-  bool console_on = true;
-  bool find_on = false;
+  size_t m_num = 5;
 
-  if ((argc < 4) || (argc > 6)) {
-		std::cout << "Usage: ./filter <filename> <L> <map number> [--stat [--find]]]\n"
+  if ((argc < 2) || (argc > 4)) {
+		std::cout << "Usage: ./filter <filename> [<L> [<map number>]]\n"
               << "<map number> is map type:\n"
               << "1 - map, 2 - ext/hash_map, 3 - tr1/unordered_map,\n"
-              << "4 - google/sparse_hash_map, 5 - cuckoo\n"
-              << "Selects k-mer with amount > L\n" 
-              << "--stat output stat info without k-mers" 
-              << "--find search for some k-mers in a map" << std::endl;
+              << "4 - google/sparse_hash_map, 5 - cuckoo (default)\n"
+              << "Selects k-mers with amount > L (1 by default)" << std::endl;
     return 0;
   }
-	if (argc >= 4) {
+	if (argc >= 2) {
 		filename = argv[1];
+  }
+  if (argc >= 3) {
     L = atoi(argv[2]);
+  }
+  if (argc == 4) {
     m_num = atoi(argv[3]);
-  }
-  if (argc >= 5) {
-    if (std::string(argv[4]) == "--stat") {
-      stat_on = true;
-    } else {
-      std::cout << "Wrong option!\n";
-      return 0;
-    }
-  }
-  if (argc == 6) {
-    if (std::string(argv[5]) == "--find") {
-      find_on = true;
-    } else {
-      std::cout << "Wrong option!\n";
-      return 0;
-    }
   }
   switch (m_num) {
   case 1: 
-    seq_filter<K, hm1>::filter(filename, L, stat_on, console_on, 
-                               find_on, "map");
+    seq_filter<K, hm1>::filter(filename, L);
     break;
   case 2: 
-    seq_filter<K, hm2>::filter(filename, L, stat_on, console_on, 
-                               find_on, "hash_map");
+    seq_filter<K, hm2>::filter(filename, L);
     break;
   case 3: 
-    seq_filter<K, hm3>::filter(filename, L, stat_on, console_on, 
-                               find_on, "unordered_map");
+    seq_filter<K, hm3>::filter(filename, L);
     break;
   case 4: 
-    seq_filter<K, hm4>::filter(filename, L, stat_on, console_on, 
-                               find_on, "sparse_hash_map");
+    seq_filter<K, hm4>::filter(filename, L);
     break;
   case 5: 
-    seq_filter<K, hm5>::filter(filename, L, stat_on, console_on, 
-                               find_on, "cuckoo");
+    seq_filter<K, hm5>::filter(filename, L);
     break;
   default:
     std::cout << "Map number is incorrect!\n";
--- assembler/tools/seq_filter.hpp
+++ assembler/tools/seq_filter.hpp
@@ -3,135 +3,30 @@
  * 
  * Created on: 17.04.2011
  *     Author: Mariya Fomkina
- *   Modified: 25.04.2011 by author
+ *   Modified: 15.05.2011 by author
  */
 
 #ifndef _SEQ_FILTER_HPP_
 #define _SEQ_FILTER_HPP_
  
 #include "ireadstream.hpp"
-#include "../test/memory.hpp"
-#include <sys/time.h>
 
-// Class seq_filter is used for getting all k-mer from all read of a given file
+// @description Class seq_filter is used for getting all 
+// k-mers from all reads of a given file
 // and selecting k-mers which amount is more than a given number.
-// Template parameters are:
-// size_t size - the size of k-mer
-// hm - hash map class
-// The only public method filter takes 4 parameters:
-// const std::string& in - input file name
-// const size_t L - given number for filtration
-// const bool stat - if false, k-mers are printed to output, 
-// else stat info in printed
-// const bool console - if true, k-mers are printed to output, 
-// else k-mers are added to vector and stat option is ignored
+// @paramater size the size of k-mer
+// @parameter hm hash map class
+// @parameter in input file name
+// @parameter L given number for filtration
 
 template<size_t size, class hm>
 class seq_filter {		
 public:
-	static std::vector<Seq<size> > filter(const std::string& in, 
-                                        const size_t L = 1, 
-                                        const bool stat = false, 
-                                        const bool console = true,
-                                        const bool find = false, 
-                                        const std::string& name = "") {
-    double vm1 = 0;
-    double rss1 = 0;
-    process_mem_usage(vm1, rss1);
-    timeval tim;
-    gettimeofday(&tim, NULL);
-    double t1 = tim.tv_sec + ((float)tim.tv_usec/1e6);
-
+	static void filter(const std::string& in, const size_t L = 1) { 
     hm map;
     std::vector<Seq<size> > seqs;
 		add_seqs_from_file_to_map(in, map);
-    if (console) {
-      write_seqs_from_map_to_stdout(map, L, stat);
-    } else {
-      seqs = get_seqs_from_map(map, L);
-    }
-
-    double vm2 = 0;
-    double rss2 = 0;
-    process_mem_usage(vm2, rss2);
-    gettimeofday(&tim, NULL);
-    double t2 = tim.tv_sec + ((float)tim.tv_usec/1e6);
-
-    //hm map2;
-    size_t n = 0;
-    if (find) {
-      Seq<size> seq;
-      typename hm::iterator it;
-      for (it = map.begin(); it != map.end(); ++it) {
-        seq = (*it).first;
-        map.find(seq);
-        ++n;
-        //map2.insert(std::pair<Seq<31>, size_t>(seq, 0));
-      }
-    }
-
-    gettimeofday(&tim, NULL);
-    double t3 = tim.tv_sec + ((float)tim.tv_usec/1e6);
-
-    if ((stat) && (console)) {
-      std::cout << "Memory: " << (vm2 - vm1) << " " << name << std::endl;
-      std::cout << "Insert: " << (t2 - t1) << " " << name << std::endl;
-      std::cout << "Find: " << (t3 - t2) << " " << name << std::endl;
-      std::cout << "Elements to find: " << n << std::endl;
-    }
-    return seqs;
-	}
-
-  // Only for cuckoo!!!
-  // Code is dublicated, needs refactoring
-	static std::vector<Seq<size> > filter(const std::string& in, 
-                                        const size_t L, 
-                                        const bool stat, 
-                                        const bool console,
-                                        const bool find,
-                                        const size_t max_loop) {
-    double vm1 = 0;
-    double rss1 = 0;
-    process_mem_usage(vm1, rss1);
-    timeval tim;
-    gettimeofday(&tim, NULL);
-    double t1 = tim.tv_sec + ((float)tim.tv_usec/1e6);
-
-    hm map;
-    map.set_up(4, 100, max_loop, 1.2);
-
-    std::vector<Seq<size> > seqs;
-		add_seqs_from_file_to_map(in, map);
-    if (console) {
-      write_seqs_from_map_to_stdout(map, L, stat);
-    } else {
-      seqs = get_seqs_from_map(map, L);
-    }
-
-    double vm2 = 0;
-    double rss2 = 0;
-    process_mem_usage(vm2, rss2);
-    gettimeofday(&tim, NULL);
-    double t2 = tim.tv_sec + ((float)tim.tv_usec/1e6);
-
-    if (find) {
-      Seq<size> seq;
-      typename hm::iterator it;
-      for (it = map.begin(); it != map.end(); ++it) {
-        seq = (*it).first;
-        map.find(seq);
-      }
-    }
-
-    gettimeofday(&tim, NULL);
-    double t3 = tim.tv_sec + ((float)tim.tv_usec/1e6);
-
-    if ((stat) && (console)) {
-      std::cout << "Memory: " << (vm2 - vm1) << std::endl;
-      std::cout << "Insert: " << (t2 - t1) << std::endl;
-      std::cout << "Find: " << (t3 - t2) << std::endl;
-    }
-    return seqs;
+    write_seqs_from_map_to_stdout(map, L);
 	}
 
 private:
@@ -171,32 +66,11 @@ private:
 		}
 	}
 
-	static std::vector<Seq<size> > get_seqs_from_map(hm& map, const size_t& L) {
-		std::vector<Seq<size> > seqs;
+	static void write_seqs_from_map_to_stdout(hm& map, const size_t& L) {
 		typename hm::iterator end = map.end();
 		for (typename hm::iterator it = map.begin(); it != end; ++it) {
-			if ((*it).second > L) seqs.push_back((*it).first);
+			if ((*it).second > L) std::cout << (*it).first << std::endl;		
 		}
-		return seqs;
-	}
-
-	static void write_seqs_from_map_to_stdout(hm& map, const size_t& L, bool stat) {
-		typename hm::iterator end = map.end();
-		size_t cnt = 0;
-		for (typename hm::iterator it = map.begin(); it != end; ++it) {
-			if ((*it).second > L) {
-        if (stat) {
-          ++cnt;
-        } else {
-          std::cout << (*it).first << std::endl;
-				}
-			}
-		}
-    if (stat) {
-      std::cout << "Selected " << cnt << " k-mers from " << map.size() 
-                << ", removed "  << ((float)map.size() - cnt)/map.size()*100 
-                << "% of k-mers." << std::endl; 
-    }
 	}
 };
 
