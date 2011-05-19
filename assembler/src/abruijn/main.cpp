434a0b9b3313e3ea4de9f821cc3b21eee2ec917e
Nikolay Vyahhi <vyahhi@gmail.com>
2011-05-19 18:16:53 +0400

    abruijn fix

--- assembler/src/abruijn/main.cpp
+++ assembler/src/abruijn/main.cpp
@@ -1,6 +1,8 @@
 #include "toyexamples.hpp"
 #include "graphBuilder.hpp"
 #include "logging.hpp"
+#include "strobe_reader.hpp"
+#include "strobe_read.hpp"
 #include "libs/getopt_pp/getopt_pp_standalone.h"
 #include <iostream>
 
