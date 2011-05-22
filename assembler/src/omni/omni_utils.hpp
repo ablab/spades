6d0a3b67f4b54c975dc423f2f311421d1fd00e55
Nikolay Vyahhi <vyahhi@gmail.com>
2011-05-22 22:57:24 +0400

    priority queue rererefactorings :)

--- assembler/src/omni/omni_utils.hpp
+++ assembler/src/omni/omni_utils.hpp
@@ -416,7 +416,7 @@ public:
 	}
 
 	virtual void HandleAdd(VertexId v) {
-		super::queue_.offer(v);
+		super::push(v);
 	}
 
 	virtual void HandleDelete(VertexId v) {
@@ -456,7 +456,7 @@ public:
 	}
 
 	virtual void HandleAdd(EdgeId v) {
-		super::queue_.offer(v);
+		super::push(v);
 	}
 
 	virtual void HandleDelete(EdgeId v) {
