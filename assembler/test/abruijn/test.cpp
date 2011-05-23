66f5db21ea9b4e1e0334fe29739bf1af3c1c6110
Alexander Kulikov <alexander.s.kulikov@gmail.com>
2011-05-24 00:23:38 +0400

    added a test on spelling a reference genome through a bruijn graph

--- assembler/test/abruijn/test.cpp
+++ assembler/test/abruijn/test.cpp
@@ -2,10 +2,12 @@
 #include "ide_listener.h"
 #include "cute_runner.h"
 #include "hashTest.hpp"
+#include "spellgenometest.hpp"
 
 void runSuite() {
 	 cute::suite s;
 	 s += HashSuite();
+         s += SpellingGenomeSuite();
 	 cute::ide_listener lis;
 	 cute::makeRunner(lis)(s, "The Suite");
  }
