479b7b264103165bc25c91307f52505f00a43492
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2011-06-08 19:38:06 +0400

    GraphLabelers implemented

---
+++ assembler/src/omni/graph_labeler.hpp
@@ -0,0 +1,68 @@
+/*
+ * graph_labeler.hpp
+ *
+ *  Created on: 08.06.2011
+ *      Author: misha
+ */
+
+#ifndef GRAPH_LABELER_HPP_
+#define GRAPH_LABELER_HPP_
+
+namespace gvis {
+
+template<class Graph>
+class GraphLabeler {
+protected:
+public:
+	typedef typename Graph::EdgeId EdgeId;
+	typedef typename Graph::VertexId VertexId;
+//	GraphPrinter(const string &name, ostream &out = cout) : name_(name), out_(out) {
+//	}
+
+	virtual ~GraphLabeler() {
+	}
+
+	virtual std::string label(VertexId vertexId) = 0;
+
+	virtual std::string label(EdgeId edgeId) = 0;
+};
+
+template<class Graph>
+class EmptyGraphLabeler : public GraphLabeler<Graph> {
+	typedef GraphLabeler<Graph> super;
+	typedef typename super::EdgeId EdgeId;
+	typedef typename super::VertexId VertexId;
+public:
+	EmptyGraphLabeler() {}
+
+	std::string label(VertexId vertexId) {
+		return "";
+	}
+
+	std::string label(EdgeId edgeId) {
+		return "";
+	}
+};
+
+template<class Graph>
+class StrGraphLabeler : public GraphLabeler<Graph> {
+protected:
+	typedef GraphLabeler<Graph> super;
+	typedef typename super::EdgeId EdgeId;
+	typedef typename super::VertexId VertexId;
+	Graph& g_;
+public:
+	StrGraphLabeler(Graph& g) : g_(g) {}
+
+	std::string label(VertexId vertexId) {
+		return g_.str(vertexId);
+	}
+
+	std::string label(EdgeId edgeId) {
+		return g_.str(edgeId);
+	}
+};
+
+}
+
+#endif /* GRAPH_LABELER_HPP_ */
