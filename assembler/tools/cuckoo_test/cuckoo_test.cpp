9ace2b9eb5979f10150fca1da943fd0b589bb611
Mariya Fomkina <mariyafomkina@gmail.com>
2011-06-13 23:46:22 +0400

    Added buckets conseption to cuckoo. Tests fail.

--- assembler/tools/cuckoo_test/cuckoo_test.cpp
+++ assembler/tools/cuckoo_test/cuckoo_test.cpp
@@ -12,24 +12,26 @@
 int main(int argc, char** argv) {
   std::string filename = "";
   size_t d = 0;
+  size_t lbs = 0;
   double step = 0;
   size_t mld = 1;
   std::string label = "";
 
-	if (argc == 6) {
+	if (argc == 7) {
 		filename = argv[1];
     d = atoi(argv[2]);
-    step = atoi(argv[3]) / 10.;
-    mld = atoi(argv[4]);
-    label = argv[5];
+    lbs = atoi(argv[3]);
+    step = atoi(argv[4]) / 10.;
+    mld = atoi(argv[5]);
+    label = argv[6];
   } else {
-		std::cout << "Usage: ./cuckoo_test <filename>" << 
-      " <d> <step*10> <max_loop_denom> <label>" << std::endl;
+		std::cout << "Usage: ./cuckoo_test <filename> <d> " <<
+      "<log_2(bucket_size)> <step*10> <max_loop_denom> <label>" << std::endl;
     return 0;
   }
 
   seq_filter_stat<K, cuckoo<Seq<K>, size_t, Seq<K>::multiple_hash, 
-    Seq<K>::equal_to> >::filter(filename, d, step, mld, label);
+    Seq<K>::equal_to> >::filter(filename, d, lbs, step, mld, label);
 
   return 0;
 }
