55afca15fc3585f3b9d6416b50091c1a41f30d8d
Mariya Fomkina <mariyafomkina@gmail.com>
2011-06-14 00:26:33 +0400

    Tests for log_2(bucket_size) are added.

--- assembler/tools/cuckoo_test/cuckoo_test.sh
+++ assembler/tools/cuckoo_test/cuckoo_test.sh
@@ -3,12 +3,12 @@
 DATA_DIR=../../../data/input
 EXEC_DIR=../../build/tools/cuckoo_test
 
-FILE1=s_6.first1000_1.fastq.gz 
-FILE2=s_6.first1000_1.fastq.gz 
-FILE3=s_6.first1000_1.fastq.gz 
-#FILE1=s_6.first10000_1.fastq.gz 
-#FILE2=s_6.first100000_1.fastq.gz 
-#FILE3=s_6.first400000_1.fastq.gz 
+#FILE1=s_6.first1000_1.fastq.gz 
+#FILE2=s_6.first1000_1.fastq.gz 
+#FILE3=s_6.first1000_1.fastq.gz 
+FILE1=s_6.first10000_1.fastq.gz 
+FILE2=s_6.first100000_1.fastq.gz 
+FILE3=s_6.first400000_1.fastq.gz 
 
 make
 cp diagrams.gnu $EXEC_DIR
@@ -88,5 +88,31 @@ done
 
 COM="MAP1='1';MAP2='2';MAP3='5';MAP4='10';MAP5='20';FN_INS='mlf_t_ins.png';FN_FIND='mlf_t_find.png';FN_MEM='mlf_mem.png';"
 gnuplot -e $COM diagrams.gnu
+
+step=15
+mlf=10
+for d in 2 3 4;
+  do rm -rf temp.tmp time_insert.tmp time_find.tmp memory.tmp
+  echo "d:" $d;
+  for lbs in 0 1 2 3; 
+    do echo "log_2(bucket_size):" $lbs;
+    echo " " > temp.tmp
+    for f in $DATA_DIR/$FILE1 $DATA_DIR/$FILE2 $DATA_DIR/$FILE3; 
+      do echo "File:" $f; 
+      ./cuckoo_test $f $d $lbs $step $mlf ${f##*/} >> temp.tmp
+    done
+    cat temp.tmp | grep Insert >> time_insert.tmp 
+    echo " " >> time_insert.tmp
+    echo " " >> time_insert.tmp
+    cat temp.tmp | grep Find >> time_find.tmp 
+    echo " " >> time_find.tmp
+    echo " " >> time_find.tmp
+    cat temp.tmp | grep Memory >> memory.tmp 
+    echo " " >> memory.tmp
+    echo " " >> memory.tmp
+  done
+COM="MAP1='0';MAP2='1';MAP3='2';MAP4='3';MAP5='4';FN_INS='lbs_"$d"_t_ins.png';FN_FIND='lbs_"$d"_t_find.png';FN_MEM='lbs_"$d$"_mem.png';"
+gnuplot -e $COM diagrams.gnu
+done
 
 echo "Diagrams are done!"
