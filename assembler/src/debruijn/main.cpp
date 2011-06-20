86a8236df9d205e1034af73882cb9ef59c0e6fb0
Sergey Nurk <sergeynurk@gmail.com>
2011-06-20 17:59:41 +0400

    launch architecture improved

--- assembler/src/debruijn/main.cpp
+++ assembler/src/debruijn/main.cpp
@@ -7,18 +7,35 @@
 #include "common/logging.hpp"
 DECL_PROJECT_LOGGER("d")
 
+namespace {
 
-int main()
-{
+std::string MakeLaunchTimeDirName() {
+	time_t rawtime;
+	struct tm * timeinfo;
+	char buffer[80];
+
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime(buffer, 80, "%d.%m.%y %H:%M", timeinfo);
+	return string(buffer);
+}
+}
+int main() {
 	// read configuration file (dataset path etc.)
 	string input_dir = CONFIG.read<string> ("input_dir");
-	string output_dir = CONFIG.read<string> ("output_dir");
+	string output_dir = CONFIG.read<string> ("output_dir")
+			+ MakeLaunchTimeDirName() + "/";
 	string dataset = CONFIG.read<string> ("dataset");
-	string genome_filename = input_dir + "/" + CONFIG.read<string> ("reference_genome");
-	string reads_filename1 = input_dir + "/" + CONFIG.read<string> (dataset + "_1");
-	string reads_filename2 = input_dir + "/" + CONFIG.read<string> (dataset + "_2");
+	string genome_filename = input_dir + "/" + CONFIG.read<string> (
+			"reference_genome");
+	string reads_filename1 = input_dir + "/" + CONFIG.read<string> (
+			dataset + "_1");
+	string reads_filename2 = input_dir + "/" + CONFIG.read<string> (
+			dataset + "_2");
 	int insert_size = CONFIG.read<int> (dataset + "_IS");
 	int dataset_len = CONFIG.read<int> (dataset + "_LEN");
+	bool paired_mode = CONFIG.read<bool> ("paired_mode");
 
 	// typedefs :)
 	typedef MateReader<Read, ireadstream>::type ReadStream;
@@ -39,10 +56,17 @@ int main()
 		genome_stream >> full_genome;
 		genome = full_genome.getSequenceString().substr(0, dataset_len); // cropped
 	}
-
+	INFO("Working with " << dataset << " dataset");
 	// assemble
-	debruijn_graph::DeBruijnGraphTool<K, RCStream>(rcStream, genome, output_dir);
+	if (paired_mode) {
+		debruijn_graph::DeBruijnGraphWithPairedInfoTool<K, RCStream>(rcStream,
+				genome, output_dir);
+	} else {
+		debruijn_graph::DeBruijnGraphTool<K, RCStream>(rcStream, genome,
+				output_dir);
+	}
 
 	// OK
 	return 0;
 }
+
