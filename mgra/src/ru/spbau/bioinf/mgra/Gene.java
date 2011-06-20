ace62ca9ac09405904f80ed84ddb01bc4039049e
Yakov Sirotkin <yasha@telamon.ru>
2011-06-20 18:59:28 +0400

    Initial version. http://ablab.myjetbrains.com/youtrack/issue/MGRA-11

---
+++ mgra/src/ru/spbau/bioinf/mgra/Gene.java
@@ -0,0 +1,21 @@
+package ru.spbau.bioinf.mgra;
+
+import org.jdom.Element;
+
+public class Gene {
+
+    private int id;
+    private Direction direction;
+
+    public Gene(int id, Direction direction) {
+        this.id = id;
+        this.direction = direction;
+    }
+
+    public Element toXml() {
+        Element gene = new Element("gene");
+        XmlUtil.addElement(gene, "id", id);
+        XmlUtil.addElement(gene, "direction", direction.toString());
+        return gene;
+    }
+}
