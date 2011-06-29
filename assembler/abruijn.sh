da2b957eb130f75a6369a7efba3f29902315e972
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2011-06-29 12:52:51 +0400

    launch config

--- assembler/abruijn.sh
+++ assembler/abruijn.sh
@@ -1,1 +1,1 @@
-./build/abruijn/abruijn --input2 ./data/input/cropped/s_6.first50000_1.fastq.gz --input ./data/input/MG1655-K12_emul1.fastq.gz --output ./data/abruijn/output.dot --take 1 --mode 1 --cut 1000
+./build/abruijn/abruijn --input ./data/input/E.Coli.K12.MG1655/EAS20_8/quaked/cropped/s_6.first10000_1.fastq.gz --output ./data/abruijn/output.dot --take 1 --mode 1 --cut 1000
