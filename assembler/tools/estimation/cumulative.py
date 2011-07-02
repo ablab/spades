f3f5566bf74549b1f1cdcc2bbc0f8417905befa4
Nikolay Vyahhi <vyahhi@gmail.com>
2011-07-02 17:06:53 +0400

    new improved cumulative drawer

--- assembler/tools/estimation/cumulative.py
+++ assembler/tools/estimation/cumulative.py
@@ -1,71 +1,60 @@
 import sys
-
-# check command line arguments
-if len(sys.argv) < 3:
-	print "Draws cumulative contigs lengths plot"
-	print "Usage: ", sys.argv[0], " contigs_file1.fasta contigs_file2.fasta [0 < mul <= 1; default mul = 0.5]"
-	exit(0)
-
-
-if len(sys.argv) >= 4:
-	mul = float(sys.argv[3])
-else:
-	mul = 0.5
-
-# get lengths of contigs from fasta-file #1
-lengths1 = []
-l = 0
-for line in open(sys.argv[1]):
-	if (line[0] == '>'):
-		if l != 0: # not first sequence in fasta
-			lengths1.append(l)
-			l = 0
-	else:
-		l += len(line.strip())
-lengths1.append(l)
-lengths1.sort(reverse = True)
-ln1 = len(lengths1)
-
-# get lengths of contigs from fasta-file #2
-lengths2 = []
-l = 0
-for line in open(sys.argv[2]):
-	if (line[0] == '>'):
-		if l != 0: # not first sequence in fasta
-			lengths2.append(l)
-			l = 0
-	else:
-		l += len(line.strip())
-lengths2.append(l)
-lengths2.sort(reverse = True)
-ln2 = len(lengths2)
-
-# calculate Nx values
-vals_percent1 = []
-vals_length1 = []
-lcur = 0
-lind = 0
-for l in lengths1:
-	lcur += l
-	lind += 1
-	vals_percent1.append(lind * 100.0 / ln1)
-	vals_length1.append(lcur * mul)
-
-vals_percent2 = []
-vals_length2 = []
-lcur = 0
-lind = 0
-for l in lengths2:
-	lcur += l
-	lind += 1
-	vals_percent2.append(lind * 100.0 / ln2)
-	vals_length2.append(lcur)
-
-# plot vals
+import itertools
 import pylab
 import matplotlib.ticker
-pylab.plot(vals_percent1, vals_length1)
-pylab.plot(vals_percent2, vals_length2)
+# mailto: vyahhi@gmail.com
+
+# check command line arguments
+if len(sys.argv) < 2:
+	print "Draws cumulative contigs lengths plot"
+	print
+	print "Usage:", sys.argv[0], "FASTA1 [MUL1 [FASTA1 [MUL2 ...]"
+	print "FASTA is path to .fasta file."
+	print "MUL is multiplicator for scaling. Default for last optional mul is 1."
+	print
+	print "Example: python", sys.argv[0], "../../data/debruijn/we_contigs.fasta 0.5 ../../data/debruijn/velvet_contigs.fa 1"
+	print
+	exit(0)
+if len(sys.argv) % 2 == 0:
+	sys.argv.append("1.0")
+
+def get_lengths_from_fastafile(filename):
+	lengths = []
+	l = 0
+	for line in open(filename):
+		if (line[0] == '>'):
+			if l != 0: # not first sequence in fasta
+				lengths.append(l)
+				l = 0
+		else:
+			l += len(line.strip())
+	lengths.append(l)
+	return lengths
+
+	
+lengthses = []
+muls = []
+
+
+for filename, mul in itertools.izip(sys.argv[1::2], sys.argv[2::2]):
+	lengthses.append(get_lengths_from_fastafile(filename))
+	muls.append(float(mul))
+
+for lengths, mul in itertools.izip(lengthses, muls):
+	lengths.sort(reverse = True)
+	vals_percent = []
+	vals_length = []
+	ln = len(lengths)
+	lcur = 0
+	lind = 0
+	for l in lengths:
+		lcur += l
+		lind += 1
+		vals_percent.append(lind * 100.0 / ln)
+		vals_length.append(lcur * mul)
+	pylab.plot(vals_percent, vals_length)
+	
+	
 #pylab.yscale('log')
 pylab.xlabel('Contigs (percentage)')
 pylab.ylabel('Cumulative length')
@@ -74,7 +63,7 @@ pylab.grid(True)
 ax = pylab.gca()
 formatter = matplotlib.ticker.FormatStrFormatter('%.f')
 ax.yaxis.set_major_formatter(formatter)
-ax.legend((sys.argv[1], sys.argv[2]), loc='lower right')
+ax.legend(sys.argv[1::2], loc='lower right')
 pylab.savefig('cumulative_plot')
 print "Saved to ./cumulative_plot.png"
 pylab.show()
