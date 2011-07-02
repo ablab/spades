c11ffbf53374b160a772d0b56272cfe35743d725
Nikolay Vyahhi <vyahhi@gmail.com>
2011-07-02 15:19:47 +0400

    Quake was added. cumulative plotter was changed to compare two files.

--- assembler/tools/estimation/cumulative.py
+++ assembler/tools/estimation/cumulative.py
@@ -1,46 +1,71 @@
 import sys
 
 # check command line arguments
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
 	print "Draws cumulative contigs lengths plot"
-	print "Usage: ", sys.argv[0], " contigs_file.fasta"
+	print "Usage: ", sys.argv[0], " contigs_file1.fasta contigs_file2.fasta [0 < mul <= 1; default mul = 0.5]"
 	exit(0)
-	
-# get lengths of contigs from fasta-file
-lengths = []
+
+
+if len(sys.argv) >= 4:
+	mul = float(sys.argv[3])
+else:
+	mul = 0.5
+
+# get lengths of contigs from fasta-file #1
+lengths1 = []
 l = 0
 for line in open(sys.argv[1]):
 	if (line[0] == '>'):
 		if l != 0: # not first sequence in fasta
-			lengths.append(l)
+			lengths1.append(l)
 			l = 0
 	else:
 		l += len(line.strip())
-lengths.append(l)
+lengths1.append(l)
+lengths1.sort(reverse = True)
+ln1 = len(lengths1)
 
-# prepare lengths
-lengths.sort(reverse = True)
-ln = len(lengths)
+# get lengths of contigs from fasta-file #2
+lengths2 = []
+l = 0
+for line in open(sys.argv[2]):
+	if (line[0] == '>'):
+		if l != 0: # not first sequence in fasta
+			lengths2.append(l)
+			l = 0
+	else:
+		l += len(line.strip())
+lengths2.append(l)
+lengths2.sort(reverse = True)
+ln2 = len(lengths2)
 
 # calculate Nx values
-vals_percent = []
-vals_length = []
+vals_percent1 = []
+vals_length1 = []
 lcur = 0
 lind = 0
-for l in lengths:
+for l in lengths1:
 	lcur += l
 	lind += 1
-	#vals_Nx.append(vals_Nx[-1] + 1e-10) # eps
-	#vals_l.append(lcur)
-	vals_percent.append(lind * 100.0 / ln)
-	vals_length.append(lcur)
-#print vals_Nx
-#print vals_l
+	vals_percent1.append(lind * 100.0 / ln1)
+	vals_length1.append(lcur * mul)
+
+vals_percent2 = []
+vals_length2 = []
+lcur = 0
+lind = 0
+for l in lengths2:
+	lcur += l
+	lind += 1
+	vals_percent2.append(lind * 100.0 / ln2)
+	vals_length2.append(lcur)
 
 # plot vals
 import pylab
 import matplotlib.ticker
-pylab.plot(vals_percent, vals_length)
+pylab.plot(vals_percent1, vals_length1)
+pylab.plot(vals_percent2, vals_length2)
 #pylab.yscale('log')
 pylab.xlabel('Contigs (percentage)')
 pylab.ylabel('Cumulative length')
@@ -49,6 +74,7 @@ pylab.grid(True)
 ax = pylab.gca()
 formatter = matplotlib.ticker.FormatStrFormatter('%.f')
 ax.yaxis.set_major_formatter(formatter)
+ax.legend((sys.argv[1], sys.argv[2]), loc='lower right')
 pylab.savefig('cumulative_plot')
 print "Saved to ./cumulative_plot.png"
 pylab.show()
