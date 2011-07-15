54cd37854a4df1c818013459465df8383491f945
Mariya Fomkina <mariyafomkina@gmail.com>
2011-07-15 20:13:52 +0400

    Added comments for doxygen in paired_read and single_read.

--- assembler/src/common/io/paired_read.hpp
+++ assembler/src/common/io/paired_read.hpp
@@ -23,45 +23,107 @@
 
 class PairedRead {
 private:
+  /*
+   * @variable First single read in the pair.
+   */
 	SingleRead first_;
-	SingleRead second_;
-	size_t distance_;
-public:
+	/*
+   * @variable Second single read in the pair.
+   */
+  SingleRead second_;
+	/*
+   * @variable Distance between two single reads.
+   */
+  size_t distance_;
 
+public:
+  /* 
+   * Default constructor.
+   */
 	PairedRead() {}
 
+  /*
+   * Conctructor from single reads.
+   *
+   * @param first First single read in the pair.
+   * @param second Second single read in the pair.
+   * @param distance Distance between two single reads.
+   */
 	PairedRead(const SingleRead& first, const SingleRead& second, size_t distance) 
     : first_(first), second_(second), distance_(distance) {}
 
+  /*
+   * Return first single read in the pair.
+   *
+   * @return First single read.
+   */
 	const SingleRead& first() const {
 		return first_;
 	}
 
+  /*
+   * Return second single read in the pair.
+   *
+   * @return Second single read.
+   */
 	const SingleRead& second() const {
 		return second_;
 	}
 
+  /* 
+   * Return distance of paired read.
+   *
+   * @return Distance.
+   */
 	size_t distance() const {
 		return distance_;
 	}
 
+  /*
+   * Check whether paired read is valid.
+   *
+   * @return true if paired read is valid (both single reads are correct), 
+   * and false otherwise.
+   */
 	bool IsValid() const {
 		return first_.IsValid() && second_.IsValid();
 	}
 
-	const SingleRead& operator[] (size_t index) const {
-		if (index == 0) {
+  /* 
+   * Return ith single read of pair (0th or 1st). If index 
+   * is not 0 or 1, the assertion happens.
+   *
+   * @param i SingleRead index.
+   * @return SingleRead on ith position of pair.
+   */
+	const SingleRead& operator[] (size_t i) const {
+		if (i == 0) {
 			return first_;
-		} else if (index == 1) {
+		} else if (i == 1) {
 			return second_;
 		}
 		assert(false);
 	}
   
+  /* 
+   * Return reversed complimentary paired read (paired read with 
+   * reserve complimentary first and second single reads 
+   * and the same distance.
+   *
+   * @return Reversed complimentary paired read.
+   */
 	const PairedRead operator!() const {
 		return PairedRead(!second_, !first_, distance_);
   }
 
+  /* 
+   * Check whether two paired reads are equal.
+   *
+   * @param pairedread The paired read we want to compare ours with.
+   * @return true if these two paired reads have similar 
+   * first and second single reads and distance,
+   * and false otherwise.
+   */
   bool operator==(const PairedRead& pairedread) const {
     return first_ == pairedread.first_ &&
       second_ == pairedread.second_ &&
--- assembler/src/common/io/single_read.hpp
+++ assembler/src/common/io/single_read.hpp
@@ -29,49 +29,175 @@
 #include "simple_tools.hpp"
 
 class SingleRead {
+private:
+  /* 
+   * @variable The name of single read in input file.
+   */
+  std::string name_;
+  /*
+   * @variable The sequence of nucleotides.
+   */
+  std::string seq_;
+  /*
+   * @variable The quality of single read.
+   */
+  std::string qual_;
+  /*
+   * @variable The flag of single read correctness.
+   */
+  bool valid_;
+  friend class ireadstream;
+  
+  /* 
+   * Set name of single read.
+   * 
+   * @param new_name New name.
+   */
+  void set_name(const char* new_name) {
+    name_ = new_name;
+  }
+  
+  /* 
+   * Set sequence of single read.
+   * 
+   * @param new_sequence New sequence.
+   */
+  void set_sequence(const char* new_sequence) {
+    seq_ = new_sequence;
+    valid_ = UpdateValid();
+  }
+  
+  /* 
+   * Set quality of single read.
+   *
+   * @param new_quality New quality of single read.
+   * @param offset The offset of single read quality 
+   * (PHRED_OFFSET by default).
+   */
+  void set_quality(const char* new_quality, int offset = PHRED_OFFSET) {
+    qual_ = new_quality;
+    for (size_t i = 0; i < qual_.size(); ++i) {
+      qual_[i] -= offset;
+    }
+  }
+  
+  /* 
+   * Update valid_ flag.
+   * 
+   * @see IsValid()
+   */
+  const bool UpdateValid() const {
+    if (seq_.size() == 0) {
+      return false;
+    }
+    if (seq_.size() != qual_.size()) {
+      return false;
+    }
+    for (size_t i = 0; i < seq_.size(); ++i) {
+      if (!is_nucl(seq_[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 public:
   static const int PHRED_OFFSET = 33;
   static const int BAD_QUALITY_THRESHOLD = 2;
   
+  /*
+   * Default constructor.
+   */
   SingleRead() : valid_(false) {}
   
-  SingleRead(const std::string &name, 
-             const std::string &seq, 
-             const std::string &qual) 
-    : name_(name), seq_(seq), qual_(qual) { // for test only!
+  /*
+   * Test constructor.
+   *
+   * @param name The name of the single read (id in input file).
+   * @param seq The sequence of ATGC letters.
+   * @param qual The quality of the single read sequence.
+   */
+  SingleRead(const std::string& name, 
+             const std::string& seq, 
+             const std::string& qual) 
+    : name_(name), seq_(seq), qual_(qual) { 
     valid_ = UpdateValid();
   }
 
+  /*
+   * Check whether single read is valid.
+   *
+   * @return true if single read is valid (there is no N in sequence
+   * and sequence size is equal to quality size), and false otherwise
+   */
   bool IsValid() const {
     return valid_;
   }
 
+  /*
+   * Return Sequence object, got from sequence string.
+   *
+   * @return Single read sequence.
+   */ 
   Sequence sequence() const {
     assert(valid_);
     return Sequence(seq_);
   }
 
+  /*
+   * Return Quality object, got from quality string.
+   *
+   * @return Single read quality.
+   */ 
   Quality quality() const {
     assert(valid_);
     return Quality(qual_);
   }
 
+  /* 
+   * Return name of single read.
+   *
+   * @return Single read name.
+   */
   const std::string& name() const {
     return name_;
   }
 
+  /*
+   * Return size of single read.
+   *
+   * @return The size of single read sequence.
+   */
   size_t size() const { 
     return seq_.size();
   }
 
+  /* 
+   * Return single read sequence string (in readable form with ATGC).
+   *
+   * @return Single read sequence string.
+   */
   const std::string& GetSequenceString() const { 
     return seq_;
   }
 
+  /* 
+   * Return single read quality string (in readable form).
+   *
+   * @return Single read quality string.
+   */
   const std::string& GetQualityString() const {  
     return qual_;
   }
 
+  /*
+   * Return single read quality string, where every quality value is 
+   * increased by offset (need for normalization of quality values). 
+   * Do not modify original quality values.
+   *
+   * @param offset The offset of single read quality (PHRED_OFFSET by default).
+   * @return Modified single read quality string.
+   */
   std::string GetPhredQualityString(int offset = PHRED_OFFSET) const {
     std::string res = qual_;
     for (size_t i = 0; i < res.size(); ++i) {
@@ -80,11 +206,24 @@ public:
     return res;
   }
 
+  /*
+   * Return ith nucleotide of single read sequence in unreadable form 
+   * (0, 1, 2 or 3).
+   *
+   * @param i Nucleotide index.
+   * @return Nucleotide on ith position of single read sequence.
+   */
   char operator[](size_t i) const {
     assert(is_nucl(seq_[i]));
     return dignucl(seq_[i]);
   }
 
+  /* 
+   * Return reversed complimentary single read (single read with new name, 
+   * reversed complimentary sequence, and reversed quality).
+   *
+   * @return Reversed complimentary single read.
+   */
   SingleRead operator!() const {
     std::string new_name;
     if (name_ == "" || name_[0] != '!') {
@@ -95,43 +234,15 @@ public:
     return SingleRead(new_name, ReverseComplement(seq_), Reverse(qual_));
   }
 
+  /* 
+   * Check whether two single reads are equal.
+   *
+   * @param singleread The single read we want to compare ours with.
+   * @return true if these two single reads have similar sequences,
+   * and false otherwise.
+   */
   bool operator==(const SingleRead& singleread) const {
     return seq_ == singleread.seq_;
-  }
-
-private:
-  std::string name_;
-  std::string seq_;
-  std::string qual_;
-  bool valid_;
-  friend class ireadstream;
-  
-  void set_name(const char* s) {
-    name_ = s;
-  }
-  
-  void set_quality(const char* s, int offset = PHRED_OFFSET) {
-    qual_ = s;
-    for (size_t i = 0; i < qual_.size(); ++i) {
-      qual_[i] -= offset;
-    }
-  }
-  
-  void set_sequence(const char* s) {
-    seq_ = s;
-    valid_ = UpdateValid();
-  }
-  
-  const bool UpdateValid() const {
-    if (seq_.size() == 0) {
-      return false;
-    }
-    for (size_t i = 0; i < seq_.size(); ++i) {
-      if (!is_nucl(seq_[i])) {
-        return false;
-      }
-    }
-    return true;
   }
 };
 
