b81b0bdca9b811e09e800639140b526c062cb816
Andrey <andrewprzh@gmail.com>
2011-07-16 04:50:37 +0400

    All extention, simple quality check and loop detection added

--- assembler/src/debruijn/long_contigs/main.cpp
+++ assembler/src/debruijn/long_contigs/main.cpp
@@ -5,73 +5,182 @@
  *      Author: andrey
  */
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "../launch.hpp"
+#include "../config.hpp"
+#include "../../common/logging.hpp"
+#include "../../common/simple_tools.hpp"
+
 #include "long_contigs.hpp"
-#include "graph.hpp"
 
-namespace long_contigs {
+namespace {
 
-void BuildSeeds(Graph& g, std::vector<BiderectionalPath>& seeds) {
-	BuildDeBruijnGraph(g);
-	FindSeeds(g, seeds);
+std::string MakeLaunchTimeDirName() {
+	time_t rawtime;
+	struct tm * timeinfo;
+	char buffer[80];
+
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime(buffer, 80, "%m.%d_%H_%M", timeinfo);
+	return string(buffer);
+}
 }
 
-void PrintSeedEdgeLengthStats(std::vector<BiderectionalPath>& seeds, std::ostream &os) {
-	std::map<size_t, size_t> lengthMap;
+DECL_PROJECT_LOGGER("d")
 
-	for(auto iter = seeds.begin(); iter != seeds.end(); ++iter) {
-		++lengthMap[iter->size()];
+template<size_t k, class ReadStream>
+void BuildDeBruijnGraph(ReadStream& stream,
+		const Sequence& genome, bool paired_mode, bool etalon_info_mode,
+		bool from_saved, size_t insert_size, size_t max_read_length,
+		const string& output_folder, const string& work_tmp_dir,
+		Graph& g, PairedInfoIndex<Graph>& paired_index, EdgeIndex<k + 1, Graph>& index) {
+
+	INFO("Edge graph construction tool started");
+	INFO("Paired mode: " << (paired_mode ? "Yes" : "No") );
+	INFO("Etalon paired info mode: " << (etalon_info_mode ? "Yes" : "No"))
+	INFO("From file: " << (from_saved ? "Yes" : "No"))
+	mkdir(work_tmp_dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | S_IWOTH);
+
+	IdTrackHandler<Graph> IntIds(g);
+
+	if (!from_saved) {
+
+		if (paired_mode) {
+			if (etalon_info_mode) {
+				ConstructGraphWithEtalonPairedInfo<k, ReadStream> (g, index,
+						paired_index, stream, insert_size, max_read_length,
+						genome);
+			} else {
+				ConstructGraphWithPairedInfo<k, ReadStream> (g, index,
+						paired_index, stream);
+			}
+		} else {
+			typedef SimpleReaderWrapper<ReadStream> UnitedStream;
+			UnitedStream united_stream(stream);
+			ConstructGraphWithCoverage<k, UnitedStream> (g, index,
+					united_stream);
+		}
+
+		ProduceInfo<k> (g, index, genome, output_folder + "edge_graph.dot",
+				"edge_graph");
+
+		SimplifyGraph<k> (g, index, 3, genome, output_folder);
+
+		ProduceInfo<k> (g, index, genome,
+				output_folder + "simplified_graph.dot", "simplified_graph");
+
+		WriteGraphComponents<k> (g, index, genome,
+				output_folder + "graph_components" + "/", "graph.dot",
+				"graph_component", insert_size);
+		if (paired_mode) {
+			ProducePairedInfo(g, insert_size, max_read_length, paired_index,
+					output_folder, etalon_info_mode);
+		}
 	}
 
-	for(auto iter = lengthMap.begin(); iter != lengthMap.end(); ++iter) {
-		os << iter->first << " : " << iter->second << std::endl;
-	}
+	INFO("Building de Bruijn graph finished");
 }
 
-void PrintSeedLengthStats(Graph& g, std::vector<BiderectionalPath>& seeds, std::ostream &os) {
-	std::map<size_t, size_t> lengthMap;
-
-	for(auto iter = seeds.begin(); iter != seeds.end(); ++iter) {
-		++lengthMap[PathLength(g, *iter)];
+template<size_t k>
+void BuildDeBruijnGraph(Graph& g,  PairedInfoIndex<Graph>& paired_index, EdgeIndex<k + 1, Graph>& index,
+		Sequence& sequence) {
+	// check config.hpp parameters
+	if (K % 2 == 0) {
+		FATAL("K in config.hpp must be odd!\n");
 	}
+	std::cout << CONFIG_FILENAME << std::endl;
+	checkFileExistenceFATAL(CONFIG_FILENAME);
 
-	for(auto iter = lengthMap.begin(); iter != lengthMap.end(); ++iter) {
-		os << iter->first << " : " << iter->second << std::endl;
+	// read configuration file (dataset path etc.)
+	string input_dir = CONFIG.read<string>("input_dir");
+	string dataset = CONFIG.read<string>("dataset");
+	string output_root = CONFIG.read<string>("output_dir");
+	string output_dir_suffix = MakeLaunchTimeDirName()+ "." + dataset + "/";
+	string output_dir = output_root + output_dir_suffix;
+	string work_tmp_dir = output_root + "tmp/";
+//	std::cout << "here " << mkdir(output_dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH| S_IWOTH) << std::endl;
+	mkdir(output_dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | S_IWOTH);
+	string genome_filename = input_dir
+			+ CONFIG.read<string>("reference_genome");
+	string reads_filename1 = input_dir + CONFIG.read<string>(dataset + "_1");
+	string reads_filename2 = input_dir + CONFIG.read<string>(dataset + "_2");
+	checkFileExistenceFATAL(genome_filename);
+	checkFileExistenceFATAL(reads_filename1);
+	checkFileExistenceFATAL(reads_filename2);
+
+	size_t insert_size = CONFIG.read<size_t>(dataset + "_IS");
+	size_t max_read_length = 100; //CONFIG.read<size_t> (dataset + "_READ_LEN");
+	size_t dataset_len = CONFIG.read<int>(dataset + "_LEN");
+	bool paired_mode = CONFIG.read<bool>("paired_mode");
+	bool etalon_info_mode = CONFIG.read<bool>("etalon_info_mode");
+	bool from_saved = CONFIG.read<bool>("from_saved_graph");
+	// typedefs :)
+	typedef MateReader<Read, ireadstream>::type ReadStream;
+	typedef PairedReader<ireadstream> PairedReadStream;
+	typedef RCReaderWrapper<PairedReadStream, PairedRead> RCStream;
+
+	// read data ('reads')
+	const string reads[2] = {reads_filename1, reads_filename2};
+	ReadStream reader(reads);
+	PairedReadStream pairStream(reader, insert_size);
+	RCStream rcStream(pairStream);
+
+	// read data ('genome')
+	std::string genome;
+	{
+		ireadstream genome_stream(genome_filename);
+		Read full_genome;
+		genome_stream >> full_genome;
+		genome = full_genome.getSequenceString().substr(0, dataset_len); // cropped
 	}
+	sequence = Sequence(genome);
+	// assemble it!
+
+	BuildDeBruijnGraph<K, RCStream>(rcStream, sequence, paired_mode, etalon_info_mode,
+			from_saved, insert_size, max_read_length, output_dir, work_tmp_dir, g, paired_index, index);
 }
 
-//void PrintSeedCoverage(Graph& g, std::vector<BiderectionalPath>& seeds, std::ostream &os) {
-//	std::multiset<EdgeId> covered;
-//
-//	for(auto path = seeds.begin(); path != seeds.end(); ++path) {
-//		for(BiderectionalPath::iterator iter = path->back(); iter != path->end(); ++iter) {
-//			covered.insert(*iter);
-//		}
-//	}
-//
-//	std::map<size_t, size_t> coveredTimes;
-//	size_t edgeCount;
-//	for (auto iter = g.SmartEdgeBegin(); !iter.IsEnd(); ++iter) {
-//		++coveredTimes[covered.count(*iter)];
-//		++edgeCount;
-//	}
-//
-//	for(auto iter = coveredTimes.begin(); iter != coveredTimes.end(); ++iter) {
-//		os << iter->first << " : " << iter->second << std::endl;
-//	}
-//	os << "Total edge coverage: " << (double) coveredTimes[0]/edgeCount << std::endl;
-//}
-
-}
 
 int main() {
 	using namespace long_contigs;
 
 	Graph g(K);
-	std::vector<BiderectionalPath> seeds;
+	EdgeIndex<K + 1, Graph> index(g);
+	PairedInfoIndex<Graph> paired_index(g, 5);
+	Sequence sequence("");
 
-	BuildSeeds(g, seeds);
-	PrintSeedLengthStats(g ,seeds, std::cout);
+	std::vector<BidirectionalPath> seeds;
+	std::vector<BidirectionalPath> paths;
 
+	BuildDeBruijnGraph<K>(g, paired_index, index, sequence);
+
+	FindSeeds(g, seeds);
+
+	for(auto iter = seeds.begin(); iter != seeds.end(); ++iter) {
+		PrintPath(g, *iter);
+	}
+
+	INFO("Seeds length");
+	PrintPathLengthStats(g ,seeds);
+	INFO("Seeds edges count");
+	PrintPathEdgeLengthStats(seeds);
+	INFO("Seeds coverage");
+	PrintPathCoverage(g, seeds);
+
+	FindPaths(g, seeds, paired_index, paths, true);
+
+	INFO("Final paths");
+	for(auto path = paths.begin(); path != paths.end(); ++path) {
+		PrintPath(g, *path);
+	}
+
+	size_t found = PathsInGenome<K>(g, index, sequence, paths);
+	INFO("Total paths found " << found);
 	return 0;
 }
 
