a73115db3e6903db09ee42f569ae61e7482a73e8
Alex Davydow <adavydow@yandex.ru>
2011-07-18 13:19:08 +0400

    Again, kmer_functions rewritten to be independent from hammer_config

--- assembler/src/hammer/kmer_functions.hpp
+++ assembler/src/hammer/kmer_functions.hpp
@@ -74,24 +74,47 @@ void AddKMers(const Read &r, KMerStatMap *v) {
 }
 
 /**
+ * get next valid kmer in a new position
+ */
+template<uint32_t kK>
+int32_t getKmerAnew( const std::string & seq, int32_t pos, Seq<kK> & kmer ) {
+  int32_t curHypothesis = pos;
+  int32_t i = pos;
+  for (; i < seq.size(); ++i) {
+    if (i >= kK + curHypothesis) {
+      kmer = Seq<kK>(seq.data() + curHypothesis, false);
+      return curHypothesis + 1;
+    }
+    if (!is_nucl(seq[i])) {
+      curHypothesis = i + 1;
+    }
+  }
+  if (i >= kK + curHypothesis) {
+    kmer = Seq<kK>(seq.data() + curHypothesis, false);
+    return curHypothesis + 1;
+  }
+  return -1;
+}
+
+/**
  * @param kmer get next valid k-mer
  * @param pos starting point
  * @return the first starting point of a valid k-mer >=start; return
  * -1 if no such place exists
  */
-int32_t NextValidKmer(const Read &r, int32_t pos, KMer & kmer);
-
-/**
- * get next valid kmer in a new position
- */
-int32_t getKmerAnew( const std::string & seq, int32_t pos, KMer & kmer );
-
-/**
- * add k-mers from read to map
- */
-void AddKMers(const Read &r, uint64_t readno, KMerStatMap *v);
-
-vector<KMer> GetKMers(const Read &r);
-
+template<uint32_t kK>
+int32_t NextValidKmer(const Read &r, int32_t pos, Seq<kK> & kmer) {
+  const std::string &seq = r.getSequenceString();
+  if (pos == -1) { // need to get first valid kmer
+    return getKmerAnew(seq, 0, kmer);
+  } else {
+    if (pos + kK < r.size() && is_nucl(seq[pos + kK])) {
+      kmer = kmer << r[pos + kK];
+      return (pos + 1);
+    } else {
+      return getKmerAnew(seq, pos, kmer);
+    }
+  }
+}
 
 #endif  // HAMMER_KMERFUNCTIONS_HPP_
