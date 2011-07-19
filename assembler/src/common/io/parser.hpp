9317ab6d571b80e4f94ad918bea39ef5bc894678
Mariya Fomkina <mariyafomkina@gmail.com>
2011-07-19 14:01:41 +0400

    Added Parser class - parent for all other parsers.

---
+++ assembler/src/common/io/parser.hpp
@@ -0,0 +1,68 @@
+/**
+ * @file    parser.hpp
+ * @author  Mariya Fomkina
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * @section DESCRIPTION
+ *
+ * Parser is the parent class for all streams that read data from
+ * different file types (fastq, fasta, sam etc).
+ */
+
+#ifndef COMMON_IO_PARSER_HPP
+#define COMMON_IO_PARSER_HPP
+
+#include <string>
+#include "common/io/single_read.hpp"
+
+class Parser {
+ public:
+  Parser(const std::string& filename,
+         int offset = SingleRead::PHRED_OFFSET)
+      : offset_(offset), filename_(filename) {}
+
+  virtual ~Parser() {
+    close();
+  }
+
+  bool is_open() const {
+    return is_open_;
+  }
+
+  virtual bool eof() const {
+    return eof_;
+  }
+
+  virtual Parser& operator>>(SingleRead &read) = 0;
+
+  virtual void close() = 0;
+
+  void reset() {
+    close();
+    open(filename_);
+  }
+
+ private:
+  std::string filename_;
+  bool is_open_;
+  bool eof_;
+  int offset_;
+
+  virtual bool open() = 0;
+};
+
+Parser* SelectParser(const std::string filename_,
+                     int offset = SingleRead::PHRED_OFFSET) {
+  // get extension
+  // select one of parsers according to extension
+  // return parser*
+}
+
+#endif /* COMMON_IO_PARSER_HPP */
