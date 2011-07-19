aa8d32fb0a360d6634ef5e64fd6ec6352b125312
Alex Davydow <adavydow@yandex.ru>
2011-07-19 12:40:12 +0400

    Tests actualised

--- assembler/test/hammer/test.cpp
+++ assembler/test/hammer/test.cpp
@@ -1,12 +1,10 @@
 #include "cute/cute.h"
 #include "cute/cute_runner.h"
 #include "cute/ide_listener.h"
-#include "kmer_functions_test.hpp"
 #include "valid_kmer_generator_test.hpp"
 
 void runSuite() {
   cute::suite s;
-  s += KMerFunctionsSuite();
   s += ValidKMerGeneratorSuite();
   cute::ide_listener lis;
   cute::makeRunner(lis)(s, "The Suite");
