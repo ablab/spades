cfd17fdda60af49abe15852281e0dc9dad76b749
Son Pham <kspham@eng.ucsd.edu>
2011-07-21 05:29:12 -0700

    Rectangle graph

---
+++ assembler/src/debruijn/rectangleRepeatResolver.hpp
@@ -0,0 +1,57 @@
+#ifndef RECTANGLEREPEATRESOLVER_H
+
+#define RECTANGLEREPEATRESOLVER_H
+
+
+
+
+#include "weakerGluer.hpp"
+#include "graphCopy.hpp"
+#include "distanceEstimator.hpp"
+namespace debruijn_graph {
+
+
+
+template<class Graph>
+class RectangleRepeatResolver {
+private:
+    Graph &graph_;
+    Graph &resultGraph_;
+    PairInfoIndexData<typename Graph::EdgeId> &index_;
+    size_t errorDistance_;
+public:
+
+    RectangleRepeatResolver(Graph &graph, PairInfoIndexData<typename Graph::EdgeId> &index, Graph &resultGraph, size_t errorDistance) :
+         graph_(graph), resultGraph_(resultGraph), index_(index), errorDistance_(errorDistance) {}
+
+    void Process()
+    {
+        NonconjugateDeBruijnGraph myRed(graph_.k());
+        NonconjugateDeBruijnGraph myBlue(graph_.k());
+
+        SimpleDistanceEstimator<Graph>  simpleDistanceEstimator(graph_);
+        SingleQuotientGraph<NonconjugateDeBruijnGraph> myquotientGraph(graph_);
+        SimpleGraphFilter<Graph> simpleGraphFilter;
+        WeakerGluer<NonconjugateDeBruijnGraph> myweakerGluer(graph_, myquotientGraph, simpleGraphFilter, 
+                simpleDistanceEstimator,  index_, errorDistance_, myRed, myBlue );
+
+        //TODO combine red and blue graph for better contigs.
+        GraphCopy<Graph> resultCopy(myRed);
+        resultCopy.Copy(resultGraph_);
+    }
+};
+/*
+            unordered_map<VertexId, VertexId> old_to_new;
+            unordered_map<EdgeId, EdgeId> old_to_new_edge;
+
+
+            NonconjugateDeBruijnGraph mydeBruijn(graph_.k());
+           
+            PairInfoIndexData<typename NonconjugateDeBruijnGraph::EdgeId> myNewIndex;
+            GraphCopy<Graph> mycopy(graph_);
+            mycopy.Copy(mydeBruijn,piid, myNewIndex); 
+        
+
+*/
+}
+#endif /* end of include guard: RECTANGLEREPEATRESOLVER_H */
