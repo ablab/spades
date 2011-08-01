3e0bcfd63835075c4f4b6720971eeb5049b342f3
Anton <anton.bankevich@gmail.com>
2011-08-01 17:24:03 +0400

    temporary bug fix

--- assembler/src/omni/one_many_contigs_enlarger.hpp
+++ assembler/src/omni/one_many_contigs_enlarger.hpp
@@ -22,13 +22,13 @@ public:
 		INFO("one_many_resolve");
 		int inc_count;
 		for (auto iter = g_.SmartVertexBegin(); ! iter.IsEnd(); ++iter) {
-			DEBUG(*iter);
-			if ((g_.OutgoingEdgeCount(*iter) == 1) && ((inc_count = g_.IncomingEdgeCount(*iter)) > 1)){
-				EdgeId unique = g_.GetUniqueOutgoingEdge(*iter);
-				vector<EdgeId> incEdges = g_.IncomingEdges(*iter);
+			VertexId vertex = *iter;
+			DEBUG(vertex);
+			if ((g_.OutgoingEdgeCount(vertex) == 1) && ((inc_count = g_.IncomingEdgeCount(vertex)) > 1)){
+				EdgeId unique = g_.GetUniqueOutgoingEdge(vertex);
+				vector<EdgeId> incEdges = g_.IncomingEdges(vertex);
 				for(int j = 0; j < inc_count; j++) {
 					VertexId tmp_v = g_.AddVertex();
-
 					EdgeId edge2 = g_.AddEdge(tmp_v, g_.EdgeEnd(unique), g_.EdgeNucls(unique));
 					EdgeId edge1 = g_.AddEdge(g_.EdgeStart(incEdges[j]), tmp_v, g_.EdgeNucls(incEdges[j]));
 					vector<EdgeId> toMerge;
@@ -36,20 +36,19 @@ public:
 					toMerge.push_back(edge2);
 					g_.MergePath(toMerge);
 				}
-				g_.ForceDeleteVertex(*iter);
+				g_.ForceDeleteVertex(vertex);
 			}
 		}
 
 
 		for (auto iter = g_.SmartVertexBegin(); ! iter.IsEnd(); ++iter){
-			if ((g_.OutgoingEdgeCount(*iter) > 1) && ((inc_count = g_.IncomingEdgeCount(*iter)) == 1)){
-				EdgeId unique = g_.GetUniqueIncomingEdge(*iter);
-				vector<EdgeId> incEdges = g_.OutgoingEdges(*iter);
+			VertexId vertex = *iter;
+			if ((g_.OutgoingEdgeCount(vertex) > 1) && ((inc_count = g_.IncomingEdgeCount(vertex)) == 1)){
+				EdgeId unique = g_.GetUniqueIncomingEdge(vertex);
+				vector<EdgeId> incEdges = g_.OutgoingEdges(vertex);
 				for(int j = 0; j < inc_count; j++) {
 					VertexId tmp_v = g_.AddVertex();
-
 					EdgeId edge2 = g_.AddEdge(g_.EdgeStart(unique), tmp_v, g_.EdgeNucls(unique));
-					;
 					EdgeId edge1 = g_.AddEdge(tmp_v, g_.EdgeEnd(incEdges[j]), g_.EdgeNucls(incEdges[j]));
 					vector<EdgeId> toMerge ;
 					toMerge.push_back(edge2);
@@ -57,7 +56,7 @@ public:
 
 					g_.MergePath(toMerge);
 				}
-				g_.ForceDeleteVertex(*iter);
+				g_.ForceDeleteVertex(vertex);
 			}
 		}
 
