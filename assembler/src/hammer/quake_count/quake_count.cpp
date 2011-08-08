50b45a7e888ccadd10c3375b88724b60ebbb098c
Alex Davydow <adavydow@yandex.ru>
2011-08-08 06:25:16 -0700

    ISSUE:HAMMER-22

--- assembler/src/hammer/quake_count/quake_count.cpp
+++ assembler/src/hammer/quake_count/quake_count.cpp
@@ -135,11 +135,11 @@ void SplitToFiles(ireadstream ifs, const vector<FILE*> &ofiles,
     ifs >> r;
     KMer::hash hash_function;
     for (ValidKMerGenerator<kK> gen(r, error_threshold); gen.HasMore(); gen.Next()) {
-      FILE *cur_file = ofiles[hash_function(gen.kmer()) % file_number];
       KMer kmer = gen.kmer();
       if (KMer::less2()(!kmer, kmer)) {
         kmer = !kmer;
       }
+      FILE *cur_file = ofiles[hash_function(gen.kmer()) % file_number];
       KMer::BinWrite(cur_file, kmer);
       if (q_mers) {
         double correct_probability = gen.correct_probability();
--- assembler/src/hammer/quake_enhanced/count/count.cpp
+++ assembler/src/hammer/quake_enhanced/count/count.cpp
@@ -129,11 +129,11 @@ void SplitToFiles(ireadstream ifs, const vector<FILE*> &ofiles,
     ifs >> r;
     KMer::hash hash_function;
     for (ValidKMerGenerator<kK> gen(r, error_threshold); gen.HasMore(); gen.Next()) {
-      FILE *cur_file = ofiles[hash_function(gen.kmer()) % file_number];
       KMer kmer = gen.kmer();
       if (KMer::less2()(!kmer, kmer)) {
         kmer = !kmer;
       }
+      FILE *cur_file = ofiles[hash_function(gen.kmer()) % file_number];
       KMer::BinWrite(cur_file, kmer);
       double correct_probability = gen.correct_probability();
       fwrite(&correct_probability, sizeof(correct_probability), 1, cur_file);
