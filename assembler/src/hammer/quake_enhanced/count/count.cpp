4d45a9222aaebba84fae0ae005707fbe7dcf8dbe
Alex Davydow <adavydow@yandex.ru>
2011-08-09 21:07:20 +0400

    typo fixed

--- assembler/src/hammer/quake_enhanced/count/count.cpp
+++ assembler/src/hammer/quake_enhanced/count/count.cpp
@@ -133,7 +133,7 @@ void SplitToFiles(ireadstream ifs, const vector<FILE*> &ofiles,
       if (KMer::less2()(!kmer, kmer)) {
         kmer = !kmer;
       }
-      FILE *cur_file = ofiles[hash_function(kmer()) % file_number];
+      FILE *cur_file = ofiles[hash_function(kmer) % file_number];
       KMer::BinWrite(cur_file, kmer);
       double correct_probability = gen.correct_probability();
       fwrite(&correct_probability, sizeof(correct_probability), 1, cur_file);
