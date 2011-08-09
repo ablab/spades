442710514c7b6760d90d1f14eb54763c90d3b6fe
Alexander Sirotkin <alexander.sirotkin@gmail.com>
2011-08-09 21:28:12 +0400

    position labels for compliment path is negative FIX

--- assembler/src/debruijn/launch.hpp
+++ assembler/src/debruijn/launch.hpp
@@ -133,11 +133,13 @@ void FillEdgesPos(Graph& g, const EdgeIndex<k + 1, Graph>& index,
 	//CurPos = 1000000000;
 	CurPos = 0;
 	Path<typename Graph::EdgeId> path2 = FindGenomePath<k> (!genome, g, index);
-//	for (auto it = path2.sequence().begin(); it != path2.sequence().end(); ++it) {
-	for(size_t i = path2.sequence().size()-1; i>=0; i--){
-		EdgeId ei = path2.sequence()[i];
+	for (auto it = path2.sequence().begin(); it != path2.sequence().end(); ++it) {
+		CurPos -= g.length(*it);
+	}
+	for (auto it = path2.sequence().begin(); it != path2.sequence().end(); ++it) {
+		EdgeId ei = *it;
 		edgesPos.AddEdgePosition(ei, CurPos - 1, CurPos + g.length(ei));
-		CurPos -= g.length(ei);
+		CurPos += g.length(ei);
 	}
 }
 
