2526e2e42b716b236c33d3734400e2fb81a210f8
Sergey Nurk <sergeynurk@gmail.com>
2011-08-12 19:05:05 +0400

    launch refactoring

---
+++ assembler/src/debruijn/debruijn_stats.hpp
@@ -0,0 +1,214 @@
+/*
+ * debruijn_stats.hpp
+ *
+ *  Created on: Aug 12, 2011
+ *      Author: sergey
+ */
+
+#ifndef DEBRUIJN_STATS_HPP_
+#define DEBRUIJN_STATS_HPP_
+
+#include "visualization_utils.hpp"
+#include "statistics.hpp"
+#include "new_debruijn.hpp"
+#include "edges_position_handler.hpp"
+#include "EdgeVertexFilter.hpp"
+//#include <boost/filesystem.hpp>
+#include "read/osequencestream.hpp"
+#include <sys/types.h>
+#include <sys/stat.h>
+
+namespace debruijn_graph {
+
+template<size_t k, class Graph>
+void CountStats(Graph& g, const EdgeIndex<k + 1, Graph>& index,
+		const Sequence& genome) {
+	INFO("Counting stats");
+	StatCounter<Graph, k> stat(g, index, genome);
+	stat.Count();
+	INFO("Stats counted");
+}
+
+void CountPairedInfoStats(Graph &g, size_t insert_size, size_t max_read_length,
+		PairedInfoIndex<Graph> &paired_index,
+		PairedInfoIndex<Graph> &etalon_paired_index,
+		const string &output_folder) {
+	INFO("Counting paired info stats");
+	EdgePairStat<Graph> (g, paired_index, output_folder).Count();
+
+	//todo remove filtration if launch on etalon info is ok
+	UniquePathStat<Graph> (g, etalon_paired_index, insert_size,
+			max_read_length, 0.1 * insert_size, 40.0).Count();
+	UniqueDistanceStat<Graph> (etalon_paired_index).Count();
+	INFO("Paired info stats counted");
+}
+
+void CountClusteredPairedInfoStats(Graph &g, size_t insert_size,
+		size_t max_read_length, PairedInfoIndex<Graph> &paired_index,
+		PairedInfoIndex<Graph> &clustered_index,
+		PairedInfoIndex<Graph> &etalon_paired_index,
+		const string &output_folder) {
+	INFO("Counting paired info stats");
+	EstimationQualityStat<Graph> estimation_stat(g, paired_index,
+			clustered_index, etalon_paired_index);
+	estimation_stat.Count();
+	string stat_folder = output_folder + "/pair_inf_stat";
+//	boost::filesystem::create_directory(stat_folder.c_str());
+	mkdir(stat_folder.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | S_IWOTH);
+	estimation_stat.WriteEstmationStats(stat_folder);
+	ClusterStat<Graph> (clustered_index).Count();
+	INFO("Paired info stats counted");
+}
+
+void WriteToDotFile(Graph &g, const string& file_name, string graph_name,
+		Path<EdgeId> path1/* = Path<EdgeId> ()*/, Path<EdgeId> path2/* = Path<EdgeId> ()*/) {
+	INFO("Writing graph '" << graph_name << "' to file " << file_name);
+	omnigraph::WritePaired(file_name, graph_name, g, path1, path2);
+	INFO("Graph '" << graph_name << "' written to file " << file_name);
+}
+
+void DetailedWriteToDot(Graph &g, const string& file_name, string graph_name,
+		Path<EdgeId> path1/* = Path<EdgeId> ()*/, Path<EdgeId> path2/* = Path<EdgeId> ()*/) {
+	INFO("Writing graph '" << graph_name << "' to file " << file_name);
+	omnigraph::WriteToFile(file_name, graph_name, g, path1, path2);
+	INFO("Graph '" << graph_name << "' written to file " << file_name);
+}
+
+template<size_t k>
+Path<typename Graph::EdgeId> FindGenomePath(const Sequence& genome,
+		const Graph& g, const EdgeIndex<k + 1, Graph>& index) {
+	SimpleSequenceMapper<k + 1, Graph> srt(g, index);
+	return srt.MapSequence(genome);
+}
+
+template<size_t k>
+void ProduceInfo(Graph& g, const EdgeIndex<k + 1, Graph>& index,
+		const Sequence& genome, const string& file_name,
+		const string& graph_name) {
+	CountStats<k> (g, index, genome);
+	Path<typename Graph::EdgeId> path1 = FindGenomePath<k> (genome, g, index);
+	Path<typename Graph::EdgeId> path2 = FindGenomePath<k> (!genome, g, index);
+	WriteToDotFile(g, file_name, graph_name, path1, path2);
+}
+
+
+template<size_t k>
+void ProduceNonconjugateInfo(NCGraph& g,
+		const EdgeIndex<k + 1, NCGraph>& index, const string& genome,
+		const string& work_tmp_dir, const string& graph_name,
+		const IdTrackHandler<NCGraph> &IdTrackLabelerResolved) {
+	CountStats<k> (g, index, genome);
+	//	omnigraph::WriteSimple( file_name, graph_name, g, IdTrackLabelerResolved);
+	//	omnigraph::WriteSimple( work_tmp_dir, graph_name, g, IdTrackLabelerResolved);
+
+}
+
+template<size_t k>
+void ProduceDetailedInfo(Graph& g, const EdgeIndex<k + 1, Graph>& index,
+		const Sequence& genome, const string& folder, const string& file_name,
+		const string& graph_name) {
+	CountStats<k> (g, index, genome);
+	Path<typename Graph::EdgeId> path1 = FindGenomePath<k> (genome, g, index);
+	Path<typename Graph::EdgeId> path2 = FindGenomePath<k> (!genome, g, index);
+
+	mkdir(folder.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | S_IWOTH);
+	DetailedWriteToDot(g, folder + file_name, graph_name, path1, path2);
+}
+
+template<size_t k>
+void WriteGraphComponents(Graph& g, const EdgeIndex<k + 1, Graph>& index,
+		const Sequence& genome, const string& folder, const string &file_name,
+		const string &graph_name, size_t split_edge_length) {
+	Path<typename Graph::EdgeId> path1 = FindGenomePath<k> (genome, g, index);
+	Path<typename Graph::EdgeId> path2 = FindGenomePath<k> (!genome, g, index);
+	mkdir(folder.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | S_IWOTH);
+	WriteComponents(folder + file_name, graph_name, g, split_edge_length,
+			path1, path2);
+
+}
+
+string ConstructComponentName(string file_name, size_t cnt) {
+	stringstream ss;
+	ss << cnt;
+	string res = file_name;
+	res.insert(res.length(), ss.str());
+	return res;
+}
+
+template<class Graph>
+int PrintGraphComponents(const string& file_name, Graph& g,
+		size_t split_edge_length, IdTrackHandler<Graph> &old_IDs,
+		PairedInfoIndex<Graph>  &paired_index,
+		EdgesPositionHandler<Graph> &edges_positions) {
+	LongEdgesSplitter<Graph> inner_splitter(g, split_edge_length);
+	ComponentSizeFilter<Graph> checker(g, split_edge_length);
+	FilteringSplitterWrapper<Graph> splitter(inner_splitter, checker);
+	size_t cnt = 1;
+	while (!splitter.Finished() && cnt <= 1000) {
+		string component_name = ConstructComponentName(file_name, cnt).c_str();
+		auto component = splitter.NextComponent();
+		EdgeVertexFilter<Graph> filter(g, component);
+		printGraph(g, old_IDs, component_name, paired_index, edges_positions,
+				&filter);
+		cnt++;
+	}
+	return (cnt - 1);
+
+}
+
+template<class Graph>
+void OutputContigs(Graph& g, const string& contigs_output_filename) {
+	INFO("-----------------------------------------");
+	INFO("Outputting contigs to " << contigs_output_filename);
+	osequencestream oss(contigs_output_filename);
+	for (auto it = g.SmartEdgeBegin(); !it.IsEnd(); ++it) {
+		oss << g.EdgeNucls(*it);
+	}INFO("Contigs written");
+}
+
+template<class Graph>
+void OutputSingleFileContigs(Graph& g, const string& contigs_output_dir) {
+	INFO("-----------------------------------------");
+	INFO("Outputting contigs to " << contigs_output_dir);
+	int n = 0;
+	mkdir(contigs_output_dir.c_str(),
+			S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | S_IWOTH);
+	char n_str[20];
+	for (auto it = g.SmartEdgeBegin(); !it.IsEnd(); ++it) {
+		sprintf(n_str, "%d.fa", n);
+
+		osequencestream oss(contigs_output_dir + n_str);
+
+		//		osequencestream oss(contigs_output_dir + "tst.fasta");
+		oss << g.EdgeNucls(*it);
+		n++;
+	}INFO("Contigs written");
+}
+
+
+template<size_t k>
+void FillEdgesPos(Graph& g, const EdgeIndex<k + 1, Graph>& index,
+		const Sequence& genome, EdgesPositionHandler<Graph>& edgesPos) {
+	Path<typename Graph::EdgeId> path1 = FindGenomePath<k> (genome, g, index);
+	int CurPos = 0;
+	for (auto it = path1.sequence().begin(); it != path1.sequence().end(); ++it) {
+		EdgeId ei = *it;
+		edgesPos.AddEdgePosition(ei, CurPos + 1, CurPos + g.length(ei));
+		CurPos += g.length(ei);
+	}
+	//CurPos = 1000000000;
+	CurPos = 0;
+	Path<typename Graph::EdgeId> path2 = FindGenomePath<k> (!genome, g, index);
+	for (auto it = path2.sequence().begin(); it != path2.sequence().end(); ++it) {
+		CurPos -= g.length(*it);
+	}
+	for (auto it = path2.sequence().begin(); it != path2.sequence().end(); ++it) {
+		EdgeId ei = *it;
+		edgesPos.AddEdgePosition(ei, CurPos, CurPos + g.length(ei)-1);
+		CurPos += g.length(ei);
+	}
+}
+
+}
+
+#endif /* DEBRUIJN_STATS_HPP_ */
---
+++ assembler/src/debruijn/graph_construction.hpp
@@ -0,0 +1,118 @@
+/*
+ * graph_construction.hpp
+ *
+ *  Created on: Aug 12, 2011
+ *      Author: sergey
+ */
+
+#ifndef GRAPH_CONSTRUCTION_HPP_
+#define GRAPH_CONSTRUCTION_HPP_
+
+#include "debruijn_graph_constructor.hpp"
+#include "edges_position_handler.hpp"
+#include "new_debruijn.hpp"
+#include "paired_info.hpp"
+
+namespace debruijn_graph {
+
+template<size_t k, class ReadStream>
+void FillPairedIndex(Graph &g, const EdgeIndex<k + 1, Graph>& index, PairedInfoIndex<Graph>& paired_info_index,
+		ReadStream& stream) {
+	typedef SimpleSequenceMapper<k + 1, Graph> SequenceMapper;
+	INFO("-----------------------------------------");
+	stream.reset();
+	INFO("Counting paired info");
+	SequenceMapper mapper(g, index);
+	PairedIndexFiller<k + 1, Graph, SequenceMapper, ReadStream> pif(g, mapper, stream);
+	pif.FillIndex(paired_info_index);
+	INFO("Paired info counted");
+}
+
+template<size_t k>
+void FillEtalonPairedIndex(const Graph &g, PairedInfoIndex<Graph>& etalon_paired_index,
+		const EdgeIndex<k + 1, Graph>& index, size_t insert_size, size_t read_length,
+		const Sequence& genome) {
+	INFO("-----------------------------------------");
+	INFO("Counting etalon paired info");
+	EtalonPairedInfoCounter<k, Graph> etalon_paired_info_counter(g, index,
+			insert_size, read_length, insert_size * 0.1);
+	etalon_paired_info_counter.FillEtalonPairedInfo(genome, etalon_paired_index);
+	//////////////////DEBUG
+//	SimpleSequenceMapper<k + 1, Graph> simple_mapper(g, index);
+//	Path<EdgeId> path = simple_mapper.MapSequence(genome);
+//	SequenceBuilder sequence_builder;
+//	sequence_builder.append(Seq<k>(g.EdgeNucls(path[0])));
+//	for (auto it = path.begin(); it != path.end(); ++it) {
+//		sequence_builder.append(g.EdgeNucls(*it).Subseq(k));
+//	}
+//	Sequence new_genome = sequence_builder.BuildSequence();
+//	NewEtalonPairedInfoCounter<k, Graph> new_etalon_paired_info_counter(g, index,
+//			insert_size, read_length, insert_size * 0.1);
+//	PairedInfoIndex<Graph> new_paired_info_index(g);
+//	new_etalon_paired_info_counter.FillEtalonPairedInfo(new_genome, new_paired_info_index);
+//	CheckInfoEquality(etalon_paired_index, new_paired_info_index);
+	//////////////////DEBUG
+	INFO("Paired info counted");
+}
+
+template<size_t k, class ReadStream>
+void FillCoverage(Graph& g,
+		ReadStream& stream, EdgeIndex<k + 1, Graph>& index) {
+	typedef SimpleSequenceMapper<k + 1, Graph> SequenceMapper;
+	INFO("-----------------------------------------");
+	stream.reset();
+	INFO("Counting coverage");
+	SequenceMapper read_threader(g, index);
+	g.coverage_index().FillIndex<ReadStream, SequenceMapper> (stream, read_threader);
+	INFO("Coverage counted");
+}
+
+template<size_t k, class ReadStream>
+void ConstructGraph(Graph& g, EdgeIndex<k + 1, Graph>& index,
+		ReadStream& stream) {
+	typedef SeqMap<k + 1, typename Graph::EdgeId> DeBruijn;
+	INFO("-----------------------------------------");
+	INFO("Constructing DeBruijn graph");
+	DeBruijn& debruijn = index.inner_index();
+	INFO("Filling DeBruijn graph");
+	debruijn.Fill(stream);
+	INFO("DeBruijn graph constructed");
+
+	INFO("Condensing graph");
+	DeBruijnGraphConstructor<k, Graph> g_c(debruijn);
+	g_c.ConstructGraph(g, index);
+	INFO("Graph condensed");
+}
+
+template<size_t k, class ReadStream>
+void ConstructGraphWithCoverage(Graph& g, EdgeIndex<k + 1, Graph>& index, ReadStream& stream) {
+	ConstructGraph<k, ReadStream> (g, index, stream);
+	FillCoverage<k, ReadStream> (g, stream, index);
+}
+
+template<size_t k, class PairedReadStream>
+void ConstructGraphWithPairedInfo(Graph& g, EdgeIndex<k + 1, Graph>& index,
+		PairedInfoIndex<Graph>& paired_index, PairedReadStream& stream) {
+	typedef io::ConvertingReaderWrapper UnitedStream;
+	UnitedStream united_stream(&stream);
+	ConstructGraphWithCoverage<k, UnitedStream> (g,
+			index/*, coverage_handler*/, united_stream);
+	FillPairedIndex<k, PairedReadStream> (g, index, paired_index, stream);
+}
+
+template<size_t k, class PairedReadStream>
+void ConstructGraphWithEtalonPairedInfo(Graph& g,
+		EdgeIndex<k + 1, Graph>& index, PairedInfoIndex<Graph>& paired_index,
+		PairedReadStream& stream, size_t insert_size, size_t read_length,
+		const Sequence& genome) {
+	typedef io::ConvertingReaderWrapper UnitedStream;
+	UnitedStream united_stream(&stream);
+	ConstructGraphWithCoverage<k, UnitedStream> (g,
+			index/*, coverage_handler*/, united_stream);
+	FillEtalonPairedIndex<k> (g, paired_index, index, insert_size, read_length,
+			genome);
+}
+
+}
+
+#endif /* GRAPH_CONSTRUCTION_HPP_ */
