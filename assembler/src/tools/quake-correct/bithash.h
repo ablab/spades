e60f925a5e0b59d6de581ff71da519413572c683
Nikolay Vyahhi <vyahhi@gmail.com>
2011-08-29 20:20:55 +0400

    quake-correct changes to meet Hammer realities

---
+++ assembler/src/tools/quake-correct/bithash.h
@@ -0,0 +1,40 @@
+#ifndef BITHASH_H
+#define BITHASH_H
+
+#include <string>
+#include <vector>
+#include <cmath>
+#include <boost/dynamic_bitset.hpp>
+using namespace::std;
+
+class bithash {
+ public:
+  bithash(int _k);
+  ~bithash();
+  void add(long long unsigned kmer);
+  bool check(unsigned kmer[]);
+  bool check(unsigned kmer[], long long unsigned & kmermap);
+  bool check(long long unsigned & kmermap, unsigned last, unsigned next);
+  bool check(long long unsigned kmermap);
+  void meryl_file_load(const char* merf, const double boundary);
+  void tab_file_load(istream & mer_in, const double boundary, unsigned long long atgc[]);
+  void tab_file_load(istream & mer_in, const vector<double> boundary, unsigned long long atgc[]);
+  void hammer_file_load(istream & hammer_in, unsigned long long atgc[]);
+  long long unsigned binary_kmer(const string &s);
+  long long unsigned binary_rckmer(const string &s);
+  void binary_file_output(char* outf);
+
+  void binary_file_input(char* inf, unsigned long long atgc[]);
+  unsigned int num_kmers();
+
+  static int k;
+ private:  
+  unsigned binary_nt(char ch);
+  int count_at(string seq);
+  int count_at(unsigned long long seq);
+
+  boost::dynamic_bitset<> bits;
+  unsigned long long mask;
+};
+
+#endif
