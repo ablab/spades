84376e8dd93c0bd5432f43c9cdb3b6537eb65dcb
Nikolay Vyahhi <vyahhi@gmail.com>
2011-08-31 20:18:10 +0400

    mapreads small fix

--- assembler/src/tools/mapreads/mapreads.py
+++ assembler/src/tools/mapreads/mapreads.py
@@ -15,6 +15,7 @@ reads1, reads2 = sys.argv[2:4]
 min_insert_size = sys.argv[4]
 max_insert_size = sys.argv[5]
 
-os.system(BOWTIE_DIR + '/bowtie-build ' + reference)
-os.system(BOWTIE_DIR + '/bowtie ' + reference + ' -1 ' + reads1 + ' -2 ' + reads2 + 
+os.system(BOWTIE_DIR + '/bowtie-build ' + reference + ' ./ebwt_outfile_base')
+os.system(BOWTIE_DIR + '/bowtie ./ebwt_outfile_base -1 ' + reads1 + ' -2 ' + reads2 + 
            ' --sam ' + ' --minins ' + min_insert_size + ' --maxins ' + max_insert_size)
+os.system('rm ./ebwt_outfile_base*')
