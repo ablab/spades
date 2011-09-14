dc8505c2cc882dfe442e7a547a3466190dba2043
AlexeyP <lexpyh@gmail.com>
2011-09-14 13:50:16 -0700

    config corrected

--- assembler/src/include/omni/peak_finder.hpp
+++ assembler/src/include/omni/peak_finder.hpp
@@ -163,7 +163,7 @@ private:
 				} else if (j < i && outf[index_max - min][0] == outf[j - min][0] ) index_max = j;
 			
 
-            if (!((index_max > i - (range / 3)) && (index_max < i + (range / 3)))) continue;
+            if (!((index_max > i - (range >> 1)) && (index_max < i + (range >> 1)))) continue;
             if  (abs(index_max - peak) <= delta) return true;
 		}
 		return false;
