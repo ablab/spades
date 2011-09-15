83e6e8cba453730fd795b460a0531c9a84b48662
Sergey Nurk <sergeynurk@gmail.com>
2011-09-15 14:34:36 +0400

    tons of debug output into cerr

--- assembler/src/include/omni/abstract_graph.hpp
+++ assembler/src/include/omni/abstract_graph.hpp
@@ -68,7 +68,8 @@ private:
 			const vector<VertexId> &verticesToDelete) {
 		for (auto it = edgesToDelete.begin(); it != edgesToDelete.end(); ++it)
 			FireDeleteEdge(*it);
-		for (auto it = verticesToDelete.begin(); it != verticesToDelete.end(); ++it)
+		for (auto it = verticesToDelete.begin(); it != verticesToDelete.end();
+				++it)
 			FireDeleteVertex(*it);
 	}
 
@@ -76,7 +77,8 @@ private:
 			const vector<VertexId> &verticesToDelete) {
 		for (auto it = edgesToDelete.begin(); it != edgesToDelete.end(); ++it)
 			HiddenDeleteEdge(*it);
-		for (auto it = verticesToDelete.begin(); it != verticesToDelete.end(); ++it)
+		for (auto it = verticesToDelete.begin(); it != verticesToDelete.end();
+				++it)
 			HiddenDeleteVertex(*it);
 	}
 
@@ -85,230 +87,244 @@ public:
 	typedef typename base::SmartEdgeIt SmartEdgeIt;
 
 	AbstractGraph(HandlerApplier<VertexId, EdgeId>* applier,
-			const DataMaster& master) :
-		base(applier), master_(master) {
+	const DataMaster& master) :
+			base(applier), master_(master) {
 	}
 
 	virtual ~AbstractGraph() {
-TRACE	("~AbstractGraph")
-	//		doesn't work this way because call to virtual function is needed
-	//		for (auto it = this->SmartVertexBegin(); !it.IsEnd(); ++it) {
-	//			ForceDeleteVertex(*it);
-	//		}
-}
+		TRACE("~AbstractGraph")
+		//		doesn't work this way because call to virtual function is needed
+		//		for (auto it = this->SmartVertexBegin(); !it.IsEnd(); ++it) {
+		//			ForceDeleteVertex(*it);
+		//		}
+	}
 
-const DataMaster& master() const {
-	return master_;
-}
+	const DataMaster& master() const {
+		return master_;
+	}
 
-virtual void CheckGraph() {
-}
-
+	virtual void CheckGraph() {
+	}
 
 //already in base
 //	virtual const vector<EdgeId> OutgoingEdges(VertexId v) const = 0;
 
-virtual const vector<EdgeId> IncomingEdges(VertexId v) const = 0;
+	virtual const vector<EdgeId> IncomingEdges(VertexId v) const = 0;
 
-virtual size_t OutgoingEdgeCount(VertexId v) const = 0;
+	virtual size_t OutgoingEdgeCount(VertexId v) const = 0;
 
-virtual size_t IncomingEdgeCount(VertexId v) const = 0;
+	virtual size_t IncomingEdgeCount(VertexId v) const = 0;
 
 //todo delete if nobody needs it
-virtual vector<EdgeId> GetEdgesBetween(VertexId v, VertexId u) = 0;
+	virtual vector<EdgeId> GetEdgesBetween(VertexId v, VertexId u) = 0;
 
-virtual const EdgeData& data(EdgeId edge) const = 0;
+	virtual const EdgeData& data(EdgeId edge) const = 0;
 
-virtual const VertexData& data(VertexId v) const = 0;
+	virtual const VertexData& data(VertexId v) const = 0;
 
-virtual VertexId EdgeStart(EdgeId edge) const = 0;
+	virtual VertexId EdgeStart(EdgeId edge) const = 0;
 
-virtual VertexId EdgeEnd(EdgeId edge) const = 0;
+	virtual VertexId EdgeEnd(EdgeId edge) const = 0;
 
-bool CheckUniqueOutgoingEdge(VertexId v) const {
-	return OutgoingEdgeCount(v) == 1;
-}
+	bool CheckUniqueOutgoingEdge(VertexId v) const {
+		return OutgoingEdgeCount(v) == 1;
+	}
 
-EdgeId GetUniqueOutgoingEdge(VertexId v) const {
-	assert(CheckUniqueOutgoingEdge(v));
-	return OutgoingEdges(v)[0];
-}
+	EdgeId GetUniqueOutgoingEdge(VertexId v) const {
+		assert(CheckUniqueOutgoingEdge(v));
+		return OutgoingEdges(v)[0];
+	}
 
-bool CheckUniqueIncomingEdge(VertexId v) const {
-	return IncomingEdgeCount(v) == 1;
-}
+	bool CheckUniqueIncomingEdge(VertexId v) const {
+		return IncomingEdgeCount(v) == 1;
+	}
 
-EdgeId GetUniqueIncomingEdge(VertexId v) const {
-	assert(CheckUniqueIncomingEdge(v));
-	return IncomingEdges(v)[0];
-}
+	EdgeId GetUniqueIncomingEdge(VertexId v) const {
+		assert(CheckUniqueIncomingEdge(v));
+		return IncomingEdges(v)[0];
+	}
 
-std::string str(const EdgeId edge) const {
-	return master_.str(data(edge));
-}
+	std::string str(const EdgeId edge) const {
+		return master_.str(data(edge));
+	}
 
-std::string str(const VertexId v) const {
-	return master_.str(data(v));
-}
+	std::string str(const VertexId v) const {
+		return master_.str(data(v));
+	}
 
-size_t length(const EdgeId edge) const {
-	return master_.length(data(edge));
-}
+	size_t length(const EdgeId edge) const {
+		return master_.length(data(edge));
+	}
 
-size_t length(const VertexId v) const {
-	return master_.length(data(v));
-}
+	size_t length(const VertexId v) const {
+		return master_.length(data(v));
+	}
 
-VertexId AddVertex(const VertexData& data) {
-	VertexId result = HiddenAddVertex(data);
-	FireAddVertex(result);
-	return result;
-}
+	VertexId AddVertex(const VertexData& data) {
+		VertexId result = HiddenAddVertex(data);
+		FireAddVertex(result);
+		return result;
+	}
 
-void DeleteVertex(VertexId v) {
-	assert(IsDeadEnd(v) && IsDeadStart(v));
-	assert(v != NULL);
-	FireDeleteVertex(v);
-	HiddenDeleteVertex(v);
-}
+	void DeleteVertex(VertexId v) {
+		assert(IsDeadEnd(v) && IsDeadStart(v));
+		assert(v != NULL);
+		FireDeleteVertex(v);
+		HiddenDeleteVertex(v);
+	}
 
-void ForceDeleteVertex(VertexId v) {
-	DeleteAllOutgoing(v);
-	DeleteAllIncoming(v);
-	DeleteVertex(v);
-}
+	void ForceDeleteVertex(VertexId v) {
+		DeleteAllOutgoing(v);
+		DeleteAllIncoming(v);
+		DeleteVertex(v);
+	}
 
-EdgeId AddEdge(VertexId v1, VertexId v2, const EdgeData &data) {
-	EdgeId result = HiddenAddEdge(v1, v2, data);
-	FireAddEdge(result);
-	return result;
-}
+	EdgeId AddEdge(VertexId v1, VertexId v2, const EdgeData &data) {
+		EdgeId result = HiddenAddEdge(v1, v2, data);
+		FireAddEdge(result);
+		return result;
+	}
 
 //todo delete if not used
-bool HasEdge(VertexId v1, VertexId v2, const EdgeData &data) {
-	vector<EdgeId> out = OutgoingEdges(v1);
-	for (auto it = out.begin(); it != out.end(); ++it) {
-		if ((EdgeEnd(*it) == v2) && (master_.equals(data(*it), data))) {
-			return true;
+	bool HasEdge(VertexId v1, VertexId v2, const EdgeData &data) {
+		vector<EdgeId> out = OutgoingEdges(v1);
+		for (auto it = out.begin(); it != out.end(); ++it) {
+			if ((EdgeEnd(*it) == v2) && (master_.equals(data(*it), data))) {
+				return true;
+			}
 		}
+		return false;
 	}
-	return false;
-}
 
 //todo delete if not used
-EdgeId GetEdge(VertexId v1, VertexId v2, const EdgeData &edgeData) {
-	vector<EdgeId> out = OutgoingEdges(v1);
-	for (auto it = out.begin(); it != out.end(); ++it) {
-		if ((EdgeEnd(*it) == v2) && (master_.equals(data(*it), edgeData))) {
-			return *it;
+	EdgeId GetEdge(VertexId v1, VertexId v2, const EdgeData &edgeData) {
+		vector<EdgeId> out = OutgoingEdges(v1);
+		for (auto it = out.begin(); it != out.end(); ++it) {
+			if ((EdgeEnd(*it) == v2) && (master_.equals(data(*it), edgeData))) {
+				return *it;
+			}
+		}
+		return NULL;
+	}
+
+	void DeleteEdge(EdgeId edge) {
+		FireDeleteEdge(edge);
+		HiddenDeleteEdge(edge);
+	}
+
+	bool IsDeadEnd(VertexId v) const {
+		return OutgoingEdgeCount(v) == 0;
+	}
+
+	bool IsDeadStart(VertexId v) const {
+		return IncomingEdgeCount(v) == 0;
+	}
+
+	bool CanCompressVertex(VertexId v) const {
+		return OutgoingEdgeCount(v) == 1 && IncomingEdgeCount(v) == 1;
+	}
+
+	void CompressVertex(VertexId v) {
+		//assert(CanCompressVertex(v));
+		cerr << "here" << endl;
+		if (CanCompressVertex(v)
+				&& GetUniqueOutgoingEdge(v) != GetUniqueIncomingEdge(v)) {
+			cerr << "here1" << endl;
+			vector<EdgeId> toMerge;
+			toMerge.push_back(GetUniqueIncomingEdge(v));
+			toMerge.push_back(GetUniqueOutgoingEdge(v));
+			MergePath(toMerge);
+			cerr << "here1" << endl;
 		}
 	}
-	return NULL;
-}
 
-void DeleteEdge(EdgeId edge) {
-	FireDeleteEdge(edge);
-	HiddenDeleteEdge(edge);
-}
-
-bool IsDeadEnd(VertexId v) const {
-	return OutgoingEdgeCount(v) == 0;
-}
-
-bool IsDeadStart(VertexId v) const {
-	return IncomingEdgeCount(v) == 0;
-}
-
-bool CanCompressVertex(VertexId v) const {
-	return OutgoingEdgeCount(v) == 1 && IncomingEdgeCount(v) == 1;
-}
-
-void CompressVertex(VertexId v) {
-	//assert(CanCompressVertex(v));
-	if (CanCompressVertex(v) && GetUniqueOutgoingEdge(v) != GetUniqueIncomingEdge(v)) {
-		vector<EdgeId> toMerge;
-		toMerge.push_back(GetUniqueIncomingEdge(v));
-		toMerge.push_back(GetUniqueOutgoingEdge(v));
-		MergePath(toMerge);
+	EdgeId MergePath(const vector<EdgeId>& path) {
+		cerr << "here2" << endl;
+		assert(!path.empty());
+		cerr << "here2" << endl;
+		for (size_t i = 0; i < path.size(); i++)
+			for (size_t j = i + 1; j < path.size(); j++) {
+				assert(path[i] != path[j]);
+			}
+		cerr << "here2" << endl;
+		vector<EdgeId> correctedPath = CorrectMergePath(path);
+		cerr << "here2" << endl;
+		VertexId v1 = EdgeStart(correctedPath[0]);
+		VertexId v2 = EdgeEnd(correctedPath[correctedPath.size() - 1]);
+		vector<const EdgeData*> toMerge;
+		for (auto it = correctedPath.begin(); it != correctedPath.end(); ++it) {
+			toMerge.push_back(&(data(*it)));
+		}
+		cerr << "here2" << endl;
+		EdgeId newEdge = HiddenAddEdge(v1, v2, master_.MergeData(toMerge));
+		cerr << "here2" << endl;
+		FireMerge(correctedPath, newEdge);
+		cerr << "here2" << endl;
+		vector<EdgeId> edgesToDelete = EdgesToDelete(path);
+		cerr << "here2" << endl;
+		vector<VertexId> verticesToDelete = VerticesToDelete(path);
+		cerr << "here2" << endl;
+		//todo ask Anton why fire and hidden are divided here
+		FireDeletePath(edgesToDelete, verticesToDelete);
+		cerr << "here2" << endl;
+		FireAddEdge(newEdge);
+		cerr << "here2" << endl;
+		HiddenDeletePath(edgesToDelete, verticesToDelete);
+		cerr << "here2" << endl;
+		return newEdge;
 	}
-}
 
-EdgeId MergePath(const vector<EdgeId>& path) {
-	assert(!path.empty());
-	for(size_t i = 0; i < path.size(); i++)
-	for(size_t j = i + 1; j < path.size(); j++) {
-		assert(path[i] != path[j]);
+	pair<EdgeId, EdgeId> SplitEdge(EdgeId edge, size_t position) {
+		pair<VertexData, pair<EdgeData, EdgeData>> newData = master_.SplitData(
+				data(edge), position);
+		VertexId splitVertex = HiddenAddVertex(newData.first);
+		EdgeId newEdge1 = HiddenAddEdge(EdgeStart(edge), splitVertex,
+				newData.second.first);
+		EdgeId newEdge2 = HiddenAddEdge(splitVertex, EdgeEnd(edge),
+				newData.second.second);
+		FireSplit(edge, newEdge1, newEdge2);
+		FireDeleteEdge(edge);
+		FireAddVertex(splitVertex);
+		FireAddEdge(newEdge1);
+		FireAddEdge(newEdge2);
+		HiddenDeleteEdge(edge);
+		return make_pair(newEdge1, newEdge2);
 	}
-	vector<EdgeId> correctedPath = CorrectMergePath(path);
-	SequenceBuilder sb;
-	VertexId v1 = EdgeStart(correctedPath[0]);
-	VertexId v2 = EdgeEnd(correctedPath[correctedPath.size() - 1]);
-	vector<const EdgeData*> toMerge;
-	for (auto it = correctedPath.begin(); it != correctedPath.end(); ++it) {
-		toMerge.push_back(&(data(*it)));
-	}
-	EdgeId newEdge = HiddenAddEdge(v1, v2, master_.MergeData(toMerge));
-	FireMerge(correctedPath, newEdge);
-	vector<EdgeId> edgesToDelete = EdgesToDelete(path);
-	vector<VertexId> verticesToDelete = VerticesToDelete(path);
-	//todo ask Anton why fire and hidden are divided here
-	FireDeletePath(edgesToDelete, verticesToDelete);
-	FireAddEdge(newEdge);
-	HiddenDeletePath(edgesToDelete, verticesToDelete);
-	return newEdge;
-}
 
-pair<EdgeId, EdgeId> SplitEdge(EdgeId edge, size_t position) {
-	pair<VertexData, pair<EdgeData, EdgeData>> newData = master_.SplitData(data(edge), position);
-	VertexId splitVertex = HiddenAddVertex(newData.first);
-	EdgeId newEdge1 = HiddenAddEdge(EdgeStart(edge), splitVertex,
-			newData.second.first);
-	EdgeId newEdge2 = HiddenAddEdge(splitVertex, EdgeEnd(edge),
-			newData.second.second);
-	FireSplit(edge, newEdge1, newEdge2);
-	FireDeleteEdge(edge);
-	FireAddVertex(splitVertex);
-	FireAddEdge(newEdge1);
-	FireAddEdge(newEdge2);
-	HiddenDeleteEdge(edge);
-	return make_pair(newEdge1, newEdge2);
-}
+	void GlueEdges(EdgeId edge1, EdgeId edge2) {
+		TRACE("Start glue");
+		EdgeId newEdge = HiddenAddEdge(EdgeStart(edge2), EdgeEnd(edge2),
+				master_.GlueData(data(edge1), data(edge2)));
+		FireGlue(newEdge, edge1, edge2);
+		FireDeleteEdge(edge1);
+		FireDeleteEdge(edge2);
+		FireAddEdge(newEdge);
+		VertexId start = EdgeStart(edge1);
+		VertexId end = EdgeEnd(edge1);
+		HiddenDeleteEdge(edge1);
+		HiddenDeleteEdge(edge2);
+		if (IsDeadStart(start) && IsDeadEnd(start)) {
+			DeleteVertex(start);
+		}
+		if (IsDeadStart(end) && IsDeadEnd(end)) {
+			DeleteVertex(end);
+		}TRACE("End glue");
 
-void GlueEdges(EdgeId edge1, EdgeId edge2) {
-	TRACE("Start glue");
-	EdgeId newEdge = HiddenAddEdge(EdgeStart(edge2), EdgeEnd(edge2),
-			master_.GlueData(data(edge1), data(edge2)));
-	FireGlue(newEdge, edge1, edge2);
-	FireDeleteEdge(edge1);
-	FireDeleteEdge(edge2);
-	FireAddEdge(newEdge);
-	VertexId start = EdgeStart(edge1);
-	VertexId end = EdgeEnd(edge1);
-	HiddenDeleteEdge(edge1);
-	HiddenDeleteEdge(edge2);
-	if (IsDeadStart(start) && IsDeadEnd(start)) {
-		DeleteVertex(start);
+		/*/////////////////
+		 FireDeleteEdge(edge2);
+		 FireGlue(edge1, edge2);
+		 edge2->set_data(master_.GlueData(edge1->data(), edge2->data()));
+		 FireAddEdge(edge2);
+		 VertexId start = EdgeStart(edge1);
+		 VertexId end = EdgeEnd(edge1);
+		 DeleteEdge(edge1);
+		 if (IsDeadStart(start) && IsDeadEnd(start)) {
+		 DeleteVertex(start);
+		 }
+		 if (IsDeadStart(end) && IsDeadEnd(end)) {
+		 DeleteVertex(end);
+		 }*/
 	}
-	if (IsDeadStart(end) && IsDeadEnd(end)) {
-		DeleteVertex(end);
-	}
-	TRACE("End glue");
-
-	/*/////////////////
-	 FireDeleteEdge(edge2);
-	 FireGlue(edge1, edge2);
-	 edge2->set_data(master_.GlueData(edge1->data(), edge2->data()));
-	 FireAddEdge(edge2);
-	 VertexId start = EdgeStart(edge1);
-	 VertexId end = EdgeEnd(edge1);
-	 DeleteEdge(edge1);
-	 if (IsDeadStart(start) && IsDeadEnd(start)) {
-	 DeleteVertex(start);
-	 }
-	 if (IsDeadStart(end) && IsDeadEnd(end)) {
-	 DeleteVertex(end);
-	 }*/
-}
 
 };
 
