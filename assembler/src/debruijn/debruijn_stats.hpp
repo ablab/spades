ad8120e8991ac3bf9268341240b29378343766f6
Alexander <alexander.sirotkin@gmail.com>
2011-09-21 12:59:24 +0400

    fix

--- assembler/src/debruijn/debruijn_stats.hpp
+++ assembler/src/debruijn/debruijn_stats.hpp
@@ -338,7 +338,7 @@ void FillEdgesPos(Graph& g, const EdgeIndex<k + 1, Graph>& index,
 		int len = mr.mapped_range.end_pos - mr.mapped_range.start_pos;
 		if (i>0)
 			if (m_path1[i-1].first != ei)
-				if (g.EdgeStart(ei) != graph_.EdgeEnd(m_path1[i-1].first)){
+				if (g.EdgeStart(ei) != g.EdgeEnd(m_path1[i-1].first)){
 					DEBUG("Contig "<<contigId<<" maped on not adjacent edge. Position in contig is "<<m_path1[i-1].second.initial_range.start_pos+1<<"--" <<m_path1[i-1].second.initial_range.end_pos<< " and "<<mr.initial_range.start_pos+1<<"--" <<mr.initial_range.end_pos);
 				}
 		edgesPos.AddEdgePosition(ei, mr.initial_range.start_pos+1, mr.initial_range.end_pos, contigId);
