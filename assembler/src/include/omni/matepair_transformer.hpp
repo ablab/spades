2f251d96e3da647865a09872ba8f5e5c7d9dbf6d
Son Pham <kspham@eng.ucsd.edu>
2011-10-03 16:02:31 -0700

    Add matepair transformation wrapper for pathset graph

---
+++ assembler/src/include/omni/matepair_transformer.hpp
@@ -0,0 +1,83 @@
+#pragma once
+#include "paired_info.hpp"
+#include "path_set.hpp"
+
+namespace omnigraph{
+
+
+template<class Graph>
+class MatePairTransformer{
+
+
+typedef typename Graph::EdgeId EdgeId;
+typedef vector<EdgeId > Path;
+const Graph& g_;
+const PairedInfoIndex<Graph>& pair_info_;
+
+public:
+MatePairTransformer(const Graph& g, const PairedInfoIndex<Graph>& pair_info):
+    g_(g), pair_info_(pair_info){}
+
+
+void Transform(PathSetIndexData<EdgeId> & pathset_index)
+{
+	for (auto it = pair_info_.begin(); it != pair_info_.end(); ++it) {
+
+		vector < PairInfo < EdgeId >> infos = *it;
+
+        for(auto iter = infos.begin() ; iter != infos.end() ; ++iter)
+        {
+            if (gr(iter->d, 0.)) {
+                if (iter->variance == 0) {
+
+                    PathReceiverCallback<Graph> call_back(g_); 
+                    EdgeId first_edge = iter->first;
+                    EdgeId second_edge = iter->second;
+                    PathProcessor<Graph> path_processor(
+                            g_,
+                            iter->d - g_.length(first_edge), 
+                            iter->d - g_.length(first_edge),
+                            g_.EdgeEnd(first_edge),
+                            g_.EdgeStart(second_edge),
+                            call_back);
+                    path_processor.Process();
+                    PathSet<EdgeId> pathset(first_edge, second_edge, MinPathLength(call_back.paths()) , call_back.paths());
+                    PathSetFilter(pathset);
+                    pathset_index.AddPathSet(pathset);
+                }
+                else
+                {
+                    INFO("NON ZERO VARIATION " << *iter);
+                }
+            }
+        }
+	}
+
+}
+private:
+size_t MinPathLength(set<Path> paths)
+{
+    if(paths.size() == 0)
+        return 0;
+    vector<size_t> v;
+    for(auto it = paths.begin() ; it != paths. end() ; ++it)
+    {
+        size_t length = 0;
+        for(auto iter = it->begin() ; iter != it->end() ; ++iter)
+        {
+            length += g_.length(*iter);
+        }
+        v.push_back(length);
+    }
+    sort(v.begin() , v.end());
+    return v[0];
+}
+
+
+///TODO  Remove invalid paths in this path_set
+void PathSetFilter(PathSet<EdgeId> & path_set)
+{
+}
+
+};
+}
