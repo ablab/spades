5c356d1b8a436aaad8f1e50f0dcbff90cf185165
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2011-10-03 17:07:04 +0400

    bugfix

--- assembler/src/include/io/careful_filtering_reader_wrapper.hpp
+++ assembler/src/include/io/careful_filtering_reader_wrapper.hpp
@@ -50,8 +50,8 @@ PairedRead longestValid(const PairedRead& r) {
 	if (len1 == r.first().size() && len2 == r.second().size()) {
 		return r;
 	}
-	size_t dist = r.distance() - c1.first - r.second().size() + c2.second;
-	return PairedRead(r.first().Substr(c1.first, c1.second), r.second().Substr(c2.first, c2.second), dist);
+	size_t is = r.insert_size() - c1.first - c2.first;
+	return PairedRead(r.first().Substr(c1.first, c1.second), r.second().Substr(c2.first, c2.second), is);
 }
 
 template<typename ReadType>
