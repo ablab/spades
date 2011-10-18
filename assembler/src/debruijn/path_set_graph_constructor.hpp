231610382d4e3b64980127f32cdbad6d8062a3cb
Dmitry Antipov <dmitrij.antipov@gmail.com>
2011-10-18 18:43:18 +0400

    fx

---
+++ assembler/src/debruijn/path_set_graph_constructor.hpp
@@ -0,0 +1,121 @@
+#pragma once
+#include "omni/matepair_transformer.hpp"
+#include "omni/path_set.hpp"
+#include "new_debruijn.hpp"
+#include "graph_pack.hpp"
+#include "utils.hpp"
+#include "omni/omni_utils.hpp"
+
+#include "omni/omni_tools.hpp"
+#include "omni/omnigraph.hpp"
+
+#include "omni/edges_position_handler.hpp"
+#include "omni/total_labeler.hpp"
+
+namespace debruijn_graph{
+template <class Graph>
+
+class PathSetGraphConstructor {
+
+typedef typename Graph::EdgeId EdgeId;
+typedef typename Graph::VertexId VertexId;
+
+typedef vector<EdgeId > Path;
+const Graph& g_;
+const PairedInfoIndex<Graph>& pair_info_;
+
+public:
+PathSetGraphConstructor(const Graph& g,const PairedInfoIndex<Graph>& pair_info, Graph& new_graph, IdTrackHandler<Graph>& newIds, TotalLabeler<Graph>& tot_labeler_after): g_(g), pair_info_(pair_info) {
+	PathSetIndexData<EdgeId> PII ;
+	PathSetIndexData<EdgeId> PIIFilter ;
+	MatePairTransformer<Graph> transformer(g_, pair_info_);
+	transformer.Transform(PII);
+	PathSetIndex<EdgeId> PI(PII);
+	PI.RemovePrefixes(PIIFilter);
+
+	for(auto iter = PII.begin(); iter != PII.end() ; ++iter)
+	{
+		DEBUG( *iter);
+	}
+	DEBUG("FILTERED");
+	int count = 0;
+
+	for(auto iter = PIIFilter.begin(); iter != PIIFilter.end() ; ++iter)
+	{
+		VertexId v = new_graph.AddVertex();
+		newIds.AddVertexIntId(v, iter->id);
+	}
+	DEBUG("PahtSetNumber is "<< PIIFilter.size());
+	for(auto iter = PIIFilter.begin(); iter != PIIFilter.end() ; ++iter)
+	{
+		DEBUG( *iter);
+	}
+	for(auto iter = PIIFilter.begin(); iter != PIIFilter.end() ; ++iter)
+	{
+		PathSet<EdgeId> first = *iter;
+		vector<PathSet<EdgeId>> extends;
+		PI.FindExtension(PIIFilter,first, extends);
+		VertexId new_start = newIds.ReturnVertexId(iter->id);
+		EdgeId old_first_edge = iter->start;
+		DEBUG ("path-set numero " << first.id<< " has "<< extends.size()<<"extensions: ");
+		for(size_t i = 0; i < extends.size(); i++) {
+			DEBUG("to pathset "<< extends[i].id);
+			VertexId new_end = newIds.ReturnVertexId(extends[i].id);
+			DEBUG("adding edge from" << newIds.ReturnIntId(new_start) << " to " << newIds.ReturnIntId(new_end) << " of length " << g.length(old_first_edge));
+			EdgeId eid = new_graph.AddEdge(new_start, new_end, g.EdgeNucls(old_first_edge));
+			WrappedSetCoverage(new_graph, eid, (int) (g.coverage(old_first_edge) * g.length(old_first_edge)));
+			DEBUG("count was "<< count);
+//		    omnigraph::WriteSimple(new_graph, tot_labeler_after, cfg::get().output_dir  + ToString(count)+".dot", "no_repeat_graph");
+			count ++ ;
+		}
+		if (extends.size() == 0){
+			VertexId new_end = new_graph.AddVertex();
+			DEBUG("adding edge from" << newIds.ReturnIntId(new_start) << " to " << newIds.ReturnIntId(new_end) << " of length " << g.length(old_first_edge));
+
+			old_first_edge = first.start;
+			EdgeId eid = new_graph.AddEdge(new_start, new_end, g.EdgeNucls(old_first_edge));
+			WrappedSetCoverage(new_graph, eid, (int) (g.coverage(old_first_edge) * g.length(old_first_edge)));
+			new_start = new_end;
+			count++;
+
+			if (first.paths.size() != 1 ) {
+				DEBUG ("can not select between different tails:(");
+			} else {
+				DEBUG("singleton dead end!");
+				auto current_path = first.paths.begin();
+				DEBUG(current_path->size());
+			//	DEBUG(first);
+
+
+				for(auto path_iter = current_path->begin(); path_iter != current_path->end(); ++path_iter) {
+
+					VertexId new_end = new_graph.AddVertex();
+					DEBUG("adding edge from" << newIds.ReturnIntId(new_start) << " to " << newIds.ReturnIntId(new_end) << " of length " << g.length(old_first_edge));
+
+					old_first_edge = * path_iter;
+					EdgeId eid = new_graph.AddEdge(new_start, new_end, g.EdgeNucls(old_first_edge));
+					WrappedSetCoverage(new_graph, eid, (int) (g.coverage(old_first_edge) * g.length(old_first_edge)));
+					new_start = new_end;
+					count++;
+
+				}
+				VertexId new_end = new_graph.AddVertex();
+				DEBUG("adding edge from" << newIds.ReturnIntId(new_start) << " to " << newIds.ReturnIntId(new_end) << " of length " << g.length(old_first_edge));
+
+				old_first_edge = first.end;
+				EdgeId eid = new_graph.AddEdge(new_start, new_end, g.EdgeNucls(old_first_edge));
+				WrappedSetCoverage(new_graph, eid, (int) (g.coverage(old_first_edge) * g.length(old_first_edge)));
+				new_start = new_end;
+				DEBUG("and tail of length "<< iter->length);
+//				omnigraph::WriteSimple(new_graph, tot_labeler_after, cfg::get().output_dir  + ToString(count)+".dot", "no_repeat_graph");
+				count++;
+			}
+		}
+	}
+	DEBUG(count);
+
+}
+
+
+};
+}
