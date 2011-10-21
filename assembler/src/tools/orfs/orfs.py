0939e745e0c279372d9387089dcc782cf202cad1
Nikolay Vyahhi <vyahhi@gmail.com>
2011-10-22 01:27:16 +0400

    ORFs performance update

--- assembler/src/tools/orfs/orfs.py
+++ assembler/src/tools/orfs/orfs.py
@@ -14,7 +14,7 @@ if len(sys.argv) < 3:
 # http://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG11
 # 11. The Bacterial, Archaeal and Plant Plastid Code (transl_table=11)
 AAs    = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'
-Starts = '---M---------------X------------XXXM---------------M------------'
+Starts = '---M---------------X------------XXXM---------------M------------' # Ms are starts, Xs are deprecated (rare) starts
 Base1  = 'TTTTTTTTTTTTTTTTCCCCCCCCCCCCCCCCAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGG'
 Base2  = 'TTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGG'
 Base3  = 'TCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAG'
@@ -23,26 +23,29 @@ fasta_filename = sys.argv[1]
 orf_min = int(sys.argv[2])
 orf_max = int(sys.argv[3]) if len(sys.argv) >= 4 else 1e3000
 
-def find_ORFs(genome):
+# precalc of start/stop codons based on transl_table
+starts = set()
+stops = set()
+for i in xrange(64):
+	if AAs[i] == '*':
+		stops.add((Base1[i], Base2[i], Base3[i]))
+	if Starts[i] == 'M':
+		starts.add((Base1[i], Base2[i], Base3[i]))
+
+def find_ORFs(genome, start, min, max):
 	gene = False # no gene
 	l = 0 # length of ORF
 	ls = []
-	for a, b, c in itertools.izip(genome[::3], genome[1::3], genome[2::3]):
+	for a, b, c in itertools.izip(genome[start::3], genome[start+1::3], genome[start+2::3]):
 		l += 1
-		for i in xrange(64):
-			if (Base1[i] == a) and (Base2[i] == b) and (Base3[i] == c):
-				if Starts[i] == 'M' and not gene:
-					gene = True # gene
-					l = 1
-				elif AAs[i] == '*' and gene:
-					gene = False
-					ls.append(l)
-				break
+		if not gene and (a, b, c) in starts:
+			gene = True # gene
+			l = 1
+		elif gene and (a, b, c) in stops:
+			gene = False
+			if min <= l <= max:
+				ls.append(l)
 	return ls
-
-def cnt_ORFs(genome, min, max):
-	orfs = find_ORFs(genome)
-	return len(filter(lambda x: min <= x <= max, orfs))
 
 def reverse_complement(s):
 	complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N', 'a': 't', 'c': 'g', 'g': 'c', 't': 'a', 'n': 'n'}
@@ -51,12 +54,13 @@ def reverse_complement(s):
 fasta = fastaparser.read_fasta(fasta_filename)
 cnt = 0
 for name, seq in fasta:
-	cnt += cnt_ORFs(seq, orf_min, orf_max)
-	cnt += cnt_ORFs(seq[1:], orf_min, orf_max)
-	cnt += cnt_ORFs(seq[2:], orf_min, orf_max)
+	seq = seq.upper()
+	cnt += len(find_ORFs(seq, 0, orf_min, orf_max))
+	cnt += len(find_ORFs(seq, 1, orf_min, orf_max))
+	cnt += len(find_ORFs(seq, 2, orf_min, orf_max))
 	rc_seq = reverse_complement(seq)
-	cnt += cnt_ORFs(rc_seq, orf_min, orf_max)
-	cnt += cnt_ORFs(rc_seq[1:], orf_min, orf_max)
-	cnt += cnt_ORFs(rc_seq[2:], orf_min, orf_max)
+	cnt += len(find_ORFs(rc_seq, 0, orf_min, orf_max))
+	cnt += len(find_ORFs(rc_seq, 1, orf_min, orf_max))
+	cnt += len(find_ORFs(rc_seq, 2, orf_min, orf_max))
 
 print 'ORFs between', orf_min, 'and', orf_max, 'in', fasta_filename,' = ', cnt
