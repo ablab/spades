1456e9901514127ebdc3ff9be5b777a265b3e85b
Dmitry Antipov <dmitrij.antipov@gmail.com>
2011-10-24 14:11:54 +0400

    file forgot

---
+++ assembler/src/debruijn/path_set_tools.hpp
@@ -0,0 +1,39 @@
+/*
+ * path_set_tools.hpp
+ *
+ *  Created on: Oct 23, 2011
+ *      Author: undead
+ */
+#pragma once
+#include "omni/path_set.hpp"
+#include "graph_pack.hpp"
+
+namespace debruijn_graph {
+template <typename graph_pack>
+string str(const PathSet<typename graph_pack::graph_t::EdgeId>& pathSet, graph_pack& gp) {
+	stringstream pathsString;
+	size_t linecounter = 1;
+	for(auto iter = pathSet.paths.begin() ; iter != pathSet.paths.end() ; ++iter)
+	{
+		pathsString << "Path " << linecounter <<":"<< pathSet.length<< " "<<  pathSet.start <<"--" ;
+		linecounter++;
+		for(size_t i = 0 ; i < (*iter).size() ; ++i)
+		{
+			pathsString << gp.int_ids.ReturnIntId(((*iter)[i])) << " -- " ;
+		}
+		pathsString<<  pathSet.end;
+		pathsString<<endl;
+	}
+	stringstream res;
+	res << "id: "<< pathSet.id <<" weight "<< pathSet.weight <<" Start = " << gp.int_ids.ReturnIntId(pathSet.start) <<" ....... "<<"End = " << gp.int_ids.ReturnIntId(pathSet.end)<< endl<< pathsString.str() ;
+	return res.str();
+}
+
+int tst(){
+	return 23;
+}
+
+
+
+}
+
