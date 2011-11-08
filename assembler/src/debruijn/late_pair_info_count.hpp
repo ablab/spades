5a256d68361a014bd5914716ace390ec5c11efad
Dmitry Antipov <dmitrij.antipov@gmail.com>
2011-11-08 16:56:44 +0400

    more debug added

--- assembler/src/debruijn/late_pair_info_count.hpp
+++ assembler/src/debruijn/late_pair_info_count.hpp
@@ -11,14 +11,14 @@ void late_pair_info_count(conj_graph_pack& gp,
 		paired_info_index& paired_index) {
 	string reads_filename_1 = cfg::get().input_dir + cfg::get().ds.first;
 	string reads_filename2 = cfg::get().input_dir + cfg::get().ds.second;
-
+	INFO("checking reads for pair info count");
 	checkFileExistenceFATAL(reads_filename_1);
 	checkFileExistenceFATAL(reads_filename2);
 
 	io::EasyReader<io::PairedRead> stream(
 			std::make_pair(reads_filename_1, reads_filename2),
 			cfg::get().ds.IS);
-
+	INFO("reads found");
 	exec_simplification(stream, gp, paired_index);
 
 	INFO("STAGE == Counting Late Pair Info");
