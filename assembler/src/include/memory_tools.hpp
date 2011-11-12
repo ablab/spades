86d3e575749b1b304f664e5af8c0cd66a8a75e1d
Valer Lesin <Valery.Lesin@gmail.com>
2011-11-12 16:24:08 +0300

    compilation bug fix

---
+++ assembler/src/include/memory_tools.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <sys/time.h>
+#include <sys/resource.h>
+
+bool print_mem_usage(std::string const& msg)
+{
+	static size_t pid = getpid();
+	string str = (format("pmap -d %d | grep writeable/private") % pid).str();
+	cout << "==== MEM USAGE: " << msg << endl;
+	return system(str.c_str()) == 0;
+}
+
+void limit_memory(size_t limit){
+
+	rlimit rl = {limit, limit};
+
+	if (sizeof(rl.rlim_max) < 8){
+
+		INFO("Can't limit virtual memory because of 32-bit system");
+		return;
+	}
+
+	int res = setrlimit(RLIMIT_AS, &rl);
+    VERIFY(res == 0);
+}
