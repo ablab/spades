0930a8fbd3a68720751d0013f5cbe7fc60ff3240
Andrey <andrewprzh@gmail.com>
2011-11-16 02:17:20 +0300

    new seq streams with info for scaffolder

--- assembler/src/include/read/osequencestream.hpp
+++ assembler/src/include/read/osequencestream.hpp
@@ -15,9 +15,7 @@ private:
 	ofstream ofstream_;
 	int id_;
 public:
-	void * ptr;
-
-	osequencestream(const string& filename): id_(0), ptr(0) {
+	osequencestream(const string& filename): id_(0) {
 		ofstream_.open(filename.c_str());
 	}
 
@@ -28,7 +26,7 @@ public:
 	osequencestream& operator<<(const Sequence& seq) {
 //		DEBUG("outputting");
 		string s = seq.str();
-		ofstream_ << ">NODE_" << id_++ << "_length_" << s.size() << "_ID_" << ptr << endl;
+		ofstream_ << ">NODE_" << id_++ << "_length_" << s.size() << endl;
 		// Velvet format: NODE_1_length_24705_cov_358.255249
 		size_t cur = 0;
 		while (cur < s.size()) {
@@ -37,8 +35,6 @@ public:
 		}
 		return *this;
 	}
-
-
 };
 
 class osequencestream_cov {
@@ -71,8 +67,88 @@ public:
 		}
 		return *this;
 	}
+};
 
 
+class osequencestream_with_id {
+private:
+	ofstream ofstream_;
+	int id_;
+
+	size_t uid_;
+	double cov_;
+
+public:
+	osequencestream_with_id(const string& filename): id_(0), uid_(0), cov_(0.0) {
+		ofstream_.open(filename.c_str());
+	}
+
+	virtual ~osequencestream_with_id() {
+		ofstream_.close();
+	}
+
+	void setCoverage(double c) {
+		cov_ = c;
+	}
+
+	void setID(size_t uid) {
+		uid_ = uid;
+	}
+
+	osequencestream_with_id& operator<<(const Sequence& seq) {
+		string s = seq.str();
+		ofstream_ << ">NODE_" << id_++ << "_length_" << s.size() << "_cov_" << cov_ << "_ID_" << uid_ << endl;
+		size_t cur = 0;
+		while (cur < s.size()) {
+			ofstream_ << s.substr(cur, 60) << endl;
+			cur += 60;
+		}
+		return *this;
+	}
+};
+
+
+class osequencestream_with_data_for_scaffold {
+private:
+	ofstream ofstream_;
+	ofstream scstream_;
+	int id_;
+
+	size_t uid_;
+	double cov_;
+
+public:
+	osequencestream_with_data_for_scaffold(const string& filename): id_(1), uid_(0), cov_(0.0) {
+		ofstream_.open(filename.c_str());
+		string sc_filename = filename + ".info";
+		scstream_.open(sc_filename);
+	}
+
+	virtual ~osequencestream_with_data_for_scaffold() {
+		ofstream_.close();
+		scstream_.close();
+	}
+
+	void setCoverage(double c) {
+		cov_ = c;
+	}
+
+	void setID(size_t uid) {
+		uid_ = uid;
+	}
+
+	osequencestream_with_data_for_scaffold& operator<<(const Sequence& seq) {
+		string s = seq.str();
+		scstream_ << id_ << "\tNODE_" << id_ << "\t" << s.size() << "\t" << (int) round(cov_) << endl;
+		ofstream_ << ">NODE_" << id_++ << "_length_" << s.size() << "_cov_" << cov_ << "_ID_" << uid_ << endl;
+
+		size_t cur = 0;
+		while (cur < s.size()) {
+			ofstream_ << s.substr(cur, 60) << endl;
+			cur += 60;
+		}
+		return *this;
+	}
 };
 
 #endif /* OSEQUENCESTREAM_HPP_ */
