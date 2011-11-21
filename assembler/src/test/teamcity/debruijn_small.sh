31c7afbdc7a9618d392e24c5aeff0f75eeb123d2
ABLab TeamCity <teamcity@spbsu.ru>
2011-11-21 17:56:53 +0400

    mauve removed

--- assembler/src/test/teamcity/debruijn_small.sh
+++ assembler/src/test/teamcity/debruijn_small.sh
@@ -3,7 +3,7 @@ cd ../../../
 ./cpcfg
 ./run rd
 cd src/tools/quality/
-./run_EColi_morality.sh ../../../data/debruijn/QUAKE_CROPPED_1K/K55/latest/resolved_and_cleared_*.fasta
+./run_EColi_morality.sh -m ../../../data/debruijn/QUAKE_CROPPED_1K/K55/latest/resolved_and_cleared_*.fasta
 cd ../test/teamcity/
 
 #python teamcity.py
