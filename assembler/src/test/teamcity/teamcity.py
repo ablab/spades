8c0c008ae4e526be3953a269e1510832c0a19c04
ABLab TeamCity <teamcity@spbsu.ru>
2011-11-21 19:15:59 +0400

    teamcity.py

--- assembler/src/test/teamcity/teamcity.py
+++ assembler/src/test/teamcity/teamcity.py
@@ -10,12 +10,16 @@ cur_metric_id = 0
 cur_line = 0;      
 for line in logfile_out:
 	if cur_line == 1:
-		if line.split('|')[1] < 1000:
+		n50 = int(line.split('|')[1]);
+		mis = int(line.split('|')[9]);
+		print('n50 = ' + str(n50) + ' missasemblies ' + str(mis));
+		if n50 < 845:
+			print('genome too short')
 			sys.exit(1);
-		if line.split('|')[9] > 0:
+		if (mis > 0):
+			print('too much miss')
 			sys.exit(1);			
-		print(line.split('|')[1])
-		print(line.split('|')[9])
+		
 	cur_line += 1;
 	
 logfile_out.close()
