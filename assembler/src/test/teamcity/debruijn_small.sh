9ef76e3ad5126f499280faadc50749476e65114d
ABLab TeamCity <teamcity@spbsu.ru>
2011-11-21 18:57:56 +0400

    still tc

--- assembler/src/test/teamcity/debruijn_small.sh
+++ assembler/src/test/teamcity/debruijn_small.sh
@@ -4,6 +4,6 @@ cd ../../../
 ./run rd
 cd src/tools/quality/
 ./run_EColi_morality.sh -m ../../../data/debruijn/QUAKE_CROPPED_1K/K55/latest/resolved_and_cleared_*.fasta
-cd ../test/teamcity/
+cd ../../test/teamcity/
 
-#python teamcity.py
+python teamcity.py
