51a212dab8c0529b03decfd0df3f89e85e23ec4a
Andrey <andrey@andrey-ThinkPad-ABlab.(none)>
2011-11-22 21:21:04 +0400

    stat tools

--- assembler/src/tools/reads_utils/cov_single_wc.py
+++ assembler/src/tools/reads_utils/cov_single_wc.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python -O
 
+#Create raw file for single reads using chromosome map file
+#Chromosome map file should contain choromosome name (as used in bowtie) and its start position in genome, one chromosome per line
+
 import sys
+
+if len(sys.argv) != 3 && len(sys.argv) != 2:
+        print("Usage: <bowtie log file> <output> [chromosomes file]");
+        exit(0)
 
 inFile = open(sys.argv[1])
 outFile = open(sys.argv[2], 'w')
-chrsF = open(sys.argv[3])
+
+wChrs = (len(sys.argv) == 3)
 
 chrs = {" ":0}
-for line in chrsF:
-        chrs[line.split()[0]] =	int(line.split()[1])
+if wChrs:	 
+	chrsF = open(sys.argv[3])
+	for line in chrsF:
+		chrs[line.split()[0]] =	int(line.split()[1])
 
 delim = '/'
 
 for line in inFile:
-	chr = line.split('\t',5)[2]
+	chr1 = line.split('\t',5)[2]
 	pos1 = int(line.split('\t', 5)[3])
      	len1 = len(line.split('\t', 5)[4])
-		
-	outFile.write(str(pos1 + chrs[chr]) + ' ' + str(len1))
-	outFile.write('\n')
 
+	addPos = 0
+	if wChrs:
+		addPos = chrs[chr1] 	
+		
+	outFile.write(str(pos1 + addPos) + ' ' + str(len1) + '\n')
 
 inFile.close()
 outFile.close()
