2771b37a1693ee11e40f207761a93d221f15c42e
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2011-12-22 19:27:16 +0400

    datasets.py now can tar datasets

--- assembler/src/tools/dataset.py
+++ assembler/src/tools/dataset.py
@@ -17,27 +17,47 @@ def miss(f):
 	return False
     return not os.path.exists(f)
 
-cfg = sys.argv[1]
-if not os.path.exists(cfg):
-    print "no such file:", cfg
-cfg = open(cfg, 'r')
-while 1:
-    ds = readline(cfg)
-    if not ds: break
-    if ds.startswith("#include"): continue
-    s = readline(cfg)
-    if s != "{":
-	print "'{' expected, but found", s
-	exit(1)
-    p = {}
+files = ["first", "second"]
+files += ["single_" + x for x in files]
+files += ["jumping_" + x for x in files]
+files += ["reference_genome"]
+
+def check(ds):
+    m = reduce(lambda x, y: x or y, [miss(ds.get(f)) for f in files])
+    print ds["name"], not m
+
+def tar(ds):
+    s = [ds.get(f) for f in files]
+    s = filter(lambda x: x, s)
+    s = reduce(lambda x, y: x + " " + y, s)
+    print "tar -cf", ds["name"] + ".tar", s
+
+def process(cfg, func, filt):
+    if not os.path.exists(cfg):
+        print "no such file:", cfg
+    cfg = open(cfg, 'r')
     while 1:
-	s = readline(cfg)
-	if s == "}":
-	    break
-	s = s.split();
-	if (len(s) != 2):
-	    print "Invalid property line:", s
-	    exit(2)
-	p[s[0]] = s[1]
-    m = miss(p.get("reference_genome")) or miss(p.get("first")) or miss(p.get("second")) or miss(p.get("single_first")) or miss(p.get("single_second")) or miss(p.get("jumping_first")) or miss(p.get("jumping_second")) or miss(p.get("jumping_single_first")) or miss(p.get("jumping_single_second"))
-    print ds, not m
+        ds = readline(cfg)
+        if not ds: break
+        if ds.startswith("#include"): continue
+        s = readline(cfg)
+        if s != "{":
+	    print "'{' expected, but found", s
+	    exit(1)
+        ds = {"name": ds}
+        while 1:
+	    s = readline(cfg)
+	    if s == "}":
+	        break
+	    s = s.split();
+	    if (len(s) != 2):
+	        print "Invalid property line:", s
+	        exit(2)
+	    ds[s[0]] = s[1]
+	if filt(ds): func(ds)
+
+if sys.argv[1] == "check":
+    process(sys.argv[2], check, lambda ds: True);
+if sys.argv[1] == "tar":
+    sets = sys.argv[3:]
+    process(sys.argv[2], tar, lambda ds: ds["name"] in sets);
