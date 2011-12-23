9aad046492fa3736123f04972b112308cb182a17
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2011-12-23 21:56:10 +0400

    script to run hammer

--- assembler/src/tools/dataset.py
+++ assembler/src/tools/dataset.py
@@ -16,12 +16,12 @@ def readline(f):
 def missFile(f):
     if (not f) or (f.lower() == "n/a"):
 	return False
-    return not os.path.exists(f)
+    return not os.path.isfile(f)
 
 def presentFile(f):
     if (not f) or (f.lower() == "n/a"):
 	return False
-    return os.path.exists(f)
+    return os.path.isfile(f)
 
 files = ["first", "second"]
 files += ["single_" + x for x in files]
@@ -48,7 +48,7 @@ def md5(ds):
 	os.system("md5sum " + f)
 
 def process(cfg, func, filt):
-    if not os.path.exists(cfg):
+    if not os.path.isfile(cfg):
         print "no such file:", cfg
     cfg = open(cfg, 'r')
     while 1:
@@ -75,6 +75,15 @@ def process(cfg, func, filt):
 	    else:
 		func(ds)
 
+def printDS(p):
+    print "{"
+    for (a, b) in p:
+	print "\t" + a + "\t" + b
+    print "}"
+
+def hammer(prefix):
+    os.system('((ls -1 ' + prefix + '* 2> /dev/null | grep left.cor | grep -v single) || echo "") | head -n 1')
+
 if sys.argv[1] == "check":
     process(sys.argv[2], check, lambda ds: True);
 if sys.argv[1] == "tar":
@@ -87,3 +96,5 @@ if sys.argv[1] == "md5":
     if 3 < len(sys.argv): regexp = "^.*" + sys.argv[3] + ".*$"
     filt = lambda ds: re.match(regexp, ds["name"])
     process(sys.argv[2], md5, filt);
+if sys.argv[1] == "hammer":
+    hammer(sys.argv[2])
