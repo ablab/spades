b092b6fe552c733a7134880740121da2857889c2
Anton <anton.bankevich@gmail.com>
2012-01-13 20:19:42 +0400

    ups. compilation fix

--- assembler/src/debruijn/config_struct.hpp
+++ assembler/src/debruijn/config_struct.hpp
@@ -386,10 +386,9 @@ public:
 inline void load(debruijn_config::simplification::tip_clipper& tc, boost::property_tree::ptree const& pt, bool complete)
 {
 	using config_common::load;
-	load(tc.max_tip_length			, pt, "max_tip_length"		 );
-	load(estimate_max_coverage		, pt, "estimate_max_coverage");
-	load(tc.max_coverage			, pt, "max_coverage"		 );
-	load(tc.max_relative_coverage	, pt, "max_relative_coverage");
+	load(tc.max_tip_length				, pt, "max_tip_length"		 );
+	load(tc.max_coverage				, pt, "max_coverage"		 );
+	load(tc.max_relative_coverage		, pt, "max_relative_coverage");
 }
 
 inline void load(working_stage& entry_point, boost::property_tree::ptree const& pt, std::string const& key, bool complete)
@@ -434,7 +433,7 @@ inline void load(debruijn_config::simplification::erroneous_connections_remover&
 	using config_common::load;
 
 	ec.threshold_percentile = pt.get_optional<double>("threshold_percentile");
-
+	load(ec.estimate_max_coverage	, pt, "estimate_max_coverage");
 	load(ec.max_coverage    	, pt, "max_coverage"    );
 	load(ec.max_length_div_K	, pt, "max_length_div_K");
 }
