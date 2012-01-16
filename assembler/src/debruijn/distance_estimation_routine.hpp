a79a42e993f01932e8522418cce15a368f43fa87
Valer Lesin <Valery.Lesin@gmail.com>
2012-01-16 16:14:59 +0400

    default: gap_closer is turned off

--- assembler/src/debruijn/distance_estimation_routine.hpp
+++ assembler/src/debruijn/distance_estimation_routine.hpp
@@ -32,7 +32,6 @@ void estimate_distance(conj_graph_pack& gp, paired_info_index& paired_index,
 	INFO("STAGE == Estimating Distance");
 
 	if (cfg::get().paired_mode) {
-
 	    if (cfg::get().gc.enable)
 	        CloseShortGaps(gp.g, paired_index, gp.edge_pos,	cfg::get().gc.minimal_intersection);
 		GraphDistanceFinder<Graph> dist_finder(gp.g, *cfg::get().ds.IS, cfg::get().ds.RL, cfg::get().de.delta);
