b30208269880f2786cdef206a0007d851fa21027
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2012-01-18 20:44:27 +0400

    teamcity starts using pipeline

--- assembler/src/test/teamcity/debruijn_small.sh
+++ assembler/src/test/teamcity/debruijn_small.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-echo "Executing $0, here is its code:"
-echo ""
-cat $0
-echo ""
-./debruijn_make.sh
 pushd ../../../
 ./cpcfg
-./run rd
-#cd src/tools/quality/
-#./run_EColi_morality.sh -m ../../../data/debruijn/QUAKE_CROPPED_1K/K55/latest/resolved_and_cleared_*.fasta
-#cd ../../test/teamcity/
-
-#python teamcity.py
+./spades.py src/test/teamcity/spades_config.small.info
