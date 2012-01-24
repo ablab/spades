d064061ae7427d2a4c756bd10efab9284c66e8ba
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2012-01-24 21:56:02 +0400

    TeamCity is now updated with respect to new quality interface

--- assembler/src/test/teamcity/assess.py
+++ assembler/src/test/teamcity/assess.py
@@ -5,23 +5,18 @@ import os
 import shutil
 import re
 import subprocess
-logfile_out = open('../../tools/quality/results/all.txt', 'r')
-cur_metric_id = 0  
-cur_line = 0;      
-for line in logfile_out:
-	if cur_line == 1:
-		n50 = int(line.split('|')[1]);
-		mis = int(line.split('|')[11]);
-		print('n50 = ' + str(n50))
-		print('missasembled contigs = ' + str(mis));
-		if n50 < 75000:
-			print('N50 is too small')
-			sys.exit(1);
-		if (mis > 2):
-			print('too many missassemblies')
-			sys.exit(1);
-	cur_line += 1;
 
-logfile_out.close()
-sys.exit(0);
-
+f = open(sys.argv[1], 'r')
+columns = map(lambda s: s.strip(), f.readline().split('\t'))
+values = map(lambda s: s.strip(), f.readline().split('\t'))
+n50 = int(values[columns.index("N50")])
+mis = int(values[columns.index("Misassemblies")])
+print 'n50 = ', n50
+print 'missasembled contigs = ', mis
+if n50 < 75000:
+    print('N50 is too small')
+    sys.exit(1)
+if (mis > 2):
+    print('too many missassemblies')
+    sys.exit(1)
+f.close()
--- assembler/src/test/teamcity/debruijn_full.sh
+++ assembler/src/test/teamcity/debruijn_full.sh
@@ -2,10 +2,9 @@
 set -e
 pushd ../../../
 [ -e data/debruijn/ECOLI_IS220_QUAKE/K55/latest ] && rm data/debruijn/ECOLI_IS220_QUAKE/K55/latest
-[ -e src/tools/quality/results ] && rm -rf src/tools/quality/results
+[ -e data/quality ] && rm -rf data/quality
 make clean
 ./cpcfg
 ./spades.py src/test/teamcity/spades_config.full.info
-src/tools/quality/run_Ecoli.sh -m data/debruijn/ECOLI_IS220_QUAKE/K55/latest/resolved_and_cleared_*.fasta
-popd
-python assess.py
+src/tools/quality/run_Ecoli.sh -o data/quality data/debruijn/ECOLI_IS220_QUAKE/K55/latest/final_contigs.fasta
+python src/test/teamcity/assess.py data/quality/all.tab
