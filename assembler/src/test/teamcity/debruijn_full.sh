dd36dd2f07a49741158dfd991227f9087dadb3c3
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2012-01-25 13:58:14 +0400

    tab-separared report file is now .tsv

--- assembler/src/test/teamcity/debruijn_full.sh
+++ assembler/src/test/teamcity/debruijn_full.sh
@@ -7,4 +7,4 @@ make clean
 ./cpcfg
 ./spades.py src/test/teamcity/spades_config.full.info
 src/tools/quality/run_Ecoli.sh -o data/quality data/debruijn/ECOLI_IS220_QUAKE/K55/latest/final_contigs.fasta
-python src/test/teamcity/assess.py data/quality/all.tab
+python src/test/teamcity/assess.py data/quality/all.tsv
