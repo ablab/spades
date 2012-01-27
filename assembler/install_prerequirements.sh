ad2e5f17f39f091749805ba73784b6f95821ff11
Valer Lesin <Valery.Lesin@gmail.com>
2012-01-27 13:48:00 +0400

    simple script installing prerequiments

---
+++ assembler/install_prerequirements.sh
@@ -0,0 +1,15 @@
+apt-get --yes install cmake-curses-gui
+apt-get --yes install g++-4.4
+apt-get --yes install python2.6-dev
+apt-get --yes install java-common
+apt-get --yes install cmake
+apt-get --yes install liblog4cxx10-dev
+apt-get --yes install libboost1.42-all-dev
+apt-get --yes install zlib1g-dev
+apt-get --yes install python-mathplotlib
+apt-get --yes install python-numpy
+apt-get --yes install bioperl
+apt-get --yes install mummer
+apt-get --yes install r-recommended
+apt-get --yes install pdftk
+
