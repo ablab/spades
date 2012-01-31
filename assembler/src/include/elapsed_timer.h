b7cc42e3ca67cd777e56a1c9bbbc339123fc4f0f
Nikolay Vyahhi <vyahhi@gmail.com>
2012-01-31 14:23:40 -0800

    MacOSX don't have clock_gettime, got platform independent solution

--- assembler/src/include/elapsed_timer.h
+++ assembler/src/include/elapsed_timer.h
@@ -1,6 +1,11 @@
 #pragma once
 #include <time.h>
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
 struct elapsed_timer
 {
    elapsed_timer()
@@ -11,16 +16,29 @@ struct elapsed_timer
    double elapsed() const
    {
       timespec ts;
-      clock_gettime(CLOCK_REALTIME, &ts);
-
+      my_clock_gettime(ts);
       return ts.tv_sec - ts_.tv_sec + (ts.tv_nsec - ts_.tv_nsec) * 1e-9;
    }
 
    void restart()
    {
-      clock_gettime(CLOCK_REALTIME, &ts_);
+      my_clock_gettime(ts_);
    }
 
 private:
    timespec ts_;
+
+   void my_clock_gettime(timespec &ts) const {
+      #ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+         clock_serv_t cclock;
+         mach_timespec_t mts;
+         host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+         clock_get_time(cclock, &mts);
+         mach_port_deallocate(mach_task_self(), cclock);
+         ts.tv_sec = mts.tv_sec;
+         ts.tv_nsec = mts.tv_nsec;
+      #else
+         clock_gettime(CLOCK_REALTIME, &ts);
+      #endif      
+   }
 };
