4a6ac718d83b35565036c8488bb461730d88b9e1
Valer Lesin <Valery.Lesin@gmail.com>
2012-01-31 12:22:31 +0400

    changed parameters in is220 quality test

--- assembler/src/test/teamcity/spades_mc_is220.sh
+++ assembler/src/test/teamcity/spades_mc_is220.sh
@@ -7,5 +7,5 @@ make clean
 ./cpcfg
 ./spades.py src/test/teamcity/spades_config_mc_is220.info
 src/tools/quality/run_Ecoli.sh -o data/quality data/debruijn/ECOLI_IS220_QUAKE/K55/latest/final_contigs.fasta
-python src/test/teamcity/assess.py data/quality/all.tsv 75000 2
+python src/test/teamcity/assess.py data/quality/all.tsv 80000 3
 popd
