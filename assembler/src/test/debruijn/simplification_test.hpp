f45686173c919c7528a93a66f5ffcca728d040c7
Anton <anton.bankevich@gmail.com>
2012-02-03 19:15:06 +0400

    test compilation fix

--- assembler/src/test/debruijn/simplification_test.hpp
+++ assembler/src/test/debruijn/simplification_test.hpp
@@ -10,12 +10,16 @@ BOOST_AUTO_TEST_SUITE(graph_simplification_tests)
 
 static debruijn_config::simplification::bulge_remover standard_br_config_generation() {
 	debruijn_config::simplification::bulge_remover br_config;
-	br_config.max_length_div_K = 3;
+	br_config.max_bulge_length_coefficient = 4;
 	br_config.max_coverage = 1000.;
 	br_config.max_relative_coverage = 1.2;
 	br_config.max_delta = 3;
 	br_config.max_relative_delta = 0.1;
 	return br_config;
+}
+
+static size_t standard_read_length() {
+	return 100;
 }
 
 debruijn_config::simplification::bulge_remover standard_br_config() {
@@ -26,7 +30,7 @@ debruijn_config::simplification::bulge_remover standard_br_config() {
 static debruijn_config::simplification::erroneous_connections_remover standard_ec_config_generation() {
 	debruijn_config::simplification::erroneous_connections_remover ec_config;
 	ec_config.max_coverage = 30;
-	ec_config.max_length_div_K = 2;
+	ec_config.max_ec_length_coefficient = 20;
 	return ec_config;
 }
 
@@ -37,7 +41,7 @@ debruijn_config::simplification::erroneous_connections_remover standard_ec_confi
 
 static debruijn_config::simplification::topology_based_ec_remover topology_based_ec_config_generation() {
 	debruijn_config::simplification::topology_based_ec_remover tec_config;
-	tec_config.max_length = 100;
+	tec_config.max_ec_length_coefficient = 20;
 	tec_config.plausibility_length = 200;
 	tec_config.uniqueness_length = 3000;
 	return tec_config;
@@ -45,7 +49,7 @@ static debruijn_config::simplification::topology_based_ec_remover topology_based
 
 static debruijn_config::simplification::max_flow_ec_remover max_flow_based_ec_config_generation() {
 	debruijn_config::simplification::max_flow_ec_remover mfec_config;
-	mfec_config.max_length = 100;
+	mfec_config.max_ec_length_coefficient = 20;
 	mfec_config.plausibility_length = 200;
 	mfec_config.uniqueness_length = 3000;
 	return mfec_config;
@@ -65,7 +69,7 @@ static debruijn_config::simplification::tip_clipper standard_tc_config_generatio
 	debruijn_config::simplification::tip_clipper tc_config;
 	tc_config.max_coverage = 1000.;
 	tc_config.max_relative_coverage = 1.2;
-	tc_config.max_tip_length = 100;
+	tc_config.max_tip_length_coefficient = 2.5;
 	return tc_config;
 }
 
@@ -79,7 +83,7 @@ BOOST_AUTO_TEST_CASE( SimpleTipClipperTest ) {
 	IdTrackHandler<Graph> int_ids(g);
 	ScanBasicGraph("./src/test/debruijn/graph_fragments/simpliest_tip/simpliest_tip", g, int_ids);
 
-	ClipTips(g, standard_tc_config());
+	ClipTips(g, standard_tc_config(), standard_read_length());
 
 	BOOST_CHECK_EQUAL(g.size(), 4);
 }
@@ -99,7 +103,7 @@ BOOST_AUTO_TEST_CASE( TipobulgeTest ) {
 	IdTrackHandler<Graph> int_ids(g);
 	ScanBasicGraph("./src/test/debruijn/graph_fragments/tipobulge/tipobulge", g, int_ids);
 
-	ClipTips(g, standard_tc_config());
+	ClipTips(g, standard_tc_config(), standard_read_length());
 
 	RemoveBulges(g, standard_br_config());
 
