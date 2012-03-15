b91315302171adc554b7619816445a652b90c351
Nikolay Vyahhi <vyahhi@gmail.com>
2012-03-15 15:21:05 -0700

    TC minifix.

--- assembler/src/test/teamcity/spades_rectangles_is220.sh
+++ assembler/src/test/teamcity/spades_rectangles_is220.sh
@@ -14,6 +14,7 @@ make clean
 ./cpcfg
 sed -r 's/^resolving_mode[ \t]*split/resolving_mode rectangle/' configs/debruijn/config.info > configs/debruijn/config.info
 sed -r 's/ECOLI_IS220_QUAKE_1K/ECOLI_IS220_QUAKE/' configs/debruijn/config.info > configs/debruijn/config.info
+make rd
 ./run
 src/tools/quality/run_Ecoli.sh -o data/quality data/debruijn/ECOLI_IS220_QUAKE/K55/latest/saves/rectangle_after.fasta
 python src/test/teamcity/assess.py data/quality/all.tsv 80000 3
