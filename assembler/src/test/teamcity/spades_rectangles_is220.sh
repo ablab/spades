164161c661c585bc0df81864b70ce4f06f5a068c
Nikolay Vyahhi <vyahhi@gmail.com>
2012-03-15 01:04:27 -0700

    TC fix

--- assembler/src/test/teamcity/spades_rectangles_is220.sh
+++ assembler/src/test/teamcity/spades_rectangles_is220.sh
@@ -6,6 +6,6 @@ rm -rf data/quality
 make clean
 ./cpcfg
 ./spades.py src/test/teamcity/spades_config_rectangles_is220.info
-src/tools/quality/run_Ecoli.sh -o data/quality data/debruijn/ECOLI_IS220_QUAKE/K55/latest/final_contigs.fasta
+src/tools/quality/run_Ecoli.sh -o data/quality data/debruijn/ECOLI_IS220_QUAKE/K55/latest/saves/rectangle_after.fasta
 python src/test/teamcity/assess.py data/quality/all.tsv 80000 3
 popd
