7cdc068d8163b2f1e8ff343101130fd34cecb301
Nikolay Vyahhi <vyahhi@gmail.com>
2012-03-15 01:07:53 -0700

    TC fix

--- assembler/src/test/teamcity/spades_rectangles_is220.sh
+++ assembler/src/test/teamcity/spades_rectangles_is220.sh
@@ -3,6 +3,7 @@ set -e
 pushd ../../../
 rm -f data/debruijn/ECOLI_IS220_QUAKE/K55/latest
 rm -rf data/quality
+./prepare_cfg
 make clean
 ./cpcfg
 ./spades.py src/test/teamcity/spades_config_rectangles_is220.info
