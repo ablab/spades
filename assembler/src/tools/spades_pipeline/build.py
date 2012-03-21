dd2cd3dd37f57aba3577f5f38a6c53e2af784490
Yakov Sirotkin <yasha@telamon.ru>
2012-03-21 07:41:56 -0700

    Makefile automatically understands what we need to recompile based on file dates. Use fcntl for locks. http://ablab.myjetbrains.com/youtrack/issue/Assembler-155

--- assembler/src/tools/spades_pipeline/build.py
+++ assembler/src/tools/spades_pipeline/build.py
@@ -1,28 +1,51 @@
 #!/usr/bin/env python
-
 import os
 import support
 import shutil
-import time
+import fcntl
 from os import path
 
 
 def build_spades(dir):
-    support.sys_call('cmake src', dir)
-    print(dir)
+    if not path.exists(path.join(dir, "Makefile")):
+        support.sys_call('cmake src', dir)
+
     support.sys_call('make debruijn', dir)
+
+def syncFiles(src, dest):
+    if path.isfile(src):
+        if not path.exists(dest):
+            shutil.copy2(src, dest)
+        elif str(path.getmtime(src)) != str(path.getmtime(dest)):
+            shutil.copy2(src, dest)
+    else:
+        if not path.exists(dest):
+            os.makedirs(dest)
+        for file in os.listdir(dest):
+            if not file == "k.hpp":
+                destFile = path.join(dest, file)
+                if path.isfile(destFile):
+                    if not path.exists(path.join(src, file)):
+                        os.unlink(destFile)
+
+        for file in os.listdir(src):
+            syncFiles(path.join(src, file), path.join(dest, file))
 
 def build_k(spades_folder, str_k, spades_home):
 
     build_folder_k = path.join(spades_folder, "build" + str_k)
-    if os.path.exists(build_folder_k) :
-        shutil.rmtree(build_folder_k)
 
-    shutil.copytree(path.join(spades_home, "src"  ), path.join(build_folder_k, "src"  ))
-    shutil.copytree(path.join(spades_home, "ext"  ), path.join(build_folder_k, "ext"  ))
-    shutil.copy    (path.join(spades_home, "gen_k"), path.join(build_folder_k, "gen_k"))
+    syncFiles(path.join(spades_home, "src"), path.join(build_folder_k, "src"))
+    syncFiles(path.join(spades_home, "ext"), path.join(build_folder_k, "ext"))
 
-    os.system(path.join(build_folder_k, "gen_k") + " " + str_k)
+    kFile = path.join(build_folder_k, "src/debruijn/k.hpp")
+    if not path.exists(kFile):
+        fo = open(kFile, "w")
+        fo.write("#pragma once\n\n")
+        fo.write("namespace debruijn_graph {\n")
+        fo.write("  const size_t K = " + str_k + ";\n")
+        fo.write("}\n")
+        fo.close()
 
     print("\n== Compiling with K=" + str_k + " ==\n")
     build_spades(build_folder_k)
@@ -31,36 +54,22 @@ def build_spades_n_copy(cfg, spades_home):
 
     precompiled_folder = path.join(os.getenv('HOME'), '.spades/precompiled/')
 
-    if path.exists(precompiled_folder):
-        if os.path.getmtime(precompiled_folder) < os.path.getmtime(__file__):
-            shutil.rmtree(precompiled_folder)
-
     print("\n== Compilation started ==\n")
 
     for K in cfg.iterative_K:
 
-        if not os.path.exists(precompiled_folder) :
+        if not path.exists(precompiled_folder) :
             os.makedirs(precompiled_folder)
 
         str_k = str(K)
 
-        compiledFlag = path.join(precompiled_folder, "compiled") + str_k
-        lockFlag     = path.join(precompiled_folder, "lock")     + str_k
+        lockFlag = path.join(precompiled_folder, "lock") + str_k
 
-        if not os.path.exists(compiledFlag) :
-            while os.path.exists(lockFlag) :
-                try:
-                    time.sleep(10)
-                    os.unlink(lockFile)
-                except:
-                    print("Failed to delete " + lockFlag)
-            if not os.path.exists(compiledFlag) :
-                open(lockFlag, "w")
-                try :
-                    build_k(precompiled_folder, str_k, spades_home)
-                    compiledFile = open(compiledFlag, "w")
-                    compiledFile.close()
-                finally :
-                    os.unlink(lockFlag)
+        fo = open(lockFlag, "w")
+        fcntl.lockf(fo, fcntl.LOCK_EX)
+        try :
+            build_k(precompiled_folder, str_k, spades_home)
+        finally :
+            fcntl.lockf(fo, fcntl.LOCK_UN)
 
     print("\n== Compilation finished successfully ==\n")
