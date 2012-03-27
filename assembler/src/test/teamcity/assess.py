5e46574c1b76ee16caa6d14131b9851130e07612
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2012-03-27 14:36:43 +0400

    minor verbosity fix

--- assembler/src/test/teamcity/assess.py
+++ assembler/src/test/teamcity/assess.py
@@ -14,12 +14,12 @@ columns = map(lambda s: s.strip(), f.readline().split('\t'))
 values = map(lambda s: s.strip(), f.readline().split('\t'))
 n50 = int(values[columns.index("N50")])
 mis = int(values[columns.index("Misassemblies")])
-print 'n50 = ', n50
-print 'missasembled contigs = ', mis
+print 'N50 = ', n50
+print 'Misasembled contigs = ', mis
 if n50 < n50_limit:
     print('N50 is too small')
     sys.exit(1)
 if mis > mis_limit:
-    print('too many missassemblies')
+    print('Too many misassemblies')
     sys.exit(1)
 f.close()
