191234a232a8b8829c1a801007373a930e9c0b9b
Alexey Gurevich <alexeigurevich@gmail.com>
2012-03-28 20:35:28 +0400

    forgotten file

---
+++ assembler/src/hammer/kmer_stat.cpp
@@ -0,0 +1,47 @@
+/*
+ * kmer_stat.cpp
+ *
+ *  Created on: 10.03.2012
+ *      Author: snikolenko
+ */
+
+#include "standard.hpp"
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+#include <boost/format.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/utility.hpp>
+
+#include <time.h>
+#include <sys/resource.h>
+#include <iomanip>
+#include <algorithm>
+
+#include "read/ireadstream.hpp"
+#include "defs.hpp"
+#include "mathfunctions.hpp"
+#include "valid_kmer_generator.hpp"
+#include "position_kmer.hpp"
+#include "globals.hpp"
+#include "config_struct_hammer.hpp"
+#include "subkmers.hpp"
+#include "hammer_tools.hpp"
+
+#include "kmer_stat.hpp"
+
+
+char getQual(const KMerCount & kmc, int i) {
+	if (Globals::use_common_quality) return Globals::common_quality * kmc.second.count;
+	if (!kmc.second.qual.q.size()) {
+		// if (Globals::blobquality[kmc.first.start() + i] < Globals::char_offset + 2) cout << "Zero! " << kmc.first.str() << endl;
+		return Globals::blobquality[kmc.first.start() + i] - Globals::char_offset;
+	}
+	else return kmc.second.qual[i];
+}
+
