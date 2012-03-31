11a3d5a8a662c24313aeeec3b7222170dae00ea2
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2012-03-31 16:27:23 +0400

    teamcity: quality is now measured inside spades.py

--- assembler/src/test/teamcity/spades_mc_is220.sh
+++ assembler/src/test/teamcity/spades_mc_is220.sh
@@ -5,6 +5,5 @@ rm -f data/debruijn/ECOLI_IS220_QUAKE/K55/latest
 rm -rf data/quality
 ./cpcfg
 ./spades.py src/test/teamcity/spades_config_mc_is220.info
-src/tools/quality/run_Ecoli.sh -o data/quality data/debruijn/ECOLI_IS220_QUAKE/K55/latest/final_contigs.fasta
 python src/test/teamcity/assess.py data/quality/all.tsv 80000 3
 popd
--- assembler/src/test/teamcity/spades_sc_lane1.sh
+++ assembler/src/test/teamcity/spades_sc_lane1.sh
@@ -5,6 +5,5 @@ rm -f data/debruijn/ECOLI_SC_LANE_1_BH_woHUMAN/K55/latest
 rm -rf data/quality
 ./cpcfg
 ./spades.py src/test/teamcity/spades_config_sc_lane1.info
-src/tools/quality/run_Ecoli.sh -o data/quality data/debruijn/ECOLI_SC_LANE_1_BH_woHUMAN/K55/latest/final_contigs.fasta
 python src/test/teamcity/assess.py data/quality/all.tsv 47000 1
 popd
