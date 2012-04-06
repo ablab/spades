86f8decc33665c8e863d2d268b337ccf08ac4af6
Yakov Sirotkin <yasha@telamon.ru>
2012-04-06 02:05:31 -0700

    Switch to version 2, remove extra files from the package. http://ablab.myjetbrains.com/youtrack/issue/Assembler-155 http://ablab.myjetbrains.com/youtrack/issue/Assembler-191

--- make-spades-debian.sh
+++ make-spades-debian.sh
@@ -1,13 +1,46 @@
-# git clone git@github.com:ablab/algorithmic-biology.git
-rm -r build
-rm spades_1.0.0*
-cp -r assembler build
+rm -r spades-*
+rm spades_*
+cp -r assembler spades-2.0.0
 
-cd build
-cp makefileDebian makefile
-cat makefile
+cd spades-2.0.0
+rm -r src/abruijn
+rm -r src/bayes
+rm -r src/test
+rm -r data
+rm -r configs/debruijn/datasets_archive
+rm -r configs/debruijn/datasets
+rm -r docs
+find . -name ".?*" | xargs rm -r
+rm -r src/tools/blast-fasta
+rm -r src/tools/clean_contaminations
+rm -r src/tools/consensus
+rm -r src/tools/contig_analysis
+rm -r src/tools/cuckoo_test
+rm -r src/tools/divide_fastq
+rm -r src/tools/estimation
+rm -r src/tools/estimation_deprecated
+rm -r src/tools/filter
+rm -r src/tools/kmerstats
+rm -r src/tools/mapreads
+rm -r src/tools/maps_test
+rm -r src/tools/quake-correct
+rm -r src/tools/ukonnen
+rm -r src/tools/copy_ungzip.sh
+rm -r src/tools/dot_conv.sh
+rm -r src/tools/run_velvet_unpaired.sh
+rm -r src/tools/run_velvet2.sh
+
+rm gen_k
+rm editcfg
+rm install_prerequirements.sh
+rm prepare_cfg
+rm prepare_cfg_eclipse
+rm run
+
+mv makefileDebian makefile
+
 debuild -us -uc
 
 cd ..
-scp spades_1.0.0* 192.168.222.223:
+scp spades_2.0.0* 192.168.222.223:spades2/
 
