18aaad0f9aaac527e6c7caf5cb4f6ab3b0b44146
Yakov Sirotkin <yasha@telamon.ru>
2012-04-08 22:53:24 -0700

    SPAdes version 2. http://ablab.myjetbrains.com/youtrack/issue/Assembler-158

--- make-rpm.sh
+++ make-rpm.sh
@@ -1,15 +1,14 @@
  rpmdev-setuptree
  cp spades.spec rpmbuild/SPECS
  cd rpmbuild/SOURCES   
- scp yasha@192.168.222.223:spades_1.0.0.tar.gz .
- tar -xvf spades_1.0.0.tar.gz
- mv build SPAdes-1.0.0
- cd SPAdes-1.0.0
+ scp yasha@192.168.222.223:spades2/spades_2.0.0.tar.gz .
+ tar -xvf spades_2.0.0.tar.gz
+ cd spades-2.0.0
  touch configure
  chmod a+x configure 
  cd ..
- rm spades_1.0.0.tar.gz
- tar -czvf spades_1.0.0.tar.gz SPAdes-1.0.0
+ rm spades_2.0.0.tar.gz
+ tar -czvf spades_2.0.0.tar.gz spades-2.0.0
  cd ../SPECS
  rpmbuild -ba spades.spec
- scp ../RPMS/x86_64/SPAdes-1.0.0-1.fc16.x86_64.rpm yasha@192.168.222.223:rpm
+ scp ../RPMS/x86_64/spades-2.0.0-1.fc16.x86_64.rpm yasha@192.168.222.223:rpm
