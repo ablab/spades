a5b2e9d70589da3734b6aa9aa0820caf32ed7d83
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2012-04-12 13:10:34 +0400

    teamcity fixes

--- assembler/src/test/teamcity/spades_mc_is220.sh
+++ assembler/src/test/teamcity/spades_mc_is220.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 pushd ../../../
-rm -f data/debruijn/ECOLI_IS220_QUAKE/latest
+rm -f spades_output/ECOLI_IS220_QUAKE/latest
 ./cpcfg
 ./spades.py src/test/teamcity/spades_config_mc_is220.info
-python src/test/teamcity/assess.py data/debruijn/ECOLI_IS220_QUAKE/latest/quality_results/all.tsv 80000 3
+python src/test/teamcity/assess.py spades_output/ECOLI_IS220_QUAKE/latest/quality_results/all.tsv 80000 3
 popd
--- assembler/src/test/teamcity/spades_sc_lane1.sh
+++ assembler/src/test/teamcity/spades_sc_lane1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 pushd ../../../
-rm -f data/debruijn/ECOLI_SC_LANE_1_BH_woHUMAN/latest
+rm -f spades_output/ECOLI_SC_LANE_1_BH_woHUMAN/latest
 ./cpcfg
 ./spades.py src/test/teamcity/spades_config_sc_lane1.info
-python src/test/teamcity/assess.py data/debruijn/ECOLI_SC_LANE_1_BH_woHUMAN/latest/quality_results/all.tsv 47000 1
+python src/test/teamcity/assess.py spades_output/ECOLI_SC_LANE_1_BH_woHUMAN/latest/quality_results/all.tsv 47000 1
 popd
