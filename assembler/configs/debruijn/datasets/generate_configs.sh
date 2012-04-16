7b7c283fd63fe7331bd7da59dceee77befb64967
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2012-04-16 19:14:04 +0400

    script for running datasets

--- assembler/configs/debruijn/datasets/generate_configs.sh
+++ assembler/configs/debruijn/datasets/generate_configs.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-./../../../src/tools/spades_pipeline/generate_config.py _generic.info *.info
+set -e
+./../../../src/tools/spades_pipeline/generate_config.py _generic.template $*
+cd _generic
+for f in *
+do
+    pushd ../../../../
+    echo ./spades.py src/debruijn/datasets/_generic/$f &
+    #./spades.py src/debruijn/datasets/_generic/$f &
+    popd
+done
