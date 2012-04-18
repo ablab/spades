9c8ca7adb50152e29bec39d079e0fcb684f569e0
Yakov Sirotkin <yasha@telamon.ru>
2012-04-18 01:09:33 -0700

    Include new scripts for /usr/bin and use builder account. http://ablab.myjetbrains.com/youtrack/issue/Assembler-158

--- make-rpm.sh
+++ make-rpm.sh
@@ -1,14 +1,15 @@
- rpmdev-setuptree
- cp spades.spec rpmbuild/SPECS
- cd rpmbuild/SOURCES   
- scp yasha@192.168.222.223:spades2/spades_2.0.0.tar.gz .
- tar -xvf spades_2.0.0.tar.gz
- cd spades-2.0.0
- touch configure
- chmod a+x configure 
- cd ..
- rm spades_2.0.0.tar.gz
- tar -czvf spades_2.0.0.tar.gz spades-2.0.0
- cd ../SPECS
- rpmbuild -ba spades.spec
- scp ../RPMS/x86_64/spades-2.0.0-1.fc16.x86_64.rpm yasha@192.168.222.223:rpm
+rm -r rpmbuild
+rpmdev-setuptree
+cp spades.spec rpmbuild/SPECS
+cd rpmbuild/SOURCES   
+scp builder@192.168.222.254:spades2.0.0/spades_2.0.0.tar.gz .
+tar -xvf spades_2.0.0.tar.gz
+cd spades-2.0.0
+touch configure
+chmod a+x configure 
+cd ..
+rm spades_2.0.0.tar.gz
+tar -czvf spades_2.0.0.tar.gz spades-2.0.0
+cd ../SPECS
+rpmbuild -ba spades.spec
+scp ../RPMS/x86_64/spades-2.0.0-1.fc16.x86_64.rpm builder@192.168.222.254:rpm
