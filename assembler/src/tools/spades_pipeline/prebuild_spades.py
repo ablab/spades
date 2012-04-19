a164a57660cd90d17b393fd8b350d8a311334b71
Valery Lesin <Valery.Lesin@gmail.com>
2012-04-19 17:44:38 +0400

    prebuild_spades now builds static hammer too

--- assembler/src/tools/spades_pipeline/prebuild_spades.py
+++ assembler/src/tools/spades_pipeline/prebuild_spades.py
@@ -16,23 +16,23 @@ def write_k_file(kFile, k):
 
 def safe_mkdir(path):
     if not os.path.exists(path):
-        os.mkdir(path)
+        os.makedirs(path)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print('Usage prebuild_spades.py <path to copy to> <from> <to>')
+    if len(sys.argv) != 3:
+        print('Usage prebuild_spades.py <from> <to>')
         exit(1)
 
-    safe_mkdir('build')
     safe_mkdir('build/static')
-    
     support.sys_call('cmake ../../src -Dstatic_build=\"\"', './build/static')
 
-    prebuild_folder = sys.argv[1]
+    prebuild_folder = 'prebuild_spades'
     safe_mkdir(prebuild_folder)
 
-    from_k = int(sys.argv[2])
-    to_k   = int(sys.argv[3]) + 2
+    #building SPAdes
+
+    from_k = int(sys.argv[1])
+    to_k   = int(sys.argv[2]) + 1
 
     for i in range(from_k, to_k, 2):
 
@@ -41,7 +41,17 @@ if __name__ == "__main__":
         write_k_file(os.path.join(os.getcwd(), "src/debruijn/k.hpp"), i)
         support.sys_call('make -C build/static/debruijn')
 
-        new_folder = os.path.join(prebuild_folder, 'K') + str(i)
+        new_folder = os.path.join(prebuild_folder, 'bin/K') + str(i)
         safe_mkdir(new_folder)
 
         shutil.copy2('build/static/debruijn/spades', new_folder)
+
+    #building BayesHammer
+    support.sys_call('make -C build/static/hammer hammer')
+    new_folder = os.path.join(prebuild_folder, 'bayeshammer')
+    safe_mkdir(new_folder)
+    shutil.copy2('build/static/hammer/hammer', new_folder)
+
+
+
+
