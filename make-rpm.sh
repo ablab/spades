bab0f653deae25310caf533dabf4e5ad9d02bf00
Yakov Sirotkin <yasha@telamon.ru>
2012-04-26 08:43:21 -0700

    Switch to 2.0.1. http://ablab.myjetbrains.com/youtrack/issue/Assembler-205

--- make-rpm.sh
+++ make-rpm.sh
@@ -2,14 +2,14 @@ rm -r rpmbuild
 rpmdev-setuptree
 cp spades.spec rpmbuild/SPECS
 cd rpmbuild/SOURCES   
-scp builder@192.168.222.254:spades2.0.0/spades_2.0.0.tar.gz .
-tar -xvf spades_2.0.0.tar.gz
-cd spades-2.0.0
+scp builder@192.168.222.254:spades2.0.1/spades_2.0.1.tar.gz .
+tar -xvf spades_2.0.1.tar.gz
+cd spades-2.0.1
 touch configure
 chmod a+x configure 
 cd ..
-rm spades_2.0.0.tar.gz
-tar -czvf spades_2.0.0.tar.gz spades-2.0.0
+rm spades_2.0.1.tar.gz
+tar -czvf spades_2.0.1.tar.gz spades-2.0.1
 cd ../SPECS
 rpmbuild -ba spades.spec
-scp ../RPMS/x86_64/spades-2.0.0-1.fc16.x86_64.rpm builder@192.168.222.254:rpm
+scp ../RPMS/x86_64/spades-2.0.1-1.fc16.x86_64.rpm builder@192.168.222.254:rpm
