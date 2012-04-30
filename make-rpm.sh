b8a8a1f0924bcc1910a282e6ff502f48e6023851
Yakov Sirotkin <yasha@telamon.ru>
2012-04-30 17:28:47 +0400

    We need to copy files. http://ablab.myjetbrains.com/youtrack/issue/Assembler-204

--- make-rpm.sh
+++ make-rpm.sh
@@ -1,7 +1,7 @@
 set -e -x
 rm -r rpmbuild
 rpmdev-setuptree
-#scp builder@192.168.222.254:release$1/spades.spec .
+scp builder@192.168.222.254:release$1/spades.spec .
 cp spades.spec rpmbuild/SPECS
 cd rpmbuild/SOURCES   
 scp builder@192.168.222.254:release$1/spades_$1.tar.gz .
@@ -14,4 +14,4 @@ rm spades_*.tar.gz
 tar -czvf spades_$1.tar.gz spades-$1
 cd ../SPECS
 rpmbuild --define 'version '$1 -ba spades.spec
-#scp ../RPMS/x86_64/spades-$1-1.fc16.x86_64.rpm builder@192.168.222.254:rpm
+scp ../RPMS/x86_64/spades-$1-1.fc16.x86_64.rpm builder@192.168.222.254:rpm
