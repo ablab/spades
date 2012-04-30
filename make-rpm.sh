15e4a824cd366982edc434674b70c6f2bb1076cd
Yakov Sirotkin <yasha@telamon.ru>
2012-04-30 17:29:52 +0400

    Pass version as the parameter to the script. http://ablab.myjetbrains.com/youtrack/issue/Assembler-204

--- make-rpm.sh
+++ make-rpm.sh
@@ -1,15 +1,17 @@
+set -e -x
 rm -r rpmbuild
 rpmdev-setuptree
+scp builder@192.168.222.254:release$1/spades.spec .
 cp spades.spec rpmbuild/SPECS
 cd rpmbuild/SOURCES   
-scp builder@192.168.222.254:spades2.0.0/spades_2.0.0.tar.gz .
-tar -xvf spades_2.0.0.tar.gz
-cd spades-2.0.0
+scp builder@192.168.222.254:release$1/spades_$1.tar.gz .
+tar -xvf spades_$1.tar.gz
+cd spades-$1
 touch configure
 chmod a+x configure 
 cd ..
-rm spades_2.0.0.tar.gz
-tar -czvf spades_2.0.0.tar.gz spades-2.0.0
+rm spades_*.tar.gz
+tar -czvf spades_$1.tar.gz spades-$1
 cd ../SPECS
-rpmbuild -ba spades.spec
-scp ../RPMS/x86_64/spades-2.0.0-1.fc16.x86_64.rpm builder@192.168.222.254:rpm
+rpmbuild --define 'version '$1 -ba spades.spec
+scp ../RPMS/x86_64/spades-$1-1.fc16.x86_64.rpm builder@192.168.222.254:rpm
