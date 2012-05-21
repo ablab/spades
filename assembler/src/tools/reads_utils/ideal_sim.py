fc6ecc8bd591cf385333ec8d76dd73dadf9dd89a
Nikolay Vyahhi <vyahhi@gmail.com>
2012-05-21 12:09:57 +0400

    py script to simulate ideal reads

---
+++ assembler/src/tools/reads_utils/ideal_sim.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# makes single reads of length RL
+RL = 220
+
+f = open('MG1655-K12.fasta')
+f.readline()
+s = ''
+for line in f:
+  s += line.strip()
+l = len(s)
+s += s[:RL] # make it circular
+for i in xrange(l):
+  print '@read%d' % i 
+  print s[i:i+RL] 
+  print '+'
+  print 'B' * RL
