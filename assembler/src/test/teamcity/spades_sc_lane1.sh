d9e889a87fe631772b9daccaaf213f024400c4b4
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2012-05-23 15:46:06 +0400

    teamcity sc test: 2 misassemblies are ok

--- assembler/src/test/teamcity/spades_sc_lane1.sh
+++ assembler/src/test/teamcity/spades_sc_lane1.sh
@@ -10,5 +10,5 @@ set -e
 pushd ../../../
 rm -f spades_output/ECOLI_SC_LANE_1_BH_woHUMAN/latest
 ./spades.py src/test/teamcity/spades_config_sc_lane1.info
-python src/test/teamcity/assess.py spades_output/ECOLI_SC_LANE_1_BH_woHUMAN/latest/quality_results/all.tsv 47000 1
+python src/test/teamcity/assess.py spades_output/ECOLI_SC_LANE_1_BH_woHUMAN/latest/quality_results/all.tsv 47000 2
 popd
