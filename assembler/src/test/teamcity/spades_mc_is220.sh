4ed643c45d84ecde97fd0f8f944466cb305dab72
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2012-05-28 17:41:07 +0400

    teamcity fix in accord with quality tool changes

--- assembler/src/test/teamcity/spades_mc_is220.sh
+++ assembler/src/test/teamcity/spades_mc_is220.sh
@@ -10,5 +10,5 @@ set -e
 pushd ../../../
 rm -f spades_output/ECOLI_IS220_QUAKE/latest
 ./spades.py src/test/teamcity/spades_config_mc_is220.info
-python src/test/teamcity/assess.py spades_output/ECOLI_IS220_QUAKE/latest/quality_results/all.tsv 80000 3
+python src/test/teamcity/assess.py spades_output/ECOLI_IS220_QUAKE/latest/quality_results/transposed_report.tsv 80000 3
 popd
--- assembler/src/test/teamcity/spades_sc_lane1.sh
+++ assembler/src/test/teamcity/spades_sc_lane1.sh
@@ -10,5 +10,5 @@ set -e
 pushd ../../../
 rm -f spades_output/ECOLI_SC_LANE_1_BH_woHUMAN/latest
 ./spades.py src/test/teamcity/spades_config_sc_lane1.info
-python src/test/teamcity/assess.py spades_output/ECOLI_SC_LANE_1_BH_woHUMAN/latest/quality_results/all.tsv 80000 3
+python src/test/teamcity/assess.py spades_output/ECOLI_SC_LANE_1_BH_woHUMAN/latest/quality_results/transposed_report.tsv 80000 3
 popd
