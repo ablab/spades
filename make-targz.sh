4a8fe07399399187f685a0e1a7366ca9b587eed8
Yakov Sirotkin <yasha@telamon.ru>
2012-06-14 00:16:50 -0700

    Don't copy all src folder. http://ablab.myjetbrains.com/youtrack/issue/Assembler-261

--- make-targz.sh
+++ make-targz.sh
@@ -2,8 +2,16 @@ rm -r spades-*
 rm spades_*
 
 VERSION="$(cat assembler/VERSION)"
-mkdir spades-$VERSION
-cp -r assembler/src spades-$VERSION/src
+mkdir -p spades-$VERSION/src/tools
+
+cp -r assembler/src/debruijn spades-$VERSION/src/debruijn
+cp -r assembler/src/hammer spades-$VERSION/src/hammer
+cp -r assembler/src/include spades-$VERSION/src/include
+cp -r assembler/src/io spades-$VERSION/src/io
+cp -r assembler/src/tools/quality spades-$VERSION/src/tools/quality
+cp -r assembler/src/tools/spades_pipeline spades-$VERSION/src/tools/spades_pipeline
+cp assembler/src/CMakeLists.txt spades-$VERSION/src/CMakeLists.txt
+
 cp -r assembler/configs spades-$VERSION/configs
 cp -r assembler/debian spades-$VERSION/debian
 cp -r assembler/ext spades-$VERSION/ext
@@ -22,36 +30,10 @@ cp assembler/manual.html spades-$VERSION
 cp assembler/quality.html spades-$VERSION
 
 cd spades-$VERSION
-rm src/CMakeListsInternal.txt
 touch src/CMakeListsInternal.txt
-rm -r src/abruijn
-rm -r src/test
 rm -r configs/debruijn/datasets_archive
 rm -r configs/debruijn/datasets
 find . -name ".?*" | xargs rm -r
-rm -r src/tools/blast-fasta
-rm -r src/tools/clean_contaminations
-rm -r src/tools/consensus
-rm -r src/tools/contig_analysis
-rm -r src/tools/cuckoo_test
-rm -r src/tools/divide_fastq
-rm -r src/tools/estimation
-rm -r src/tools/estimation_deprecated
-rm -r src/tools/filter
-rm -r src/tools/kmerstats
-rm -r src/tools/mapreads
-rm -r src/tools/maps_test
-rm -r src/tools/quake-correct
-rm -r src/tools/ukonnen
-rm -r src/tools/copy_ungzip.sh
-rm -r src/tools/dot_conv.sh
-rm -r src/tools/run_velvet_unpaired.sh
-rm -r src/tools/run_velvet2.sh
-rm -r src/tools/quality/libs/mauve
-rm -r src/tools/quality/libs/gage
-rm -r src/tools/quality/libs/genemark_suite_linux_64
-rm -r src/tools/quality/libs/report
-rm -r src/tools/spades_build/
 
 cp configs/debruijn/config.info.template configs/debruijn/config.info
 cp configs/hammer/config.info.template configs/hammer/config.info
