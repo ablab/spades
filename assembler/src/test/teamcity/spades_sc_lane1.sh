cca544d9458e04a8f35b99792fa3492877f98d20
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2012-07-06 18:53:03 +0400

    TeamCity: new thresholds for sc_lane1 - 85kbp for N50, 6 for misassembles

--- assembler/src/test/teamcity/spades_sc_lane1.sh
+++ assembler/src/test/teamcity/spades_sc_lane1.sh
@@ -10,5 +10,5 @@ set -e
 pushd ../../../
 rm -f spades_output/ECOLI_SC_LANE_1_BH_woHUMAN/latest
 ./spades.py src/test/teamcity/spades_config_sc_lane1.info
-python src/test/teamcity/assess.py spades_output/ECOLI_SC_LANE_1_BH_woHUMAN/latest/quality_results/transposed_report.tsv 80000 4
+python src/test/teamcity/assess.py spades_output/ECOLI_SC_LANE_1_BH_woHUMAN/latest/quality_results/transposed_report.tsv 85000 6
 popd
