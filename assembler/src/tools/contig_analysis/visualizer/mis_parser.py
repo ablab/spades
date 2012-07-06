313aabab5154333896af33c9a48ed8dc31965c5e
Alexey Gurevich <alexeigurevich@gmail.com>
2012-07-06 16:11:12 +0400

    improved plantagora output parser: reports all alignments in ext. misassemblies http://ablab.myjetbrains.com/youtrack/issue/Assembler-282

--- assembler/src/tools/contig_analysis/visualizer/mis_parser.py
+++ assembler/src/tools/contig_analysis/visualizer/mis_parser.py
@@ -11,11 +11,14 @@ if len(sys.argv) < 2:
 
 in_file = open(sys.argv[1], "r")
 
-outFileName, ext = os.path.splitext(sys.argv[1])
-outFileName += ".txt.mis"
-outFile = open(outFileName, "w")
+basename, ext = os.path.splitext(sys.argv[1])
+out_IDs_filename = basename + ".txt.mis"
+out_aligns_filename = basename + ".aligns"
+out_IDs_file = open(out_IDs_filename, "w")
+out_aligns_file = open(out_aligns_filename, "w")
 
 mis_contigs_ids = []
+mis_contigs_aligns = []
 
 #skipping prologue
 for line in in_file:
@@ -24,20 +27,38 @@ for line in in_file:
 
 # main part of plantagora output
 cur_contig_id = ""
+cur_contig_align = ""
+was_extensive_mis = False
 for line in in_file:
-    if line.startswith("	CONTIG:"):
-        cur_contig_id = line.split("	CONTIG:")[1].strip()
-    if (line.find("Extensive misassembly") != -1) and (cur_contig_id != ""):
-        mis_contigs_ids.append(cur_contig_id.split()[0])
-        cur_contig_id = ""
     if line.startswith("Analyzing coverage..."):
         break
+
+    if line.startswith("	CONTIG:"):
+        cur_contig_id = line.split("	CONTIG:")[1].strip()
+        if was_extensive_mis:
+            was_extensive_mis = False
+            mis_contigs_aligns.append(cur_contig_align)
+        cur_contig_align = ""
+    cur_contig_align += line
+
+    if (line.find("Extensive misassembly") != -1) and (cur_contig_id != ""):
+        was_extensive_mis = True
+        mis_contigs_ids.append(cur_contig_id.split()[0])
+        cur_contig_id = ""
+
+if was_extensive_mis:
+    mis_contigs_aligns.append(cur_contig_align)
             
 # printing IDs of misassembled contigs
 
+print "IDs of misassembled contigs saved in " + out_IDs_filename
 for contig_id in mis_contigs_ids:
-    outFile.write(contig_id + '\n') 
+    out_IDs_file.write(contig_id + '\n') 
+
+print "Alignments of misassembled contigs saved in " + out_aligns_filename
+for contig_align in mis_contigs_aligns:
+    out_aligns_file.write(contig_align) 
 
 in_file.close()
-outFile.close()
-
+out_IDs_file.close()
+out_aligns_file.close()
