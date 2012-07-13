fb6eed8f48c53291df453d7d4fef259f4275f902
Dmitry <dmitrij.antipov@gmail.com>
2012-07-13 16:25:44 +0400

    read_cropper tool

---
+++ assembler/src/tools/reads_utils/crop/crop_reads.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python -O
+
+import sys
+import os
+
+# crop genome to first n basepairs
+gf = open(sys.argv[1])
+n = int(sys.argv[2])
+ogf = open(sys.argv[3], 'w')
+
+cnt = 0
+for line in gf:
+    line = line.strip()
+    if (n <= 0):
+	break;
+    if n < len(line):
+	line = line[0:n]
+    ogf.write(line)
+    ogf.write('\n')
+   
+
+gf.close()
+ogf.close()
