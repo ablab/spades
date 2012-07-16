71435b8804ee22c48462dd01092c435d807af857
Anton Korobeynikov <anton@korobeynikov.info>
2012-07-16 19:29:08 +0400

    Make sure file is self-contaning

--- assembler/src/include/simple_tools.hpp
+++ assembler/src/include/simple_tools.hpp
@@ -17,6 +17,9 @@
 #include "verify.hpp"
 #include "io/ireader.hpp"
 #include <memory>
+#include <string>
+#include <set>
+#include <vector>
 #include <boost/format.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/filesystem.hpp>
