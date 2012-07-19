d7998762e95044cca9d75e7510ab0753476e5462
Yakov Sirotkin <yasha@telamon.ru>
2012-07-19 06:19:23 -0700

    No need in spades_build_dir. http://ablab.myjetbrains.com/youtrack/issue/Assembler-293

--- assembler/spades_init.py
+++ assembler/spades_init.py
@@ -9,7 +9,6 @@ import sys
 
 spades_home = os.path.abspath(sys.path[0])
 spades_version = ''
-spades_build_dir = os.path.join(spades_home, "build")
 
 def init():
     global spades_home
@@ -17,7 +16,6 @@ def init():
     global spades_compilation_dir
     if spades_home == "/usr/bin":
         spades_home = "/usr/share/spades"
-        spades_build_dir = os.path.join(os.getenv('HOME'), '.spades')
 
     sys.path.append(os.path.join(spades_home, "src/tools/spades_pipeline/"))
     sys.path.append(os.path.join(spades_home, "src/tools/quality/"))
