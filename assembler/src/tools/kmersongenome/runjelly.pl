d3059d08ad9b331fe8edf773973e373cf18a81c4
Sergey Nikolenko <snikolenko@gmail.com>
2012-07-23 17:21:14 +0400

    -- fixed runjelly

--- assembler/src/tools/kmersongenome/runjelly.pl
+++ assembler/src/tools/kmersongenome/runjelly.pl
@@ -6,14 +6,14 @@ use Getopt::Long;
 
 
 if ($#ARGV < 3) {
-	die "\nUsage: runjelly.pl min_k max_k output_prefix input_fasta";
+	die "\nUsage: runjelly.pl min_k max_k output_dir input_fasta";
 }
 
 my $min_k = $ARGV[0];
 my $max_k = $ARGV[1];
 my $pref = $ARGV[2];
 my $inp = $ARGV[3];
-my $mem = "50G";
+my $mem = "2G";
 my $indbytes = "7";
 my $numthreads = "32";
 
@@ -24,7 +24,6 @@ my $result = GetOptions ("-s=s" => \$mem,
 			 "-t=s" => \$numthreads
 );
 
-
 sub my_exec {
 	my $cmd = shift;
 	print $cmd . "\n";
@@ -32,7 +31,12 @@ sub my_exec {
 }
 
 for (my $k = $min_k; $k <= $max_k; $k += 2) {
-	my_exec( "jellyfish count --both-strands -m $k -o $pref$k -c $indbytes -s $mem -t $numthreads $inp" );
+	my_exec("mkdir $pref/$k");
+	my_exec( "jellyfish count --both-strands -m $k -o $pref/$k/jh -c $indbytes -s $mem -t $numthreads $inp" );
+	my_exec( "jellyfish dump -c -t -o $pref/$k/dump.tsv $pref/$k/jh_0" );
+	print "   === k-mer statistics for k=$k ===\n";
+	my_exec( "jellyfish stats $pref/$k/jh_0" );
+	print "   === k-mer statistics for k=$k ===\n";
 }
 
 die("All done.");
