5e9bddb9b328fddf1556c9c390d9f7bcd3451dec
Dmitry Antipov <dmitrij.antipov@gmail.com>
2012-07-23 21:43:13 +0400

    spades_dev.sh fixed

--- assembler/spades_dev.sh
+++ assembler/spades_dev.sh
@@ -4,6 +4,6 @@ make rh;
 mkdir -p bin;
 rm bin/*;
 cp build/release/debruijn/spades bin/spades;
-cp build/release/build_hammer/hammer/hammer bin/hammer;
+cp build/release/hammer/hammer bin/hammer;
 python spades.py "$@";
 
