38ec80511e0bf622622980eca6d4759cc82de180
Anton Korobeynikov <anton@korobeynikov.info>
2012-08-02 12:27:08 +0400

    Make spades_compile.sh build in separate dir. This should unbreak unittests.

--- assembler/spades_compile.sh
+++ assembler/spades_compile.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 PREFIX=`pwd`
+BUILD_DIR=build_spades
 
-rm -rf build
-mkdir -p build
-cd build
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$PREFIX ../src $*
 make
 make install
-cd $PWD
+cd $PREFIX
