a5f8cc0bc10c2114ac8ffba4312c1c73e3dbc6f4
Yakov Sirotkin <yasha@telamon.ru>
2012-08-07 01:43:57 -0700

    *.dataset renamed to dataset.info. http://ablab.myjetbrains.com/youtrack/issue/Assembler-321

--- assembler/src/test/teamcity/bh_ecoli_1k.sh
+++ assembler/src/test/teamcity/bh_ecoli_1k.sh
@@ -10,6 +10,6 @@ set -e
 pushd ../../../
 rm -rf spades_output/ECOLI_1K_TEAMCITY_BH
 ./spades.py src/test/teamcity/bh_config_ecoli_1k.info
-python src/tools/reads_utils/reads_quality.py -r test_dataset/reference_1K.fa.gz -o spades_output/ECOLI_1K_TEAMCITY_BH/reads_quality_results spades_output/ECOLI_1K_TEAMCITY_BH/corrected/ECOLI_1K_TEAMCITY_BH.dataset
+python src/tools/reads_utils/reads_quality.py -r test_dataset/reference_1K.fa.gz -o spades_output/ECOLI_1K_TEAMCITY_BH/reads_quality_results spades_output/ECOLI_1K_TEAMCITY_BH/corrected/dataset.info
 python src/test/teamcity/assess_reads.py spades_output/ECOLI_1K_TEAMCITY_BH/reads_quality_results/all.tsv 100 100
 popd
