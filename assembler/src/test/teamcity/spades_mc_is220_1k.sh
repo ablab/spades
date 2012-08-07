894b1b7b8752fb877bfad8b86ca23da512aff305
Yakov Sirotkin <yasha@telamon.ru>
2012-08-07 05:41:01 -0700

    Do not use latest here. http://ablab.myjetbrains.com/youtrack/issue/Assembler-321

--- assembler/src/test/teamcity/spades_mc_is220_1k.sh
+++ assembler/src/test/teamcity/spades_mc_is220_1k.sh
@@ -8,7 +8,7 @@
 
 set -e
 pushd ../../../
-rm -f spades_output/ECOLI_IS220_QUAKE_1K/latest
+rm -f spades_output/ECOLI_IS220_QUAKE_1K
 ./spades.py src/test/teamcity/spades_config_mc_is220_1k.info
-python src/test/teamcity/assess.py spades_output/ECOLI_IS220_QUAKE_1K/latest/quality_results/transposed_report.tsv 1000 0
+python src/test/teamcity/assess.py spades_output/ECOLI_IS220_QUAKE_1K/quality_results/transposed_report.tsv 1000 0
 popd
