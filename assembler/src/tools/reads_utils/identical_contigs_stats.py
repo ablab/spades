7d8e8476bcfb94e0de3d77e545c913e857e292d1
Dmitry <dmitrij.antipov@gmail.com>
2012-08-08 18:32:18 +0400

    http://ablab.myjetbrains.com/youtrack/issue/Assembler-326

---
+++ assembler/src/tools/reads_utils/identical_contigs_stats.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python -O
+
+#Calculate statistics for reads by reference file
+
+import argparse
+import sys
+import os
+
+if len(sys.argv) != 2:
+        print("Usage: " + sys.argv[0] + "  <contigs_file>")     
+	print("identical contigs names outputs to repeated_contigs_names.txt file")
+        sys.exit()
+
+contFileName = sys.argv[1]
+contFile = open(contFileName, "r")
+outFileName = "repeated_contigs_names.txt";
+outFile = open(outFileName, "w")
+
+
+line = contFile.readline()
+contigs = set([])
+exact_repeats = 0;
+total_contigs = 0;
+while 1:
+	if not line:
+		break
+#	print(line)
+	if (line[0] != '>'):
+		print("something wrong" + str(line[1]))
+		break
+	contig = ""
+	contig_name = line;
+	line = contFile.readline()
+#	print(line);
+	while ((line[0] !='>')):
+		contig += line;
+		line = contFile.readline()		
+	        if not line:
+        	        break
+
+	if (contig in contigs):
+		outFile.write(contig_name)
+		exact_repeats += 1
+	total_contigs += 1
+	contigs.add(contig);	
+print ("Exact repeats: " + str(exact_repeats) + "; total contigs: " + str(total_contigs));	
+
