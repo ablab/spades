f56a8d61f457b1e7aa1a396354e6c86d18191ebb
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2012-08-13 15:18:09 +0400

    Handling compile errors in TC script http://ablab.myjetbrains.com/youtrack/issue/Assembler-315

--- assembler/src/test/teamcity/teamcity.sh
+++ assembler/src/test/teamcity/teamcity.sh
@@ -10,6 +10,7 @@
 echo "### PREPROCESSING ###"
 
 pushd ../../../
+
 ./prepare_cfg
 errlvl=$?
 if [ "$errlvl" -ne 0 ]
@@ -17,9 +18,16 @@ then
     echo "prepare_cfg finished with exit code $errlvl"
     exit $errlvl
 fi
-./spades_compile.sh
-popd
 
+./spades_compile.sh
+errlvl=$?
+if [ "$errlvl" -ne 0 ]
+then
+    echo "spades_compile finished with exit code $errlvl"
+    exit $errlvl
+fi
+
+popd
 pushd ../../../data
 ./link_teamcity.sh
 popd
