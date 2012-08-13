ac37bb9bc5e21fe464469ad7a1224fbb11746bb6
Dmitry Antipov <dmitrij.antipov@gmail.com>
2012-08-13 17:11:58 +0400

    tc fix

--- assembler/src/test/teamcity/spades_bh_ecoli_1k.sh
+++ assembler/src/test/teamcity/spades_bh_ecoli_1k.sh
@@ -8,8 +8,11 @@
 
 set -e
 pushd ../../../
-project_dir="spades_output/ECOLI_1K/"
-options_dir="/storage/data/input/E.coli.K12/1K/"
+project_name="ECOLI_1K/"
+creature_name="E.coli.K12/"
+project_dir="spades_output/"$project_name
+output_pref="/storage/data/"
+options_dir=$output_pref"input/"$creature_name$project_name
 rm -rf $project_dir
 read line < $options_dir"spades.options"
 echo $line
@@ -18,12 +21,16 @@ python ./spades.py $line -o $project_dir
 read line < $options_dir"quast.options"
 echo $line
 
-dir="/storage/data/contigs/teamcity/Ecoli1K/"$(date +%Y%m%d_%H%M%S)"/"
+dir=$output_pref"contigs/teamcity/"$creature_name$project_name #$(date +%Y%m%d_%H%M%S)"/"
 echo $dir
-mkdir $dir
-cp $project_dir"contigs.fasta" $dir
+mkdir $dir -p
+cp $project_dir"contigs.fasta" $dir$(date +%Y%m%d_%H%M%S)".fasta"
 
 python /storage/quast1.0/quast/quast.py $project_dir"contigs.fasta" $line -o $project_dir"/quality_results/"
-python src/test/teamcity/assess.py $project_dir"quality_results/transposed_report.tsv" 1000 0
+#espected results
+read line < $options_dir"results.options"
+echo $line
+opts=( $line )
+python src/test/teamcity/assess.py $project_dir"quality_results/transposed_report.tsv" ${opts[1]} ${opts[3]}
 
 popd
