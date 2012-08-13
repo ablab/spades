05d5497a02ea4062de6c3a9c64aaed924c16398d
Dmitry Antipov <dmitrij.antipov@gmail.com>
2012-08-13 05:16:13 +0400

    tc scripts fx

--- assembler/src/test/teamcity/spades_bh_ecoli_1k.sh
+++ assembler/src/test/teamcity/spades_bh_ecoli_1k.sh
@@ -8,8 +8,8 @@
 
 set -e
 pushd ../../../
-project_dir="spades_output/ECOLI_SC_lane1/"
-options_dir="/storage/data/input/E.coli.K12/mda_lane_1/"
+project_dir="spades_output/ECOLI_1K/"
+options_dir="/storage/data/input/E.coli.K12/1K/"
 rm -rf $project_dir
 read line < $options_dir"spades.options"
 echo $line
@@ -18,12 +18,12 @@ python ./spades.py $line -o $project_dir
 read line < $options_dir"quast.options"
 echo $line
 
-dir="/storage/data/contigs/teamcity/mda_lane_1/"$(date +%Y%m%d_%H%M%S)"/"
+dir="/storage/data/contigs/teamcity/Ecoli1K/"$(date +%Y%m%d_%H%M%S)"/"
 echo $dir
 mkdir $dir
 cp $project_dir"contigs.fasta" $dir
 
 python /storage/quast1.0/quast/quast.py $project_dir"contigs.fasta" $line -o $project_dir"/quality_results/"
-python src/test/teamcity/assess.py $project_dir"quality_results/transposed_report.tsv" 105000 3
+python src/test/teamcity/assess.py $project_dir"quality_results/transposed_report.tsv" 1000 0
 
 popd
