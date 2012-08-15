45092d93cc0420419400b007c6306595fa77c16d
Dmitry Antipov <dantipov@debruijn.ucsd.edu>
2012-08-15 09:13:34 -0700

    move fullruns to debruijn

--- assembler/src/test/teamcity/full_run.sh
+++ assembler/src/test/teamcity/full_run.sh
@@ -11,7 +11,7 @@ pushd ../../../
 project_name=$2
 creature_name=$1
 project_dir="spades_output/"$creature_name$project_name
-output_pref="/storage/data/"
+output_pref="/home/dantipov/"
 options_dir=$output_pref"input/"$creature_name$project_name
 rm -rf $project_dir
 mkdir $project_dir -p
@@ -28,7 +28,7 @@ echo $dir
 mkdir $dir -p
 cp $project_dir"contigs.fasta" $dir$(date +%Y%m%d_%H%M%S)".fasta"
 
-python /storage/quast1.0/quast/quast.py $project_dir"contigs.fasta" $line -o $project_dir"/quality_results/"
+python2.6 ~/quast/quast.py $project_dir"contigs.fasta" $line -o $project_dir"/quality_results/"
 #espected results
 read line < $options_dir"results.options"
 echo $line
