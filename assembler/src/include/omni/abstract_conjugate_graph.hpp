2d80b0e8e962b811f46f0dbfd773fa4c41ef3c64
Anton <anton.bankevich@gmail.com>
2012-08-17 20:02:16 +0400

    self rc fix with edge adding

--- assembler/src/include/omni/abstract_conjugate_graph.hpp
+++ assembler/src/include/omni/abstract_conjugate_graph.hpp
@@ -229,8 +229,8 @@ private:
 		TRACE("Adding edge between verteices " << v1 << " and " << v2);
 		VERIFY(this->vertices_.find(v1) != this->vertices_.end() && this->vertices_.find(v2) != this->vertices_.end());
 		EdgeId result = AddSingleEdge(v1, v2, data);
-		if (this->master().isSelfConjugate(data)) {
-			VERIFY(v1 == conjugate(v2));
+		if (this->master().isSelfConjugate(data) && v1 == conjugate(v2)) {
+//	VERIFY(v1 == conjugate(v2));
 			result->set_conjugate(result);
 			return result;
 		}
