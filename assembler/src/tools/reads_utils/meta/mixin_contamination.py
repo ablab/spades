4e5b173b1d9a67183e9ca5ff2e934f263bc29e89
Dmitry <dmitrij.antipov@gmail.com>
2012-09-10 01:37:35 +0400

    mixing script modified

--- assembler/src/tools/reads_utils/meta/mixin_contamination.py
+++ assembler/src/tools/reads_utils/meta/mixin_contamination.py
@@ -6,16 +6,20 @@ import sys
 import os
 import random
 
-if len(sys.argv) != 5:
-	print("Usage: " + sys.argv[0] + " <part> <contamination_reads> <main_reads> <output_file>")	
+if len(sys.argv) < 6 or (len(sys.argv) %2 != 0) :
+	print("Usage: " + sys.argv[0] + " {<part> <contamination_reads>}   <output_file>")
 	print ("add 1/<part> reads from contamination_reads to main reads")
 	sys.exit()
+len_arg = len(sys.argv)
+file_names = []
+files = []
+parts = []
+for i in range(0, len/2 - 1 ):
+    file_names.append(sys.argv[i + 2])
+    parts.append(sys.argv[i + 1])
+    files.append(open(file_names[i + 2]))
 
-contFileName = sys.argv[2]
-contFile = open(contFileName, "r")
-mainFileName = sys.argv[3]
-mainFile = open(mainFileName, "r")
-outFileName = sys.argv[4]
+outFileName = sys.argv[len_arg-1];
 outFile = open(outFileName, "w")
 
 threshold = 1/float(sys.argv[1])
@@ -27,22 +31,19 @@ contName, context = os.path.splitext(contFileName)
 
 #outFile = open(mainName + "+"+ str(threshold) + "* contamunation"  + ext, "w") 
 line = contFile.readline()
-while 1:
-	if not line:
-		break
-	out = random.random() < threshold
-	for i in range(8):
-		if out:
-			outFile.write(line)
-		line = contFile.readline()
+for i in range(len(file_names)):
 
+    while 1:
+        if not line:
+            break
+        out = random.random() < (1/float(parts[i]))
+        for i in range(8):
+            if out:
+                outFile.write(line)
+            line = files.readline()
 
+    files[i].close()
 
-line = mainFile.readline()
-
-while line:
-	outFile.write(line)
-	line = mainFile.readline()
 
 
 #c_len, contig = read_contig(inFile)
@@ -55,8 +56,6 @@ while line:
 #	c_len, contig = read_contig(inFile)
 
 
-contFile.close()
-mainFile.close()
 outFile.close()
 
 
