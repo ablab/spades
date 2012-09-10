4e93697d879bdb523bc870ecad5720542f2cab5f
Anton Korobeynikov <anton@korobeynikov.info>
2012-09-10 15:50:56 +0400

    Generalize

--- assembler/src/hammer/pointer_iterator.hpp
+++ assembler/src/hammer/pointer_iterator.hpp
@@ -11,6 +11,7 @@ template<typename T>
 class pointer_iterator : public std::iterator<std::random_access_iterator_tag, T> {
 protected:
   T *data_;
+  size_t stride_;
 
 public:
   typedef std::random_access_iterator_tag iterator_category;
@@ -19,51 +20,58 @@ public:
   typedef typename std::iterator<std::random_access_iterator_tag, T>::reference reference;
   typedef typename std::iterator<std::random_access_iterator_tag, T>::pointer   pointer;
 
-  pointer_iterator() : data_(NULL) {}
+  pointer_iterator() : data_(NULL), stride_(1) {}
 
   template<typename T2>
-  pointer_iterator(const pointer_iterator<T2> &r) : data_(&(*r)) {}
+  pointer_iterator(const pointer_iterator<T2> &r) : data_(&(*r)), stride_(r.stride_) {}
 
-  pointer_iterator(pointer data) : data_(data) {}
+  pointer_iterator(pointer data, size_t stride = 1) : data_(data), stride_(stride) {}
 
   template<typename T2>
   pointer_iterator& operator=(const pointer_iterator<T2> &r) {
     data_ = &(*r);
+    stride_ = r.stride_;
     return *this;
   }
 
   pointer_iterator& operator++() {
-    ++data_;
+    data_ += stride_;
     return *this;
   }
 
   pointer_iterator& operator--() {
-    --data_;
+    data_ -= stride_;
     return *this;
   }
 
   pointer_iterator operator++(int) {
-    return pointer_iterator(data_++);
+    pointer_iterator res = *this;
+    data_ += stride_;
+
+    return res;
   }
 
   pointer_iterator operator--(int) {
-    return pointer_iterator(data_--);
+    pointer_iterator res = *this;
+    data_ -= stride_;
+
+    return res;
   }
 
   pointer_iterator operator+(const difference_type &n) const {
-    return pointer_iterator(data_ + n);
+    return pointer_iterator(data_ + n*stride_);
   }
 
   pointer_iterator& operator+=(const difference_type &n) {
-    data_ += n; return *this;
+    data_ += n*stride_; return *this;
   }
 
   pointer_iterator operator-(const difference_type &n) const {
-    return pointer_iterator(pointer(data_ - n));
+    return pointer_iterator(pointer(data_ - n*stride_));
   }
 
   pointer_iterator& operator-=(const difference_type &n) {
-    data_ -= n; return *this;
+    data_ -= n*stride_; return *this;
   }
 
   reference operator*() const {
@@ -75,7 +83,7 @@ public:
   }
 
   reference operator[](const difference_type &n) const {
-    return data_[n];
+    return data_[n*stride_];
   }
 
   template<typename T2>
@@ -116,13 +124,15 @@ public:
 template<typename T>
 inline bool operator==(const pointer_iterator<T> &r1,
                        const pointer_iterator<T> &r2) {
-  return (r1.data_ == r2.data_);
+  return (r1.data_ == r2.data_ &&
+          r1.stride_ == r2.stride_);
 }
 
 template<typename T>
 inline bool operator!=(const pointer_iterator<T> &r1,
                        const pointer_iterator<T> &r2) {
-  return (r1.data_ != r2.data_);
+  return (r1.data_ != r2.data_ ||
+          r1.stride_ != r2.stride_);
 }
 
 template<typename T>
@@ -151,16 +161,9 @@ inline bool operator>=(const pointer_iterator<T> &r1,
 
 template<typename T>
 inline typename pointer_iterator<T>::difference_type
-operator+(const pointer_iterator<T> &r1,
-          const pointer_iterator<T> &r2) {
-  return pointer_iterator<T>(r1.data_ + r2.data_);
-}
-
-template<typename T>
-inline typename pointer_iterator<T>::difference_type
 operator-(const pointer_iterator<T> &r1,
           const pointer_iterator<T> &r2) {
-  return r1.data_ - r2.data_;
+  return (r1.data_ - r2.data_) / r1.stride_;
 }
 
 #endif // __HAMMER_POINTER_ITERATOR_HPP__
