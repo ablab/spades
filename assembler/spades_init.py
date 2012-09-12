b0d9c55a995278e3437b0b94f8685e30c6942d2f
Yakov Sirotkin <Yakov Sirotkin@.(none)>
2012-09-12 10:59:39 +0400

    undo previous commit with wrong message

--- assembler/spades_init.py
+++ assembler/spades_init.py
@@ -18,5 +18,7 @@ def init():
         spades_home = "/usr/share/spades"
 
     sys.path.append(os.path.join(spades_home, "src/tools/spades_pipeline/"))
+    sys.path.append(os.path.join(spades_home, "src/tools/quality/"))
+    sys.path.append(os.path.join(spades_home, "src/tools/quality/libs"))
 
     spades_version =  open(os.path.join(spades_home, 'VERSION'), 'r').readline()
