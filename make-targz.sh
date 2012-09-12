65a57234d899bbcc627a736d4e516668f792beb2
Yakov Sirotkin <Yakov Sirotkin@.(none)>
2012-09-12 11:05:21 +0400

    Remove quality support and other unused files. http://ablab.myjetbrains.com/youtrack/issue/Assembler-342

--- make-targz.sh
+++ make-targz.sh
@@ -8,7 +8,6 @@ cp -r assembler/src/hammer spades-$VERSION/src/hammer
 cp -r assembler/src/include spades-$VERSION/src/include
 cp -r assembler/src/io spades-$VERSION/src/io
 cp -r assembler/src/cmake spades-$VERSION/src/cmake
-cp -r assembler/src/tools/quality spades-$VERSION/src/tools/quality
 cp -r assembler/src/tools/spades_pipeline spades-$VERSION/src/tools/spades_pipeline
 cp assembler/src/CMakeLists.txt spades-$VERSION/src/CMakeLists.txt
 
@@ -31,12 +30,13 @@ cp assembler/spades_download_binary.py spades-$VERSION
 cp assembler/spades_compile.sh spades-$VERSION
 cp assembler/spades_init.py spades-$VERSION
 cp assembler/manual.html spades-$VERSION
-cp assembler/quality.html spades-$VERSION
 
 cd spades-$VERSION
 touch src/CMakeListsInternal.txt
 rm -r configs/debruijn/datasets_archive
 rm -r configs/debruijn/datasets
+rm  configs/debruijn/datasets*
+rm  configs/debruijn/deprecated*
 find . -name ".?*" | xargs rm -r
 
 cp configs/debruijn/config.info.template configs/debruijn/config.info
@@ -44,8 +44,6 @@ cp configs/hammer/config.info.template configs/hammer/config.info
 cp configs/debruijn/detail_info_printer.info.template configs/debruijn/detail_info_printer.info
 cp configs/debruijn/simplification.info.template configs/debruijn/simplification.info
 cp configs/debruijn/distance_estimation.info.template configs/debruijn/distance_estimation.info
-cp configs/debruijn/long_contigs/lc_config.info.template configs/debruijn/long_contigs/lc_config.info
-cp configs/debruijn/long_contigs/lc_params.info.template configs/debruijn/long_contigs/lc_params.info
 cp configs/debruijn/path_extend/pe_params.info.template configs/debruijn/path_extend/pe_params.info
 
 cd ..
