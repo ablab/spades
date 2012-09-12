717dd153eae1a3a4e6aeda1468a9098a08ce136b
Alexey Gurevich <alexeigurevich@gmail.com>
2012-09-12 16:47:23 +0400

    Another script for merging repeated nucleotides in a single nucl

---
+++ assembler/src/tools/reads_utils/conversion/fasta_merge_repeated_nucleotides.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# Convert contigs (i.e a reference) for experiment of running SPAdes on E. coli MC reads in "IonTorrent" mode
+# (all series of repeated nucleotides are changed to single nucleotides).
+
+import sys
+import os
+import fastaparser
+
+# MAIN
+if len(sys.argv) < 3:
+	print("Usage: " + sys.argv[0] + " <input fasta> <output fasta>")	
+	sys.exit()
+
+new_fasta = []
+for name, seq in fastaparser.read_fasta(sys.argv[1]): 
+    new_seq = seq[0]
+    for i in range(1, len(seq)):
+        if seq[i - 1] != seq[i]:
+            new_seq += seq[i]
+    new_fasta.append((name, new_seq))
+
+fastaparser.write_fasta_to_file(sys.argv[2], new_fasta)
