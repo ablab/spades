c32a7d73fea13b126f36559f3cd349a8c16d5d7f
Sergey Nurk <sergeynurk@gmail.com>
2012-10-17 21:07:05 +0400

    100k diffs test now recompiles spades and uses detect_diffs.sh

--- assembler/src/test/teamcity/spades_mc_is220_100k_saves.sh
+++ assembler/src/test/teamcity/spades_mc_is220_100k_saves.sh
@@ -8,35 +8,43 @@
 
 set -e
 pushd ../../../
-rm -rf spades_output/ECOLI_IS220_QUAKE_100K_SAVES
-#etalon=spades_output/ECOLI_IS220_QUAKE_100K_SAVES/etalon
+
+target=spades_output/ECOLI_IS220_QUAKE_100K_SAVES
+if [ -d $target ]; then
+	rm -rf $target #etalon=spades_output/ECOLI_IS220_QUAKE_100K_SAVES/etalon
+fi	
+
 etalon=/smallnas/teamcity/etalon_output/ECOLI_IS220_QUAKE_100K_SAVES/etalon
-if [ ! -d $etalon ]; then
-    echo "Error: no etalon saves at $etalon"
-    exit 9
-fi
+
+#if [ ! -d $etalon ]; then
+#    echo "Error: no etalon saves at $etalon"
+#    exit 9
+#fi
+
+./spades_compile.sh
 ./spades.py --config-file src/test/teamcity/spades_config_mc_is220_100k_saves.info
 
-pushd spades_output/ECOLI_IS220_QUAKE_100K_SAVES
-diffs=0
-    for f in saves/*
-    do
-        set +e
-        diff $f $etalon/link_K55/$f >> diff_with_etalon.txt
-        errlvl=$?
-        if [ $errlvl -ne 0 ]; then
-            if [ $errlvl -eq 1 ]; then
-                echo "^^^^^^^ it was $f" >> diff_with_etalon.txt
-                echo "BAD: difference found in $f"
-            else
-                echo "BAD: unable to compare with $f"
-            fi
-            (( diffs += 1 ))
-        fi
-        set -e
-    done
-popd
+#pushd spades_output/ECOLI_IS220_QUAKE_100K_SAVES
+#diffs=0
+#    for f in saves/*
+#    do
+#        set +e
+#        diff $f $etalon/link_K55/$f >> diff_with_etalon.txt
+#        errlvl=$?
+#        if [ $errlvl -ne 0 ]; then
+#            if [ $errlvl -eq 1 ]; then
+#                echo "^^^^^^^ it was $f" >> diff_with_etalon.txt
+#                echo "BAD: difference found in $f"
+#            else
+#                echo "BAD: unable to compare with $f"
+#            fi
+#            (( diffs += 1 ))
+#        fi
+#        set -e
+#    done
+#popd
 popd
 
-echo $diffs differences with etalon saves found
-exit $diffs
+#echo $diffs differences with etalon saves found
+./detect_diffs.sh ../../../$target $etalon
+exit $?
