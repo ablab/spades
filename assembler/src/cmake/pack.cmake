c021c6aa2645540d37965b4a09a72a2cb4e57910
Anton Korobeynikov <anton@korobeynikov.info>
2012-10-23 21:29:47 +0400

    Some nifty stuff for source releases

--- assembler/src/cmake/pack.cmake
+++ assembler/src/cmake/pack.cmake
@@ -1,5 +1,6 @@
 # -*- cmake -*-
 
+# Binary stuff
 set(CPACK_CMAKE_GENERATOR "Unix Makefiles")
 if (APPLE)
   set(CPACK_GENERATOR "STGZ;TGZ;TBZ2;PackageMaker")
@@ -13,6 +14,11 @@ set(CPACK_PACKAGE_VERSION "2.3.0")
 set(CPACK_PACKAGE_VERSION_MAJOR "2")
 set(CPACK_PACKAGE_VERSION_MINOR "3")
 set(CPACK_PACKAGE_VERSION_PATCH "0")
+set(CPACK_STRIP_FILES bin/debruijn bin/hammer)
 
+# Source stuff
+set(CPACK_SOURCE_GENERATOR "TBZ2")
+set(CPACK_SOURCE_IGNORE_FILES tools test web_service online_vis cap)
+set(CPACK_SOURCE_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
 
 include(CPack)
