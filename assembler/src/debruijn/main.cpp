904a5fa6d1613b88f02817ff137e60da01e7020a
Anton Korobeynikov <anton@korobeynikov.info>
2012-11-01 15:48:49 +0400

    Cleanup

--- assembler/src/debruijn/main.cpp
+++ assembler/src/debruijn/main.cpp
@@ -30,89 +30,82 @@
 
 #include "runtime_k.hpp"
 
-void link_output(std::string const& link_name)
-{
-    if (!cfg::get().run_mode)
-        return;
+void link_output(std::string const& link_name) {
+  if (!cfg::get().run_mode)
+    return;
 
-    std::string link = cfg::get().output_root + link_name;
-    unlink(link.c_str());
-    if (symlink(cfg::get().output_suffix.c_str(), link.c_str()) != 0)
-        WARN( "Symlink to \"" << link << "\" launch failed");
+  std::string link = cfg::get().output_root + link_name;
+  unlink(link.c_str());
+  if (symlink(cfg::get().output_suffix.c_str(), link.c_str()) != 0)
+    WARN( "Symlink to \"" << link << "\" launch failed");
 }
 
-void link_previous_run(std::string const& previous_link_name, std::string const& link_name){
-   if (!cfg::get().run_mode)
-       return;
+void link_previous_run(std::string const& previous_link_name, std::string const& link_name) {
+  if (!cfg::get().run_mode)
+    return;
 
-   char buf[255];
+  char buf[255];
 
-   std::string link = cfg::get().output_dir + previous_link_name;
-   unlink(link.c_str());
-   int count = readlink((cfg::get().output_root + link_name).c_str(), buf, sizeof(buf) - 1);
-   if (count >= 0){
-       buf[count] = '\0';
-       std::string previous_run("../");
-       previous_run = previous_run + buf;
-       if (symlink(previous_run.c_str(), link.c_str()) != 0) {
-           DEBUG( "Symlink to \"" << link << "\" launch failed : " << previous_run);
-       }
-   } else {
-	   DEBUG( "Symlink to \"" << link << "\" launch failed");
-   }
+  std::string link = cfg::get().output_dir + previous_link_name;
+  unlink(link.c_str());
+  int count = readlink((cfg::get().output_root + link_name).c_str(), buf, sizeof(buf) - 1);
+  if (count >= 0){
+    buf[count] = '\0';
+    std::string previous_run("../");
+    previous_run = previous_run + buf;
+    if (symlink(previous_run.c_str(), link.c_str()) != 0) {
+      DEBUG( "Symlink to \"" << link << "\" launch failed : " << previous_run);
+    }
+  } else {
+    DEBUG( "Symlink to \"" << link << "\" launch failed");
+  }
 }
 
-struct on_exit_output_linker
-{
-    on_exit_output_linker(std::string const& link_name) :
-            link_name_(link_name)
-    {
-    }
+struct on_exit_output_linker {
+  on_exit_output_linker(std::string const& link_name) :
+      link_name_(link_name) { }
 
-    ~on_exit_output_linker()
-    {
-        link_previous_run("previous", link_name_);
-        link_output(link_name_);
-    }
+  ~on_exit_output_linker() {
+    link_previous_run("previous", link_name_);
+    link_output(link_name_);
+  }
 
-private:
-    std::string link_name_;
+ private:
+  std::string link_name_;
 };
 
-void copy_configs(string cfg_filename, string to)
-{
-	if (!cfg::get().run_mode)
-	   return;
+void copy_configs(string cfg_filename, string to) {
+  if (!cfg::get().run_mode)
+    return;
 
-    using namespace debruijn_graph;
+  using namespace debruijn_graph;
 
-    if (!make_dir(to)) {
-    	WARN("Could not create files use in /tmp directory");
-    }
-    copy_files_by_ext(path::parent_path(cfg_filename), to, ".info", true);
+  if (!make_dir(to)) {
+    WARN("Could not create files use in /tmp directory");
+  }
+  copy_files_by_ext(path::parent_path(cfg_filename), to, ".info", true);
 }
 
-void load_config(string cfg_filename)
-{
-    checkFileExistenceFATAL(cfg_filename);
+void load_config(string cfg_filename) {
+  checkFileExistenceFATAL(cfg_filename);
 
-    cfg::create_instance(cfg_filename);
+  cfg::create_instance(cfg_filename);
 
-    if (!cfg::get().project_name.empty()){
-        make_dir(cfg::get().output_base + cfg::get().project_name);
-    }
+  if (!cfg::get().project_name.empty()) {
+    make_dir(cfg::get().output_base + cfg::get().project_name);
+  }
 
-    make_dir(cfg::get().output_root);
+  make_dir(cfg::get().output_root);
 
-    make_dir(cfg::get().output_dir);
-    if (cfg::get().make_saves) {
-        make_dir(cfg::get().output_saves);
-    }
+  make_dir(cfg::get().output_dir);
+  if (cfg::get().make_saves) {
+    make_dir(cfg::get().output_saves);
+  }
 
-    make_dir(cfg::get().temp_bin_reads_path);
+  make_dir(cfg::get().temp_bin_reads_path);
 
-    string path_to_copy = path::append_path(cfg::get().output_dir, "configs");
-        copy_configs(cfg_filename, path_to_copy);
+  string path_to_copy = path::append_path(cfg::get().output_dir, "configs");
+  copy_configs(cfg_filename, path_to_copy);
 }
 
 void create_console_logger(string cfg_filename) {
@@ -128,78 +121,60 @@ void create_console_logger(string cfg_filename) {
   attach_logger(lg);
 }
 
-int main(int argc, char** argv)
-{
-	//BOOST_STATIC_ASSERT(debruijn_graph::K % 2 != 0);
+int main(int argc, char** argv) {
+  perf_counter pc;
 
-	perf_counter pc;
-    
-    const size_t GB = 1 << 30;
-    
-    segfault_handler sh(bind(link_output, "latest"));
+  const size_t GB = 1 << 30;
 
-    srand(42);
-    srandom(42);
+  segfault_handler sh(bind(link_output, "latest"));
 
-    try
-    {
-        using namespace debruijn_graph;
+  srand(42);
+  srandom(42);
 
-        string cfg_filename = argv[1];
+  try {
+    using namespace debruijn_graph;
 
-        load_config          (cfg_filename);
-        create_console_logger(cfg_filename);
+    string cfg_filename = argv[1];
 
-        on_exit_output_linker try_linker("latest");
+    load_config          (cfg_filename);
+    create_console_logger(cfg_filename);
 
-        VERIFY(cfg::get().K >= runtime_k::MIN_K && cfg::get().K < runtime_k::MAX_K);
-        VERIFY(cfg::get().K % 2 != 0);
+    on_exit_output_linker try_linker("latest");
 
-        // read configuration file (dataset path etc.)
+    VERIFY(cfg::get().K >= runtime_k::MIN_K && cfg::get().K < runtime_k::MAX_K);
+    VERIFY(cfg::get().K % 2 != 0);
 
-        // typedefs :)
-//        typedef io::EasyReader ReadStream;
-//        typedef io::PairedEasyReader PairedReadStream;
+    // read configuration file (dataset path etc.)
 
-        limit_memory(cfg::get().max_memory * GB);
+    limit_memory(cfg::get().max_memory * GB);
 
-//        if (cfg::get().use_multithreading) {
-//            debruijn_graph::convert_reads_to_binary();
-//        }
+    // assemble it!
+    INFO("Assembling " << cfg::get().dataset_name << " dataset (" << cfg::get().dataset_file << ")");
+    INFO("with K=" << cfg::get().K);
 
-        // assemble it!
-        INFO("Assembling " << cfg::get().dataset_name << " dataset (" << cfg::get().dataset_file << ")");
-        INFO("with K=" << cfg::get().K);
+    debruijn_graph::assemble_genome();
 
-        debruijn_graph::assemble_genome();
+    link_output("latest_success");
 
-        link_output("latest_success");
+    INFO("Assembling " << cfg::get().dataset_name << " dataset with K=" << cfg::get().K << " finished");
 
-        INFO("Assembling " << cfg::get().dataset_name << " dataset with K=" << cfg::get().K << " finished");
+  } catch (std::bad_alloc const& e) {
+    std::cerr << "Not enough memory to run SPAdes. " << e.what() << std::endl;
+    return EINTR;
+  } catch (std::exception const& e) {
+    std::cerr << "Exception caught " << e.what() << std::endl;
+    return EINTR;
+  } catch (...) {
+    std::cerr << "Unknown exception caught " << std::endl;
+    return EINTR;
+  }
 
-    }
-    catch (std::bad_alloc const& e)
-    {
-    	std::cerr << "Not enough memory to run SPAdes. " << e.what() << std::endl;
-    	return EINTR;
-    }
-    catch (std::exception const& e)
-    {
-        std::cerr << "Exception caught " << e.what() << std::endl;
-        return EINTR;
-    }
-    catch (...)
-    {
-        std::cerr << "Unknown exception caught " << std::endl;
-        return EINTR;
-    }
-    int ms = int(pc.time_ms());
-    int secs = (ms / 1000) % 60;
-    int mins = (ms / 1000 / 60) % 60;
-    int hours = (ms / 1000 / 60 / 60);
-    INFO("Assembling time: " << hours << " hours " << mins << " minutes " << secs << " seconds");
+  unsigned ms = pc.time_ms();
+  unsigned secs = (ms / 1000) % 60;
+  unsigned mins = (ms / 1000 / 60) % 60;
+  unsigned hours = (ms / 1000 / 60 / 60);
+  INFO("Assembling time: " << hours << " hours " << mins << " minutes " << secs << " seconds");
 
-    // OK
-    return 0;
+  // OK
+  return 0;
 }
-
