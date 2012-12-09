92962fd4597d9b24180b1329a474d5b5ece5b25f
Anton Korobeynikov <anton@korobeynikov.info>
2012-12-09 17:57:57 +0400

    Fix paths: unbreak TC

--- assembler/src/test/teamcity/bh_ecoli_1k.sh
+++ assembler/src/test/teamcity/bh_ecoli_1k.sh
@@ -11,5 +11,5 @@ pushd ../../../
 rm -rf spades_output/ECOLI_1K_TEAMCITY_BH
 ./spades.py --config-file src/test/teamcity/bh_config_ecoli_1k.info
 python src/tools/reads_utils/reads_quality.py -r test_dataset/reference_1K.fa.gz -o spades_output/ECOLI_1K_TEAMCITY_BH/reads_quality_results spades_output/ECOLI_1K_TEAMCITY_BH/corrected/dataset.info
-python src/test/teamcity/assess_reads.py spades_output/ECOLI_1K_TEAMCITY_BH/reads_quality_results/all.tsv 100 100
+python src/test/teamcity/assess_reads.py spades_output/ECOLI_1K_TEAMCITY_BH/reads_quality_results/report.tsv 100 100
 popd
--- assembler/src/test/teamcity/bh_ecoli_mda_lane1.sh
+++ assembler/src/test/teamcity/bh_ecoli_mda_lane1.sh
@@ -4,5 +4,5 @@ pushd ../../../
 rm -rf spades_output/ECOLI_MDA_LANE1_TEAMCITY_BH
 ./spades.py --config-file src/test/teamcity/bh_config_ecoli_mda_lane1.info
 python src/tools/reads_utils/reads_quality.py -r data/input/E.coli/MG1655-K12.fasta.gz -o spades_output/ECOLI_MDA_LANE1_TEAMCITY_BH/reads_quality_results spades_output/ECOLI_MDA_LANE1_TEAMCITY_BH/corrected/dataset.info
-python src/test/teamcity/assess_reads.py spades_output/ECOLI_MDA_LANE1_TEAMCITY_BH/reads_quality_results/all.tsv 90 97 # 97 97
+python src/test/teamcity/assess_reads.py spades_output/ECOLI_MDA_LANE1_TEAMCITY_BH/reads_quality_results/report.tsv 90 97 # 97 97
 popd
