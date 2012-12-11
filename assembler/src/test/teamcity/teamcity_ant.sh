3fbd55efc2b36603ed239b223c96ddc4eaa179bf
Andrey Prjibelski <andrewprzh@gmail.com>
2012-12-12 03:31:26 +0400

    test for ant

---
+++ assembler/src/test/teamcity/teamcity_ant.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+############################################################################
+# Copyright (c) 2011-2012 Saint-Petersburg Academic University
+# All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
+
+echo "### PREPROCESSING ###"
+
+pushd ../../../
+
+./prepare_cfg
+errlvl=$?
+if [ "$errlvl" -ne 0 ]
+then
+    echo "prepare_cfg finished with exit code $errlvl"
+    exit $errlvl
+fi
+
+./spades_compile.sh
+errlvl=$?
+if [ "$errlvl" -ne 0 ]
+then
+    echo "spades_compile finished with exit code $errlvl"
+    exit $errlvl
+fi
+
+popd
+pushd ../../../data
+
+popd
+
+echo "### PRINTING CODE TO BE EXECUTED: $1 ###"
+
+cat $1
+
+echo "### RUNNING ###"
+
+./$1
+errlvl=$?
+
+echo "### POSTPROCESSING ###"
+
+pushd ../../../data
+
+popd
+
+echo "### TEAMCITY INVOKATION COMPLETE, EXIT CODE = $errlvl ###"
+
+exit $errlvl
