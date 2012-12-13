8725154096946393afeaebb6eab9454d130cfd04
Andrey Prjibelski <andrewprzh@gmail.com>
2012-12-13 20:17:52 +0400

    more for ant01

---
+++ assembler/src/test/teamcity/spades_sc_jgi_lane7_pre.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+############################################################################
+# Copyright (c) 2011-2012 Saint-Petersburg Academic University
+# All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
+set -e
+pushd ../../../
+
+rm -rf /tmp/data/output/spades_output/ECOLI_SC_JGI_LANE_7_PRENORM
+
+#./spades_compile.sh
+./spades.py --sc -m 160 -k 21,33,55 --12 data/input/E.coli/jgi_lane_7/prenormalized/Eco_no7.subsample.contam.artifact.clean.fastq -o /tmp/data/output/spades_output/ECOLI_SC_JGI_LANE_7_PRENORM
+
+rm -rf ~/quast-1.3/ECOLI_SC_JGI_LANE_7_PRENORM/
+
+python ~/quast-1.3/quast.py -R data/input/E.coli/MG1655-K12.fasta.gz -G data/input/E.coli/genes/genes.gff -O data/input/E.coli/genes/operons.gff -o ~/quast-1.3/ECOLI_SC_JGI_LANE_7_PRENORM/ /tmp/data/output/spades_output/ECOLI_SC_JGI_LANE_7_PRENORM/contigs.fasta
+
+python src/test/teamcity/assess.py ~/quast-1.3/ECOLI_SC_JGI_LANE_7_PRENORM/transposed_report.tsv 115000 4 4260 99.8 3.2 0.7
+exitlvl=$?
+popd
+
---
+++ assembler/src/test/teamcity/spades_sc_pmarinus.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+############################################################################
+# Copyright (c) 2011-2012 Saint-Petersburg Academic University
+# All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
+set -e
+pushd ../../../
+
+rm -rf /tmp/data/output/spades_output/PMARINUS
+
+#./spades_compile.sh
+./spades.py --sc -m 160 -k 21,33,55 -1 data/input/P.marinus/lane6-index21-GTTTCG-AC-193-C02_GTTTCG_L006_R1_001.fastq.gz -1 data/input/P.marinus/lane6-index21-GTTTCG-AC-193-C02_GTTTCG_L006_R1_002.fastq.gz -1 data/input/P.marinus/lane6-index21-GTTTCG-AC-193-C02_GTTTCG_L006_R1_003.fastq.gz -1 data/input/P.marinus/lane6-index21-GTTTCG-AC-193-C02_GTTTCG_L006_R1_004.fastq.gz -2 data/input/P.marinus/lane6-index21-GTTTCG-AC-193-C02_GTTTCG_L006_R2_001.fastq.gz -2 data/input/P.marinus/lane6-index21-GTTTCG-AC-193-C02_GTTTCG_L006_R2_002.fastq.gz -2 data/input/P.marinus/lane6-index21-GTTTCG-AC-193-C02_GTTTCG_L006_R2_003.fastq.gz -2 data/input/P.marinus/lane6-index21-GTTTCG-AC-193-C02_GTTTCG_L006_R2_004.fastq.gz -o /tmp/data/output/spades_output/PMARINUS
+
+rm -rf ~/quast-1.3/PMARINUS/
+
+python ~/quast-1.3/quast.py -R data/input/E.coli/MG1655-K12.fasta.gz -G data/input/E.coli/genes/genes.gff -O data/input/E.coli/genes/operons.gff -o ~/quast-1.3/PMARINUS/ /tmp/data/output/spades_output/PMARINUS/contigs.fasta
+
+python src/test/teamcity/assess.py ~/quast-1.3/PMARINUS/transposed_report.tsv 292000 0 1612 95.3 3.4 1.2
+exitlvl=$?
+popd
+
