329deee56ae179c2d6d92da67ccdb7df3fd4e984
Andrey Prjibelski <andrewprzh@gmail.com>
2012-12-13 16:21:49 +0400

    tc bh 1k fx

--- assembler/src/test/teamcity/bh_ecoli_mda_lane1.sh
+++ assembler/src/test/teamcity/bh_ecoli_mda_lane1.sh
@@ -2,7 +2,7 @@
 set -e
 pushd ../../../
 rm -rf spades_output/ECOLI_MDA_LANE1_TEAMCITY_BH
-./spades.py --config-file src/test/teamcity/bh_config_ecoli_mda_lane1.info
+./spades.py --sc --only-error-correction --12 ./data/input/E.coli/sc_lane_1/ecoli_mda_lane1.fastq.gz -m 160 -o spades_output/ECOLI_MDA_LANE1_TEAMCITY_BH
 python src/tools/reads_utils/reads_quality.py -r data/input/E.coli/MG1655-K12.fasta.gz -o spades_output/ECOLI_MDA_LANE1_TEAMCITY_BH/reads_quality_results spades_output/ECOLI_MDA_LANE1_TEAMCITY_BH/corrected/dataset.info
 python src/test/teamcity/assess_reads.py spades_output/ECOLI_MDA_LANE1_TEAMCITY_BH/reads_quality_results/report.horizontal.tsv 90 97 # 97 97
 popd
