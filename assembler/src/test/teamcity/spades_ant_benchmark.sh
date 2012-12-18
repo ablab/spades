ac13e6b5507df3951e3cff71e6d0f2465058470d
Andrey Prjibelski <andrewprzh@gmail.com>
2012-12-18 20:50:42 +0400

    benchmark moved to other ants

--- assembler/src/test/teamcity/spades_ant_benchmark.sh
+++ assembler/src/test/teamcity/spades_ant_benchmark.sh
@@ -18,24 +18,24 @@ fi
 
 mkdir -p ./ant_benchmark
 
-echo "Starting E.coli is220 on ant10..."
+echo "Starting E.coli is220 on ant16..."
 rm -f ./ant_benchmark/ECOLI_IS220.log
-srun -w ant10 ./src/test/teamcity/teamcity.py /tmp/data/input/E.coli/is220/ECOLI_IS200.info >> ./ant_benchmark/ECOLI_IS220.log 2>> ./ant_benchmark/ECOLI_IS220.log &
+srun -w ant16 ./src/test/teamcity/teamcity.py /tmp/data/input/E.coli/is220/ECOLI_IS200.info >> ./ant_benchmark/ECOLI_IS220.log 2>> ./ant_benchmark/ECOLI_IS220.log &
 echo "done"
 
-echo "Starting E.coli UCSD lane 1 on ant11..."
+echo "Starting E.coli UCSD lane 1 on ant17..."
 rm -f ./ant_benchmark/ECOLI_SC_LANE_1.log
-srun -w ant11 ./src/test/teamcity/teamcity.py /tmp/data/input/E.coli/sc_lane_1/ECOLI_SC_LANE_1.info >> ./ant_benchmark/ECOLI_SC_LANE_1.log 2>> ./ant_benchmark/ECOLI_SC_LANE_1.log &
+srun -w ant17 ./src/test/teamcity/teamcity.py /tmp/data/input/E.coli/sc_lane_1/ECOLI_SC_LANE_1.info >> ./ant_benchmark/ECOLI_SC_LANE_1.log 2>> ./ant_benchmark/ECOLI_SC_LANE_1.log &
 echo "done"
 
-echo "Starting M.ruber JGI lane 9 on ant12..."
+echo "Starting M.ruber JGI lane 9 on ant18..."
 rm -f ./ant_benchmark/MRUBER_JGI_LANE_9.log
-srun -w ant12 ./src/test/teamcity/teamcity.py /tmp/data/input/M.ruber/jgi_lane_9/MRUBER_JGI_LANE_9.info >> ./ant_benchmark/MRUBER_JGI_LANE_9.log 2>> ./ant_benchmark/MRUBER_JGI_LANE_9.log &
+srun -w ant18 ./src/test/teamcity/teamcity.py /tmp/data/input/M.ruber/jgi_lane_9/MRUBER_JGI_LANE_9.info >> ./ant_benchmark/MRUBER_JGI_LANE_9.log 2>> ./ant_benchmark/MRUBER_JGI_LANE_9.log &
 echo "done"
 
 
-echo "Starting P.heparinus JGI lane 7 on ant13..."
+echo "Starting P.heparinus JGI lane 7 on ant19..."
 rm -f ./ant_benchmark/PHEPARINUS_JGI_LANE_7.log
-srun -w ant13 ./src/test/teamcity/teamcity.py /tmp/data/input/P.heparinus/jgi_lane_7/PHEPARINUS_JGI_LANE_7.info >> ./ant_benchmark/PHEPARINUS_JGI_LANE_7.log 2>> ./ant_benchmark/PHEPARINUS_JGI_LANE_7.log &
+srun -w ant19 ./src/test/teamcity/teamcity.py /tmp/data/input/P.heparinus/jgi_lane_7/PHEPARINUS_JGI_LANE_7.info >> ./ant_benchmark/PHEPARINUS_JGI_LANE_7.log 2>> ./ant_benchmark/PHEPARINUS_JGI_LANE_7.log &
 echo "done"
 
