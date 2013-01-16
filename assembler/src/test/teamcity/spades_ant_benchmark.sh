dbc00bd7e03fd860c36559158bf9fb14de246dea
Andrey Prjibelski <andrewprzh@gmail.com>
2013-01-16 21:19:58 +0400

    changed ants again for benchmark

--- assembler/src/test/teamcity/spades_ant_benchmark.sh
+++ assembler/src/test/teamcity/spades_ant_benchmark.sh
@@ -29,14 +29,14 @@ rm -f ./ant_benchmark/ECOLI_IS220.log
 srun -w ant16 ./src/test/teamcity/teamcity.py /tmp/data/input/E.coli/is220/ECOLI_IS200.info $1 >> ./ant_benchmark/ECOLI_IS220_$timestamp.log 2>> ./ant_benchmark/ECOLI_IS220_$timestamp.log &
 echo "done"
 
-echo "Starting E.coli UCSD lane 1 on ant17..."
+echo "Starting E.coli UCSD lane 1 on ant01..."
 rm -f ./ant_benchmark/ECOLI_SC_LANE_1.log
-srun -w ant17 ./src/test/teamcity/teamcity.py /tmp/data/input/E.coli/sc_lane_1/ECOLI_SC_LANE_1.info $1 >> ./ant_benchmark/ECOLI_SC_LANE_1_$timestamp.log 2>> ./ant_benchmark/ECOLI_SC_LANE_1_$timestamp.log &
+srun -w ant01 ./src/test/teamcity/teamcity.py /tmp/data/input/E.coli/sc_lane_1/corrected/ECOLI_SC_LANE_1_CORR.info $1 >> ./ant_benchmark/ECOLI_SC_LANE_1_$timestamp.log 2>> ./ant_benchmark/ECOLI_SC_LANE_1_$timestamp.log &
 echo "done"
 
-echo "Starting M.ruber JGI lane 9 on ant18..."
+echo "Starting M.ruber JGI lane 9 on ant02..."
 rm -f ./ant_benchmark/MRUBER_JGI_LANE_9.log
-srun -w ant18 ./src/test/teamcity/teamcity.py /tmp/data/input/M.ruber/jgi_lane_9/MRUBER_JGI_LANE_9.info $1 >> ./ant_benchmark/MRUBER_JGI_LANE_9_$timestamp.log 2>> ./ant_benchmark/MRUBER_JGI_LANE_9_$timestamp.log &
+srun -w ant02 ./src/test/teamcity/teamcity.py /tmp/data/input/M.ruber/jgi_lane_9/MRUBER_JGI_LANE_9_MANUAL.info $1 >> ./ant_benchmark/MRUBER_JGI_LANE_9_$timestamp.log 2>> ./ant_benchmark/MRUBER_JGI_LANE_9_$timestamp.log &
 echo "done"
 
 
--- assembler/src/test/teamcity/spades_ant_multicell_benchmark.sh
+++ assembler/src/test/teamcity/spades_ant_multicell_benchmark.sh
@@ -29,19 +29,19 @@ rm -f ./ant_benchmark/ECOLI_IS220.log
 srun -w ant16 ./src/test/teamcity/teamcity.py /tmp/data/input/E.coli/is220/ECOLI_IS200.info >> ./ant_benchmark/ECOLI_IS220_$timestamp.log 2>> ./ant_benchmark/ECOLI_IS220_$timestamp.log &
 echo "done"
 
-echo "Starting E.coli is480 on ant17..."
+echo "Starting E.coli is480 on ant03..."
 rm -f ./ant_benchmark/ECOLI_IS480_QUAKE.log
-srun -w ant17 ./src/test/teamcity/teamcity.py /tmp/data/input/E.coli/is480/ECOLI_IS480_QUAKE.info >> ./ant_benchmark/ECOLI_IS480_QUAKE_$timestamp.log 2>> ./ant_benchmark/ECOLI_IS480_QUAKE_$timestamp.log &
+srun -w ant03 ./src/test/teamcity/teamcity.py /tmp/data/input/E.coli/is480/ECOLI_IS480_QUAKE_MANUAL.info >> ./ant_benchmark/ECOLI_IS480_QUAKE_$timestamp.log 2>> ./ant_benchmark/ECOLI_IS480_QUAKE_$timestamp.log &
 echo "done"
 
-echo "Starting L.grasseri on ant18..."
+echo "Starting L.grasseri on ant03..."
 rm -f ./ant_benchmark/LGRASSERI_QUAKE.log
-srun -w ant18 ./src/test/teamcity/teamcity.py /tmp/data/input/L.grasseri/LGRASSERI_QUAKE.info >> ./ant_benchmark/LGRASSERI_QUAKE_$timestamp.log 2>> ./ant_benchmark/LGRASSERI_QUAKE_$timestamp.log &
+srun -w ant03 ./src/test/teamcity/teamcity.py /tmp/data/input/L.grasseri/LGRASSERI_QUAKE_MANUAL.info >> ./ant_benchmark/LGRASSERI_QUAKE_$timestamp.log 2>> ./ant_benchmark/LGRASSERI_QUAKE_$timestamp.log &
 echo "done"
 
 
-echo "Starting B.faecium on ant18..."
+echo "Starting B.faecium on ant03..."
 rm -f ./ant_benchmark/BFAECIUM_QAUKE.log
-srun -w ant18 ./src/test/teamcity/teamcity.py /tmp/data/input/B.faecium/BFAECIUM_QAUKE.info >> ./ant_benchmark/BFAECIUM_QAUKE_$timestamp.log 2>> ./ant_benchmark/BFAECIUM_QAUKE_$timestamp.log &
+srun -w ant03 ./src/test/teamcity/teamcity.py /tmp/data/input/B.faecium/BFAECIUM_QAUKE_MANUAL.info >> ./ant_benchmark/BFAECIUM_QAUKE_$timestamp.log 2>> ./ant_benchmark/BFAECIUM_QAUKE_$timestamp.log &
 echo "done"
 
