29a1753bbbd004f0be8e8287f5a5e934805bb28c
Andrey Prjibelski <andrewprzh@gmail.com>
2013-01-17 22:38:38 +0400

    ace benchmarks

---
+++ assembler/src/test/teamcity/spades_ace_benchmark.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+./prepare_cfg
+errlvl=$?
+if [ "$errlvl" -ne 0 ]
+then
+    echo "prepare_cfg finished with exit code $errlvl"
+    exit $errlvl
+fi
+
+./spades_compile.sh
+errlvl=$?
+if [ "$errlvl" -ne 0 ]
+then
+    echo "spades_compile finished with exit code $errlvl"
+    exit $errlvl
+fi
+
+timestamp="`date +%Y-%m-%d_%H-%M-%S`"_$1
+if [ "$#" > 0 ]
+then
+    timestamp=$timestamp"_"$1
+fi
+
+mkdir -p ./ace_benchmark
+
+echo "Starting E.coli is220 on ace..."
+rm -f ./ace_benchmark/ECOLI_IS220.log
+./src/test/teamcity/teamcity.py /storage/acestorage/data/input/E.coli/is220/ECOLI_IS220_QUAKE_BM.info $1 >> ./ace_benchmark/ECOLI_IS220_$timestamp.log 2>> ./ace_benchmark/ECOLI_IS220_$timestamp.log
+echo "finished with code "$?
+
+echo "Starting E.coli UCSD lane 1 on ace..."
+rm -f ./ace_benchmark/ECOLI_SC_LANE_1.log
+./src/test/teamcity/teamcity.py /storage/acestorage/data/input/E.coli/sc_lane_1/bh20130116/ECOLI_SC_LANE_1_CORR.info $1 >> ./ace_benchmark/ECOLI_SC_LANE_1_$timestamp.log 2>> ./ace_benchmark/ECOLI_SC_LANE_1_$timestamp.log
+echo "finished with code "$?
+
+echo "Starting M.ruber JGI lane 9 on ace..."
+rm -f ./ace_benchmark/MRUBER_JGI_LANE_9.log
+./src/test/teamcity/teamcity.py /storage/acestorage/data/input/M.ruber/jgi_lane_9/MRUBER_JGI_LANE_9_MANUAL.info $1 >> ./ace_benchmark/MRUBER_JGI_LANE_9_$timestamp.log 2>> ./ace_benchmark/MRUBER_JGI_LANE_9_$timestamp.log
+echo "finished with code "$?
+
+echo "Starting P.heparinus JGI lane 7 on ace..."
+rm -f ./ace_benchmark/PHEPARINUS_JGI_LANE_7.log
+./src/test/teamcity/teamcity.py /storage/acestorage/data/input/P.heparinus/jgi_lane_7/PHEPARINUS_JGI_LANE_7_MANUAL.info $1 >> ./ace_benchmark/PHEPARINUS_JGI_LANE_7_$timestamp.log 2>> ./ace_benchmark/PHEPARINUS_JGI_LANE_7_$timestamp.log
+echo "finished with code "$?
+
---
+++ assembler/src/test/teamcity/spades_ace_multicell_benchmark.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+./prepare_cfg
+errlvl=$?
+if [ "$errlvl" -ne 0 ]
+then
+    echo "prepare_cfg finished with exit code $errlvl"
+    exit $errlvl
+fi
+
+./spades_compile.sh
+errlvl=$?
+if [ "$errlvl" -ne 0 ]
+then
+    echo "spades_compile finished with exit code $errlvl"
+    exit $errlvl
+fi
+
+timestamp="`date +%Y-%m-%d_%H-%M-%S`"
+if [ "$#" > 0 ]
+then
+    timestamp=$timestamp"_"$1
+fi
+
+mkdir -p ./ace_benchmark
+
+echo "Starting E.coli is220 on ace..."
+rm -f ./ace_benchmark/ECOLI_IS220.log
+./src/test/teamcity/teamcity.py /storage/acestorage/data/input/E.coli/is220/ECOLI_IS220_QUAKE_BM.info $1 >> ./ace_benchmark/ECOLI_IS220_$timestamp.log 2>> ./ace_benchmark/ECOLI_IS220_$timestamp.log
+echo "finished with code "$?
+
+echo "Starting E.coli is480 on ace..."
+rm -f ./ace_benchmark/ECOLI_IS480_QUAKE.log
+./src/test/teamcity/teamcity.py /storage/acestorage/data/input/E.coli/is480/ECOLI_IS480_QUAKE_MANUAL.info >> ./ace_benchmark/ECOLI_IS480_QUAKE_$timestamp.log 2>> ./ace_benchmark/ECOLI_IS480_QUAKE_$timestamp.log 
+echo "finished with code "$?
+
+echo "Starting L.grasseri on ace..."
+rm -f ./ace_benchmark/LGRASSERI_QUAKE.log
+./src/test/teamcity/teamcity.py /storage/acestorage/data/input/L.grasseri/LGRASSERI_QUAKE_MANUAL.info >> ./ace_benchmark/LGRASSERI_QUAKE_$timestamp.log 2>> ./ace_benchmark/LGRASSERI_QUAKE_$timestamp.log 
+echo "finished with code "$?
+
+echo "Starting B.faecium on ace..."
+rm -f ./ace_benchmark/BFAECIUM_QAUKE.log
+./src/test/teamcity/teamcity.py /storage/acestorage/data/input/B.faecium/BFAECIUM_QAUKE_MANUAL.info >> ./ace_benchmark/BFAECIUM_QAUKE_$timestamp.log 2>> ./ace_benchmark/BFAECIUM_QAUKE_$timestamp.log 
+echo "finished with code "$?
