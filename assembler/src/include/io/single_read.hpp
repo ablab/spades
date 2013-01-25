6e51ec05261e3bb2581f5b3b1aaa7dd0d9d66b95
Anton Korobeynikov <anton@korobeynikov.info>
2013-01-25 21:10:14 +0400

    Cleanup

--- assembler/src/include/io/single_read.hpp
+++ assembler/src/include/io/single_read.hpp
@@ -35,409 +35,397 @@
 
 namespace io {
 
-//class ISingleRead {
-//
-//public:
-//    virtual ~ISingleRead() = 0;
-//
-//    virtual bool operator==(const SingleReadSeq& singleread) const = 0;
-//
-//    virtual const Sequence& sequence() const = 0;
-//
-//    virtual size_t size() const = 0;
-//};
-
-/* 
+/*
  * This enumerate contains offset type.
  * UnknownOffset is equal to "offset = 0".
  * PhredOffset is equal to "offset = 33".
  * SolexaOffset is equal to "offset = 64".
  */
 enum OffsetType {
-	UnknownOffset, PhredOffset, SolexaOffset
+  UnknownOffset, PhredOffset, SolexaOffset
 };
 
 //todo extract code about offset from here
 class SingleRead {
 
-public:
-	static string EmptyQuality(const string& seq) {
-		return std::string(seq.size(), (char) 33);
-	}
+ public:
+  static string EmptyQuality(const string& seq) {
+    return std::string(seq.size(), (char) 33);
+  }
 
-	static const int UNKNOWN_OFFSET = 0;
-	static const int PHRED_OFFSET = 33;
-	static const int SOLEXA_OFFSET = 64;
-	static const int BAD_QUALITY_THRESHOLD = 2;
+  static const int UNKNOWN_OFFSET = 0;
+  static const int PHRED_OFFSET = 33;
+  static const int SOLEXA_OFFSET = 64;
+  static const int BAD_QUALITY_THRESHOLD = 2;
 
-	/*
-	 * Type of variables which will store file names for reading from
-	 * Reader stream.
-	 */
-	typedef std::string FilenameType;
+  /*
+   * Type of variables which will store file names for reading from
+   * Reader stream.
+   */
+  typedef std::string FilenameType;
 
-	/*
-	 * Default constructor.
-	 */
-	SingleRead() :
-			name_(""), seq_(""), qual_(""), valid_(false) {
-	}
+  /*
+   * Default constructor.
+   */
+  SingleRead() :
+      name_(""), seq_(""), qual_(""), valid_(false) {
+  }
 
-	/*
-	 * Test constructor.
-	 *
-	 * @param name The name of the SingleRead (id in input file).
-	 * @param seq The sequence of ATGC letters.
-	 * @param qual The quality of the SingleRead sequence.
-	 */
-	SingleRead(const std::string& name, const std::string& seq,
-			const std::string& qual, OffsetType offset_type) :
-			name_(name), seq_(seq), qual_(qual) {
-		Init();
-		int offset = GetOffset(offset_type);
-		for (size_t i = 0; i < qual_.size(); ++i) {
-			qual_[i] -= offset;
-		}
-	}
-
-	SingleRead(const std::string& name, const std::string& seq,
-			const std::string& qual) :
-			name_(name), seq_(seq), qual_(qual) {
-		Init();
-	}
-
-	SingleRead(const std::string& name, const std::string& seq) :
-			name_(name), seq_(seq), qual_(EmptyQuality(seq_)) {
-		Init();
-	}
-
-	/*
-	 * Check whether SingleRead is valid.
-	 *
-	 * @return true if SingleRead is valid (there is no N in sequence
-	 * and sequence size is equal to quality size), and false otherwise
-	 */
-	bool IsValid() const {
-		return valid_;
-	}
-
-	/*
-	 * Return Sequence object, got from sequence string.
-	 *
-	 * @return SingleRead sequence.
-	 */
-	Sequence sequence() const {
-		VERIFY(valid_);
-		return Sequence(seq_);
-	}
-
-	/*
-	 * Return Quality object, got from quality string.
-	 *
-	 * @return SingleRead quality.
-	 */
-	Quality quality() const {
-		VERIFY(valid_);
-		return Quality(qual_);
-	}
-
-	/*
-	 * Return name of single read.
-	 *
-	 * @return SingleRead name.
-	 */
-	const std::string& name() const {
-		return name_;
-	}
-
-	/*
-	 * Return size of SingleRead.
-	 *
-	 * @return The size of SingleRead sequence.
-	 */
-	size_t size() const {
-		return seq_.size();
-	}
-
-    size_t nucl_count() const {
-        return size();
+  /*
+   * Test constructor.
+   *
+   * @param name The name of the SingleRead (id in input file).
+   * @param seq The sequence of ATGC letters.
+   * @param qual The quality of the SingleRead sequence.
+   */
+  SingleRead(const std::string& name, const std::string& seq,
+             const std::string& qual, OffsetType offset_type) :
+      name_(name), seq_(seq), qual_(qual) {
+    Init();
+    int offset = GetOffset(offset_type);
+    for (size_t i = 0; i < qual_.size(); ++i) {
+      qual_[i] -= offset;
     }
+  }
 
-	/*
-	 * Return SingleRead sequence string (in readable form with ATGC).
-	 *
-	 * @return SingleRead sequence string.
-	 */
-	const std::string& GetSequenceString() const {
-		return seq_;
-	}
+  SingleRead(const std::string& name, const std::string& seq,
+             const std::string& qual) :
+      name_(name), seq_(seq), qual_(qual) {
+    Init();
+  }
 
-	/*
-	 * Return SingleRead quality string (in readable form).
-	 *
-	 * @return SingleRead quality string.
-	 */
-	const std::string& GetQualityString() const {
-		return qual_;
-	}
+  SingleRead(const std::string& name, const std::string& seq) :
+      name_(name), seq_(seq), qual_(EmptyQuality(seq_)) {
+    Init();
+  }
 
-	/*
-	 * Return SingleRead quality string, where every quality value is
-	 * increased by PhredOffset (need for normalization of quality values).
-	 * Do not modify original quality values.
-	 *
-	 * @return Modified SingleRead quality string.
-	 */
-	std::string GetPhredQualityString() const {
-		int offset = PHRED_OFFSET;
-		std::string res = qual_;
-		for (size_t i = 0; i < res.size(); ++i) {
-			res[i] += offset;
-		}
-		return res;
-	}
+  /*
+   * Check whether SingleRead is valid.
+   *
+   * @return true if SingleRead is valid (there is no N in sequence
+   * and sequence size is equal to quality size), and false otherwise
+   */
+  bool IsValid() const {
+    return valid_;
+  }
 
-	/*
-	 * Return ith nucleotide of SingleRead sequence in unreadable form
-	 * (0, 1, 2 or 3).
-	 *
-	 * @param i Nucleotide index.
-	 * @return Nucleotide on ith position of SingleRead sequence.
-	 */
-	char operator[](size_t i) const {
-		VERIFY(is_nucl(seq_[i]));
-		return dignucl(seq_[i]);
-	}
+  /*
+   * Return Sequence object, got from sequence string.
+   *
+   * @return SingleRead sequence.
+   */
+  Sequence sequence() const {
+    VERIFY(valid_);
+    return Sequence(seq_);
+  }
 
-	/*
-	 * Return reversed complimentary SingleRead (SingleRead with new
-	 * name, reversed complimentary sequence, and reversed quality).
-	 *
-	 * @return Reversed complimentary SingleRead.
-	 */
-	SingleRead operator!() const {
-		std::string new_name;
-		if (name_.length() >= 3 && name_.substr(name_.length() - 3) == "_RC") {
-			new_name = name_.substr(0, name_.length() - 3);
-		} else {
-			new_name = name_ + "_RC";
-		}
-//		TODO make naming nicer
-//		if (name_ == "" || name_[0] != '!') {
-//			new_name = '!' + name_;
-//		} else {
-//			new_name = name_.substr(1, name_.length());
-//		}
-		return SingleRead(new_name, ReverseComplement(seq_), Reverse(qual_));
-	}
+  /*
+   * Return Quality object, got from quality string.
+   *
+   * @return SingleRead quality.
+   */
+  Quality quality() const {
+    VERIFY(valid_);
+    return Quality(qual_);
+  }
 
-	SingleRead SubstrStrict(size_t from, size_t to) const {
-		size_t len = to - from;
-//		return SingleRead(name_, seq_.substr(from, len), qual_.substr(from, len));
-//		TODO make naming nicer
-		std::string new_name;
-		if (name_.length() >= 3 && name_.substr(name_.length() - 3) == "_RC") {
-			new_name = name_.substr(0, name_.length() - 3) + "_SUBSTR(" + ToString(size() - to) + "," + ToString(size() - from) + ")" + "_RC";
-		} else {
-			new_name = name_ + "_SUBSTR(" + ToString(from) + "," + ToString(to) + ")";
-		}
-		return SingleRead(new_name, seq_.substr(from, len), qual_.substr(from, len));
-	}
+  /*
+   * Return name of single read.
+   *
+   * @return SingleRead name.
+   */
+  const std::string& name() const {
+    return name_;
+  }
 
-	SingleRead Substr(size_t from, size_t to) const {
-		size_t len = to - from;
-		if (len == size()) {
-			return *this;
-		}
-		if (len == 0) {
-			return SingleRead();
-		}
-		return SubstrStrict(from, to);
-	}
+  /*
+   * Return size of SingleRead.
+   *
+   * @return The size of SingleRead sequence.
+   */
+  size_t size() const {
+    return seq_.size();
+  }
 
-	std::string original_name() const {
-		size_t len = name_.length();
-		for (size_t i = 0; i < len; ++i) {
-			if (name_[i] != '_') {
-				continue;
-			}
-			if (name_.substr(i, 3) == "_RC" || name_.substr(i, 8) == "_SUBSTR(") {
-				return name_.substr(0, i);
-			}
-		}
-		return name_;
-	}
+  size_t nucl_count() const {
+    return size();
+  }
 
-	pair<size_t, size_t> position_in_original() const {
-		for (int i = name_.length() - 1; i >= 0; --i) {
-			if (name_[i] != '_') {
-				continue;
-			}
-			if (name_.substr(i, 8) == "_SUBSTR(") {
-				int from, to;
-				sscanf(name_.substr(i + 8).c_str(), "%d,%d)", &from, &to);
-				return make_pair((size_t) from, (size_t) to);
-			}
-		}
-		return make_pair(0, size());
-	}
+  /*
+   * Return SingleRead sequence string (in readable form with ATGC).
+   *
+   * @return SingleRead sequence string.
+   */
+  const std::string& GetSequenceString() const {
+    return seq_;
+  }
 
-	/*
-	 * Check whether two SingleReads are equal.
-	 *
-	 * @param singleread The SingleRead we want to compare ours with.
-	 *
-	 * @return true if these two single reads have similar sequences,
-	 * and false otherwise.
-	 */
-	bool operator==(const SingleRead& singleread) const {
-		return seq_ == singleread.seq_;
-	}
+  /*
+   * Return SingleRead quality string (in readable form).
+   *
+   * @return SingleRead quality string.
+   */
+  const std::string& GetQualityString() const {
+    return qual_;
+  }
 
-	void ChangeName(const string& new_name) {
-		name_ = new_name;
-	}
-
-//	void ClearQuality() {
-//		qual_ = std::string(seq_.size(), (char) 0);
-//		UpdateValid();
-//	}
-
-	static bool IsValid(const string& seq) {
-		for (size_t i = 0; i < seq.size(); ++i) {
-			if (!is_nucl(seq[i])) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-
-    bool BinWrite(std::ostream& file) const {
-        return sequence().BinWrite(file);
+  /*
+   * Return SingleRead quality string, where every quality value is
+   * increased by PhredOffset (need for normalization of quality values).
+   * Do not modify original quality values.
+   *
+   * @return Modified SingleRead quality string.
+   */
+  std::string GetPhredQualityString() const {
+    int offset = PHRED_OFFSET;
+    std::string res = qual_;
+    for (size_t i = 0; i < res.size(); ++i) {
+      res[i] += offset;
     }
+    return res;
+  }
 
+  /*
+   * Return ith nucleotide of SingleRead sequence in unreadable form
+   * (0, 1, 2 or 3).
+   *
+   * @param i Nucleotide index.
+   * @return Nucleotide on ith position of SingleRead sequence.
+   */
+  char operator[](size_t i) const {
+    VERIFY(is_nucl(seq_[i]));
+    return dignucl(seq_[i]);
+  }
 
-    void print_size() const {
-        std::cerr << size() << std::endl;
+  /*
+   * Return reversed complimentary SingleRead (SingleRead with new
+   * name, reversed complimentary sequence, and reversed quality).
+   *
+   * @return Reversed complimentary SingleRead.
+   */
+  SingleRead operator!() const {
+    std::string new_name;
+    if (name_.length() >= 3 && name_.substr(name_.length() - 3) == "_RC") {
+      new_name = name_.substr(0, name_.length() - 3);
+    } else {
+      new_name = name_ + "_RC";
     }
+    //		TODO make naming nicer
+    //		if (name_ == "" || name_[0] != '!') {
+    //			new_name = '!' + name_;
+    //		} else {
+    //			new_name = name_.substr(1, name_.length());
+    //		}
+    return SingleRead(new_name, ReverseComplement(seq_), Reverse(qual_));
+  }
+
+  SingleRead SubstrStrict(size_t from, size_t to) const {
+    size_t len = to - from;
+    //		return SingleRead(name_, seq_.substr(from, len), qual_.substr(from, len));
+    //		TODO make naming nicer
+    std::string new_name;
+    if (name_.length() >= 3 && name_.substr(name_.length() - 3) == "_RC") {
+      new_name = name_.substr(0, name_.length() - 3) + "_SUBSTR(" + ToString(size() - to) + "," + ToString(size() - from) + ")" + "_RC";
+    } else {
+      new_name = name_ + "_SUBSTR(" + ToString(from) + "," + ToString(to) + ")";
+    }
+    return SingleRead(new_name, seq_.substr(from, len), qual_.substr(from, len));
+  }
+
+  SingleRead Substr(size_t from, size_t to) const {
+    size_t len = to - from;
+    if (len == size()) {
+      return *this;
+    }
+    if (len == 0) {
+      return SingleRead();
+    }
+    return SubstrStrict(from, to);
+  }
+
+  std::string original_name() const {
+    size_t len = name_.length();
+    for (size_t i = 0; i < len; ++i) {
+      if (name_[i] != '_') {
+        continue;
+      }
+      if (name_.substr(i, 3) == "_RC" || name_.substr(i, 8) == "_SUBSTR(") {
+        return name_.substr(0, i);
+      }
+    }
+    return name_;
+  }
+
+  pair<size_t, size_t> position_in_original() const {
+    for (int i = name_.length() - 1; i >= 0; --i) {
+      if (name_[i] != '_') {
+        continue;
+      }
+      if (name_.substr(i, 8) == "_SUBSTR(") {
+        int from, to;
+        sscanf(name_.substr(i + 8).c_str(), "%d,%d)", &from, &to);
+        return make_pair((size_t) from, (size_t) to);
+      }
+    }
+    return make_pair(0, size());
+  }
+
+  /*
+   * Check whether two SingleReads are equal.
+   *
+   * @param singleread The SingleRead we want to compare ours with.
+   *
+   * @return true if these two single reads have similar sequences,
+   * and false otherwise.
+   */
+  bool operator==(const SingleRead& singleread) const {
+    return seq_ == singleread.seq_;
+  }
+
+  void ChangeName(const string& new_name) {
+    name_ = new_name;
+  }
+
+  //	void ClearQuality() {
+  //		qual_ = std::string(seq_.size(), (char) 0);
+  //		UpdateValid();
+  //	}
+
+  static bool IsValid(const string& seq) {
+    for (size_t i = 0; i < seq.size(); ++i) {
+      if (!is_nucl(seq[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
 
 
-private:
-	/*
-	 * @variable The name of SingleRead in input file.
-	 */
-	std::string name_;
-	/*
-	 * @variable The sequence of nucleotides.
-	 */
-	std::string seq_;
-	/*
-	 * @variable The quality of SingleRead.
-	 */
-	std::string qual_;
-	/*
-	 * @variable The flag of SingleRead correctness.
-	 */
-	bool valid_;
-
-	/*
-	 * todo move from here!!!
-	 * Return quality offset value from offset type.
-	 *
-	 * @param offset_type One of possible enum values.
-	 * @return Quality offset value.
-	 */
-	int GetOffset(OffsetType offset_type) const {
-		switch (offset_type) {
-		case UnknownOffset:
-			return 0;
-		case PhredOffset:
-			return 33;
-		case SolexaOffset:
-			return 64;
-		}
-		return -1;
-	}
+  bool BinWrite(std::ostream& file) const {
+    return sequence().BinWrite(file);
+  }
 
 
+  void print_size() const {
+    std::cerr << size() << std::endl;
+  }
 
-//	/*
-//	 * Set quality of SingleRead.
-//	 *
-//	 * @param new_quality New quality of SingleRead.
-//	 * @param offset The offset of SingleRead quality
-//	 * (PHRED_OFFSET by default).
-//	 */
-//	void SetQuality(const string& new_quality, OffsetType offset_type =
-//			PhredOffset) {
-//		int offset = GetOffset(offset_type);
-//		qual_ = new_quality;
-//		for (size_t i = 0; i < qual_.size(); ++i) { // oh, really does it work with char* copying?
-//			qual_[i] -= offset;
-//		}
-//		cout << "Set quality" << endl;
-//	}
 
-	void Init() {
-		VERIFY(seq_.size() == qual_.size());
-		valid_ = SingleRead::IsValid(seq_);
-	}
+ private:
+  /*
+   * @variable The name of SingleRead in input file.
+   */
+  std::string name_;
+  /*
+   * @variable The sequence of nucleotides.
+   */
+  std::string seq_;
+  /*
+   * @variable The quality of SingleRead.
+   */
+  std::string qual_;
+  /*
+   * @variable The flag of SingleRead correctness.
+   */
+  bool valid_;
+
+  /*
+   * todo move from here!!!
+   * Return quality offset value from offset type.
+   *
+   * @param offset_type One of possible enum values.
+   * @return Quality offset value.
+   */
+  int GetOffset(OffsetType offset_type) const {
+    switch (offset_type) {
+      case UnknownOffset:
+        return 0;
+      case PhredOffset:
+        return 33;
+      case SolexaOffset:
+        return 64;
+    }
+    return -1;
+  }
+
+
+
+  //	/*
+  //   * Set quality of SingleRead.
+  //   *
+  //   * @param new_quality New quality of SingleRead.
+  //   * @param offset The offset of SingleRead quality
+  //   * (PHRED_OFFSET by default).
+  //   */
+  //	void SetQuality(const string& new_quality, OffsetType offset_type =
+  //			PhredOffset) {
+  //		int offset = GetOffset(offset_type);
+  //		qual_ = new_quality;
+  //		for (size_t i = 0; i < qual_.size(); ++i) { // oh, really does it work with char* copying?
+  //			qual_[i] -= offset;
+  //		}
+  //		cout << "Set quality" << endl;
+  //	}
+
+  void Init() {
+    VERIFY(seq_.size() == qual_.size());
+    valid_ = SingleRead::IsValid(seq_);
+  }
 
 };
 
 inline ostream& operator<<(ostream& os, const SingleRead& read) {
-	os << "Single read name=" << read.name() << " sequence=" << read.GetSequenceString() << endl;
-	return os;
+  os << "Single read name=" << read.name() << " sequence=" << read.GetSequenceString() << endl;
+  return os;
 }
 
 class SingleReadSeq {
 
-public:
-    SingleReadSeq(const Sequence& s): seq_(s) {
-    }
+ public:
+  SingleReadSeq(const Sequence& s): seq_(s) {
+  }
 
-    SingleReadSeq() {
-    }
+  SingleReadSeq() {
+  }
 
-    bool BinRead(std::istream& file) {
-        return seq_.BinRead(file);
-    }
+  bool BinRead(std::istream& file) {
+    return seq_.BinRead(file);
+  }
 
-    bool BinWrite(std::ostream& file) const {
-        return seq_.BinWrite(file);
-    }
+  bool BinWrite(std::ostream& file) const {
+    return seq_.BinWrite(file);
+  }
 
-//    SingleReadSeq(std::istream& file): seq_(file, true) {
-//    }
+  //    SingleReadSeq(std::istream& file): seq_(file, true) {
+  //    }
 
-    bool operator==(const SingleReadSeq& singleread) const {
-        return seq_ == singleread.seq_;
-    }
+  bool operator==(const SingleReadSeq& singleread) const {
+    return seq_ == singleread.seq_;
+  }
 
-    const Sequence sequence() const {
-        return seq_;
-    }
+  const Sequence sequence() const {
+    return seq_;
+  }
 
-    size_t size() const {
-        return seq_.size();
-    }
+  size_t size() const {
+    return seq_.size();
+  }
 
-    size_t nucl_count() const {
-        return size();
-    }
+  size_t nucl_count() const {
+    return size();
+  }
 
-    SingleReadSeq operator!() const {
-        return SingleReadSeq(!seq_);
-    }
+  SingleReadSeq operator!() const {
+    return SingleReadSeq(!seq_);
+  }
 
-private:
-    Sequence seq_;
+ private:
+  Sequence seq_;
 };
 
 inline ostream& operator<<(ostream& os, const SingleReadSeq& read) {
-	os << "Single read sequence=" << read.sequence() << endl;
-	return os;
+  os << "Single read sequence=" << read.sequence() << endl;
+  return os;
 }
 
 }
