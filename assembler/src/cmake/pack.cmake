18aee936e3b0f6e1101ff061af5de32fc0715a0b
Anton Korobeynikov <anton@korobeynikov.info>
2013-02-22 20:40:20 +0400

    Add license to installer. Move GPL to separate file whiel there.

--- assembler/src/cmake/pack.cmake
+++ assembler/src/cmake/pack.cmake
@@ -3,19 +3,20 @@
 # Binary stuff
 set(CPACK_CMAKE_GENERATOR "Unix Makefiles")
 if (APPLE)
-  set(CPACK_GENERATOR "STGZ;TGZ;TBZ2;PackageMaker")
+  set(CPACK_GENERATOR "STGZ;TGZ;PackageMaker")
 else()
-  set(CPACK_GENERATOR "STGZ;TGZ;TBZ2")
+  set(CPACK_GENERATOR "STGZ;TGZ")
 endif()
 
 set(CPACK_PACKAGE_NAME "SPAdes")
 set(CPACK_PACKAGE_VENDOR "Saint Petersburg Academic University")
 set(CPACK_PACKAGE_DESCRIPTION_FILE "${SPADES_MAIN_SRC_DIR}/../README")
+set(CPACK_RESOURCE_FILE_LICENSE "${SPADES_MAIN_SRC_DIR}/../LICENSE")
 set(CPACK_PACKAGE_VERSION "2.4.0")
 set(CPACK_PACKAGE_VERSION_MAJOR "2")
 set(CPACK_PACKAGE_VERSION_MINOR "4")
 set(CPACK_PACKAGE_VERSION_PATCH "0")
-set(CPACK_STRIP_FILES bin/debruijn bin/hammer bin/bwa)
+set(CPACK_STRIP_FILES bin/debruijn bin/hammer bin/spades-bwa)
 
 # Source stuff
 set(CPACK_SOURCE_GENERATOR "TBZ2")
