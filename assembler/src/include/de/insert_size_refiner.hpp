5b0dd01e3914ad919f27f1f99b4deb129068563f
Anton Korobeynikov <anton@korobeynikov.info>
2013-03-11 20:20:35 +0400

    Fix all the weirdness of IS refiner

--- assembler/src/include/de/insert_size_refiner.hpp
+++ assembler/src/include/de/insert_size_refiner.hpp
@@ -11,11 +11,11 @@
 
 namespace omnigraph {
 
-double get_median(const map<int, size_t> &hist) {
+double get_median(const std::map<int, size_t> &hist) {
   double S = 0;
-  for (auto iter = hist.begin(); iter != hist.end(); ++iter) {
+  for (auto iter = hist.begin(); iter != hist.end(); ++iter)
     S += iter->second;
-  }
+
   double sum = S;
   for (auto iter = hist.begin(); iter != hist.end(); ++iter) {
     sum -= iter->second;
@@ -27,8 +27,8 @@ double get_median(const map<int, size_t> &hist) {
   return -1;
 }
 
-double get_mad(const map<int, size_t> &hist, double median) { // median absolute deviation
-  map<int, size_t> hist2;
+double get_mad(const std::map<int, size_t> &hist, double median) { // median absolute deviation
+  std::map<int, size_t> hist2;
   for (auto iter = hist.begin(); iter != hist.end(); ++iter) {
     double x = fabs(iter->first - median);
     hist2[x] = iter->second;
@@ -50,116 +50,62 @@ class InsertSizeHistogramCounter {
   typedef std::map<int, size_t> HistType;
 
  public:
-  InsertSizeHistogramCounter(graph_pack& gp, size_t edge_length_threshold): gp_(gp), hist_(),
-  edge_length_threshold_(edge_length_threshold), total_(0), counted_(0), k_(gp.k_value)
-  {
-  }
+  InsertSizeHistogramCounter(const graph_pack& gp, size_t edge_length_threshold, bool ignore_negative = false)
+      : gp_(gp), hist_(), edge_length_threshold_(edge_length_threshold),
+        total_(0), counted_(0), k_(gp.k_value), ignore_negative_(ignore_negative) { }
 
-  HistType GetHist() {
-    return hist_;
-  }
-
-  size_t GetTotal() const {
-    return total_;
-  }
-
-  size_t GetCounted() const {
-    return counted_;
-  }
-
-  void FilterPositiveValues() {
-    DEBUG("Filtering only positive values in the histogram");
-    for (HistType::iterator iter = hist_.begin(); iter != hist_.end(); ++iter) {
-      if (iter->first > 0) {
-        hist_.erase(hist_.begin(), iter);
-        break;
-      }
-    }
-  }
-
-  //TODO: complete it
-  //void FilterValues() {
-  //vector<size_t> maxima;
-  //FindLocalMaxima(hist, maxima);
-
-  //DEBUG("Filtering values in the neighbourhood of the maximums in the histogram");
-  //}
+  HistType hist() { return hist_; }
+  size_t total() const { return total_; }
+  size_t mapped() const { return counted_; }
 
   template<class PairedRead>
-    void CountHistogram(io::IReader<PairedRead>& stream) {
+  void Count(io::ReadStreamVector< io::IReader<PairedRead> >& streams) {
+    hist_.clear();
+
+    size_t nthreads = streams.size();
+    std::vector<HistType*> hists(nthreads);
+    hists[0] = &hist_;
+    for (size_t i = 1; i < nthreads; ++i)
+      hists[i] = new HistType();
+
+    INFO("Processing paired reads (takes a while)");
+    size_t counted = 0;
+    size_t total = 0;
+#pragma omp parallel for num_threads(nthreads) reduction(+ : counted, total)
+    for (size_t i = 0; i < nthreads; ++i) {
+      PairedRead r;
+      io::IReader<PairedRead>& stream = streams[i];
       stream.reset();
-      hist_.clear();
-      counted_ = 0;
-      total_ = 0;
 
-      INFO("Processing paired reads (takes a while)");
       while (!stream.eof()) {
-        PairedRead r;
         stream >> r;
-        counted_ += ProcessPairedRead(r, hist_);
-        ++total_;
+        counted += ProcessPairedRead(r, *hists[i]);
+        ++total;
       }
     }
 
-  template<class PairedRead>
-    void CountHistogramParallel(io::ReadStreamVector< io::IReader<PairedRead> >& streams) {
-      hist_.clear();
-
-      size_t nthreads = streams.size();
-      vector<HistType*> hists(nthreads);
-      hists[0] = &hist_;
-      for (size_t i = 1; i < nthreads; ++i) {
-        hists[i] = new HistType();
+    total_ = total;
+    counted_ = counted;
+    INFO("Merging insert size histogram");
+    for (size_t i = 1; i < nthreads; ++i) {
+      for (auto it = hists[i]->begin(); it != hists[i]->end(); ++it) {
+        (*hists[0])[it->first] += it->second;
       }
-
-      INFO("Processing paired reads (takes a while)");
-      size_t counted = 0;
-      size_t total = 0;
-      #pragma omp parallel num_threads(nthreads)
-      {
-        #pragma omp for reduction(+ : counted, total)
-        for (size_t i = 0; i < nthreads; ++i) {
-
-          PairedRead r;
-          io::IReader<PairedRead>& stream = streams[i];
-          stream.reset();
-
-          while (!stream.eof()) {
-            stream >> r;
-            counted += ProcessPairedRead(r, *hists[i]);
-            ++total;
-          }
-        }
-      }
-
-      total_ = total;
-      counted_ = counted;
-
-      INFO("Merging insert size histogram");
-      for (size_t i = 1; i < nthreads; ++i) {
-        for (auto it = hists[i]->begin(); it != hists[i]->end(); ++it) {
-          (*hists[0])[it->first] += it->second;
-        }
-        delete hists[i];
-      }
-
+      delete hists[i];
     }
+  }
 
   void FindMean(double& mean, double& delta, std::map<size_t, size_t>& percentiles) const {
-    size_t often = 0;
-    size_t mode = -1;
-    for (auto iter = hist_.begin(); iter != hist_.end(); ++iter) {
-      if (iter->second > often) {
-        often = iter->second;
-        mode = iter->first;
-      }
-    }
+    double median = get_median(hist_);
+    double mad = get_mad(hist_, median);
+    double low = median - 5. * 1.4826 * mad;
+    double high = median + 5. * 1.4826 * mad;
 
-    DEBUG("Mode " << mode);
-    int low = -mode;
-    int high = 3 * mode;
+    DEBUG("Median IS: " << median);
+    DEBUG("MAD: " << mad);
+    DEBUG("Thresholds set to: [" << low << ", " << high << "]");
 
-    int n = 0;
+    size_t n = 0;
     double sum = 0.;
     double sum2 = 0.;
     DEBUG("Counting average");
@@ -177,6 +123,10 @@ class InsertSizeHistogramCounter {
     low = mean - 5 * delta;
     high = mean + 5 * delta;
 
+    DEBUG("Mean IS: " << mean);
+    DEBUG("sd: " << delta);
+    DEBUG("Thresholds set to: [" << low << ", " << high << "]");
+
     n = 0;
     sum = 0.;
     sum2 = 0.;
@@ -190,6 +140,9 @@ class InsertSizeHistogramCounter {
     }
     mean = sum / n;
     delta = sqrt(sum2 / n - mean * mean);
+
+    DEBUG("Mean IS: " << mean);
+    DEBUG("sd: " << delta);
 
     size_t m = 0;
 
@@ -226,29 +179,29 @@ class InsertSizeHistogramCounter {
   }
 
  private:
-  graph_pack& gp_;
+  const graph_pack& gp_;
   HistType hist_;
   size_t edge_length_threshold_;
   size_t total_;
   size_t counted_;
   size_t k_;
+  bool ignore_negative_;
 
   template<class PairedRead>
-  size_t ProcessPairedRead(PairedRead& r, HistType& hist) {
+  size_t ProcessPairedRead(const PairedRead& r, HistType& hist) {
     Sequence sequence_left = r.first().sequence();
     Sequence sequence_right = r.second().sequence();
 
-    if (sequence_left.size() <= k_ || sequence_right.size() <= k_) {
+    if (sequence_left.size() <= k_ || sequence_right.size() <= k_)
       return 0;
-    }
+
     runtime_k::RtSeq left = sequence_left.end<runtime_k::RtSeq>(k_ + 1);
     runtime_k::RtSeq right = sequence_right.start<runtime_k::RtSeq>(k_ + 1);
     left = gp_.kmer_mapper.Substitute(left);
     right = gp_.kmer_mapper.Substitute(right);
-    if (!gp_.index.contains(left) || !gp_.index.contains(right)) {
-      // TODO rather use binary search.
+    if (!gp_.index.contains(left) || !gp_.index.contains(right))
       return 0;
-    }
+
     auto pos_left = gp_.index.get(left);
     auto pos_right = gp_.index.get(right);
     if (pos_left.first != pos_right.first || gp_.g.length(pos_left.first) < edge_length_threshold_) {
@@ -256,7 +209,8 @@ class InsertSizeHistogramCounter {
     }
     int is = pos_right.second - pos_left.second - k_ - 1 - r.insert_size()
              + sequence_left.size() + sequence_right.size();
-    hist[is] += 1;
+    if (is > 0 || !ignore_negative_)
+      hist[is] += 1;
 
     return 1;
   }
@@ -266,55 +220,37 @@ typedef std::map<int, size_t> HistType;
 
 template<class graph_pack, class PairedRead>
 void refine_insert_size(io::ReadStreamVector<io::IReader<PairedRead> >& streams, graph_pack& gp,
-        size_t edge_length_threshold,
-        double& mean,
-        double& delta,
-        double& median,
-        double& mad,
-        std::map<size_t, size_t>& percentiles,
-        HistType& hist)
-  {
-    INFO("SUBSTAGE == Refining insert size and its distribution");
-    InsertSizeHistogramCounter<graph_pack> hist_counter(gp, edge_length_threshold);
+                        size_t edge_length_threshold,
+                        double& mean, double& delta,
+                        double& median, double& mad,
+                        std::map<size_t, size_t>& percentiles,
+                        HistType& hist) {
+  INFO("SUBSTAGE == Refining insert size and its distribution");
+  InsertSizeHistogramCounter<graph_pack> hist_counter(gp, edge_length_threshold, /* ignore negative */ true);
+  hist_counter.Count(streams);
 
-    if (streams.size() == 1) {
-        hist_counter.CountHistogram(streams.back());
-    } else {
-        hist_counter.CountHistogramParallel(streams);
-    }
+  if (hist_counter.mapped() == 0)
+    return;
 
-    hist_counter.FilterPositiveValues();
-
-    size_t n = hist_counter.GetCounted();
-    size_t total = hist_counter.GetTotal();
-
-    if (n == 0)
-        return;
-
-    INFO(n << " paired reads (" << (n * 100.0 / total) << "% of all) aligned to long edges");
-    hist_counter.FindMean(mean, delta, percentiles);
-    hist_counter.FindMedian(median, mad, hist);
+  INFO(hist_counter.mapped() << " paired reads (" << (hist_counter.mapped() * 100.0 / hist_counter.total()) << "% of all) aligned to long edges");
+  hist_counter.FindMean(mean, delta, percentiles);
+  hist_counter.FindMedian(median, mad, hist);
 }
 
 template<class graph_pack, class PairedRead>
 void GetInsertSizeHistogram(io::ReadStreamVector< io::IReader<PairedRead> >& streams,
                             graph_pack& gp,
-                            double insert_size,
-                            double delta,
-                            HistType& hist)
-{
-    size_t edge_length_threshold = Nx(gp.g, 50);//500;
-    InsertSizeHistogramCounter<graph_pack> hist_counter(gp, edge_length_threshold);
-    if (streams.size() == 1) {
-        hist_counter.CountHistogram(streams.back());
-    } else {
-        hist_counter.CountHistogramParallel(streams);
-    }
-    double low = insert_size - 5. * delta;
-    double high = insert_size + 5. * delta;
+                            double insert_size, double delta,
+                            HistType& hist) {
+  size_t edge_length_threshold = Nx(gp.g, 50); //500;
+  InsertSizeHistogramCounter<graph_pack> hist_counter(gp, edge_length_threshold);
+  hist_counter.Count(streams);
 
-    INFO("Cropping the histogram");
-    hist_crop(hist_counter.GetHist(), low, high, hist);
+  double low = insert_size - 5. * delta;
+  double high = insert_size + 5. * delta;
+
+  INFO("Cropping the histogram");
+  hist_crop(hist_counter.hist(), low, high, hist);
 }
 
 }
