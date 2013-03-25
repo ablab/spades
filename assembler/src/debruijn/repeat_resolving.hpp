7963d9ce78a0698a51a9ed1d36062b7e1baa37f1
Sergey Nurk <sergeynurk@gmail.com>
2013-03-25 16:57:08 +0400

    legacy code removed from repeat_resolving.hpp; one_many_contigs_enlarger removed

--- assembler/src/debruijn/repeat_resolving.hpp
+++ assembler/src/debruijn/repeat_resolving.hpp
@@ -16,10 +16,10 @@
 namespace debruijn_graph {
 
 template<class Graph>
-void ResolveRepeats(Graph &g, IdTrackHandler<Graph> &old_IDs,
+void ResolveRepeats(Graph &g, IdTrackHandler<Graph> &old_ids,
 		const PairedInfoIndexT<Graph> &info,
 		EdgesPositionHandler<Graph> &edges_pos, Graph &new_graph,
-		IdTrackHandler<Graph> &new_IDs,
+		IdTrackHandler<Graph> &new_ids,
 		EdgesPositionHandler<Graph> &edges_pos_new, const string& output_folder,
 		EdgeLabelHandler<Graph> &LabelsAfter, bool developer_mode) {
 	INFO("SUBSTAGE == Resolving primitive repeats");
@@ -37,8 +37,8 @@ void ResolveRepeats(Graph &g, IdTrackHandler<Graph> &old_IDs,
 	}
 	DeletedVertexHandler<Graph> tmp_deleted_handler(new_graph);
 	TRACE("deleted handler created");
-	RepeatResolver<Graph> repeat_resolver(g, old_IDs, info, edges_pos,
-			new_graph, new_IDs, edges_pos_new, tmp_deleted_handler, LabelsAfter,
+	RepeatResolver<Graph> repeat_resolver(g, old_ids, info, edges_pos,
+			new_graph, new_ids, edges_pos_new, tmp_deleted_handler, LabelsAfter,
 			developer_mode);
 	make_dir(output_folder);
 	auto edge_labels = repeat_resolver.GetEdgeLabels();
@@ -47,129 +47,5 @@ void ResolveRepeats(Graph &g, IdTrackHandler<Graph> &old_IDs,
 	repeat_resolver.ResolveRepeats(output_folder);
 	DEBUG("Primitive repeats resolved");
 }
-
-//void ResolveOneComponent(const string& load_from_dir, const string& save_to_dir,
-//		int component_id, int k) {
-//	string load_from = ConstructComponentName(load_from_dir + "/graphCl",
-//			component_id);
-//	string save_to = ConstructComponentName(save_to_dir + "/graph",
-//			component_id);
-//
-//	string save_resolving_history = ConstructComponentName(
-//			save_to_dir + "/resolve", component_id);
-//	make_dir(save_resolving_history);
-//
-//	NCGraph new_graph(k);
-//	IdTrackHandler<NCGraph> NewIntIds(new_graph);
-//	PairedInfoIndex<NCGraph> new_index(new_graph);
-//	EdgesPositionHandler<NCGraph> EdgePosBefore(new_graph);
-//	scanNCGraph(new_graph, NewIntIds, load_from, &new_index, EdgePosBefore);
-//
-//	RealIdGraphLabeler<NCGraph> IdTrackLabelerAfter(new_graph, NewIntIds);
-//
-//	omnigraph::WriteSimple(new_graph, IdTrackLabelerAfter, save_to + "_before.dot", "no_repeat_graph");
-//
-//	NonconjugateDeBruijnGraph resolved_graph(k);
-//	IdTrackHandler<NCGraph> Resolved_IntIds(resolved_graph);
-//	EdgesPositionHandler<NCGraph> EdgePosAfter(resolved_graph);
-//	EdgeLabelHandler<NCGraph> LabelsAfter(resolved_graph, new_graph);
-//
-//	ResolveRepeats(new_graph, NewIntIds, new_index, EdgePosBefore,
-//			resolved_graph, Resolved_IntIds, EdgePosAfter,
-//			save_resolving_history + "/", LabelsAfter);
-//
-//	RealIdGraphLabeler<NCGraph> IdTrackLabelerResolved(resolved_graph,
-//			Resolved_IntIds);
-//
-//	TotalLabelerGraphStruct<NCGraph> graph_struct_before(new_graph,
-//				&NewIntIds, &EdgePosBefore, NULL);
-//	TotalLabelerGraphStruct<NCGraph> graph_struct_after(resolved_graph,
-//				&Resolved_IntIds, &EdgePosAfter, &LabelsAfter);
-//	TotalLabeler<NCGraph> TotLabAfter(&graph_struct_after,
-//				&graph_struct_before);
-//
-//	omnigraph::WriteSimple(resolved_graph, TotLabAfter, save_to + "_after.dot", "no_repeat_graph");
-//
-//	EdgesPosGraphLabeler<NCGraph> EdgePosLAfterLab(resolved_graph,
-//			EdgePosAfter);
-//
-//	omnigraph::WriteSimple(resolved_graph, TotLabAfter,
-//			save_resolving_history + "/repeats_resolved_after_pos.dot",
-//			"no_repeat_graph");
-//
-//	ClipTips(resolved_graph);
-//	RemoveLowCoverageEdgesForResolver(resolved_graph);
-//
-//	omnigraph::WriteSimple(resolved_graph, TotLabAfter,
-//			save_resolving_history
-//					+ "/repeats_resolved_after_und_cleared_pos.dot",
-//			"no_repeat_graph");
-//	omnigraph::WriteSimple(resolved_graph, TotLabAfter,
-//			save_resolving_history + "/repeats_resolved_und_cleared.dot",
-//			"no_repeat_graph");
-//	one_many_contigs_enlarger<NCGraph> N50enlarger(resolved_graph, cfg::get().ds.IS);
-//	N50enlarger.one_many_resolve_with_vertex_split();
-//	omnigraph::WriteSimple(resolved_graph, IdTrackLabelerResolved, save_to + "_finished.dot", "no_repeat_graph");
-//}
-
-//void ConjugateResolveOneComponent(const string& load_from_dir, const string& save_to_dir,
-//		int component_id, int k) {
-//	string file_name = ConstructComponentName(load_from_dir + "/graphCl",
-//			component_id);
-//	string save_to = ConstructComponentName(save_to_dir + "/graph",
-//			component_id);
-//
-//	string save_resolving_history = ConstructComponentName(
-//			save_to_dir + "/resolve", component_id);
-//	make_dir(save_resolving_history);
-//
-//	Graph graph(k);
-//	IdTrackHandler<Graph> int_ids(graph);
-//	PairedInfoIndexT<Graph> paired_index(graph);
-//	EdgesPositionHandler<Graph> edge_pos(graph, cfg::get().pos.max_single_gap);
-//
-//	ConjugateDataScanner<Graph> scanner(graph, int_ids);
-//	ScanBasicGraph(file_name, scanner);
-//	scanner.loadPositions(file_name, edge_pos);
-//	scanner.loadPaired(file_name, paired_index);
-//
-//	RealIdGraphLabeler<Graph> id_track_labeler(graph, int_ids);
-//
-//	omnigraph::WriteSimple(graph, id_track_labeler, save_to + "_before.dot", "no_repeat_graph");
-//
-//	ConjugateDeBruijnGraph resolved_graph(k);
-//	IdTrackHandler<Graph> resolved_int_ids(resolved_graph);
-//	EdgesPositionHandler<Graph> resolved_edge_pos(resolved_graph, cfg::get().pos.max_single_gap);
-//	EdgeLabelHandler<Graph> resolve_mapper(resolved_graph, graph);
-//
-//	ResolveRepeats(graph, int_ids, paired_index, edge_pos,
-//			resolved_graph, resolved_int_ids, resolved_edge_pos,
-//			save_resolving_history + "/", resolve_mapper);
-//
-//	RealIdGraphLabeler<Graph> resolved_id_track_labeler(resolved_graph,
-//			resolved_int_ids);
-//	omnigraph::WriteSimple(resolved_graph, resolved_id_track_labeler, save_to + "_after.dot", "no_repeat_graph");
-//
-//	EdgesPosGraphLabeler<Graph> resolved_edge_pos_labeler(resolved_graph,
-//			resolved_edge_pos);
-//
-//	omnigraph::WriteSimple(resolved_graph, resolved_edge_pos_labeler,
-//			save_resolving_history + "/repeats_resolved_after_pos.dot",
-//			"no_repeat_graph");
-//
-//	ClipTips(resolved_graph);
-//	RemoveLowCoverageEdgesForResolver(resolved_graph);
-//
-//	omnigraph::WriteSimple(resolved_graph, resolved_edge_pos_labeler,
-//			save_resolving_history
-//					+ "/repeats_resolved_after_und_cleared_pos.dot",
-//			"no_repeat_graph");
-////	omnigraph::WriteSimple(resolved_graph, resolved_id_track_labeler,
-////			save_resolving_history + "/repeats_resolved_und_cleared.dot",
-////			"no_repeat_graph");
-////	one_many_contigs_enlarger<Graph> N50enlarger(resolved_graph, cfg::get().ds.IS);
-////	N50enlarger.one_many_resolve_with_vertex_split();
-////	omnigraph::WriteSimple(resolved_graph, resolved_id_track_labeler, save_to + "_finished.dot", "no_repeat_graph");
-//}
 
 }
--- assembler/src/include/omni/abstract_graph.hpp
+++ assembler/src/include/omni/abstract_graph.hpp
@@ -11,131 +11,130 @@
 
 namespace omnigraph {
 
-
 class CoveredEdge {
-private:
-	int coverage_;
+ private:
+    int coverage_;
 
-public:
-	CoveredEdge() :
-			coverage_(0) { }
+ public:
+    CoveredEdge()
+            : coverage_(0) {
+    }
 
-	void SetCoverage(int coveradge) {
-		coverage_ = coveradge;
-	}
+    void SetCoverage(int coveradge) {
+        coverage_ = coveradge;
+    }
 
-	void IncCoverage(int value) {
-		coverage_ += value;
-	}
+    void IncCoverage(int value) {
+        coverage_ += value;
+    }
 
-	//not length normalized
-	int GetRawCoverage() const {
-		return coverage_;
-	}
+    //not length normalized
+    int GetRawCoverage() const {
+        return coverage_;
+    }
 };
 
-
-
 template<typename VertexIdT, typename EdgeIdT, class DataMasterT>
-class AbstractGraph: public AbstractEditableGraph<VertexIdT, EdgeIdT, DataMasterT, typename set<VertexIdT>::const_iterator> {
-	typedef AbstractEditableGraph<VertexIdT, EdgeIdT, DataMasterT, typename set<VertexIdT>::const_iterator> base;
+class AbstractGraph : public AbstractEditableGraph<VertexIdT, EdgeIdT,
+        DataMasterT, typename set<VertexIdT>::const_iterator> {
+    typedef AbstractEditableGraph<VertexIdT, EdgeIdT, DataMasterT,
+            typename set<VertexIdT>::const_iterator> base;
 
-public:
-	typedef VertexIdT VertexId;
-	typedef EdgeIdT EdgeId;
-	typedef DataMasterT DataMaster;
-	typedef typename DataMaster::VertexData VertexData;
-	typedef typename DataMaster::EdgeData EdgeData;
-	typedef typename base::VertexIterator VertexIterator;
-  typedef typename VertexId::type::edge_const_iterator edge_const_iterator;
+ public:
+    typedef VertexIdT VertexId;
+    typedef EdgeIdT EdgeId;
+    typedef DataMasterT DataMaster;
+    typedef typename DataMaster::VertexData VertexData;
+    typedef typename DataMaster::EdgeData EdgeData;
+    typedef typename base::VertexIterator VertexIterator;
+    typedef typename VertexId::type::edge_const_iterator edge_const_iterator;
 
-protected:
+ protected:
 
-	set<VertexId> vertices_;
+    set<VertexId> vertices_;
 
-public:
-	AbstractGraph(HandlerApplier<VertexId, EdgeId>* applier,
-			const DataMaster& master) :
-		base(applier, master) {
-	}
+ public:
+    AbstractGraph(HandlerApplier<VertexId, EdgeId>* applier,
+                  const DataMaster& master)
+            : base(applier, master) {
+    }
 
-	virtual ~AbstractGraph() {
-		TRACE("~AbstractGraph");
-	}
+    virtual ~AbstractGraph() {
+        TRACE("~AbstractGraph");
+    }
 
-	virtual const vector<EdgeId> OutgoingEdges(VertexId v) const {
-		return v->OutgoingEdges();
-	}
+    virtual const vector<EdgeId> OutgoingEdges(VertexId v) const {
+        return v->OutgoingEdges();
+    }
 
-  virtual edge_const_iterator out_begin(VertexId v) const {
-    return v->out_begin();
-  }
+    virtual edge_const_iterator out_begin(VertexId v) const {
+        return v->out_begin();
+    }
 
-  virtual edge_const_iterator out_end(VertexId v) const {
-    return v->out_end();
-  }
+    virtual edge_const_iterator out_end(VertexId v) const {
+        return v->out_end();
+    }
 
-	virtual const vector<EdgeId> IncomingEdges(VertexId v) const {
-		return v->IncomingEdges();
-	}
+    virtual const vector<EdgeId> IncomingEdges(VertexId v) const {
+        return v->IncomingEdges();
+    }
 
-  virtual edge_const_iterator in_begin(VertexId v) const {
-    return v->in_begin();
-  }
+    virtual edge_const_iterator in_begin(VertexId v) const {
+        return v->in_begin();
+    }
 
-  virtual edge_const_iterator in_end(VertexId v) const {
-    return v->in_end();
-  }
+    virtual edge_const_iterator in_end(VertexId v) const {
+        return v->in_end();
+    }
 
-	virtual size_t OutgoingEdgeCount(VertexId v) const {
-		return v->OutgoingEdgeCount();
-	}
+    virtual size_t OutgoingEdgeCount(VertexId v) const {
+        return v->OutgoingEdgeCount();
+    }
 
-	virtual size_t IncomingEdgeCount(VertexId v) const {
-		return v->IncomingEdgeCount();
-	}
+    virtual size_t IncomingEdgeCount(VertexId v) const {
+        return v->IncomingEdgeCount();
+    }
 
-	virtual vector<EdgeId> GetEdgesBetween(VertexId v, VertexId u) const {
-		return v->OutgoingEdgesTo(u);
-	}
+    virtual vector<EdgeId> GetEdgesBetween(VertexId v, VertexId u) const {
+        return v->OutgoingEdgesTo(u);
+    }
 
-	virtual const EdgeData& data(EdgeId edge) const {
-		return edge->data();
-	}
+    virtual const EdgeData& data(EdgeId edge) const {
+        return edge->data();
+    }
 
-	virtual const VertexData& data(VertexId v) const {
-		return v->data();
-	}
+    virtual const VertexData& data(VertexId v) const {
+        return v->data();
+    }
 
-	virtual VertexIterator begin() const {
-		return vertices_.begin();
-	}
+    virtual VertexIterator begin() const {
+        return vertices_.begin();
+    }
 
-	virtual VertexIterator end() const {
-		return vertices_.end();
-	}
+    virtual VertexIterator end() const {
+        return vertices_.end();
+    }
 
-	size_t size() const {
-		return vertices_.size();
-	}
+    size_t size() const {
+        return vertices_.size();
+    }
 
-	virtual VertexId EdgeStart(EdgeId edge) const {
-		return edge->start();
-	}
+    virtual VertexId EdgeStart(EdgeId edge) const {
+        return edge->start();
+    }
 
-	virtual VertexId EdgeEnd(EdgeId edge) const {
-		return edge->end();
-	}
+    virtual VertexId EdgeEnd(EdgeId edge) const {
+        return edge->end();
+    }
 
+ protected:
 
-protected:
+    virtual void AddVertexToGraph(VertexId vertex) = 0;
 
-	virtual void AddVertexToGraph(VertexId vertex) = 0;
+    virtual void DeleteVertexFromGraph(VertexId vertex) = 0;
 
-	virtual void DeleteVertexFromGraph(VertexId vertex) = 0;
-
-
-private:
-DECL_LOGGER("AbstractGraph");
+ private:
+    DECL_LOGGER("AbstractGraph")
+    ;
 };
 }
