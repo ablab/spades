d1ebca20ae691ac3da44c351810acd6096787b26
Ksenia Krasheninnikova <ksenia@ace.bioinf.spbau.ru>
2013-07-23 13:09:09 +0400

    some info

--- assembler/src/debruijn/repeat.hpp
+++ assembler/src/debruijn/repeat.hpp
@@ -5,6 +5,39 @@ namespace debruijn_graph {
 
 	typedef enum { TOPOLOGY, LENGTH, PAIREDINFO } kind_of_repeat;
 
+	bool if_unique_val(double val, const vector<double>& ar, double epsilon=0.000001) {
+		unsigned counter = 0;
+		for (auto it = ar.begin(); it != ar.end(); ++it) {
+			if (fabs(val - *it)<=epsilon) counter+=1;
+		}
+		return (counter==1);
+	}
+
+	double get_max(const vector<vector<double>>& vec, int& max_id_i, int& max_id_j, double epsilon=0.000001) {
+		double max_val = 0;
+		max_id_i = -1;
+		max_id_j = -1;
+		for (unsigned i = 0; i < vec.size(); ++i) {
+			for (unsigned j = 0; j < vec[i].size(); ++j) {
+				if (vec[i][j] - max_val > epsilon ){
+					max_val = vec[i][j];
+					max_id_i = i;
+					max_id_j = j;
+				}
+			}
+		}
+		return max_val;
+	}
+	
+	void hide(vector<vector<double>>& vec, int i, double default_val = -1) {
+		for (unsigned j = 0; j < vec[i].size(); ++j) vec[i][j] = default_val;
+	}
+
+	void hide(vector<vector<double>>& vec, int i, int j, double default_val = -1) {
+		for (unsigned k = 0; k < vec[i].size(); ++k) vec[i][k] = default_val;
+		for (unsigned k = 0; k < vec.size(); ++k) vec[k][j] = default_val;
+	}
+
 	template <class graph_pack>
 	class Repeat {
 		
@@ -18,10 +51,26 @@ namespace debruijn_graph {
 		const double repeat_length_upper_threshold_;
 		const map<EdgeId, kind_of_repeat> edge_to_kind_;
 
-		void ChoosePairsGreedy( const vector< vector <double> > transition_probabilities, vector<pair<EdgeId,EdgeId>>& pairs_of_edges) {
-			unsigned k = 0;			
-			set<unsigned> matched;
-			DEBUG("MATCH: ");
+	
+		void ChoosePairsGreedy(vector<vector<double>>& transition_probabilities, vector<pair<EdgeId,EdgeId>>& pairs_of_edges, double match_quality_threshold) {
+			unsigned counter(0);
+			while (counter < transition_probabilities.size()) {
+				int max_id_i(0), max_id_j(0);
+				double max_val = get_max(transition_probabilities, max_id_i, max_id_j);
+				if (max_id_i != -1) {
+					if (!if_unique_val(max_val, transition_probabilities[max_id_i])) {
+						hide(transition_probabilities, max_id_i);
+					}
+					else {
+						hide(transition_probabilities, max_id_i, max_id_j);
+						if (max_val > match_quality_threshold) {
+							pairs_of_edges.push_back(make_pair(incoming_edges_[max_id_i], outgoing_edges_[max_id_j]));
+							INFO("pair: " << max_id_i << " " << max_id_j << " " << max_val);
+						}
+					}
+				}
+				counter += 1;
+			}
 		}
 
 		//TODO: Dima: make gp_.edge_pos.IsConsistentWithGenome() a const method
@@ -107,48 +156,47 @@ namespace debruijn_graph {
 
 		template <class DetailedCoverage>
 		void GetComponentInfo(const DetailedCoverage& coverage, const vector< vector <double> >& transition_probabilities, const EdgeQuality<typename graph_pack::graph_t>& quality_labeler ) const  {
-			DEBUG("Component: ");
+			INFO("Component: ");
 			for ( auto iter = component_.begin(); iter != component_.end(); ++iter ) {
-				DEBUG( gp_.g.int_id(*iter)  << " edge length: " << gp_.g.length(*iter) <<  " average edge coverage " 
+				INFO( gp_.g.int_id(*iter)  << " edge length: " << gp_.g.length(*iter) <<  " average edge coverage " 
 					<< gp_.g.coverage(*iter) << " quality: " << quality_labeler.quality(*iter) << " ");
 				auto repeat_type = edge_to_kind_.find(*iter);
 				VERIFY(repeat_type != edge_to_kind_.end()); 
 				if ( repeat_type->second == TOPOLOGY){
-					DEBUG("TOPOLOGY");
+					INFO("TOPOLOGY");
 				}
 				else if  (repeat_type->second == LENGTH ){
-					DEBUG("LENGTH");
+					INFO("LENGTH");
 				}
 				else if (repeat_type->second == PAIREDINFO ){
-					DEBUG("PAIREDINFO");
+					INFO("PAIREDINFO");
 				}
 			 }
-			DEBUG("\n");
-			DEBUG("incoming edges: ");
+			INFO("\n");
+			INFO("incoming edges: ");
 			for ( auto iter = incoming_edges_.begin(); iter != incoming_edges_.end(); ++iter ) {
-			 	DEBUG(gp_.g.int_id(*iter)  << " edge length: " << gp_.g.length(*iter) << " outgoing edge coverage: " << coverage.GetOutCov(*iter) << 
-					" average edge coverage " << gp_.g.coverage(*iter) << " quality: " << quality_labeler.quality(*iter) << "\n");
+			 	INFO(gp_.g.int_id(*iter)  << " edge length: " << gp_.g.length(*iter) << " outgoing edge coverage: " << coverage.GetOutCov(*iter) << 
+					" average edge coverage " << gp_.g.coverage(*iter) << " quality: " << quality_labeler.quality(*iter));
 			}
-			DEBUG("\n");
-			DEBUG("outgoing edges: ");
+			INFO("\n");
+			INFO("outgoing edges: ");
 			for ( auto iter = outgoing_edges_.begin(); iter != outgoing_edges_.end(); ++iter ) {
-				DEBUG(gp_.g.int_id(*iter)  << " edge length: " << gp_.g.length(*iter) << " incoming edge coverage: " << coverage.GetInCov(*iter) <<
-					" average edge coverage " << gp_.g.coverage(*iter) << " quality: " << quality_labeler.quality(*iter) << "\n");
+				INFO(gp_.g.int_id(*iter)  << " edge length: " << gp_.g.length(*iter) << " incoming edge coverage: " << coverage.GetInCov(*iter) <<
+					" average edge coverage " << gp_.g.coverage(*iter) << " quality: " << quality_labeler.quality(*iter));
 			}
 			bool correct_component = IfRepeatByQuality( quality_labeler  );
 			if (!correct_component) {
-				DEBUG("repeat is detected incorrectly\n");
+				INFO("repeat is detected incorrectly");
 			}
 			if (transition_probabilities.size() > 0) {
-			 	DEBUG("transition probabilities:\n");
+			 	INFO("transition probabilities:\n");
 				for (auto vec = transition_probabilities.begin(); vec != transition_probabilities.end(); ++vec ) {
 					for ( auto prob = vec->begin(); prob != vec->end(); ++prob ) {
-						DEBUG(*prob << " ");
+						INFO(*prob << " ");
 					}
-					DEBUG("\n");
+					INFO("\n");
 				}
 			}
-			DEBUG("\n");
 				
 		}
 
@@ -188,10 +236,44 @@ namespace debruijn_graph {
 			return maxPathLen;
 		}
 
+		void dfs( const VertexId& vStart, const VertexId& vEnd, set<EdgeId>& visited,  vector<EdgeId>& path) const {
+			for ( auto edge = component_.begin(); edge != component_.end(); ++edge ){
+				if (visited.find(*edge) != visited.end()){
+					continue;
+				}
+				if ( vStart == gp_.g.EdgeStart(*edge) ){
+					if ( vEnd == gp_.g.EdgeEnd(*edge) ){
+						 path.push_back(*edge);
+						 visited.insert(*edge);
+						 return;
+					}
+					visited.insert(*edge);
+					dfs( gp_.g.EdgeEnd(*edge), vEnd, visited, path );
+					path.insert(path.begin(), *edge);
+					return;
+				}
+			}
+			return;
+		}
+
+		void SetPaths(const vector<pair<EdgeId,EdgeId>>& pairs_of_edges, vector<vector<EdgeId>>& resolved_paths) {
+			for (unsigned i = 0; i < pairs_of_edges.size(); ++i) {
+				EdgeId in_edge = pairs_of_edges[i].first;
+				EdgeId out_edge = pairs_of_edges[i].second;
+				vector<EdgeId> path;
+				set<EdgeId> visited;
+				dfs(gp_.g.EdgeEnd(in_edge), gp_.g.EdgeStart(out_edge), visited, path);
+				path.insert(path.begin(),in_edge);
+				path.push_back(out_edge);
+				resolved_paths.push_back(path);
+			}
+
+		}
+
 		template <class DetailedCoverage>
 		bool Resolve( BucketMapper<Graph> &bm, const DetailedCoverage& coverage,
 							const EdgeQuality<typename graph_pack::graph_t>& quality_labeler,
-							set<vector<EdgeId>>& resolved_paths) { 
+							vector<vector<EdgeId>>& resolved_paths) { 
 			vector<pair<EdgeId,EdgeId>> pairs_of_edges;
 			vector< vector <double> > transition_probabilities ;
 			for ( unsigned i = 0; i < incoming_edges_.size(); i++) {
@@ -213,8 +295,12 @@ namespace debruijn_graph {
 				}
 			}
 			GetComponentInfo(coverage, transition_probabilities, quality_labeler );
-			if (pairs_of_edges.size() > 0) return true;
-
+			double match_quality_threshold = 0.05;
+			ChoosePairsGreedy(transition_probabilities, pairs_of_edges, match_quality_threshold);
+			if (pairs_of_edges.size() > 0) {
+				SetPaths(pairs_of_edges, resolved_paths);
+				return true;
+			}
 			return false;
 		}
 	};
