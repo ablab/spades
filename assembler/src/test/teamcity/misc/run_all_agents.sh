68201a4ee801df0b9da706b5a5da61e169590411
Andrey Prjibelski <andrewprzh@gmail.com>
2013-08-22 17:00:01 +0400

    misc scripts for tc

---
+++ assembler/src/test/teamcity/misc/run_all_agents.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+############################################################################
+# Copyright (c) 2011-2013 Saint-Petersburg Academic University
+# All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
+for i in ant00Agent ant00Agent_2 ant01agent ant02agent ant03agent ant04agent ant05agent ant06agent ant07agent ant13agent
+do
+        echo Starting $i...
+	./$i/bin/agent.sh start
+        echo Done
+done
+
