e09f4e0b1857948d4dd40eebdd4c9e11403519ae
Anton Korobeynikov <anton@korobeynikov.info>
2013-08-27 02:50:21 +0400

    Better handle memory / disk tradeof for DSU`

--- assembler/src/include/adt/concurrent_dsu.hpp
+++ assembler/src/include/adt/concurrent_dsu.hpp
@@ -131,13 +131,22 @@ class ConcurrentDSU {
     return count;
   }
 
-    void get_sets2(const std::string& FileName) {
+  size_t extract_to_file(const std::string& Prefix) {
     // First, touch all the sets to make them directly connect to the root
 #   pragma omp parallel for
     for (size_t x = 0; x < size; ++x)
         (void)find_set(x);
 
     std::unordered_map<size_t, size_t> sizes;
+
+#if 0
+    for (size_t x = 0; x < size; ++x) {
+        if (data[x].next != x) {
+            size_t t = data[x].next;
+            VERIFY(data[t].next == t)
+        }
+    }
+#endif
 
     // Insert all the root elements into the map
     for (size_t x = 0; x < size; ++x) {
@@ -167,16 +176,17 @@ class ConcurrentDSU {
     }
 
     // Write down the entries
-    MMappedRecordWriter<size_t> os(FileName);
-    os.reserve(off);
-    size_t *out = os.data();
+    std::vector<size_t> out(off);
     for (size_t x = 0; x < size; ++x) {
         size_t& entry = sizes[data[x].next];
         out[entry++] = x;
     }
+    std::ofstream os(Prefix, std::ios::binary | std::ios::out);
+    os.write((char*)&out[0], out.size() * sizeof(out[0]));
+    os.close();
 
     // Write down the sizes
-    MMappedRecordWriter<size_t> index(FileName + ".idx");
+    MMappedRecordWriter<size_t> index(Prefix + ".idx");
     index.reserve(sizes.size());
     size_t *idx = index.data();
     for (size_t x = 0, i = 0, sz = 0; x < size; ++x) {
@@ -186,6 +196,7 @@ class ConcurrentDSU {
         }
     }
 
+    return sizes.size();
   }
 
   void get_sets(std::vector<std::vector<size_t> > &otherWay) {
