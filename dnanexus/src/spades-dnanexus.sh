abbca1ffa953812e87f1f662f1b78eb5f5f0624d
Alexey Gurevich <alexeigurevich@gmail.com>
2013-08-27 18:33:53 +0400

    SPAdes 2.5.* DNAnexus App http://ablab.myjetbrains.com/youtrack/issue/Assembler-617

--- dnanexus/src/spades-dnanexus.sh
+++ dnanexus/src/spades-dnanexus.sh
@@ -1,31 +1,70 @@
+MAX_LIBS_NUMBER=5
+
+get_orientation() { 
+  gtable_id=$1
+  orientation='null'
+
+  for f in $(dx get_details $gtable_id); do
+    if [ $orientation == 'now' ]; then        
+      orientation=${f//\"/}  # removing double quotes
+      break
+    fi
+    if [ $f == '"pair_orientation":' ]; then
+      orientation='now'
+    fi
+  done
+
+  echo ${orientation,,} # to lowercase
+}
+
+process_reads() {
+  declare -a reads_array=("${!1}")
+  type=$2  # pe, mp or single
+
+  SPADES_READS=
+  for index in ${!reads_array[@]}
+  do
+    if [ $index -eq $MAX_LIBS_NUMBER ]; then          
+      break
+    fi
+
+    reads_gtable_name=$(dx-jobutil-parse-link "${reads_array[$index]}")
+    if [ $type == 'single' ]; then 
+      read_fname="unpaired_$index.fastq"
+
+      dx-reads-to-fastq $reads_gtable_name --output $read_fname
+      SPADES_READS="$SPADES_READS -s $read_fname"
+    else
+      lread_fname="left_${type}_${index}.fastq"
+      rread_fname="right_${type}_${index}.fastq"
+
+      dx-reads-to-fastq $reads_gtable_name --output $lread_fname --output2 $rread_fname
+      SPADES_READS="$SPADES_READS --$type$(($index + 1))-1 $lread_fname --$type$(($index + 1))-2 $rread_fname"
+      orientation=$(get_orientation "$reads_gtable_name")
+      if ! [ $orientation == 'null' ]; then
+          SPADES_READS="$SPADES_READS --$type$(($index + 1))-$orientation"
+      fi
+    fi
+  done
+
+  echo $SPADES_READS
+}
+
 main() {
   set -e -x
 
   # Look for the inputs and build the cmdline
 
-  # Look for paired reads
+  # Look for reads
   SPADES_READS=
-  for index in ${!left_reads[*]}
-  do
-      lread_link="${left_reads[$index]}"
-      lread_fname="left_$index.fastq"
-      rread_link="${right_reads[$index]}"
-      rread_fname="right_$index.fastq"
+  paired_reads_result=$(process_reads paired_reads[@] 'pe')
+  SPADES_READS="$SPADES_READS $paired_reads_result"
 
-      dx download "$lread_link" -o $lread_fname
-      dx download "$rread_link" -o $rread_fname
-      SPADES_READS="$SPADES_READS -1 $lread_fname -2 $rread_fname"
-  done
+  mate_pairs_result=$(process_reads mate_pairs[@] 'mp')
+  SPADES_READS="$SPADES_READS $mate_pairs_result"
 
-  # Look for single reads
-  for index in ${!single_reads[*]}
-  do
-      uread_link="${single_reads[$index]}"
-      uread_fname="unpaired_$index.fastq"
-
-      dx download "$uread_link" -o $uread_fname
-      SPADES_READS="$SPADES_READS -s $uread_fname"
-  done
+  unpaired_reads_result=$(process_reads unpaired_reads[@] 'single')
+  SPADES_READS="$SPADES_READS $unpaired_reads_result"
 
   # Check various options
   SPADES_MODE="--disable-gzip-output"
@@ -51,7 +90,12 @@ main() {
   spades.py -o spades_dnanexus $SPADES_READS $SPADES_MODE
 
   contigs=$(dx upload spades_dnanexus/contigs.fasta --brief)
-  scaffolds=$(dx upload spades_dnanexus/scaffolds.fasta --brief)
+  if [ -f spades_dnanexus/scaffolds.fasta ]; then
+      scaffolds=$(dx upload spades_dnanexus/scaffolds.fasta --brief)
+  else
+      echo "Scaffolds not found. Outputting contigs as scaffolds."
+      scaffolds=$contigs
+  fi  
 
   dx-jobutil-add-output contigs "$contigs" --class=file
   dx-jobutil-add-output scaffolds "$scaffolds" --class=file
