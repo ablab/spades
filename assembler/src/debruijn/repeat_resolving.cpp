0c726302d312ca57eea2be9b73ba5824c1efcd05
gorbunovain <vasilinetc.ira@gmail.com>
2013-11-06 19:14:32 +0400

    delete code from repeat resolving

--- assembler/src/debruijn/repeat_resolving.cpp
+++ assembler/src/debruijn/repeat_resolving.cpp
@@ -9,73 +9,15 @@
 #include "logger/logger.hpp"
 #include "debruijn_stats.hpp"
 #include "omni_labelers.hpp"
-
 #include "de/distance_estimation.hpp"
 #include "de/smoothing_distance_estimation.hpp"
-
 #include "omni/omni_utils.hpp"
-
 #include "path_extend/path_extend_launch.hpp"
 #include "contig_output.hpp"
-
 #include "long_read_storage.hpp"
-
-#if 0
-#include "loop_filter.hpp"
-#include "pac_index.hpp"
-#include "coverage_based_rr.hpp"
-#include "pacbio_aligner.hpp"
-#endif
-
 #include "repeat_resolving.hpp"
 
 namespace debruijn_graph {
-
-#if 0
-void resolve_repeats_by_coverage(conj_graph_pack& conj_gp, size_t insert_size, std::vector< PathInfo<Graph> >& filteredPaths,
-                                 PairedIndexT& clustered_index,
-                                 const EdgeQuality<Graph, Index>& quality_labeler) {
-
-    typedef DeBruijnEdgeIndex<KmerStoringDeBruijnEdgeIndex<conj_graph_pack::graph_t, runtime_k::RtSeq>> KmerIndex;
-        KmerIndex kmer_index((unsigned) conj_gp.g.k() + 1, conj_gp.g, cfg::get().output_dir);
-        if (cfg::get().developer_mode) {
-
-        std::string path;
-        if (cfg::get().entry_point <= ws_simplification)
-            path = cfg::get().output_dir + "/saves/debruijn_kmer_index_after_construction";
-        else
-            path = cfg::get().load_from + "/debruijn_kmer_index_after_construction";
-        bool val = LoadEdgeIndex(path, kmer_index);
-        VERIFY_MSG(val, "can not open file "+path+".kmidx");
-        INFO("Updating index from graph");
-
-        //DeBruijnEdgeIndexBuilder<runtime_k::RtSeq>().UpdateIndexFromGraph(kmer_index, conj_gp.g);
-        EdgeInfoUpdater<KmerIndex, Graph> updater(conj_gp.g, kmer_index);
-        updater.UpdateAll();
-        SaveEdgeIndex(cfg::get().output_dir + "/saves/debruijn_kmer_index_after_construction", kmer_index);
-    }
-
-    /*int number_of_buckets = 10;
-    auto bm = BucketMapper<conj_graph_pack::graph_t>(conj_gp.g, kmerIndex, cfg::get().K + 1, number_of_buckets);
-    bm.InitBuckets();
-
-    int bucket_in = 0;
-    int bucket_out = 0;
-    int repeat_distance = 500;
-    double probability = bm.GetProbablityFromBucketToBucketForDistance (bucket_in, bucket_out, repeat_distance) ;*/
-    //auto index = FlankingCoverage<Graph>(conj_gp.g, kmer_index, 50, cfg::get().K + 1);
-    FlankingCoverage<Graph, KmerIndex> index(conj_gp.g, kmer_index, 50);
-    EdgeLabelHandler<conj_graph_pack::graph_t> labels_after(conj_gp.g, conj_gp.g);
-    CoverageBasedResolution<conj_graph_pack, const EdgeQuality<Graph, Index>, KmerIndex> cov_rr
-    (conj_gp, kmer_index, quality_labeler, cfg::get().cbrr.tandem_ratio_lower_threshold,
-            cfg::get().cbrr.tandem_ratio_upper_threshold, cfg::get().cbrr.repeat_length_upper_threshold);
-    cov_rr.resolve_repeats_by_coverage(index, insert_size, labels_after, clustered_index, filteredPaths, cfg::get().output_dir + "resolved_by_coverage.fasta");
-
-    INFO("Repeats are resolved by coverage");
-
-}
-#endif
-
 
 /*
  * Return index of first paired-end library or -1 if there is no paired end library
@@ -103,29 +45,10 @@ void ConvertLongReads(LongReadContainerT& single_long_reads, vector<PathStorageI
     }
 }
 
-void PEResolving(conj_graph_pack& gp, const EdgeQuality<Graph, Index>& /* quality_labeler */) {
+void PEResolving(conj_graph_pack& gp) {
     vector<size_t> indexes;
     vector<PathStorageInfo<Graph> > long_reads_libs;
     ConvertLongReads(gp.single_long_reads, long_reads_libs);
-
-
-#if 0
-    if (cfg::get().coverage_based_rr_on == true) {
-        std::vector<PathInfo<Graph> > filteredPaths;
-        size_t pe_lib_index = GetFirstPELibIndex();
-        const io::SequencingLibrary<debruijn_config::DataSetData> &lib =
-                cfg::get().ds.reads[pe_lib_index];
-        resolve_repeats_by_coverage(gp, (size_t) lib.data().mean_insert_size,
-                                    filteredPaths, clustered_indices[0],
-                                    quality_labeler);
-        PathStorageInfo<Graph> single_storage(
-                filteredPaths,
-                cfg::get().pe_params.long_reads.coverage_base_rr.filtering,
-                cfg::get().pe_params.long_reads.coverage_base_rr.weight_priority,
-                cfg::get().pe_params.long_reads.coverage_base_rr.unique_edge_priority);
-        long_reads_libs.push_back(single_storage);
-    }
-#endif
 
     std::string name = "scaffolds.fasta";
     bool traverse_loops = true;
@@ -142,29 +65,6 @@ void RepeatResolution::run(conj_graph_pack &gp, const char*) {
     OutputContigs(gp.g, cfg::get().additional_contigs, cfg::get().use_unipaths,
                   cfg::get().simp.tec.plausibility_length);
     OutputContigs(gp.g, cfg::get().output_dir + "before_rr.fasta");
-
-    //What is this?
-//    if (cfg::get().developer_mode && cfg::get().pos.late_threading) {
-//        FillPos(gp, gp.genome, "10");
-//        FillPos(gp, !gp.genome, "11");
-//        if (!cfg::get().pos.contigs_for_threading.empty() &&
-//            FileExists(cfg::get().pos.contigs_for_threading))
-//          FillPosWithRC(gp, cfg::get().pos.contigs_for_threading, "thr_");
-//
-//        if (!cfg::get().pos.contigs_to_analyze.empty() &&
-//            FileExists(cfg::get().pos.contigs_to_analyze))
-//          FillPosWithRC(gp, cfg::get().pos.contigs_to_analyze, "anlz_");
-//    }
-
-    //todo refactor labeler creation -- and what's that?
-    total_labeler_graph_struct graph_struct(gp.g, &gp.int_ids,
-                                            &gp.edge_pos);
-    total_labeler tot_lab(&graph_struct);
-    EdgeQuality<Graph, Index> quality_labeler(gp.g, gp.index,
-                                              gp.kmer_mapper, gp.genome);
-    CompositeLabeler<Graph> labeler(tot_lab, quality_labeler);
-    detail_info_printer printer(gp, labeler, cfg::get().output_dir);
-    printer(ipp_before_repeat_resolution);
 
     bool no_valid_libs = true;
     for (size_t i = 0; i < cfg::get().ds.reads.lib_count(); ++i)
@@ -186,7 +86,7 @@ void RepeatResolution::run(conj_graph_pack &gp, const char*) {
     // Repeat resolving begins
     if (cfg::get().rm == debruijn_graph::resolving_mode::rm_path_extend) {
         INFO("Path-Extend repeat resolving");
-        PEResolving(gp, quality_labeler);
+        PEResolving(gp);
     } else {
         INFO("Unsupported repeat resolver");
         OutputContigs(gp.g, cfg::get().output_dir + "final_contigs.fasta");
