9077f3ec17cab4ba1853fa69da4c208d8d14c1fe
Dmitry <dmitrij.antipov@gmail.com>
2013-12-18 00:05:26 +0400

    merge resolved

--- assembler/src/debruijn/pacbio_aligning.cpp
+++ assembler/src/debruijn/pacbio_aligning.cpp
@@ -84,12 +84,7 @@ void align_pacbio(conj_graph_pack &gp, int lib_id) {
     std::vector<io::SingleRead> reads(read_buffer_size);
     io::SingleRead read;
     size_t buffer_no = 0;
-<<<<<<< HEAD
-    INFO("Aligning seed size: " << cfg::get().pb.pacbio_k);
-
-=======
     INFO("Seed size: " << cfg::get().pb.pacbio_k);
->>>>>>> master
     pacbio::PacBioMappingIndex<ConjugateDeBruijnGraph> pac_index(gp.g,
                                                          cfg::get().pb.pacbio_k,
                                                          cfg::get().K, cfg::get().pb.ignore_middle_alignment);
