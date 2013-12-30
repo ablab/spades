6aedc92f91a35ae2ace2445d8564bb841da41169
Andrey Prjibelski <andrewprzh@gmail.com>
2013-12-30 18:11:36 +0400

    make targz

--- make-targz.sh
+++ make-targz.sh
@@ -4,13 +4,12 @@ mkdir -p SPAdes-$VERSION/src
 
 cp -r assembler/src/debruijn SPAdes-$VERSION/src/debruijn
 cp -r assembler/src/hammer SPAdes-$VERSION/src/hammer
-cp -r assembler/src/debruijn SPAdes-$VERSION/src/dipspades
-cp -r assembler/src/hammer SPAdes-$VERSION/src/ionhammer
+cp -r assembler/src/dipspades SPAdes-$VERSION/src/dipspades
+cp -r assembler/src/ionhammer SPAdes-$VERSION/src/ionhammer
 cp -r assembler/src/include SPAdes-$VERSION/src/include
 cp -r assembler/src/io SPAdes-$VERSION/src/io
 cp -r assembler/src/mph_index SPAdes-$VERSION/src/mph_index
 cp -r assembler/src/cmake SPAdes-$VERSION/src/cmake
-cp -r assembler/src/ssw SPAdes-$VERSION/src/tools
 cp -r assembler/src/ssw SPAdes-$VERSION/src/ssw
 cp -r assembler/src/spades_pipeline SPAdes-$VERSION/src/spades_pipeline
 cp assembler/src/CMakeLists.txt SPAdes-$VERSION/src/CMakeLists.txt
@@ -34,6 +33,7 @@ cp assembler/spades.py SPAdes-$VERSION
 cp assembler/spades_compile.sh SPAdes-$VERSION
 cp assembler/spades_init.py SPAdes-$VERSION
 cp assembler/manual.html SPAdes-$VERSION
+cp assembler/changelog.html SPAdes-$VERSION
 cp assembler/GPLv2.txt SPAdes-$VERSION
 
 cd SPAdes-$VERSION
