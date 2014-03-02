1f182bff20a24b908ec221c1fbfb12be47d0e8d1
Anton Korobeynikov <anton@korobeynikov.info>
2014-03-03 00:16:51 +0400

    Add convenient functions to ask for free memory

--- assembler/src/include/memory_limit.hpp
+++ assembler/src/include/memory_limit.hpp
@@ -6,8 +6,21 @@
 
 #pragma once
 
+#if __DARWIN || __DARWIN_UNIX03
+#include <mach/task.h>
+#include <mach/mach.h>
+#else
+#include <sys/resource.h>
+#endif
+
 #include <sys/time.h>
 #include <sys/resource.h>
+
+#include "config.hpp"
+
+#ifdef SPADES_USE_JEMALLOC
+# include <jemalloc/jemalloc.h>
+#endif
 
 inline void limit_memory(size_t limit) {
   rlimit rl;
@@ -36,4 +49,37 @@ inline size_t get_memory_limit() {
              "getrlimit(2) call failed, errno = " << errno);
 
   return rl.rlim_cur;
+}
+
+#if __DARWIN || __DARWIN_UNIX03
+inline size_t get_max_rss() {
+  struct task_basic_info t_info;
+  mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
+
+  if (KERN_SUCCESS !=
+      task_info(mach_task_self(),
+                TASK_BASIC_INFO, (task_info_t)&t_info, &t_info_count))
+    return -1U;
+
+  return t_info.resident_size / 1024;
+}
+#else
+inline size_t get_max_rss() {
+  rusage ru;
+  getrusage(RUSAGE_SELF, &ru);
+
+  return ru.ru_maxrss;
+}
+#endif
+
+inline size_t get_free_memory() {
+    const size_t *cmem = 0;
+#ifdef SPADES_USE_JEMALLOC
+    size_t clen = sizeof(cmem);
+
+    je_mallctl("stats.cactive", &cmem, &clen, NULL, 0);
+    return get_memory_limit() - *cmem;
+#else
+    return get_memory_limit() - get_max_rss();
+#endif
 }
