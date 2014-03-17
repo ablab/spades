5550cdeab49f27d5372c6b82bb3a3d271e71e9c1
Anton Korobeynikov <anton@korobeynikov.info>
2014-03-17 15:47:02 +0400

    Add dummy paired fasta writer

--- assembler/src/include/io/osequencestream.hpp
+++ assembler/src/include/io/osequencestream.hpp
@@ -36,33 +36,32 @@ inline string MakeContigId(int number, size_t length, double coverage, size_t id
 }
 
 class osequencestream {
-
 protected:
-	std::ofstream ofstream_;
+    std::ofstream ofstream_;
 
-	int id_;
+    int id_;
 
-	void write_str(const std::string& s) {
+    void write_str(const std::string& s) {
         size_t cur = 0;
         while (cur < s.size()) {
             ofstream_ << s.substr(cur, 60) << endl;
             cur += 60;
         }
-	}
+    }
 
-	virtual void write_header(const std::string& s) {
+    virtual void write_header(const std::string& s) {
         // Velvet format: NODE_1_length_24705_cov_358.255249
-	    ofstream_ << ">" << MakeContigId(id_++, s.size()) << endl;
-	}
+        ofstream_ << ">" << MakeContigId(id_++, s.size()) << endl;
+    }
 
 public:
-	osequencestream(const std::string& filename): id_(0) {
-		ofstream_.open(filename.c_str());
-	}
+    osequencestream(const std::string& filename): id_(0) {
+        ofstream_.open(filename.c_str());
+    }
 
-	virtual ~osequencestream() {
-		ofstream_.close();
-	}
+    virtual ~osequencestream() {
+        ofstream_.close();
+    }
 
     osequencestream& operator<<(const std::string& s) {
         write_header(s);
@@ -70,15 +69,15 @@ public:
         return *this;
     }
 
-	osequencestream& operator<<(const Sequence& seq) {
-	    std::string s = seq.str();
-		return operator <<(s);
-	}
+    osequencestream& operator<<(const Sequence& seq) {
+        std::string s = seq.str();
+        return operator <<(s);
+    }
 
-	/**
-	 * Has different way of making headers
-	 * Doesn't increase counters, don't mix with other methods!
-	 */
+    /**
+     * Has different way of making headers
+     * Doesn't increase counters, don't mix with other methods!
+     */
     osequencestream& operator<<(const SingleRead& read) {
         ofstream_ << ">" << read.name() << endl;
         size_t cur = 0;
@@ -91,11 +90,44 @@ public:
     }
 };
 
+class PairedOutputSequenceStream {
+protected:
+    std::ofstream ofstreaml_;
+    std::ofstream ofstreamr_;
+
+  static void write(const SingleRead& read, std::ofstream& stream) {
+    stream << ">" << read.name() << std::endl;
+    size_t cur = 0;
+    std::string s = read.GetSequenceString();
+    while (cur < s.size()) {
+      stream << s.substr(cur, 60) << endl;
+      cur += 60;
+    }
+  }
+
+public:
+    PairedOutputSequenceStream(const std::string& filename1, const std::string &filename2) {
+      ofstreaml_.open(filename1);
+      ofstreamr_.open(filename2);
+    }
+
+    virtual ~PairedOutputSequenceStream() {
+        ofstreaml_.close();
+        ofstreamr_.close();
+    }
+
+    PairedOutputSequenceStream& operator<<(const PairedRead& read) {
+        write(read.first(), ofstreaml_);
+        write(read.second(), ofstreamr_);
+        return *this;
+    }
+};
+
 
 class osequencestream_cov: public osequencestream {
 protected:
 
-	double coverage_;
+    double coverage_;
 
 
     virtual void write_header(const string& s) {
@@ -105,25 +137,25 @@ protected:
 
 
 public:
-	osequencestream_cov(const std::string& filename): osequencestream(filename), coverage_(0.) {
-	}
+    osequencestream_cov(const std::string& filename): osequencestream(filename), coverage_(0.) {
+    }
 
     virtual ~osequencestream_cov() {
         ofstream_.close();
     }
 
-	osequencestream_cov& operator<<(double coverage) {
-		coverage_ = coverage;
-		return *this;
-	}
+    osequencestream_cov& operator<<(double coverage) {
+        coverage_ = coverage;
+        return *this;
+    }
 
-	osequencestream_cov& operator<<(const std::string& s) {
+    osequencestream_cov& operator<<(const std::string& s) {
         write_header(s);
         write_str(s);
         return *this;
     }
 
-	osequencestream_cov& operator<<(const Sequence& seq) {
+    osequencestream_cov& operator<<(const Sequence& seq) {
         std::string s = seq.str();
         return operator <<(s);
     }
@@ -133,41 +165,41 @@ public:
 
 class osequencestream_with_id: public osequencestream {
 protected:
-	int uid_;
+    int uid_;
 
-	double cov_;
+    double cov_;
 
     virtual void write_header(const string& s) {
         ofstream_ << ">" << MakeContigId(++id_, s.size(), cov_, uid_) << endl;
     }
 
 public:
-	osequencestream_with_id(const string& filename): osequencestream(filename), uid_(0), cov_(0.0) {
-	}
+    osequencestream_with_id(const string& filename): osequencestream(filename), uid_(0), cov_(0.0) {
+    }
 
     virtual ~osequencestream_with_id() {
         ofstream_.close();
     }
 
-	void setCoverage(double c) {
-		cov_ = c;
-	}
+    void setCoverage(double c) {
+        cov_ = c;
+    }
 
-	void setID(int uid) {
-		uid_ = uid;
-	}
+    void setID(int uid) {
+        uid_ = uid;
+    }
 
-	osequencestream_with_id& operator<<(const string& s) {
+    osequencestream_with_id& operator<<(const string& s) {
         write_header(s);
         write_str(s);
         return *this;
     }
-	osequencestream_with_id& operator<<(double coverage) {
-		cov_ = coverage;
-		return *this;
-	}
+    osequencestream_with_id& operator<<(double coverage) {
+        cov_ = coverage;
+        return *this;
+    }
 
-	osequencestream_with_id& operator<<(const Sequence& seq) {
+    osequencestream_with_id& operator<<(const Sequence& seq) {
         std::string s = seq.str();
         return operator <<(s);
     }
@@ -185,24 +217,24 @@ protected:
     }
 
 public:
-	osequencestream_with_data_for_scaffold(const string& filename): osequencestream_with_id(filename) {
-	    id_ = 1;
-		std::string sc_filename = filename + ".info";
-		scstream_.open(sc_filename.c_str());
-	}
+    osequencestream_with_data_for_scaffold(const string& filename): osequencestream_with_id(filename) {
+        id_ = 1;
+        std::string sc_filename = filename + ".info";
+        scstream_.open(sc_filename.c_str());
+    }
 
-	virtual ~osequencestream_with_data_for_scaffold() {
-	    ofstream_.close();
-		scstream_.close();
-	}
+    virtual ~osequencestream_with_data_for_scaffold() {
+        ofstream_.close();
+        scstream_.close();
+    }
 
-	osequencestream_with_data_for_scaffold& operator<<(const string& s) {
+    osequencestream_with_data_for_scaffold& operator<<(const string& s) {
         write_header(s);
         write_str(s);
         return *this;
     }
 
-	osequencestream_with_data_for_scaffold& operator<<(const Sequence& seq) {
+    osequencestream_with_data_for_scaffold& operator<<(const Sequence& seq) {
         std::string s = seq.str();
         return operator <<(s);
     }
