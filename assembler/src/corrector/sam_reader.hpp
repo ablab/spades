ae5aa51974e624dab3c7bd80e0030c157e43260e
Dmitry Antipov <dmitrij.antipov@gmail.com>
2014-07-31 15:41:16 +0400

    cleanup

--- assembler/src/corrector/sam_reader.hpp
+++ assembler/src/corrector/sam_reader.hpp
@@ -24,37 +24,11 @@ class MappedSamStream: public io::ReadStream<SingleSamRead> {
 
     bool is_open();
     bool eof();
-
-    MappedSamStream& operator>>(SingleSamRead& read);/* {
-        if (!is_open_ || eof_)
-            return *this;
-        read.set_data(seq_);
-        int tmp = samread(reader_, seq_);
-        eof_ = (0 >= tmp);
-        return *this;
-    }*/
-
-    MappedSamStream& operator >> (PairedSamRead& read);/*{
-    	TRACE("starting process paired read");
-    	SingleSamRead r1;
-    	MappedSamStream::operator >> (r1);
-    	SingleSamRead r2;
-    	MappedSamStream::operator >> (r2);
-    	TRACE(r1.GetSeq());
-    	TRACE(r2.GetSeq());
-    	TRACE(r1.GetName());
-    	VERIFY_MSG (r1.GetName() == r2.GetName(), r1.GetName() + " " + r2.GetName());
-    	read.pair(r1,r2);
-        return *this;
-    }
-*/
-    bam_header_t* ReadHeader();/*{
-    	return reader_->header;
-    }*/
-
+    MappedSamStream& operator>>(SingleSamRead& read);
+    MappedSamStream& operator >> (PairedSamRead& read);
+    bam_header_t* ReadHeader();
     string get_contig_name(int i);
     void close();
-
     void reset();
     io::ReadStreamStat get_stat() const;
 
@@ -68,5 +42,5 @@ class MappedSamStream: public io::ReadStream<SingleSamRead> {
 
     void open();
 };
-//}
+
 };
