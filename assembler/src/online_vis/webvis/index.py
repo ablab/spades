bdfa642b51b54a7177fec45b11cc7f93e15a7412
Yury Gorshkov <y.s.gorshkov@gmail.com>
2015-07-14 21:28:40 +0300

    Added the basic global session, process auto-launching, and README

--- assembler/src/online_vis/webvis/index.py
+++ assembler/src/online_vis/webvis/index.py
@@ -1,10 +1,10 @@
 #Flask imports
 import flask
-from flask import Flask
-from flask import request
+from flask import Flask, session, request
 #System imports
 from os import path
 import os
+import signal
 import subprocess
 #Aux imports
 import re
@@ -13,8 +13,6 @@ from urllib import quote, unquote
 from shellder import *
 
 app = Flask(__name__)
-
-#TODO: start session
 
 def make_url(string):
     files = re.findall(r"(?:[\w\.]+\/)+(?:\w+\.\w+)", string)
@@ -28,27 +26,51 @@ def make_url(string):
 def format_output(lines):
     return "<br/>".join(map(make_url, lines))
 
-@app.route('/', methods=['GET'])
-def index():
-    pipe = Shellder(pipe_out = "/tmp/vis_out")
-    return flask.render_template("index.html", console=format_output(pipe.get_output()))
+#Making in-out pipes
+def prepare_pipe(path):
+    if not os.path.exists(path):
+        os.mkfifo(path)
 
-@app.route('/command', methods=['POST'])
+pipe_in = "/tmp/vis_in"
+pipe_out = "/tmp/vis_out"
+env_path = "../../../"
+
+@app.route("/", methods=['GET'])
+def index():
+    log = ""
+    if "pid" not in session:
+        prepare_pipe(pipe_in)
+        prepare_pipe(pipe_out)
+        launcher = subprocess.Popen(["python", "launcher.py", pipe_in, pipe_out, env_path])
+        print "Started launcher at", launcher.pid
+        session["pid"] = launcher.pid
+        log = Shellder(pipe_out = "/tmp/vis_out").get_output()
+    return flask.render_template("index.html", console=log)
+
+@app.route("/logout", methods=['GET'])
+def logout():
+    if "pid" in session:
+        pid = session["pid"]
+        session.pop("pid", None)
+        print "Killing", pid
+        os.kill(pid, signal.SIGTERM)
+        return "You have been logged out"
+    return "No opened session"
+
+@app.route("/command", methods=['POST'])
 def command():
     pipe = Shellder("/tmp/vis_in", "/tmp/vis_out")
-    pipe.send(request.form['command'])
+    pipe.send(request.form["command"])
     return format_output(pipe.get_output())
 
-@app.route('/get')
+@app.route("/get")
 def get():
-    env_path = "../../../"
     file_path = env_path + unquote(request.args.get("file", ""))
     print("Getting", file_path)
     return flask.send_file(file_path, as_attachment=True, attachment_filename=path.basename(file_path))
 
-@app.route('/render')
+@app.route("/render")
 def render():
-    env_path = "../../../"
     pushd = os.getcwd()
     os.chdir(env_path)
     file_path = unquote(request.args.get("file", ""))
@@ -60,6 +82,7 @@ def render():
     os.chdir(pushd)
     return flask.redirect("/get?file=" + res_path)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.debug = True
+    app.secret_key = "somekey"
     app.run()
