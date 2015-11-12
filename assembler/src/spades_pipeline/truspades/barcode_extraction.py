e564af98e25961dde1d9bfd8eb94d6f74cba7d6e
Anton Bankevich <anton.bankevich@gmail.com>
2015-11-12 17:04:48 +0300

    python3 for truspades

--- assembler/src/spades_pipeline/truspades/barcode_extraction.py
+++ assembler/src/spades_pipeline/truspades/barcode_extraction.py
@@ -17,7 +17,7 @@ __author__ = 'anton'
 class Barcode:
     def __init__(self, id, libs):
         self.id = id
-        self.libs = libs
+        self.libs = list(libs)
         if id == None:
             self.set_lcs_id()
 
@@ -53,7 +53,7 @@ def check_int_ids(ids):
     return True
 
 def generate_barcode_list(barcodes):
-    ids = zip(barcodes, generate_ids(barcodes))
+    ids = list(zip(barcodes, generate_ids(barcodes)))
     if check_int_ids(ids):
         ids = sorted(ids, key=lambda barcode: int(barcode[1]))
     return [(bid, "BC_" + short_id) for bid, short_id in ids]
@@ -65,7 +65,7 @@ def GroupBy(norm, l):
     result = dict()
     for line in l:
         key = norm(line)
-        if not result.has_key(key):
+        if not key in result:
             result[key] = []
         result[key].append(line)
     return result
@@ -85,9 +85,9 @@ def ExtractBarcodes(dirs):
         for file in [os.path.abspath(os.path.join(dir, file)) for file in os.listdir(dir) if os.path.isfile(os.path.join(dir, file))]:
             files.append(file)
     barcode_dict = GroupBy(Normalize, files)
-    if not CheckSameSize(barcode_dict.itervalues()):
+    if not CheckSameSize(barcode_dict.values()):
         return None
-    for bid in list(barcode_dict.iterkeys()):
+    for bid in barcode_dict.keys():
         barcode_dict[bid] = GroupBy(NormalizeR, barcode_dict[bid]).values()
         if not CheckSameSize(barcode_dict[bid], 2):
             return None
