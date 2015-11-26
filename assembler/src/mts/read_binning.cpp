cea5c78ebd8ceb580a1752bc20e9db594f28c7e5
Sergey Nurk <sergeynurk@gmail.com>
2015-11-26 13:56:07 -0500

    adequate output read streams

--- assembler/src/mts/read_binning.cpp
+++ assembler/src/mts/read_binning.cpp
@@ -10,12 +10,57 @@
 #include "logger/log_writers.hpp"
 
 #include "graphio.hpp"
-#include "io/osequencestream.hpp"
+#include "io/io_helper.hpp"
 
 void create_console_logger() {
     logging::logger *log = logging::create_logger("", logging::L_INFO);
     log->add_writer(std::make_shared<logging::console_writer>());
     logging::attach_logger(log);
+}
+
+namespace io {
+class OSingleReadStream {
+    std::ofstream os_;
+
+public:
+    OSingleReadStream(const std::string& fn) :
+        os_(fn) {
+    }
+
+    OSingleReadStream& operator<<(const SingleRead& read) {
+        os_ << "@" << read.name() << std::endl;
+        os_ << read.GetSequenceString() << std::endl;
+        os_ << "+" << std::endl;
+        os_ << read.GetQualityString() << std::endl;
+        return *this;
+    }
+
+    void close() {
+        os_.close();
+    }
+};
+
+class OPairedReadStream {
+    OSingleReadStream l_os_;
+    OSingleReadStream r_os_;
+
+public:
+    OPairedReadStream(const std::string& l_fn, const std::string& r_fn) :
+        l_os_(l_fn), r_os_(r_fn) {
+    }
+
+    OPairedReadStream& operator<<(const PairedRead& read) {
+        l_os_ << read.first();
+        r_os_ << read.second();
+        return *this;
+    }
+
+    void close() {
+        l_os_.close();
+        r_os_.close();
+    }
+};
+
 }
 
 namespace debruijn_graph {
@@ -24,7 +69,7 @@ class ContigBinner {
     const conj_graph_pack& gp_;
     EdgeAnnotation edge_annotation_;
 
-    map<bin_id, std::shared_ptr<io::PairedOutputSequenceStream>> out_streams_;
+    map<bin_id, std::shared_ptr<io::OPairedReadStream>> out_streams_;
 
 public:
     ContigBinner(const conj_graph_pack& gp, const vector<bin_id>& bins_of_interest) :
@@ -38,7 +83,7 @@ public:
             string out_dir = output_root + "/" + ToString(bin) + "/";
             path::make_dirs(out_dir);
             out_streams_.insert(make_pair(bin,
-                                          make_shared<io::PairedOutputSequenceStream>(out_dir + sample_name + "_1.fasta",
+                                          make_shared<io::OPairedReadStream>(out_dir + sample_name + "_1.fasta",
                                                                                       out_dir + sample_name + "_2.fasta")));
         }
     }
