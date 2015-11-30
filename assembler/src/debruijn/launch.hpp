f862ea5a1b8c9f98da866e6bfc43ad0bcaae33c4
Sergey Nurk <sergeynurk@gmail.com>
2015-12-01 00:43:57 +0300

    mesmatch correction disabled in meta mode for good

--- assembler/src/debruijn/launch.hpp
+++ assembler/src/debruijn/launch.hpp
@@ -71,7 +71,7 @@ void assemble_genome() {
 
     SPAdes.add(new debruijn_graph::SimplificationCleanup());
     //currently cannot be used with two step rr
-    if (cfg::get().correct_mismatches && !two_step_rr)
+    if (cfg::get().correct_mismatches && !cfg::get().ds.meta)
         SPAdes.add(new debruijn_graph::MismatchCorrection());
     if (cfg::get().rr_enable) {
         if (two_step_rr) {
