e35d64a18147c01b641dc04f43d0c5e5448ec82d
Anton Korobeynikov <anton@korobeynikov.info>
2016-01-04 17:54:19 +0300

    Introduce histogram class as a basis for further optimizations

--- assembler/src/include/de/index_point.hpp
+++ assembler/src/include/de/index_point.hpp
@@ -185,9 +185,123 @@ inline std::ostream& operator<<(std::ostream& os, const RawPoint &point) {
     return os << point.str();
 }
 
-//typedef std::set<Point> Histogram;
-typedef btree::btree_set<Point, std::less<Point>, std::allocator<Point>, 1024> HistogramWithWeight;
-typedef btree::btree_set<RawPoint, std::less<RawPoint>, std::allocator<RawPoint>, 1024> RawHistogram;
+template<class Point>
+class Histogram {
+    typedef Histogram<Point> self_type;
+    typedef typename std::less<Point> key_compare;
+    typedef typename std::allocator<Point> allocator_type;
+    typedef typename btree::btree<btree::btree_set_params<Point, key_compare, allocator_type, 1024> > Tree;
+
+  public:
+    typedef typename Tree::params_type params_type;
+    typedef typename Tree::key_type key_type;
+    typedef typename Tree::value_type value_type;
+    typedef typename Tree::pointer pointer;
+    typedef typename Tree::const_pointer const_pointer;
+    typedef typename Tree::reference reference;
+    typedef typename Tree::const_reference const_reference;
+    typedef typename Tree::size_type size_type;
+    typedef typename Tree::difference_type difference_type;
+    typedef typename Tree::iterator iterator;
+    typedef typename Tree::const_iterator const_iterator;
+    typedef typename Tree::reverse_iterator reverse_iterator;
+    typedef typename Tree::const_reverse_iterator const_reverse_iterator;
+
+ public:
+    // Default constructor.
+    Histogram()
+            : tree_(key_compare(), allocator_type()) {
+    }
+
+    // Copy constructor.
+    Histogram(const self_type &x)
+            : tree_(x.tree_) {
+    }
+
+    template <class InputIterator>
+    Histogram(InputIterator b, InputIterator e)
+            : tree_(key_compare(), allocator_type()) {
+        insert(b, e);
+    }
+
+    // Iterator routines.
+    iterator begin() { return tree_.begin(); }
+    const_iterator begin() const { return tree_.begin(); }
+    iterator end() { return tree_.end(); }
+    const_iterator end() const { return tree_.end(); }
+    reverse_iterator rbegin() { return tree_.rbegin(); }
+    const_reverse_iterator rbegin() const { return tree_.rbegin(); }
+    reverse_iterator rend() { return tree_.rend(); }
+    const_reverse_iterator rend() const { return tree_.rend(); }
+
+    // Lookup routines.
+    iterator lower_bound(const key_type &key) { return tree_.lower_bound(key); }
+    const_iterator lower_bound(const key_type &key) const { return tree_.lower_bound(key); }
+    iterator upper_bound(const key_type &key) { return tree_.upper_bound(key); }
+    const_iterator upper_bound(const key_type &key) const { return tree_.upper_bound(key); }
+    std::pair<iterator,iterator> equal_range(const key_type &key) { return tree_.equal_range(key); }
+    std::pair<const_iterator,const_iterator> equal_range(const key_type &key) const { return tree_.equal_range(key); }
+
+    // Utility routines.
+    void clear() { tree_.clear(); }
+    void swap(self_type &x) { tree_.swap(x.tree_); }
+    void dump(std::ostream &os) const { tree_.dump(os); }
+    void verify() const { tree_.verify(); }
+
+    // Size routines.
+    size_type size() const { return tree_.size(); }
+    size_type max_size() const { return tree_.max_size(); }
+    bool empty() const { return tree_.empty(); }
+    size_type height() const { return tree_.height(); }
+    size_type internal_nodes() const { return tree_.internal_nodes(); }
+    size_type leaf_nodes() const { return tree_.leaf_nodes(); }
+    size_type nodes() const { return tree_.nodes(); }
+    size_type bytes_used() const { return tree_.bytes_used(); }
+    static double average_bytes_per_value() { return Tree::average_bytes_per_value(); }
+    double fullness() const { return tree_.fullness(); }
+    double overhead() const { return tree_.overhead(); }
+
+    // Lookup routines.
+    iterator find(const key_type &key) { return tree_.find_unique(key); }
+    const_iterator find(const key_type &key) const { return tree_.find_unique(key); }
+    size_type count(const key_type &key) const { return tree_.count_unique(key); }
+
+    // Insertion routines.
+    std::pair<iterator,bool> insert(const value_type &x) { return tree_.insert_unique(x); }
+    iterator insert(iterator position, const value_type &x) { return tree_.insert_unique(position, x); }
+    template <typename InputIterator>
+    void insert(InputIterator b, InputIterator e) { tree_.insert_unique(b, e); }
+
+    // Deletion routines.
+    int erase(const key_type &key) { return tree_.erase_unique(key); }
+    // Erase the specified iterator from the btree. The iterator must be valid
+    // (i.e. not equal to end()).  Return an iterator pointing to the node after
+    // the one that was erased (or end() if none exists).
+    iterator erase(const iterator &iter) { return tree_.erase(iter); }
+    void erase(const iterator &first, const iterator &last) { tree_.erase(first, last); }
+
+    bool operator==(const self_type& x) const {
+        if (size() != x.size())
+            return false;
+
+        for (const_iterator i = begin(), xi = x.begin(); i != end(); ++i, ++xi)
+            if (*i != *xi)
+                return false;
+
+        return true;
+    }
+
+    bool operator!=(const self_type& other) const {
+        return !operator==(other);
+    }
+
+  protected:
+    Tree tree_;
+};
+
+
+typedef Histogram<RawPoint> RawHistogram;
+typedef Histogram<Point> HistogramWithWeight;
 
 inline bool ClustersIntersect(Point p1, Point p2) {
     return math::le(p1.d, p2.d + p1.var + p2.var) &&
