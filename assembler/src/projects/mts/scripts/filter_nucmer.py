251fd9538ff2138a048543e789aeb254b6e0360d
Yuri Gorshkov <y.s.gorshkov@gmail.com>
2016-05-16 14:50:55 +0300

    Added counting of total GF for good contigs

--- assembler/src/projects/mts/scripts/filter_nucmer.py
+++ assembler/src/projects/mts/scripts/filter_nucmer.py
@@ -6,27 +6,35 @@ import sys
 
 def print_usage():
     print("For a sample assembly aligned to a reference, outputs only contigs which were aligned more than <threshold> percent of their length total, and that percent.")
-    print("Usage: filter_nucmer.py <nucmer_output> <sample> <threshold>")
+    print("Usage: filter_nucmer.py <quast_output> <threshold>")
     print("Parameters:")
-    print("<nucmer_output> is typically contig_reports/nucmer_output/yoursample.coords.filtered from QUAST output")
-    print("<sample> is sample name prefix")
+    print("<quast_output> is the QUAST output directory")
     print("<threshold> is the minimal total alignment of a contig (0-100%)")
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 3:
     print_usage()
     sys.exit(1)
 
-with open(sys.argv[1]) as input:
-    sample = sys.argv[2]
-    threshold = float(sys.argv[3])
+report = "{}/report.tsv".format(sys.argv[1])
+with open(report) as input:
+    report_data = dict(line.rstrip().split("\t", 1) for line in input)
+print(report_data)
+sample = report_data["Assembly"]
+reference_length = int(report_data["Reference length"])
+nucmer_output = "{}/contigs_reports/nucmer_output/{}.coords.filtered".format(sys.argv[1], sample)
+with open(nucmer_output) as input:
+    threshold = float(sys.argv[2])
     align_data = re.compile("\d+ \d+ \| \d+ \d+ \| \d+ (\d+) \| [\d.]+ \| [^ ]+ [^\s]+length_(\d+)[^\s]+ID_(\d+)")
     contig = None
+    total_len = 0
     contig_len = 0
     align_len = 0
-    def print_contig():
+    def process_contig():
         per = 100.0 * align_len / contig_len
         if (per > threshold):
-            print("{0}-{1}\t{2}".format(sample, contig, per))
+            print("{}-{}\t{}".format(sample, contig, per))
+            return align_len
+        return 0
     for line in input:
         res = align_data.search(line)
         if res is None:
@@ -34,11 +42,12 @@ with open(sys.argv[1]) as input:
         new_contig = res.group(3)
         if contig != new_contig:
             if contig is not None:
-                print_contig()
+                total_len += process_contig()
             contig = new_contig
             contig_len = int(res.group(2))
             align_len = 0
         #Assuming that all alignments of the same contig are consequent
         align_len += int(res.group(1))
     #Print the last contig separately
-    print_contig()
+    total_len += process_contig()
+    print("total\t{}".format(100.0 * total_len / reference_length))
