7883008c95a09f93f0e380b6798efe30d086a88d
Sergey Nurk <sergeynurk@gmail.com>
2016-05-19 22:56:04 -0400

    contig id logic changed to match recent removal of _ID_ part in master

--- assembler/src/projects/mts/formats.hpp
+++ assembler/src/projects/mts/formats.hpp
@@ -10,17 +10,21 @@ typedef std::vector<bin_id> Bins;
 typedef std::pair<contig_id, Bins> ContigAnnotation;
 
 inline contig_id GetId(const io::SingleRead& contig) {
-     std::string name = contig.name();
-     size_t pos = name.find("_ID_");
-     VERIFY(pos != std::string::npos);
-     size_t start = pos + 4;
-     VERIFY(start < name.size());
-     return name.substr(start, name.size() - start);
+//     std::string name = contig.name();
+//     size_t pos = name.find("_ID_");
+//     VERIFY(pos != std::string::npos);
+//     size_t start = pos + 4;
+//     VERIFY(start < name.size());
+//     return name.substr(start, name.size() - start);
+    return contig.name();
 }
 
 inline contig_id GetBaseId(const contig_id& id) {
     size_t pos = id.find('_');
-    return id.substr(0, pos);
+    VERIFY(pos != string::npos && id.substr(0, pos) == "NODE");
+    size_t pos2 = id.find('_', pos + 1);
+    VERIFY(pos2 != string::npos);
+    return id.substr(pos + 1, pos2 - pos - 1);
 }
 
 }
