873d65d447d39ebf7a7556ddfca57534e1af24cb
Anton Korobeynikov <anton@korobeynikov.info>
2016-05-22 14:30:08 +0300

    Properly handle spaces in spades_compile.sh

--- assembler/spades_compile.sh
+++ assembler/spades_compile.sh
@@ -15,11 +15,11 @@ fi
 BUILD_DIR=build_spades
 BASEDIR=`pwd`/`dirname $0`
 
-rm -rf $BASEDIR/$BUILD_DIR
-mkdir -p $BASEDIR/$BUILD_DIR
+rm -rf "$BASEDIR/$BUILD_DIR"
+mkdir -p "$BASEDIR/$BUILD_DIR"
 set -e
-cd $BASEDIR/$BUILD_DIR
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$PREFIX $BASEDIR/src $*
+cd "$BASEDIR/$BUILD_DIR"
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$PREFIX" "$BASEDIR/src" $*
 make -j 8
 make install
 cd $PREFIX
