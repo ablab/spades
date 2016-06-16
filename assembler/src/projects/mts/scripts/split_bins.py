0e6b3289d88dd6ba67d52502d9b0d4c646a0abae
Yuri Gorshkov <y.s.gorshkov@gmail.com>
2016-06-16 16:29:39 +0300

    Fixed an issue with edges ID collision

--- assembler/src/projects/mts/scripts/split_bins.py
+++ assembler/src/projects/mts/scripts/split_bins.py
@@ -18,6 +18,8 @@ binning = common.load_annotation(sys.argv[2])
 
 cags = set()
 for seq in SeqIO.parse(sample, "fasta"):
-    for cag in binning.get(common.get_id(seq.id, sample_name), []):
+    seq.id = common.get_id(seq.id, sample_name)
+    seq.description = ""
+    for cag in binning.get(seq.id, []):
         with open(path.join(out_dir, "{}-{}.fasta".format(sample_name, cag)), "a") as output:
             SeqIO.write(seq, output, "fasta")
