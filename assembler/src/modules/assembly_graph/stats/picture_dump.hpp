0ecfea29e2bd5f672368c4cd96d57d1d0fb2edc6
Sergey Nurk <sergeynurk@gmail.com>
2016-06-23 18:08:55 +0300

    graph pack dump in consistency with master; SPADES_REASSEMBLY setting introduced ; memory limit 400G

--- assembler/src/modules/assembly_graph/stats/picture_dump.hpp
+++ assembler/src/modules/assembly_graph/stats/picture_dump.hpp
@@ -277,20 +277,22 @@ struct detail_info_printer {
                   << stats.edges() << ", sum length of edges : " << stats.edge_length());
         }
 
-        string saves_folder = path::append_path(path::append_path(folder_, "saves/"),
-                                          ToString(call_cnt, 2) + "_" + pos_name + "/");
-        if (config.save_graph) {
-            path::make_dirs(saves_folder);
-            graphio::ConjugateDataPrinter<conj_graph_pack::graph_t> printer(gp_.g);
-            graphio::PrintBasicGraph(saves_folder + "graph", printer);
-        }
-
         if (config.save_graph_pack) {
+            string saves_folder = path::append_path(path::append_path(folder_, "saves/"),
+                                                          ToString(call_cnt++, 2) + "_" + pos_name + "/");
             path::make_dirs(saves_folder);
             graphio::ConjugateDataPrinter<conj_graph_pack::graph_t> printer(gp_.g);
             graphio::PrintGraphPack(saves_folder + "graph_pack", printer, gp_);
             //TODO: separate
             graphio::PrintClusteredIndices(saves_folder + "graph_pack", printer, gp_.clustered_indices);
+        }
+
+        if (config.save_graph) {
+            string saves_folder = path::append_path(path::append_path(folder_, "saves/"),
+                                                          ToString(call_cnt++, 2) + "_" + pos_name + "/");
+            path::make_dirs(saves_folder);
+            graphio::ConjugateDataPrinter<conj_graph_pack::graph_t> printer(gp_.g);
+            graphio::PrintBasicGraph(saves_folder + "graph", printer);
         }
 
         if (config.lib_info) {
