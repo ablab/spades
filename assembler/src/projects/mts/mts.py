dbef1db32c291a890024c92d2e3d748d36731e17
Yuri Gorshkov <y.s.gorshkov@gmail.com>
2016-06-29 21:54:59 +0300

    Added option to use existing assemblies

--- assembler/src/projects/mts/mts.py
+++ assembler/src/projects/mts/mts.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser(description="MTS - Metagenomic Time Series")
 parser.add_argument("--threads", "-t", type=int, default=8, help="Number of threads")
 parser.add_argument("dir", type=str, help="Output directory where config.yaml is located")
 parser.add_argument("--stats", "-s", action="store_true", help="Calculate stats (when the REFS parameter in config.yaml is provided)")
+parser.add_argument("--no-assembly", "-a", action="store_true", help="Use existing assemblies")
 
 args = parser.parse_args()
 
@@ -19,6 +20,8 @@ def call_snake(extra_params=[]):
     subprocess.check_call(base_params + extra_params, stdout=sys.stdout, stderr=sys.stderr)
 
 print("Step #1 - Assembly")
+if (args.no_assembly):
+    call_snake(["assemble_all", "--touch"])
 call_snake()
 
 print("Step #2 - Bin filtering")
