bfa13749105f65f8f75972d7f921c8cfa8b6b0e3
Yuri Gorshkov <y.s.gorshkov@gmail.com>
2016-06-29 17:17:27 +0300

    PCA plotting without references

--- assembler/src/projects/mts/scripts/pca.R
+++ assembler/src/projects/mts/scripts/pca.R
@@ -18,9 +18,15 @@ load_binning <- function(canopy_in, canopy_out) {
 
 load_clusters <- function(canopy_in, canopy_out, int_contigs) {
   data <- load_binning(canopy_in, canopy_out)
-  interesting <- read.table(int_contigs)
-  names(interesting) <- c('contig', 'length', 'alignment', 'ref')
-  droplevels(merge(x=data, y=interesting, by='contig'))
+  if (missing(int_contigs)) {
+    pieces <- split(data, data$clust)[1:10]
+    lims <- lapply(pieces, function(x) head(x, 50))
+    do.call(rbind, c(lims, list(make.row.names=FALSE)))
+  } else {
+    interesting <- read.table(int_contigs)
+    names(interesting) <- c('contig', 'length', 'alignment', 'ref')
+    droplevels(merge(x=data, y=interesting, by='contig'))
+  }
 }
 
 do_prc <- function(clusters) {
@@ -33,7 +39,7 @@ print_clusters <- function(pr, clust, image) {
   lev <- levels(factor(clust))
   cols <- 1:length(lev)
   #layout(rbind(1,2), heights=c(7,1))
-  plot(pr$x, col = as.numeric(clust))
+  plot(pr$x, col = as.numeric(clust), xlim=c(-100, 200), ylim=c(-50,50))
   a <- split(as.data.frame(pr$x), clust)
   for (l in lev) {
     x <- a[[l]]
@@ -57,10 +63,15 @@ local_data <- function() {
 args <- commandArgs(trailingOnly = TRUE)
 in_fn <- args[1]
 out_fn <- args[2]
-cont_fn <- args[3]
-image_out <- args[4]
+if (length(args) < 4) {
+  image_out <- args[3]
+  clusters <- load_clusters(in_fn, out_fn)
+} else {
+  cont_fn <- args[3]
+  image_out <- args[4]
+  clusters <- load_clusters(in_fn, out_fn, cont_fn)
+}
 
-clusters <- load_clusters(in_fn, out_fn, cont_fn)
 print(clusters[1:10,])
 prc_data <- do_prc(clusters)
 print_clusters(prc_data, clusters$clust, image_out)
