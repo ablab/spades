99387ad378ad56c2d4c43cdd257d5d8a895385a7
Itolstoganov <oockvay@gmail.com>
2016-07-13 21:08:13 +0300

    Initial commit
    Added trivial resolver and barcode mapper fixes
    
    Moved barcode mapper to graph pack
    
    barcode mapper fix

---
+++ assembler/src/projects/tslr_resolver/main.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include "tslr_launch.hpp"
+#include <modules/dev_support/copy_file.hpp>
+#include <projects/online_vis/vis_logger.hpp>
+
+void load_config(string cfg_filename) {
+    path::CheckFileExistenceFATAL(cfg_filename);
+
+    cfg::create_instance(cfg_filename);
+
+    if (!cfg::get().project_name.empty()) {
+        make_dir(cfg::get().output_base + cfg::get().project_name);
+    }
+
+    make_dir(cfg::get().output_dir);
+    make_dir(cfg::get().tmp_dir);
+
+    make_dir(cfg::get().output_dir);
+    if (cfg::get().developer_mode)
+        make_dir(cfg::get().output_saves);
+
+    make_dir(cfg::get().temp_bin_reads_path);
+}
+
+void create_console_logger(string cfg_filename) {
+    using namespace logging;
+
+    string log_props_file = cfg::get().log_filename;
+
+    if (!path::FileExists(log_props_file))
+        log_props_file = path::append_path(path::parent_path(cfg_filename), cfg::get().log_filename);
+
+    logger *lg = create_logger(path::FileExists(log_props_file) ? log_props_file : "");
+    lg->add_writer(std::make_shared<console_writer>());
+    attach_logger(lg);
+}
+
+int main (int /*argc*/, char** argv) {
+
+	try {
+		string cfg_filename = argv[1];
+        load_config(cfg_filename);
+		create_console_logger(cfg_filename);
+        std::cout << cfg_filename << std::endl;
+
+        string path_to_tslr_dataset = argv[2];
+        std::cout << path_to_tslr_dataset;
+		spades::run_tslr_resolver(path_to_tslr_dataset);
+	} catch (std::exception const& e) {
+        std::cerr << "Exception caught " << e.what() << std::endl;
+        return EINTR;
+	}
+	return 0;
+}
