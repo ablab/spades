66dcc483e12c49be86ebf6520aa44697f86674ce
Itolstoganov <oockvay@gmail.com>
2016-09-08 20:36:37 +0300

    Changed coverage checks in PathJoiner

--- assembler/src/projects/tslr_resolver/tslr_pe.hpp
+++ assembler/src/projects/tslr_resolver/tslr_pe.hpp
@@ -9,17 +9,6 @@
 using namespace path_extend;
 
 namespace tslr_resolver {
-
-    PathContainer FilterByLength(const PathContainer& seeds, size_t len_threshold) {
-        PathContainer result;
-        for (auto it = seeds.begin(); it != seeds.end(); ++it) {
-            if (it->first->Length() > len_threshold) {
-                result.AddPair(it->first, it->second);
-            }
-        }
-        return result;
-    }
-
     void LaunchBarcodePE (conj_graph_pack &gp) {
         path_extend::PathExtendParamsContainer params(cfg::get().pe_params,
                                                       cfg::get().output_dir,
@@ -41,24 +30,23 @@ namespace tslr_resolver {
         PathExtendResolver resolver(gp.g);
         auto min_unique_length = pset.scaffolding2015.min_unique_length;
         auto unique_variaton = pset.scaffolding2015.unique_coverage_variation;
-        ScaffoldingUniqueEdgeStorage main_unique_storage;
         auto dataset_info = cfg::get().ds;
 
-        main_unique_storage = FillUniqueEdgeStorage(gp, dataset_info,
+        ScaffoldingUniqueEdgeStorage pe_unique_storage = FillUniqueEdgeStorage(gp, dataset_info,
                                                     min_unique_length,
                                                     unique_variaton,
-                                                    false);
+                                                    pset.scaffolding2015.autodetect);
 
         //mp extender
         INFO("SUBSTAGE = paired-end libraries");
         PathExtendStage exspander_stage = PathExtendStage::PEStage;
         vector<shared_ptr<PathExtender> > all_libs =
-            MakeAllExtenders(exspander_stage, dataset_info, params, gp, cover_map, main_unique_storage);
+            MakeAllExtenders(exspander_stage, dataset_info, params, gp, cover_map, pe_unique_storage);
         size_t max_is_right_quantile = max(FindOverlapLenForStage(exspander_stage, cfg::get().ds), gp.g.k() + 100);
         size_t min_edge_len = 100;
 
         shared_ptr<CompositeExtender> mainPE = make_shared<CompositeExtender>(gp.g, cover_map, all_libs,
-                                                                              main_unique_storage,
+                                                                              pe_unique_storage,
                                                                               max_is_right_quantile,
                                                                               pset.extension_options.max_repeat_length,
                                                                               detect_repeats_online);
@@ -81,12 +69,19 @@ namespace tslr_resolver {
         size_t distance_bound = tslr_resolver_params.distance_bound;
         double abs_threshold = tslr_resolver_params.abs_threshold;
         bool join_paths = tslr_resolver_params.join_paths;
+        //fixme code duplication: unique storage can't be cleared
+        ScaffoldingUniqueEdgeStorage tslr_unique_storage = FillUniqueEdgeStorage(gp, dataset_info,
+                                                                                 min_unique_length,
+                                                                                 unique_variaton,
+                                                                                 pset.scaffolding2015.autodetect);
+        std::unordered_map <size_t, size_t> id_to_index;
+
 
         max_is_right_quantile = gp.g.k() + 10000;
         auto extension = make_shared<TrivialTSLRExtensionChooser>(gp,
                                                                   len_threshold,
                                                                   absolute_barcode_threshold,
-                                                                  main_unique_storage);
+                                                                  tslr_unique_storage);
         auto tslr_extender = make_shared<InconsistentTSLRExtender>(gp, cover_map,
                                                              extension,
                                                              2500 /*insert size*/,
@@ -97,15 +92,16 @@ namespace tslr_resolver {
                                                              abs_threshold,
                                                              len_threshold,
                                                              join_paths,
-                                                             main_unique_storage,
-                                                             gp.barcode_mapper -> AverageBarcodeCoverage());
+                                                             tslr_unique_storage,
+                                                             id_to_index);
         all_libs.push_back(tslr_extender);
         shared_ptr<PathJoiner> tslrPE = make_shared<PathJoiner>
                 (gp.g, cover_map, all_libs,
-                 main_unique_storage,
+                 pe_unique_storage,
                  max_is_right_quantile,
                  pset.extension_options.max_repeat_length,
-                 detect_repeats_online);
+                 detect_repeats_online,
+                 id_to_index);
 
 
         INFO(paths.size() << " paths from previous stage");
@@ -114,7 +110,7 @@ namespace tslr_resolver {
 
         FinalizePaths(params, final_paths, gp.g, cover_map, min_edge_len, max_is_right_quantile);
 
-        debruijn_graph::GenomeConsistenceChecker genome_checker (gp, main_unique_storage, 1000, 0.2);
+        debruijn_graph::GenomeConsistenceChecker genome_checker (gp, pe_unique_storage, 1000, 0.2);
         DebugOutputPaths(gp, params, final_paths, "final_tslr_paths");
 
         writer.OutputPaths(final_paths, params.output_dir + params.contigs_name);
