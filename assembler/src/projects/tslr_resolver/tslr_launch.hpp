138b9a94fda31697b4d130a99639f2686f7b7055
Itolstoganov <oockvay@gmail.com>
2016-11-17 12:13:25 +0300

    Basic 10X support. BarcodeMapper refactoring

--- assembler/src/projects/tslr_resolver/tslr_launch.hpp
+++ assembler/src/projects/tslr_resolver/tslr_launch.hpp
@@ -8,6 +8,7 @@
 #include <common/pipeline/genomic_info_filler.hpp>
 #include <common/stages/simplification.hpp>
 #include <projects/spades/hybrid_aligning.hpp>
+#include <projects/spades/mismatch_correction.hpp>
 #include "projects/spades/distance_estimation.hpp"
 
 namespace spades {
@@ -22,30 +23,61 @@ namespace spades {
                                                 cfg::get().flanking_range,
                                                 cfg::get().pos.max_mapping_gap,
                                                 cfg::get().pos.max_gap_diff);
+
+
         StageManager barcode_resolver_pipeline({cfg::get().developer_mode,
                               cfg::get().load_from,
                               cfg::get().output_saves});
+
         conj_gp.kmer_mapper.Attach();
+
+        bool two_step_rr = cfg::get().rr_enable && cfg::get().two_step_rr;
+
         barcode_resolver_pipeline.add(new debruijn_graph::Construction())
-                .add(new debruijn_graph::GenomicInfoFiller());
+                                 .add(new debruijn_graph::GenomicInfoFiller());
 
-        if (!cfg::get().ts_res.ideal_reads) {
-            barcode_resolver_pipeline.add(new debruijn_graph::GapClosing("early_gapcloser"))
-                   .add(new debruijn_graph::Simplification)
-                   .add(new debruijn_graph::GapClosing("late_gapcloser"))
-                   .add(new debruijn_graph::SimplificationCleanup());
+        if (cfg::get().gap_closer_enable && cfg::get().gc.before_simplify)
+            barcode_resolver_pipeline.add(new debruijn_graph::GapClosing("early_gapcloser"));
+
+        barcode_resolver_pipeline.add(new debruijn_graph::Simplification(true /*preliminary*/));
+
+        if (cfg::get().gap_closer_enable && cfg::get().gc.after_simplify)
+            barcode_resolver_pipeline.add(new debruijn_graph::GapClosing("late_gapcloser"));
+
+        barcode_resolver_pipeline.add(new debruijn_graph::SimplificationCleanup());
+
+        if (cfg::get().correct_mismatches && !cfg::get().two_step_rr)
+            barcode_resolver_pipeline.add(new debruijn_graph::MismatchCorrection());
+        if (cfg::get().rr_enable) {
+
+            if (two_step_rr) {
+//                if (cfg::get().use_intermediate_contigs)
+//                    barcode_resolver_pipeline.add(new debruijn_graph::PairInfoCount(true))
+//                                             .add(new debruijn_graph::DistanceEstimation(true))
+//                                             .add(new debruijn_graph::RepeatResolution(true))
+//                                             .add(new debruijn_graph::SecondPhaseSetup());
+                barcode_resolver_pipeline.add(new debruijn_graph::Simplification());
+
+            }
+            barcode_resolver_pipeline.add(new debruijn_graph::HybridLibrariesAligning());
+
+            //No graph modification allowed after HybridLibrariesAligning stage!
+
+            barcode_resolver_pipeline.add(new BarcodeMapConstructionStage(cfg::get().K))
+//                    .add(new debruijn_graph::PairInfoCount())
+//                    .add(new debruijn_graph::DistanceEstimation())
+                    .add(new TslrResolverStage(cfg::get().K, cfg::get().output_dir + "resolver_output.fasta"));
+
+
         }
+//        else {
+//            barcode_resolver_pipeline.add(new debruijn_graph::ContigOutput());
+//        }
 
-
-        barcode_resolver_pipeline.add(new debruijn_graph::HybridLibrariesAligning());
-
-        barcode_resolver_pipeline.add(new BarcodeMapConstructionStage(cfg::get().K))
-                .add(new debruijn_graph::PairInfoCount())
-                .add(new debruijn_graph::DistanceEstimation())
-                .add(new TslrResolverStage(cfg::get().K, cfg::get().output_dir + "resolver_output.fasta"));
         INFO("Output directory: " << cfg::get().output_dir);
 
         barcode_resolver_pipeline.run(conj_gp, cfg::get().entry_point.c_str());
-        INFO("TSLR resolver finished.");
+
+        INFO("Read cloud resolver finished.");
     }
 } //spades
