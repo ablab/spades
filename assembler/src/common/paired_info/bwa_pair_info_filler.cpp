725de450eb1ba0c571bfda12c25f7414400a9670
Anton Korobeynikov <anton@korobeynikov.info>
2016-12-04 13:58:23 +0300

    Hide a lot of implementation details of bwa pair info filler

--- assembler/src/common/paired_info/bwa_pair_info_filler.cpp
+++ assembler/src/common/paired_info/bwa_pair_info_filler.cpp
@@ -7,8 +7,149 @@
 
 #include "bwa_pair_info_filler.hpp"
 
+#include <paired_info/insert_size_refiner.hpp>
+
+#include <io/sam/sam_reader.hpp>
+#include <io/sam/read.hpp>
+#include <io/reads/osequencestream.hpp>
 
 namespace bwa_pair_info {
+
+//More compact representation of aligned read for storing in map
+class MapperReadT {
+public:
+    MapperReadT(): contig_id_(""), pos_(-1), len_(-1), is_forward_(true),
+                   left_hard_clip_(0), right_hard_clip_(0), left_soft_clip_(0), right_soft_clip_(0){}
+
+    MapperReadT(const string& ctg_id, int32_t pos, int32_t len, bool is_forward, const string& cigar):
+        contig_id_(ctg_id), pos_(pos), len_(len), is_forward_(is_forward),
+        left_hard_clip_(0), right_hard_clip_(0), left_soft_clip_(0), right_soft_clip_(0) {
+
+        ParseCigar(cigar);
+    }
+
+    bool IsValid() const {
+        return contig_id_ != "";
+    }
+
+private:
+
+    void ParseCigar(const string& cigar);
+
+public:
+    const string &get_contig_id() const {
+        return contig_id_;
+    }
+    int32_t pos() const {
+        return pos_;
+    }
+    int32_t len() const {
+        return len_;
+    }
+    bool is_forward() const {
+        return is_forward_;
+    }
+    uint32_t left_soft_clip() const {
+        return left_soft_clip_;
+    }
+    uint32_t right_soft_clip() const {
+        return right_soft_clip_;
+    }
+    uint32_t left_hard_clip() const {
+        return left_hard_clip_;
+    }
+    uint32_t right_hard_clip() const {
+        return right_hard_clip_;
+    }
+
+private:
+    string contig_id_;
+    int32_t pos_;
+    int32_t len_;
+    bool is_forward_;
+    uint32_t left_hard_clip_:16, right_hard_clip_:16;
+    uint32_t left_soft_clip_:16, right_soft_clip_:16;
+};
+
+//Class that corrects mapping positions according to lib orientation and clippings
+class BWACorrectingProcessor: public BWAPairedReadProcessor {
+protected:
+    const SequencingLibraryT& lib_;
+
+    const EdgeIdMap& edge_id_map_;
+
+    const debruijn_graph::Graph& g_;
+
+    size_t count_;
+
+public:
+
+    struct MappedPositionT {
+        EdgeId e;
+        int pos;
+
+        MappedPositionT(EdgeId e_, int pos_): e(e_), pos(pos_) {
+
+        }
+    };
+
+    BWACorrectingProcessor(const SequencingLibraryT& lib, const EdgeIdMap& edge_id_map, const debruijn_graph::Graph& g):
+        lib_(lib), edge_id_map_(edge_id_map), g_(g), count_(0) {
+    }
+
+    virtual bool CheckAlignments(const MappedPositionT& l, const MappedPositionT& r) = 0;
+
+    virtual void ProcessAlignments(const MappedPositionT& l, const MappedPositionT& r) = 0;
+//Correct read algnment according to orientation and clippings
+    virtual void ProcessPairedRead(const MapperReadT& l, const MapperReadT& r);
+};
+
+//Insert size counter
+class BWAISCounter: public BWACorrectingProcessor {
+private:
+    HistType hist_;
+    size_t min_contig_len_;
+    bool ignore_negative_;
+    size_t mapped_count_;
+    size_t negative_count_;
+
+public:
+    BWAISCounter(const SequencingLibraryT& lib, const EdgeIdMap& edge_id_map, const debruijn_graph::Graph& g,
+                 size_t min_contig_len, bool ignore_negative = false):
+        BWACorrectingProcessor(lib, edge_id_map, g), hist_(), min_contig_len_(min_contig_len),
+        ignore_negative_(ignore_negative), mapped_count_(0), negative_count_(0) {
+    }
+
+    bool CheckAlignments(const MappedPositionT& l, const MappedPositionT& r) override;
+
+    void ProcessAlignments(const MappedPositionT& l, const MappedPositionT& r) override;
+
+    bool RefineInsertSize(SequencingLibraryT& reads) const ;
+
+};
+
+//Pair info filler
+class BWAIndexFiller: public BWACorrectingProcessor {
+
+private:
+    PairedInfoIndexT& paired_index_;
+
+    size_t min_contig_len_;
+
+    EdgePair ConjugatePair(EdgePair ep) const;
+
+public:
+    BWAIndexFiller(const SequencingLibraryT& lib, const EdgeIdMap& edge_id_map, const debruijn_graph::Graph& g,
+                   PairedInfoIndexT& paired_index, size_t min_contig_len = 0):
+        BWACorrectingProcessor(lib, edge_id_map, g), paired_index_(paired_index), min_contig_len_(min_contig_len) {
+    }
+
+    bool CheckAlignments(const MappedPositionT& l, const MappedPositionT& r) override;
+
+    void ProcessAlignments(const MappedPositionT& l, const MappedPositionT& r) override;
+};
+
+using namespace sam_reader;
 
 void MapperReadT::ParseCigar(const string& cigar) {
     string num = "";
@@ -401,6 +542,5 @@ bool BWAPairInfoFiller::ProcessLib(size_t lib_index,
         path::remove_dir(lib_dir);
     return result;
 }
-
 
 }
--- assembler/src/common/paired_info/bwa_pair_info_filler.hpp
+++ assembler/src/common/paired_info/bwa_pair_info_filler.hpp
@@ -6,21 +6,15 @@
 //***************************************************************************
 
 #include "assembly_graph/core/graph.hpp"
+
 #include "pipeline/config_struct.hpp"
-
-#include <io/sam/sam_reader.hpp>
-#include <io/sam/read.hpp>
-
-#include <io/reads/osequencestream.hpp>
 #include <paired_info/paired_info.hpp>
-#include <paired_info/insert_size_refiner.hpp>
 
 #ifndef PROJECT_BWA_PAIR_INFO_FILLER_HPP_H
 #define PROJECT_BWA_PAIR_INFO_FILLER_HPP_H
 
 namespace bwa_pair_info {
 
-using namespace sam_reader;
 using debruijn_graph::EdgeId;
 
 typedef omnigraph::de::UnclusteredPairedInfoIndexT<debruijn_graph::Graph> PairedInfoIndexT;
@@ -28,148 +22,13 @@ typedef io::SequencingLibrary<debruijn_graph::config::DataSetData> SequencingLib
 typedef std::pair<debruijn_graph::EdgeId, debruijn_graph::EdgeId> EdgePair;
 typedef unordered_map<size_t, debruijn_graph::EdgeId> EdgeIdMap;
 
-//More compact representation of aligned read for storing in map
-class MapperReadT {
-public:
-    MapperReadT(): contig_id_(""), pos_(-1), len_(-1), is_forward_(true),
-                   left_hard_clip_(0), right_hard_clip_(0), left_soft_clip_(0), right_soft_clip_(0){}
-
-    MapperReadT(const string& ctg_id, int32_t pos, int32_t len, bool is_forward, const string& cigar):
-        contig_id_(ctg_id), pos_(pos), len_(len), is_forward_(is_forward),
-        left_hard_clip_(0), right_hard_clip_(0), left_soft_clip_(0), right_soft_clip_(0) {
-
-        ParseCigar(cigar);
-    }
-
-    bool IsValid() const {
-        return contig_id_ != "";
-    }
-
-private:
-
-    void ParseCigar(const string& cigar);
-
-public:
-    const string &get_contig_id() const {
-        return contig_id_;
-    }
-    int32_t pos() const {
-        return pos_;
-    }
-    int32_t len() const {
-        return len_;
-    }
-    bool is_forward() const {
-        return is_forward_;
-    }
-    uint32_t left_soft_clip() const {
-        return left_soft_clip_;
-    }
-    uint32_t right_soft_clip() const {
-        return right_soft_clip_;
-    }
-    uint32_t left_hard_clip() const {
-        return left_hard_clip_;
-    }
-    uint32_t right_hard_clip() const {
-        return right_hard_clip_;
-    }
-
-private:
-    string contig_id_;
-    int32_t pos_;
-    int32_t len_;
-    bool is_forward_;
-    uint32_t left_hard_clip_:16, right_hard_clip_:16;
-    uint32_t left_soft_clip_:16, right_soft_clip_:16;
-};
-
 //Base class for aligned read processor (simple analog of SequenceMapperListener)
+class MapperReadT;
+
 class BWAPairedReadProcessor {
 public:
     virtual void ProcessPairedRead(const MapperReadT& l, const MapperReadT& r) = 0;
-
-    virtual ~BWAPairedReadProcessor() {
-
-    }
-};
-
-//Class that corrects mapping positions according to lib orientation and clippings
-class BWACorrectingProcessor: public BWAPairedReadProcessor {
-protected:
-    const SequencingLibraryT& lib_;
-
-    const EdgeIdMap& edge_id_map_;
-
-    const debruijn_graph::Graph& g_;
-
-    size_t count_;
-
-public:
-
-    struct MappedPositionT {
-        EdgeId e;
-        int pos;
-
-        MappedPositionT(EdgeId e_, int pos_): e(e_), pos(pos_) {
-
-        }
-    };
-
-    BWACorrectingProcessor(const SequencingLibraryT& lib, const EdgeIdMap& edge_id_map, const debruijn_graph::Graph& g):
-        lib_(lib), edge_id_map_(edge_id_map), g_(g), count_(0) {
-    }
-
-    virtual bool CheckAlignments(const MappedPositionT& l, const MappedPositionT& r) = 0;
-
-    virtual void ProcessAlignments(const MappedPositionT& l, const MappedPositionT& r) = 0;
-//Correct read algnment according to orientation and clippings
-    virtual void ProcessPairedRead(const MapperReadT& l, const MapperReadT& r);
-};
-
-//Insert size counter
-class BWAISCounter: public BWACorrectingProcessor {
-private:
-    HistType hist_;
-    size_t min_contig_len_;
-    bool ignore_negative_;
-    size_t mapped_count_;
-    size_t negative_count_;
-
-public:
-    BWAISCounter(const SequencingLibraryT& lib, const EdgeIdMap& edge_id_map, const debruijn_graph::Graph& g,
-                 size_t min_contig_len, bool ignore_negative = false):
-        BWACorrectingProcessor(lib, edge_id_map, g), hist_(), min_contig_len_(min_contig_len),
-        ignore_negative_(ignore_negative), mapped_count_(0), negative_count_(0) {
-    }
-
-    bool CheckAlignments(const MappedPositionT& l, const MappedPositionT& r) override;
-
-    void ProcessAlignments(const MappedPositionT& l, const MappedPositionT& r) override;
-
-    bool RefineInsertSize(SequencingLibraryT& reads) const ;
-
-};
-
-//Pair info filler
-class BWAIndexFiller: public BWACorrectingProcessor {
-
-private:
-    PairedInfoIndexT& paired_index_;
-
-    size_t min_contig_len_;
-
-    EdgePair ConjugatePair(EdgePair ep) const;
-
-public:
-    BWAIndexFiller(const SequencingLibraryT& lib, const EdgeIdMap& edge_id_map, const debruijn_graph::Graph& g,
-                   PairedInfoIndexT& paired_index, size_t min_contig_len = 0):
-        BWACorrectingProcessor(lib, edge_id_map, g), paired_index_(paired_index), min_contig_len_(min_contig_len) {
-    }
-
-    bool CheckAlignments(const MappedPositionT& l, const MappedPositionT& r) override;
-
-    void ProcessAlignments(const MappedPositionT& l, const MappedPositionT& r) override;
+    virtual ~BWAPairedReadProcessor() {}
 };
 
 //Class for running BWA, managing and parsing SAM files
@@ -181,19 +40,12 @@ private:
     const debruijn_graph::Graph& g_;
 
     string bwa_path_;
-
     string base_dir_;
-
     string work_dir_;
-
     size_t nthreads_;
-
     string index_base_;
-
     bool index_constructed_;
-
     bool remove_tmp_files_;
-
     unordered_map<size_t, debruijn_graph::EdgeId> edge_id_map_;
 
 private:
