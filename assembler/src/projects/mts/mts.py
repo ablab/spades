b98c31dd3b41c72f71e3f123e16879fbc4930d0d
Sergey Nurk <sergeynurk@gmail.com>
2016-12-10 22:43:10 +0300

    improvements in mts.py: can be launched from anywhere; creates output dir if provided with external config

--- assembler/src/projects/mts/mts.py
+++ assembler/src/projects/mts/mts.py
@@ -6,31 +6,67 @@ import subprocess
 import sys
 import os
 import os.path
+import shutil
+
+#copied from http://stackoverflow.com/questions/431684/how-do-i-cd-in-python/13197763#13197763
+class cd:
+    """Context manager for changing the current working directory"""
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
 
 parser = argparse.ArgumentParser(description="MTS - Metagenomic Time Series")
 
 parser.add_argument("--threads", "-t", type=int, default=8, help="Number of threads")
-parser.add_argument("dir", type=str, help="Output directory where config.yaml is located")
+parser.add_argument("dir", type=str, help="Output directory")
+parser.add_argument("--config", "-c", type=str, default="", help="config.yaml to be copied to the directory (unnecessary if config.yaml is already there)")
 parser.add_argument("--stats", "-s", action="store_true", help="Calculate stats (when the REFS parameter in config.yaml is provided)")
 parser.add_argument("--reuse-assemblies", action="store_true", help="Use existing assemblies (put them in the corresponding folders)")
+parser.add_argument("--verbose", "-v", action="store_true", help="Increase verbosity level")
 
 args = parser.parse_args()
 
-LOCAL_DIR = os.path.realpath(os.path.join(os.getcwd(), "../../../"))
+exec_dir=os.path.dirname(os.path.realpath(sys.argv[0]))
+LOCAL_DIR = os.path.realpath(os.path.join(exec_dir, "../../../"))
 
-base_params = ["snakemake", "--directory", args.dir, "--cores", str(args.threads), "--config", "LOCAL_DIR" + "=" + LOCAL_DIR]
+base_params = ["snakemake", "--directory", os.path.realpath(args.dir), "--cores", str(args.threads), "--config", "LOCAL_DIR" + "=" + LOCAL_DIR]
 
-def call_snake(extra_params=[]):
-    subprocess.check_call(base_params + extra_params, stdout=sys.stdout, stderr=sys.stderr)
+if args.verbose:
+    base_params.extend(["-p", "--verbose"])
 
-print("Step #1 - Assembly")
-if args.reuse_assemblies:
-    call_snake(["assemble_all", "--touch"])
-call_snake()
+if args.config:
+    if os.path.exists(os.path.join(args.dir, "config.yaml")):
+        print("Config path specified, but config.yaml already exists in output folder " + args.dir)
+        sys.exit(239)
 
-if args.stats:
-    print("Step #2a - Assembly statistics")
-    call_snake(["--snakefile", "Snakestats", "stats_all"])
+if not os.path.exists(args.dir):
+    os.makedirs(args.dir)
 
-    print("Step #2b - Reassembly statistics")
-    call_snake(["--snakefile", "Snakestats", "stats_reassembly"])
+print("Output folder set to " + args.dir)
+
+if args.config:
+    print("Copying config from " + args.config)
+    shutil.copy(args.config, args.dir)
+
+with cd(exec_dir):
+    def call_snake(extra_params=[]):
+        subprocess.check_call(base_params + extra_params, stdout=sys.stdout, stderr=sys.stderr)
+    
+    print("Step #1 - Assembly")
+    if args.reuse_assemblies:
+        call_snake(["assemble_all", "--touch"])
+
+    call_snake()
+    
+    if args.stats:
+        print("Step #2a - Assembly statistics")
+        call_snake(["--snakefile", "Stats.snake", "stats_all"])
+    
+        print("Step #2b - Reassembly statistics")
+        call_snake(["--snakefile", "Stats.snake", "stats_reassembly"])
