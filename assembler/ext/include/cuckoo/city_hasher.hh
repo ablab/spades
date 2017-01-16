d72883c7c158210b76d8ed341658d5fb3df7be60
Anton Korobeynikov <anton@korobeynikov.info>
2017-01-16 15:25:48 +0300

    Update cuckoo hash map

--- assembler/ext/include/cuckoo/city_hasher.hh
+++ assembler/ext/include/cuckoo/city_hasher.hh
@@ -1,3 +1,5 @@
+/** \file */
+
 #ifndef _CITY_HASHER_HH
 #define _CITY_HASHER_HH
 
@@ -10,8 +12,15 @@
 template <class Key>
 class CityHasher {
 public:
+    //! The function call operator for our hash function
     size_t operator()(const Key& k) const {
-        return CityHash64((const char*) &k, sizeof(k));
+        if (sizeof(size_t) < 8) {
+            return CityHash32((const char*) &k, sizeof(k));
+        }
+        /* Although the following line should be optimized away on 32-bit
+         * builds, the cast is still necessary to stop MSVC emitting a
+         * truncation warning. */
+        return static_cast<size_t>(CityHash64((const char*) &k, sizeof(k)));
     }
 };
 
@@ -20,8 +29,15 @@ public:
 template <>
 class CityHasher<std::string> {
 public:
+    //! The function call operator for our hash function
     size_t operator()(const std::string& k) const {
-        return CityHash64(k.c_str(), k.size());
+        if (sizeof(size_t) < 8) {
+            return CityHash32(k.c_str(), k.size());
+        }
+        /* Although the following line should be optimized away on 32-bit
+         * builds, the cast is still necessary to stop MSVC emitting a
+         * truncation warning. */
+        return static_cast<size_t>(CityHash64(k.c_str(), k.size()));
     }
 };
 
--- assembler/ext/include/cuckoo/cuckoohash_util.hh
+++ assembler/ext/include/cuckoo/cuckoohash_util.hh
@@ -4,27 +4,67 @@
 #define _CUCKOOHASH_UTIL_HH
 
 #include <exception>
-#include <pthread.h>
 #include <thread>
+#include <utility>
 #include <vector>
 #include "cuckoohash_config.hh" // for LIBCUCKOO_DEBUG
 
 #if LIBCUCKOO_DEBUG
-#  define LIBCUCKOO_DBG(fmt, args...)                                   \
-     fprintf(stderr, "\x1b[32m""[libcuckoo:%s:%d:%lu] " fmt"" "\x1b[0m", \
-             __FILE__,__LINE__, (unsigned long)pthread_self(), ##args)
+//! When \ref LIBCUCKOO_DEBUG is 0, LIBCUCKOO_DBG will printing out status
+//! messages in various situations
+#  define LIBCUCKOO_DBG(fmt, ...)                                          \
+     fprintf(stderr, "\x1b[32m""[libcuckoo:%s:%d:%lu] " fmt"" "\x1b[0m",   \
+             __FILE__,__LINE__, std::hash<std::thread::id>()(std::this_thread::get_id()), \
+             __VA_ARGS__)
 #else
-#  define LIBCUCKOO_DBG(fmt, args...)  do {} while (0)
+//! When \ref LIBCUCKOO_DEBUG is 0, LIBCUCKOO_DBG does nothing
+#  define LIBCUCKOO_DBG(fmt, ...)  do {} while (0)
 #endif
 
-// For enabling certain methods based on a condition. Here's an example.
-// ENABLE_IF(some_cond, type, static, inline) method() {
-//     ...
-// }
-#define ENABLE_IF(preamble, condition, return_type)                     \
-    template <class Bogus=void*>                                        \
-    preamble typename std::enable_if<sizeof(Bogus) &&                   \
-        condition, return_type>::type
+/**
+ * alignas() requires GCC >= 4.9, so we stick with the alignment attribute for
+ * GCC.
+ */
+#ifdef __GNUC__
+#define LIBCUCKOO_ALIGNAS(x) __attribute__((aligned(x)))
+#else
+#define LIBCUCKOO_ALIGNAS(x) alignas(x)
+#endif
+
+/**
+ * At higher warning levels, MSVC produces an annoying warning that alignment
+ * may cause wasted space: "structure was padded due to __declspec(align())".
+ */
+#ifdef _MSC_VER
+#define LIBCUCKOO_SQUELCH_PADDING_WARNING __pragma(warning(suppress : 4324))
+#else
+#define LIBCUCKOO_SQUELCH_PADDING_WARNING
+#endif
+
+/**
+ * thread_local requires GCC >= 4.8 and is not supported in some clang versions,
+ * so we use __thread if thread_local is not supported
+ */
+#define LIBCUCKOO_THREAD_LOCAL thread_local
+#if defined(__clang__)
+#  if !__has_feature(cxx_thread_local)
+#    undef LIBCUCKOO_THREAD_LOCAL
+#    define LIBCUCKOO_THREAD_LOCAL __thread
+#  endif
+#elif defined(__GNUC__)
+#  if __GNUC__ == 4 && __GNUC_MINOR__ < 8
+#    undef LIBCUCKOO_THREAD_LOCAL
+#    define LIBCUCKOO_THREAD_LOCAL __thread
+#  endif
+#endif
+
+//! For enabling certain methods based on a condition. Here's an example.
+//! LIBCUCKOO_ENABLE_IF(sizeof(int) == 4, int) method() {
+//!     ...
+//! }
+#define LIBCUCKOO_ENABLE_IF(condition, return_type)                       \
+    template <class Bogus = void*>                                        \
+    typename std::enable_if<sizeof(Bogus) && condition, return_type>::type
 
 /**
  * Thrown when an automatic expansion is triggered, but the load factor of the
@@ -43,7 +83,10 @@ public:
     libcuckoo_load_factor_too_low(const double lf)
         : load_factor_(lf) {}
 
-    virtual const char* what() const noexcept {
+    /**
+     * @return a descriptive error message
+     */
+    virtual const char* what() const noexcept override {
         return "Automatic expansion triggered when load factor was below "
             "minimum threshold";
     }
@@ -51,7 +94,7 @@ public:
     /**
      * @return the load factor of the table when the exception was thrown
      */
-    double load_factor() {
+    double load_factor() const {
         return load_factor_;
     }
 private:
@@ -73,68 +116,21 @@ public:
     libcuckoo_maximum_hashpower_exceeded(const size_t hp)
         : hashpower_(hp) {}
 
-    virtual const char* what() const noexcept {
+    /**
+     * @return a descriptive error message
+     */
+    virtual const char* what() const noexcept override {
         return "Expansion beyond maximum hashpower";
     }
 
     /**
      * @return the hashpower we were trying to expand to
      */
-    size_t hashpower() {
+    size_t hashpower() const {
         return hashpower_;
     }
 private:
     const size_t hashpower_;
 };
-
-// Allocates an array of the given size and value-initializes each element with
-// the 0-argument constructor
-template <class T, class Alloc>
-T* create_array(const size_t size) {
-    Alloc allocator;
-    T* arr = allocator.allocate(size);
-    // Initialize all the elements, safely deallocating and destroying
-    // everything in case of error.
-    size_t i;
-    try {
-        for (i = 0; i < size; ++i) {
-            allocator.construct(&arr[i]);
-        }
-    } catch (...) {
-        for (size_t j = 0; j < i; ++j) {
-            allocator.destroy(&arr[j]);
-        }
-        allocator.deallocate(arr, size);
-        throw;
-    }
-    return arr;
-}
-
-// Destroys every element of an array of the given size and then deallocates the
-// memory.
-template <class T, class Alloc>
-void destroy_array(T* arr, const size_t size) {
-    Alloc allocator;
-    for (size_t i = 0; i < size; ++i) {
-        allocator.destroy(&arr[i]);
-    }
-    allocator.deallocate(arr, size);
-}
-
-// executes the function over the given range split over num_threads threads
-template <class F>
-static void parallel_exec(size_t start, size_t end,
-                          size_t num_threads, F func) {
-    size_t work_per_thread = (end - start) / num_threads;
-    std::vector<std::thread> threads(num_threads);
-    for (size_t i = 0; i < num_threads - 1; ++i) {
-        threads[i] = std::thread(func, start, start + work_per_thread);
-        start += work_per_thread;
-    }
-    threads[num_threads - 1] = std::thread(func, start, end);
-    for (std::thread& t : threads) {
-        t.join();
-    }
-}
 
 #endif // _CUCKOOHASH_UTIL_HH
