2c9eed0ef1db86356f08f412b5e382d397a71f6f
Sergey Nurk <sergeynurk@gmail.com>
2017-02-11 02:54:43 +0300

    full contig name added to the filtered nucmer report

--- assembler/src/projects/mts/scripts/filter_nucmer.py
+++ assembler/src/projects/mts/scripts/filter_nucmer.py
@@ -27,26 +27,28 @@ if not path.exists(nucmer_output_fn):
 
 with open(nucmer_output_fn, "r") as nucmer_output:
     with open(output_fn, "w") as output:
-        align_data = re.compile("\d+ \d+ \| \d+ \d+ \| \d+ (\d+) \| [\d.]+ \| [^ ]+ NODE_(\d+)_length_(\d+)")
+        align_data = re.compile("\d+ \d+ \| \d+ \d+ \| \d+ (\d+) \| [\d.]+ \| [^ ]+ (NODE_(\d+)_length_(\d+)_.*$)")
         contig = None
+        name = ""
         contig_len = 0
         align_len = 0
         def process_contig():
             per = 100.0 * align_len / contig_len
             if per > threshold and contig_len >= min_length:
-                print("{}\t{}\t{}".format(contig, contig_len, per), file=output)
+                print("{}\t{}\t{}\t{}".format(contig, contig_len, per, name), file=output)
                 return align_len
             return 0
         for line in nucmer_output:
             res = align_data.search(line)
             if res is None:
                 continue
-            new_contig = res.group(2)
+            new_contig = res.group(3)
             if contig != new_contig:
                 if contig is not None:
                     process_contig()
                 contig = new_contig
-                contig_len = int(res.group(3))
+                name = res.group(2)
+                contig_len = int(res.group(4))
                 align_len = 0
             #Assuming that all alignments of the same contig are consequent
             align_len += int(res.group(1))
