a0731d22d60c052d63b3d7c0a1ee2a72a8000b15
Anton Korobeynikov <anton@korobeynikov.info>
2017-02-27 00:33:26 +0300

    Pass the options to cmake earlier

--- assembler/spades_compile.sh
+++ assembler/spades_compile.sh
@@ -19,7 +19,7 @@ rm -rf "$BASEDIR/$BUILD_DIR"
 mkdir -p "$BASEDIR/$BUILD_DIR"
 set -e
 cd "$BASEDIR/$BUILD_DIR"
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$PREFIX" "$BASEDIR/src" $*
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$PREFIX" $* "$BASEDIR/src"
 make -j 8
 make install
 cd $PREFIX
