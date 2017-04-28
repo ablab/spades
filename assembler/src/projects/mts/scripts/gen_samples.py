1a18efc51169abbf5a73b2339ab59bfac8eb2e58
Yuri Gorshkov <y.s.gorshkov@gmail.com>
2017-04-29 02:00:24 +0300

    MTS: improved the samples generator

--- assembler/src/projects/mts/scripts/gen_samples.py
+++ assembler/src/projects/mts/scripts/gen_samples.py
@@ -13,12 +13,9 @@ from scipy.stats import expon
 
 def gen_profile(args):
     if args.distribution == "uni":
-        #def rand():
-        #    return random.randint(0, args.scale)
-        pass
+        rand = lambda: random.randint(0, args.scale)
     elif args.distribution == "exp":
-        def rand():
-            return int(expon.rvs(scale=args.scale))
+        rand = lambda: int(expon.rvs(scale=args.scale))
 
     refs = dict(gather_refs(args.references))
     if args.dump_desc:
@@ -40,39 +37,58 @@ def gen_samples(args):
     read_len = args.read_length
     adj_qual = "2" * read_len + "\n"
 
+    table = None
+
     with open(args.profile) as input:
         first_line = True
         for line in input:
             params = line.split()
-            ref_name = params[0]
-            ref_path = refs.get(ref_name)
-            if not ref_path:
-                print("Warning: no reference provided for", ref_name)
+            ref = params[0]
+            if not refs.get(ref):
+                print("Warning: no reference provided for", ref)
                 continue
-            for i, abundance in enumerate(map(int, params[1:]), start=1):
-                ref_len = os.stat(ref_path).st_size
-                reads = ref_len * abundance // read_len
-                print("Generating", reads, "reads for subsample", i, "of", ref_name)
-                sample_dir = os.path.join(args.out_dir, "sample" + str(i))
-                if first_line:
-                    shutil.rmtree(sample_dir, ignore_errors=True)
-                    subprocess.check_call(["mkdir", "-p", sample_dir])
+            if first_line:
+                table = [None] * (len(params) - 1)
+                first_line = False
+            for i, abundance in enumerate(map(int, params[1:])):
+                if not table[i]:
+                    table[i] = dict()
+                table[i][ref] = abundance
 
-                temp_1 = sample_dir + ".tmp.r1.fastq"
-                temp_2 = sample_dir + ".tmp.r2.fastq"
-                subprocess.check_call(["wgsim", "-N", str(reads), "-r", "0", "-1", str(read_len), "-2", str(read_len), "-d", "300", "-s", "10", "-e", "{:.2f}".format(args.error_rate), "-S", str(i), ref_path, temp_1, temp_2], stdout=subprocess.DEVNULL)
+    for i, abundancies in enumerate(table, start=1):
+        print("Generating reads for", i, "sample")
+        sample_dir = os.path.join(args.out_dir, "sample" + str(i))
+        shutil.rmtree(sample_dir, ignore_errors=True)
+        subprocess.check_call(["mkdir", "-p", sample_dir])
+        outs = [None] * 2
+        for dir, name in enumerate([os.path.join(sample_dir, "r1.fastq"), os.path.join(sample_dir, "r2.fastq")]):
+            if args.gzip:
+                name = name + ".gz"
+            output = open(name, "wb")
+            if args.gzip:
+                output = subprocess.Popen("gzip", stdin=subprocess.PIPE, stdout=output).stdin
+            outs[dir] = output
 
-                print("Merging temporary files")
-                for temp, out in [(temp_1, os.path.join(sample_dir, "r1.fastq")), (temp_2, os.path.join(sample_dir, "r2.fastq"))]:
-                    with open(temp) as input, open(out, "a") as output:
-                        for line in input:
-                            if line.startswith("IIIII"): #TODO: remove this hack
-                                output.write(adj_qual)
-                            else:
-                                output.write(line)
-                    os.remove(temp)
+        for ref, abundance in abundancies.items():
+            ref_path = refs.get(ref)
+            ref_len = os.stat(ref_path).st_size
+            reads = ref_len * abundance // read_len
+            print("Generating", reads, "reads for subsample", i, "of", ref)
+
+            temp_1 = sample_dir + ".tmp.r1.fastq"
+            temp_2 = sample_dir + ".tmp.r2.fastq"
+            subprocess.check_call(["wgsim", "-N", str(reads), "-r", "0", "-1", str(read_len), "-2", str(read_len), "-d", "300", "-s", "10", "-e", "{:.2f}".format(args.error_rate), "-S", str(i), ref_path, temp_1, temp_2], stdout=subprocess.DEVNULL)
+
+            print("Merging temporary files")
+            for temp, output in zip([temp_1, temp_2], outs):
+                with open(temp) as input:
+                    for line in input:
+                        if line.startswith("IIIII"): #TODO: remove this hack
+                            output.write(adj_qual.encode())
+                        else:
+                            output.write(line.encode())
+                os.remove(temp)
             print()
-            first_line = False
 
 parser = argparse.ArgumentParser(description="Metagenomic Time Series Simulator")
 parser.add_argument("--references", "-r", type=str, help="Comma-separated list of references, or a directory with them, or a desc file with reference paths prepended with @", required=True)
@@ -89,6 +105,7 @@ gen_samples_args = subparsers.add_parser("gen", help="Generate reads using a pro
 gen_samples_args.add_argument("--out-dir", "-o", type=str, help="Output directory. Will be totally overwritten!")
 gen_samples_args.add_argument("--read-length", "-l", type=int, help="Read length", default=100)
 gen_samples_args.add_argument("--error-rate", "-e", type=float, help="Base error rate", default=0)
+gen_samples_args.add_argument("--gzip", "-z", action="store_true", help="Compress resulted files")
 gen_samples_args.add_argument("profile", type=str, help="File with reference profiles")
 gen_samples_args.set_defaults(func=gen_samples)
 
