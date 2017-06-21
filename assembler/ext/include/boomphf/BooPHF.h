5cb71f2c50dbe257f3406e5397c48d4c9e12f8e2
Anton Korobeynikov <anton@korobeynikov.info>
2017-06-22 01:51:12 +0300

    Use better popcount

--- assembler/ext/include/boomphf/BooPHF.h
+++ assembler/ext/include/boomphf/BooPHF.h
@@ -168,27 +168,22 @@ class file_binary{
 };
 
 
-inline unsigned int popcount_32(unsigned int x)
-{
-    unsigned int m1 = 0x55555555;
-    unsigned int m2 = 0x33333333;
-    unsigned int m4 = 0x0f0f0f0f;
-    unsigned int h01 = 0x01010101;
-    x -= (x >> 1) & m1;               /* put count of each 2 bits into those 2 bits */
-    x = (x & m2) + ((x >> 2) & m2);   /* put count of each 4 bits in */
-    x = (x + (x >> 4)) & m4;          /* put count of each 8 bits in partie droite  4bit piece*/
-    return (x * h01) >> 24;           /* returns left 8 bits of x + (x<<8) + ... */
+#define L8 0x0101010101010101ULL // Every lowest 8th bit set: 00000001...
+#define G2 0xAAAAAAAAAAAAAAAAULL // Every highest 2nd bit: 101010...
+#define G4 0x3333333333333333ULL // 00110011 ... used to group the sum of 4 bits.
+#define G8 0x0F0F0F0F0F0F0F0FULL
+
+static inline unsigned popcount_64(uint64_t x) {
+    // Step 1:  00 - 00 = 0;  01 - 00 = 01; 10 - 01 = 01; 11 - 01 = 10;
+    x = x - ((x & G2) >> 1);
+    // step 2:  add 2 groups of 2.
+    x = (x & G4) + ((x >> 2) & G4);
+    // 2 groups of 4.
+    x = (x + (x >> 4)) & G8;
+    // Using a multiply to collect the 8 groups of 8 together.
+    x = x * L8 >> 56;
+    return x;
 }
-
-
-inline unsigned int popcount_64(uint64_t x)
-{
-    unsigned int low = x & 0xffffffff ;
-    unsigned int high = ( x >> 32LL) & 0xffffffff ;
-
-    return (popcount_32(low) + popcount_32(high));
-}
-
 
 ///// progress bar
 class Progress
