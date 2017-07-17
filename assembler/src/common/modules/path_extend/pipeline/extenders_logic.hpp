cc7302b0b9253c0ebd1f75c41a225833ecac7543
Itolstoganov <oockvay@gmail.com>
2017-07-17 19:54:07 +0300

    Cleanup: remove max_polisher_gap magic constant

--- assembler/src/common/modules/path_extend/pipeline/extenders_logic.hpp
+++ assembler/src/common/modules/path_extend/pipeline/extenders_logic.hpp
@@ -79,7 +79,6 @@ public:
 
     Extenders MakePEExtenders() const;
 
-    //fixme make private again
     shared_ptr<ExtensionChooser> MakeSimpleExtensionChooser(size_t lib_index) const;
 private:
 
