04058a19b31435972d57a792a5f0f5556d5b1a5a
Dmitry Antipov <dmitrij.antipov@gmail.com>
2017-07-23 21:03:12 +0300

    fixed unclassified detection bug

--- assembler/src/plasmid_utils/parse_blast_xml.py
+++ assembler/src/plasmid_utils/parse_blast_xml.py
@@ -3,6 +3,8 @@ import sys
 import argparse
 from glob import glob
 from joblib import Parallel, delayed
+significant_e_value = 0.001
+import os.path
 
 # input - BLAST XML of scaffolds.fasta (?)
 # output:
@@ -17,29 +19,33 @@ from joblib import Parallel, delayed
 def parse_args(args):
     ###### Command Line Argument Parser
     parser = argparse.ArgumentParser(description="Check the help flag")
-    parser.add_argument('--f', help='Path to folder with XML output')
-    
+    parser.add_argument('-i', help='Path to folder with XML output')
+    parser.add_argument('-o', help='Path to output folder')  
     return parser.parse_args()
 
 
-args = parse_args(sys.argv[1:])
+
+#return total length of alignment with respect of significant_e_value (0.001) threshold
+def get_total_len(alignment):
+    global significant_e_value
+    total_len = 0
+    for hsp in alignment.hsps:
+        if hsp.expect < significant_e_value:
+            total_len += hsp.align_length
+    return total_len
 
 
-files = glob(str(args.f)+"/*.xml")
-print (files)
+def parser(f, out_dir):
 
-
-
-
-def parser(f):
-
-    result=open(f,"r")
-    nosig = open(f[:-4]+"_no_significant.names", "w")
-    chrom = open(f[:-4]+"_chromosome.names", "w")
-    plasmids = open(f[:-4]+"_plasmid.names", "w")
-    plasmids_bad = open(f[:-4]+"_plasmids_bad.names", "w")
-    unclas = open(f[:-4]+"_unclassified.names", "w")
-    records= NCBIXML.parse(result)
+    xml_file = open(f,"r")
+    name = os.path.basename(f)
+    name = os.path.join(out_dir, name)
+    nosig = open(name[:-4]+"_no_significant.names", "w")
+    chrom = open(name[:-4]+"_chromosome.names", "w")
+    plasmids = open(name[:-4]+"_plasmid.names", "w")
+    plasmids_bad = open(name[:-4]+"_plasmids_bad.names", "w")
+    unclas = open(name[:-4]+"_unclassified.names", "w")
+    records= NCBIXML.parse(xml_file)
 #item=next(records)
     for item in records:
         print item.query
@@ -50,10 +56,7 @@ def parser(f):
             continue
         good_aln = []
         for alignment in item.alignments:        
-            total_len = 0
-            for hsp in alignment.hsps:
-                if hsp.expect <0.001:
-                    total_len += hsp.align_length
+            total_len = get_total_len(alignment)
             if (len(alignment.hsps) <= 3 and total_len > 0.9 * pl_len and pl_len > 0.9 * total_len):
                 good_aln.append(alignment.title)
         pl = 0
@@ -64,23 +67,37 @@ def parser(f):
                 pl = 1
                 break
         if pl == 0:
-            seq = item.alignments[0].title
-            if seq.find('lasmid') != -1:
-                plasmids_bad.write(item.query + '\n')
-                plasmids_bad.write(seq + "\n" + "len " + str (item.alignments[0].length) +" alnments lenghts:" + "\n")
-                for hsp in item.alignments[0].hsps:
-                    plasmids_bad.write( str (hsp.align_length )+ " ")
-                    plasmids_bad.write("\n")
-                    pl = 1
-            elif (seq.find('hromosome') != -1) or (seq.find("complete genome") != -1):
-                chrom.write(item.query + '\n')
-                chrom.write(seq + '\n')
-            else:
+            found = False
+            for alignment in item.alignments:
+                seq = alignment.title
+                total_len = get_total_len(alignment)
+#alignment is too small to decide
+                if total_len < pl_len * 0.1:
+                    continue
+                if seq.find('lasmid') != -1:
+                    plasmids_bad.write(item.query + '\n')
+                    plasmids_bad.write(seq + "\n" + "len " + str (alignment.length) +" alnments lenghts:" + "\n")
+                    for hsp in alignment.hsps:
+                        plasmids_bad.write( str (hsp.align_length )+ " ")
+                        plasmids_bad.write("\n")
+                    found = True
+                    break
+                elif (seq.find('hromosome') != -1) or (seq.find("omplete genome") != -1):
+                    chrom.write(item.query + '\n')
+                    chrom.write(seq + '\n')
+                    found = True
+                    break
+            if not found:
                 unclas.write(item.query + '\n')
                 unclas.write(item.alignments[0].title + '\n')
     return
 
-Parallel(n_jobs=30)(delayed(parser) (args) for args in files[)
+
+parsed_args = parse_args(sys.argv[1:])
+files = glob(str(parsed_args.i)+"/*.xml")
+print (files)
+
+Parallel(n_jobs=30)(delayed(parser) (infile, parsed_args.o) for infile in files)
 #Parallel(n_jobs=30)(delayed(parser) (args) for args in files)
 
 #                print('score:', hsp.score)
