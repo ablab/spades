8bf0cf4972b16154020f4e07cd34d6b2365f5d03
mikeraiko <mike.rayko@gmail.com>
2017-08-25 15:35:05 +0300

    chromosomal contigs detection

---
+++ assembler/src/plasmid_utils/chromosomal_contig_removal.py
@@ -0,0 +1,30 @@
+#from Bio.Blast import NCBIXML
+import os
+import sys
+#import argparse
+#from glob import glob
+#from joblib import Parallel, delayed
+
+# input - BLAST XML of scaffolds.fasta (?)
+# output:
+# if no alignment at all - nosig
+# if good alignment (e<0.001):
+#  if "plasmid" in name:
+#    <3 matches and close length (0.9) - plasmids
+#    others - plasmids_bad
+# if "chrom"  in name - chrom
+# others - unclass 
+
+
+
+chrom_hmms="/Nancy/mrayko/db/chromosomal_proteins_pfam/chromosome_uniq_hmms.hmm"
+hmmscan=" /Nancy/mrayko/Libs/hmmer-3.1b2-linux-intel-x86_64/binaries/hmmscan"
+prodigal="/Nancy/mrayko/Libs/Prodigal/prodigal"
+
+
+
+print (prodigal + " -i " + sys.argv[1] + "-a proteins.fa -o genes.fa" )
+print (hmmscan + " -o out_pfam --tblout tblout --cpu 10 "+ chrom_hmms + "proteins.fa")
+print ("tail -n +4 tblout | head -n -10 | awk '$5<0.001 {print $3}' > chromosomal_contigs_names.txt")
+
+
