8a4c181ea7308aa30a051f932bd39056e2f04541
Anton Korobeynikov <anton@korobeynikov.info>
2017-08-30 02:42:41 +0300

    Add reference-counting temporary file / dir

---
+++ assembler/src/common/utils/filesystem/temporary.cpp
@@ -0,0 +1,67 @@
+//***************************************************************************
+//* Copyright (c) 2017 Saint Petersburg State University
+//* All Rights Reserved
+//* See file LICENSE for details.
+//***************************************************************************
+
+#include "temporary.hpp"
+
+#include "path_helper.hpp"
+#include "utils/verify.hpp"
+#include "utils/logger/logger.hpp"
+
+#include <cerrno>
+#include <unistd.h>
+
+namespace fs {
+TmpDirImpl::TmpDirImpl(const std::string &prefix, const std::string &suffix)
+        : dir_(fs::make_temp_dir(prefix, suffix)) {
+    TRACE("Creating " << dir_);
+}
+
+TmpDirImpl::~TmpDirImpl() {
+    TRACE("Removing " << dir_);
+    fs::remove_dir(dir_);
+}
+
+TmpFile TmpDirImpl::tmp_file(const std::string &prefix) {
+    return new TmpFileImpl(prefix, this);
+}
+
+TmpFileImpl::TmpFileImpl(const std::string &prefix, TmpDir parent)
+        : parent_(parent), fd_(-1) {
+    std::string tprefix = prefix + "_XXXXXX";
+    if (parent)
+        tprefix = fs::append_path(parent->dir(), tprefix);
+    char *tempprefix = strdup(tprefix.c_str());
+    VERIFY_MSG(-1 != (fd_ = ::mkstemp(tempprefix)), "Cannot create temporary file");
+    file_ = tempprefix;
+    free(tempprefix);
+    TRACE("Creating " << file_);
+}
+
+TmpFileImpl::~TmpFileImpl() {
+    TRACE("Removing " << file_);
+    close();
+    ::unlink(file_.c_str());
+}
+
+void TmpFileImpl::close() {
+    ::close(fd_);
+}
+
+DependentTmpFile TmpFileImpl::create_dep(const std::string &suffix) {
+    return new DependentTmpFileImpl(suffix, this);
+}
+
+DependentTmpFileImpl::DependentTmpFileImpl(const std::string &suffix, TmpFile parent)
+        : parent_(parent), file_(parent_->file() + "." + suffix) {
+    TRACE("Dependent: " << file_);
+}
+
+DependentTmpFileImpl::~DependentTmpFileImpl() {
+    TRACE("Removing " << file_);
+    ::unlink(file_.c_str());
+}
+
+}
---
+++ assembler/src/common/utils/filesystem/temporary.hpp
@@ -0,0 +1,78 @@
+//***************************************************************************
+//* Copyright (c) 2017 Saint Petersburg State University
+//* All Rights Reserved
+//* See file LICENSE for details.
+//***************************************************************************
+
+#pragma once
+
+#include <llvm/ADT/IntrusiveRefCntPtr.h>
+#include <string>
+
+namespace fs {
+class TmpDirImpl;
+class TmpFileImpl;
+class DependentTmpFileImpl;
+
+typedef llvm::IntrusiveRefCntPtr<TmpDirImpl> TmpDir;
+typedef llvm::IntrusiveRefCntPtr<TmpFileImpl> TmpFile;
+typedef llvm::IntrusiveRefCntPtr<DependentTmpFileImpl> DependentTmpFile;
+
+class TmpDirImpl : public llvm::ThreadSafeRefCountedBase<TmpDirImpl> {
+  public:
+    TmpDirImpl(const std::string &prefix, const std::string &suffix);
+    ~TmpDirImpl();
+
+    const std::string &dir() const { return dir_; }
+    TmpFile tmp_file(const std::string &prefix = "tmp");
+
+  private:
+    std::string dir_;
+};
+
+class TmpFileImpl : public llvm::ThreadSafeRefCountedBase<TmpFileImpl> {
+  public:
+    TmpFileImpl(const std::string &prefix = "tmp", TmpDir parent = nullptr);
+    ~TmpFileImpl();
+
+    const std::string &file() const { return file_; }
+    const std::string &dir() const {
+        static std::string noparent("");
+        return (parent_ ? parent_->dir() : noparent);
+    }
+    int fd() const { return fd_; }
+    void close();
+    DependentTmpFile create_dep(const std::string &suffix);
+            
+  private:
+    std::string file_;
+    TmpDir parent_;
+    int fd_;
+};
+
+class DependentTmpFileImpl : public llvm::ThreadSafeRefCountedBase<DependentTmpFileImpl> {
+  public:
+    DependentTmpFileImpl(const std::string &suffix, TmpFile parent);
+    ~DependentTmpFileImpl();
+
+    const std::string &file() const { return file_; }
+    const std::string &dir() const { return parent_->dir(); }
+
+  private:
+    TmpFile parent_;
+    std::string file_;
+};
+
+namespace tmp {
+inline TmpDir make_temp_dir(const std::string &prefix, const std::string &suffix) {
+    return new TmpDirImpl(prefix, suffix);
+}
+
+inline TmpFile make_temp_file(const std::string &prefix = "tmp", TmpDir parent = nullptr) {
+    return new TmpFileImpl(prefix, parent);
+}
+
+};
+
+
+}
