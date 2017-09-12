871822e0adfec93859a5332123a4432decd20121
mikeraiko <mike.rayko@gmail.com>
2017-09-12 13:17:16 +0300

    Deep Black List

--- assembler/src/plasmid_utils/chromosomal_contig_removal.py
+++ assembler/src/plasmid_utils/chromosomal_contig_removal.py
@@ -1,6 +1,7 @@
 #from Bio.Blast import NCBIXML
 import os
 import sys
+import ntpath
 #import argparse
 #from glob import glob
 #from joblib import Parallel, delayed
@@ -17,14 +18,20 @@ import sys
 
 
 
-chrom_hmms="/Nancy/mrayko/db/chromosomal_proteins_pfam/chromosome_uniq_hmms.hmm"
+#chrom_hmms="/Nancy/mrayko/db/chromosomal_proteins_pfam/chromosome_uniq_hmms.hmm" ### Black list (2208)
+chrom_hmms="/Nancy/mrayko/db/chromosomal_proteins_pfam/chromosome_uniq_genbank_hmms.hmm" ### Deep Black list (508)
 hmmscan=" /Nancy/mrayko/Libs/hmmer-3.1b2-linux-intel-x86_64/binaries/hmmscan"
 prodigal="/Nancy/mrayko/Libs/Prodigal/prodigal"
 
 
+name = os.path.splitext(ntpath.basename(sys.argv[1]))[0]
+print name
 
-os.system (prodigal + " -i " + sys.argv[1] + " -a proteins.fa -o genes.fa" )
-os.system (hmmscan + " -o out_pfam --tblout tblout --cpu 10 "+ chrom_hmms + " proteins.fa")
-os.system ("tail -n +4 tblout | head -n -10 | awk '$5<0.001 {print $3}'| sed 's/_[^_]*$//g'| sort | uniq > chromosomal_contigs_names.txt")
+#os.system (prodigal + " -i " + sys.argv[1] + " -a proteins.fa -o genes.fa" )
+#os.system (hmmscan + " -o out_pfam --tblout tblout --cpu 10 "+ chrom_hmms + " proteins.fa")
+#os.system ("tail -n +4 tblout | head -n -10 | awk '$5<0.001 {print $3}'| sed 's/_[^_]*$//g'| sort | uniq > chromosomal_contigs_names.txt")
 
+os.system (prodigal + " -i " + sys.argv[1] + " -a "+name+"_proteins.fa -o "+name+"_genes.fa" )
+os.system (hmmscan + " -o "+name+"_out_pfam --tblout "+name+"_tblout --cpu 10 "+ chrom_hmms + " "+name+"_proteins.fa")
+os.system ("tail -n +4 "+name+"_tblout | head -n -10 | awk '$5<0.001 {print $3}'| sed 's/_[^_]*$//g'| sort | uniq > "+name+"_chromosomal_contigs_names.txt")
 
