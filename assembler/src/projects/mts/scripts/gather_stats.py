62c6bf84b76f511a212db5cbe13881888f8313f7
Yuri Gorshkov <y.s.gorshkov@gmail.com>
2017-10-03 21:25:57 +0300

    Better GF tables; nanoimprovements in pipeline

--- assembler/src/projects/mts/scripts/gather_stats.py
+++ assembler/src/projects/mts/scripts/gather_stats.py
@@ -7,15 +7,17 @@ from operator import add
 import shutil
 import sys
 
+import matplotlib.colors
 from sklearn.cluster.bicluster import SpectralCoclustering
 import numpy as np
 import pandas
 from pandas import DataFrame
 
-def bicluster(table):
+def bicluster(*cotables):
+    table = cotables[0]
     model = SpectralCoclustering(n_clusters=table.shape[1], random_state=0)
     model.fit(table.as_matrix())
-    return table.iloc[np.argsort(model.row_labels_), np.argsort(model.column_labels_)]
+    return [cotable.iloc[np.argsort(model.row_labels_), np.argsort(model.column_labels_)] for cotable in cotables]
 
 parser = argparse.ArgumentParser(description="MTS stats extractor")
 parser.add_argument("dir", type=str, help="QUAST output directory")
@@ -27,33 +29,56 @@ parser.add_argument("--plot", action="store_true", help="Draw plots for metrics"
 
 args = parser.parse_args()
 
-# Write summary table with correspondence between bins and their best references
-res_table = DataFrame(columns=["bin", "ref", "GF", "purity", "NGA50", "misassemblies"])
-gf_table = pandas.read_table(os.path.join(args.dir, "summary", "TSV", "Genome_fraction_(%).tsv"), dtype=str).set_index("Assemblies")
-gfs = gf_table.apply(pandas.to_numeric, errors="coerce")
-#Drop zeroes
-gfs.fillna(0, inplace=True)
+# Load genome fraction table
+def table_path(name, combined=False):
+    if combined:
+        return os.path.join(args.dir, "combined_reference", name)
+    return os.path.join(args.dir, "summary", "TSV", name)
+
+gf_table = pandas.read_table(table_path("Genome_fraction_(%).tsv"), index_col=0, na_values="-")
+gf_table.index = gf_table.index.map(str)
+gf_table.fillna(0, inplace=True)
+
+#Drop _broken scaffolds from MetaQUAST report
+gf_table.drop([col for col in gf_table.columns if col.endswith("_broken")], axis=1, inplace=True)
+# Drop bad bins and missing references
 EPS = 0.01
-presented_refs = gfs.apply(lambda row: row.sum() > EPS, axis=1)
-lost_refs = gfs.index[~presented_refs]
-gfs = gfs.loc[presented_refs, gfs.apply(lambda col: col.sum() > EPS)]
+presented_refs = gf_table.apply(lambda row: row.sum() > EPS, axis=1)
+lost_refs = gf_table.index[~presented_refs]
+gf_table = gf_table.loc[presented_refs, gf_table.apply(lambda col: col.sum() > EPS)]
 
-best_ref = gfs.apply(lambda col: col.idxmax())
-best_bin = gfs.apply(lambda row: row.idxmax(), axis=1)
+# Load purity table
+total_lengths = pandas.to_numeric(pandas.read_table(table_path("report.tsv", True), index_col=0).loc["Total length"])
 
+aligned_table = pandas.read_table(table_path("Total_aligned_length.tsv"), index_col=0, na_values="-")
+aligned_table.fillna(0, inplace=True)
+
+purity_table = aligned_table.apply(lambda col: col / total_lengths[col.name] * 100.)
+purity_table.index = purity_table.index.map(str)
+purity_table = purity_table[gf_table.columns]
+
+# Detect correspondence between bins and their best references
+
+#gfs = gf_table.loc[presented_refs, gfs.apply(lambda col: col.sum() > EPS)]
+
+best_ref = gf_table.apply(lambda col: col.idxmax())
+best_bin = gf_table.apply(lambda row: row.idxmax(), axis=1)
 with open(args.name + "_best.tsv", "w") as out_file:
     best_ref.to_csv(out_file, sep="\t")
 
-for bin, ref in best_ref.iteritems():
-    if type(ref) is float:
-        row = {"bin": bin, "GF": "-", "ref": "unknown", "purity": "-", "NGA50": "-", "misassemblies": "-"}
+# Prepare the summary table
+res_table = DataFrame(columns=["ref", "bin", "GF", "purity", "NGA50", "misassemblies"])
+for ref, bin in best_bin.iteritems():
+    if type(bin) is float:
+        row = {"ref": ref, "bin": "unknown", "GF": "-", "purity": "-", "NGA50": "-", "misassemblies": "-"}
     else:
         all_stats = pandas.read_table(os.path.join(args.dir, "runs_per_reference", ref, "report.tsv"), index_col=0)
         col = all_stats.get(bin)
         if col is None:
             print("WRONG:", bin, ref)
-        purity = 100 - float(col["Unaligned length"]) / float(col["Total length"]) * 100
-        row = {"bin": bin, "GF": col["Genome fraction (%)"], "ref": ref, "purity": "{0:.2f}".format(purity),
+        #purity = 100 - float(col["Unaligned length"]) / float(col["Total length"]) * 100
+        purity = purity_table.get_value(ref, bin)
+        row = {"ref": ref, "bin": bin, "GF": col["Genome fraction (%)"], "purity": "{0:.2f}".format(purity),
                "NGA50": col["NGA50"], "misassemblies": col["# misassemblies"]}
     res_table = res_table.append(row, ignore_index=True)
 res_table.to_csv(args.name + "_summary.tsv", index=False, sep="\t")
@@ -65,6 +90,66 @@ if args.plot:
         matplotlib.use('Agg')
         import matplotlib.pyplot as plt
         import seaborn as sns
+
+        # Column permutation to diagonalize the table saving the reference order
+        def best_permutation(table):
+            rows = list(table.index)
+            unused_cols = set(table.columns)
+            res = []
+            for i in range(len(rows)):
+                row = rows[i]
+                all_options = [(col, table.get_value(row, col)) for col in unused_cols]
+                all_options.sort(key=lambda x: x[1], reverse=True)
+                options = list(filter(lambda v: v[1] > 10, all_options)) # (table.loc[rows[i+1:], col] < 0.1).any()]
+                #if not options and all_options:
+                #    options = [all_options[0]]
+                new = [k for k, _ in options]
+                res += new
+                unused_cols.difference_update(new)
+            return res
+
+        def color(v1, v2):
+            return matplotlib.colors.hsv_to_rgb((v1 / 200. + 0.16, v2 / 100., 1))
+
+        def combine_with(t1, t2, f):
+            return t1.apply(lambda k: [f(x, y) for x, y in zip(k, t2[k.name])])
+
+        def diag_heatmap(t1, t2, filename, best=None):
+            perm = best_permutation(t1)
+            t1_aligned = t1[perm]
+            t2_aligned = t2.loc[t1_aligned.index, perm]
+
+            colors_aligned = combine_with(t1_aligned, t2_aligned, color)
+
+            fig, ax = plt.subplots()
+
+            #TODO: cell text
+            table = plt.table(rowLabels=t1_aligned.index, colLabels=t1_aligned.columns,
+                              cellColours=colors_aligned.as_matrix(), loc="center",
+                              bbox=(0,0,1,1))
+            table.auto_set_font_size(False)
+            table.set_fontsize(12)
+            (bx, by) = t1_aligned.shape
+            def unselect(cell):
+                cell.set_linewidth(0)
+
+            def select(cell):
+                cell.set_linewidth(2)
+
+            for (x, y), cell in table.get_celld().items():
+                unselect(cell)
+                x -= 1 #Cells indices in the table seems to be shifted by one row
+                if best is None or x < 0 or x >= bx or y < 0 or y >= by:
+                    continue
+                if best[t1_aligned.index[x]] == t1_aligned.columns[y]:
+                    select(cell)
+
+            #table.scale(0.4, 2)
+            plt.axis("off")
+
+            plt.savefig(filename, bbox_inches="tight")
+            plt.clf()
+
     except:
         print("Cannot import matplotlib and/or seaborn; drawing will be disabled")
         args.plot = False
@@ -75,10 +160,7 @@ if args.gf:
 
     # Draw GF heatmap
     if args.plot:
-        plot = sns.heatmap(bicluster(gfs), square=True)
-        fig = plot.get_figure()
-        fig.savefig(args.name + "_gf.png", bbox_inches="tight")
-        plt.gcf().clear()
+        diag_heatmap(gf_table, purity_table, args.name + "_gf.png", best_bin)
 
 # Draw NGA plots
 if args.nga:
@@ -87,18 +169,20 @@ if args.nga:
     nga_series = nga_table.max(axis=1)
     nga_series.to_csv(args.name + "_nga.tsv", sep="\t")
     if args.plot:
+        nga_series.index.name = "Reference"
         plot = nga_series.plot(kind="bar", logy=True)
+        plot.set_ylabel("Contig length (bp)")
         fig = plot.get_figure()
         fig.savefig(args.name + "_nga.png", bbox_inches="tight")
-        plt.gcf().clear()
+        plt.clf()
 
 # (Optional) Write summary for problematic references
 if args.problematic:
     BAD_THRESHOLD = 90
     ZERO_THRESHOLD = 5
-    total_gf_ref = gfs.sum(1)
-    max_gf_ref = gfs.max(1)
-    nonzeroes = gfs.applymap(lambda x: x > ZERO_THRESHOLD)
+    total_gf_ref = gf_table.sum(1)
+    max_gf_ref = gf_table.max(1)
+    nonzeroes = gf_table.applymap(lambda x: x > ZERO_THRESHOLD)
     nonzeroes_cnt_ref = nonzeroes.sum(1)
     good_refs = list()
     with open(args.name + "_problems.txt", "w") as out_file:
@@ -125,9 +209,7 @@ if args.problematic:
     if args.plot:
         drop_refs = [ref for ref in good_refs if best_bin[ref] in good_bins]
         drop_bins = [bin for bin in good_bins if best_ref[bin] in good_refs]
-        bad_table = gfs.drop(drop_refs, axis=0).drop(drop_bins, axis=1)
+        bad_table = gf_table.drop(drop_refs, axis=0).drop(drop_bins, axis=1)
         bad_table = bad_table[bad_table.apply(lambda row: row.sum() > EPS, axis=1)]
         if bad_table.size:
-            plot = sns.heatmap(bicluster(bad_table), square=True)
-            fig = plot.get_figure()
-            fig.savefig(args.name + "_bad.png", bbox_inches="tight")
+            diag_heatmap(bad_table, purity_table, args.name + "_bad.png")
