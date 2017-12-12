61dd36951635dd6936078e811851de6dc5cae96b
Mike Rayko <mrayko@chihua.cab.spbu.ru>
2017-12-12 18:54:55 +0300

    upd hmm-based extraction

--- assembler/src/plasmid_utils/hmm_based_plasmid_extraction.py
+++ assembler/src/plasmid_utils/hmm_based_plasmid_extraction.py
@@ -17,14 +17,14 @@ import sys
 
 
 
-hmm="/Nancy/mrayko/db/plasmid_specific_pfam/plasmid_hmms_12.hmm"
+hmm="/Nancy/mrayko/db/plasmid_specific_pfam/plasmid_hmms_53.hmm"
 hmmscan=" /Nancy/mrayko/Libs/hmmer-3.1b2-linux-intel-x86_64/binaries/hmmscan"
 prodigal="/Nancy/mrayko/Libs/Prodigal/prodigal"
 
 
 
-os.system (prodigal + " -i " + sys.argv[1] + " -a " + sys.argv[1] + "_proteins.fa -o " + sys.argv[1] + "_genes.fa" )
+os.system (prodigal + " -p meta -i " + sys.argv[1] + " -a " + sys.argv[1] + "_proteins.fa -o " + sys.argv[1] + "_genes.fa" )
 os.system (hmmscan + " -o " + sys.argv[1] + "_out_pfam --tblout " + sys.argv[1]+"_tblout --cpu 10 " + hmm + " " + sys.argv[1] + "_proteins.fa")
-os.system ("tail -n +4 " + sys.argv[1]+"_tblout | head -n -10 | awk '$5<0.001 {print $3}'| sed 's/_[^_]*$//g'| sort | uniq > " + sys.argv[1]+"plasmid_contigs_names.txt")
+os.system ("tail -n +4 " + sys.argv[1]+"_tblout | head -n -10 | awk '$5<0.001 {print $3}'| sed 's/_[^_]*$//g'| sort | uniq > " + sys.argv[1]+"_plasmid_contigs_names.txt")
 
 
