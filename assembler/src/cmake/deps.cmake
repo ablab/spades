c5af7fe50479c004f7b5e265af667dda381b1490
Anton Korobeynikov <anton@korobeynikov.info>
2017-12-23 02:15:03 +0300

    gcc version bump

--- assembler/src/cmake/deps.cmake
+++ assembler/src/cmake/deps.cmake
@@ -2,12 +2,12 @@
 
 if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
   # Require at least gcc 4.7
-  if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.7)
-    message(FATAL_ERROR "SPAdes requires gcc version 4.7 or later")
+  if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5.3)
+    message(FATAL_ERROR "SPAdes requires gcc version 5.3 or later")
   endif()
 elseif ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
-  if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.2)
-    message(FATAL_ERROR "SPAdes requires clang version 3.2 or later")
+  if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.8)
+    message(FATAL_ERROR "SPAdes requires clang version 3.8 or later")
   endif()
 else()
   message(WARNING "Unsupported compiler is detected. SPAdes compilation was not tested on it and may fail")
