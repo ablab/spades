5b0d8d7e5c1bdc0865b2bcc265d7346a7394fcfc
Mike Rayko <mrayko@chihua.cab.spbu.ru>
2018-01-08 23:44:19 +0300

    bash no more

--- assembler/src/plasmid_utils/chromosomal_contig_removal.py
+++ assembler/src/plasmid_utils/chromosomal_contig_removal.py
@@ -29,6 +29,20 @@ prodigal="/Nancy/mrayko/Libs/Prodigal/prodigal"
 name = os.path.splitext(ntpath.basename(sys.argv[1]))[0]
 #print name
 
-os.system (prodigal + " -i " + sys.argv[1] + " -a proteins.fa -o genes.fa" )
+os.system (prodigal + " -p meta -i " + sys.argv[1] + " -a "+name+"_proteins.fa -o "+name+"_genes.fa" )
 os.system (hmmscan + " -o "+name+"_out_pfam --tblout "+name+"_tblout --cpu 10 "+ chrom_hmms + " "+name+"_proteins.fa")
-os.system ("tail -n +4 "+name+"_tblout | head -n -10 | awk '$5<1e-06 {print $3}'| sed 's/_[^_]*$//g'| sort | uniq > "+name+"_chromosomal_contigs_names.txt")
+
+
+with open(name+"_tblout") as f:
+    table = f.read().splitlines()
+
+chrom_list=[]
+for i in table[4:7]:
+    if float (i.split()[4]) < 1e-06:
+        chrom_list.append(i.split()[2].rsplit('_', 1)[0])
+
+outfile=open (name+"_chromosomal_contigs_names.txt", "w")
+for i in chrom_list: 
+    outfile.write("%s\n" % i)
+
+#os.system ("tail -n +4 "+name+"_tblout | head -n -10 | awk '$5<1e-06 {print $3}'| sed 's/_[^_]*$//g'| sort | uniq > "+name+"_chromosomal_contigs_names.txt")
