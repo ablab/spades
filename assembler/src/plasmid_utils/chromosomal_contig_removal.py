80569064a7a6e7ed6b99fada53bcf38dee24e993
Mike Rayko <mrayko@chihua.cab.spbu.ru>
2018-01-09 16:51:32 +0300

    files inplace

--- assembler/src/plasmid_utils/chromosomal_contig_removal.py
+++ assembler/src/plasmid_utils/chromosomal_contig_removal.py
@@ -1,20 +1,6 @@
-#from Bio.Blast import NCBIXML
 import os
 import sys
 import ntpath
-#import argparse
-#from glob import glob
-#from joblib import Parallel, delayed
-
-# input - BLAST XML of scaffolds.fasta (?)
-# output:
-# if no alignment at all - nosig
-# if good alignment (e<0.001):
-#  if "plasmid" in name:
-#    <3 matches and close length (0.9) - plasmids
-#    others - plasmids_bad
-# if "chrom"  in name - chrom
-# others - unclass 
 
 
 
@@ -26,8 +12,7 @@ hmmscan=" /Nancy/mrayko/Libs/hmmer-3.1b2-linux-intel-x86_64/binaries/hmmscan"
 prodigal="/Nancy/mrayko/Libs/Prodigal/prodigal"
 
 
-name = os.path.splitext(ntpath.basename(sys.argv[1]))[0]
-#print name
+name = os.path.splitext(sys.argv[1])[0]
 
 os.system (prodigal + " -p meta -i " + sys.argv[1] + " -a "+name+"_proteins.fa -o "+name+"_genes.fa 2> prodigal.log" )
 os.system (hmmscan + " -o "+name+"_out_pfam --tblout "+name+"_tblout --cpu 10 "+ chrom_hmms + " "+name+"_proteins.fa")
@@ -38,11 +23,11 @@ with open(name+"_tblout") as f:
 
 chrom_list=[]
 for i in table[4:7]:
-    if float (i.split()[4]) < 1e-06:
-        chrom_list.append(i.split()[2].rsplit('_', 1)[0])
+   if float (i.split()[4]) < 1e-06:
+       chrom_list.append(i.split()[2].rsplit('_', 1)[0])
 
 outfile=open (name+"_chromosomal_contigs_names.txt", "w")
 for i in chrom_list: 
-    outfile.write("%s\n" % i)
+   outfile.write("%s\n" % i)
 
-#os.system ("tail -n +4 "+name+"_tblout | head -n -10 | awk '$5<1e-06 {print $3}'| sed 's/_[^_]*$//g'| sort | uniq > "+name+"_chromosomal_contigs_names.txt")
+os.system ("tail -n +4 "+name+"_tblout | head -n -10 | awk '$5<1e-06 {print $3}'| sed 's/_[^_]*$//g'| sort | uniq > "+name+"_chromosomal_contigs_names.txt")
