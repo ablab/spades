b5c6e79dc70029e2af3090ac72e99e9ae4db6117
Itolstoganov <oockvay@gmail.com>
2018-02-13 21:53:54 +0300

    Add barcode connection scaffolding

--- assembler/src/common/modules/path_extend/extension_chooser.hpp
+++ assembler/src/common/modules/path_extend/extension_chooser.hpp
@@ -2142,8 +2142,6 @@ class TenXExtensionChooser : public ReadCloudExtensionChooser {
     DECL_LOGGER("10XExtensionChooser")
 };
 
-
-//fixme replace predicate with other extension chooser
 class PredicateExtensionChooser: public ExtensionChooser {
     shared_ptr<ScaffoldVertexPredicate> predicate_;
     shared_ptr<ExtensionChooser> chooser_;
@@ -2151,7 +2149,7 @@ class PredicateExtensionChooser: public ExtensionChooser {
  public:
     PredicateExtensionChooser(const Graph& g,
                           shared_ptr<ScaffoldVertexPredicate> intermediate_predicate,
-                          const shared_ptr<ExtensionChooser> &pe_extension_chooser_) :
+                          shared_ptr<ExtensionChooser> pe_extension_chooser_) :
         ExtensionChooser(g),
         predicate_(intermediate_predicate),
         chooser_(pe_extension_chooser_) {}
@@ -2162,7 +2160,7 @@ class PredicateExtensionChooser: public ExtensionChooser {
 
         DEBUG(edges.size() << " input edges:");
         for (const auto& edge: edges) {
-            DEBUG(edge.e_.int_id());
+            TRACE(edge.e_.int_id());
         }
         EdgeContainer intermediate_edges;
         for (const auto& edge: edges) {
@@ -2172,7 +2170,7 @@ class PredicateExtensionChooser: public ExtensionChooser {
         }
         DEBUG(intermediate_edges.size() << " intermediate edges:");
         for (const auto& edge: intermediate_edges) {
-            DEBUG(edge.e_.int_id());
+            TRACE(edge.e_.int_id());
         }
         EdgeContainer result = chooser_->Filter(path, intermediate_edges);
         if (result.size() == 0) {
@@ -2184,7 +2182,7 @@ class PredicateExtensionChooser: public ExtensionChooser {
         }
         DEBUG(result.size() << "final edges");
         for (const auto& edge: result) {
-            DEBUG(edge.e_.int_id());
+            TRACE(edge.e_.int_id());
         }
         return result;
     }
