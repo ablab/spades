17fd0da3b6333de3e34d1d9484b8603edf998cda
Yuri Gorshkov <y.s.gorshkov@gmail.com>
2018-02-14 16:15:05 +0300

    Less strict choose_samples strategy

--- assembler/src/projects/mts/scripts/choose_samples.py
+++ assembler/src/projects/mts/scripts/choose_samples.py
@@ -14,6 +14,8 @@ parser.add_argument("-r", "--reads", type=str, help="Directory with reads")
 parser.add_argument("-o", "--output", type=str, help="Output path")
 parser.add_argument("prof", type=str, help="File with bin profiles")
 parser.add_argument("-f", "--filtered", type=str, help="File with filtered bins")
+parser.add_argument("--min-sample", type=float, default=4, help="Minimal coverage in a single sample to be used")
+parser.add_argument("--min-total", type=float, default=1, help="Minimal total coverage of the bin to be reassembled")
 
 args = parser.parse_args()
 
@@ -23,11 +25,6 @@ if args.filtered:
         for line in input:
             bin = line.split()[0]
             BINS.add(bin)
-
-MIN_TOTAL = 15
-FAIR_TOTAL = 50
-SOFT_MIN = 10
-HARD_MIN = 4
 
 prof_dict = dict()
 
@@ -43,33 +40,30 @@ for line in input:
     profile = list(map(float, params[1:]))
     print("Profile of", bin, ":", profile)
     if BINS and bin not in BINS:
-        print(bin, "was excluded from reassembly")
+        print(bin, "was excluded from the reassembly")
         exclude = True
-    else:
-        total = 0
+    total = 0
 
-        #Current strategy: choose all samples larger than soft threshold.
-        #If there's not enough, use the lower hard threshold.
-        #Sort samples by their abundancies
-        weighted_profile = list((i, ab)
-            for i, ab in enumerate(profile, start=1) if ab >= HARD_MIN)
-        weighted_profile.sort(key=itemgetter(1), reverse=True)
+    #Current strategy: choose all samples larger than soft threshold.
+    #If there's not enough, use the lower hard threshold.
+    #Sort samples by their abundancies
+    weighted_profile = list((i, ab)
+        for i, ab in enumerate(profile, start=1) if ab >= args.min_sample)
+    weighted_profile.sort(key=itemgetter(1), reverse=True)
 
-        for i, cov in weighted_profile:
-            if cov < SOFT_MIN and total >= FAIR_TOTAL: #Enough
-                break
-            if not os.path.exists(os.path.join(args.reads, bin, "sample{}_1.fastq.gz".format(i))):
-                print("WARNING: sample", i, "does not contain reads for", bin)
-                continue
-            total += cov
-            samples.append(i)
+    for i, cov in weighted_profile:
+        if not os.path.exists(os.path.join(args.reads, bin, "sample{}_1.fastq.gz".format(i))):
+            print("WARNING: sample", i, "does not contain reads for", bin)
+            continue
+        total += cov
+        samples.append(i)
 
-        print("Chosen samples are", samples, "with total mean abundance", total)
-        prof_dict[bin] = total
+    print("Chosen samples are", samples, "with total mean abundance", total)
+    prof_dict[bin] = total
 
-        if total < MIN_TOTAL:
-            print(bin, "is too scarce; excluding from default")
-            exclude = True
+    if total < args.min_total:
+        print(bin, "is too scarce; excluding from the reassembly")
+        exclude = True
 
     config_dir = args.output
     if exclude:
@@ -82,6 +76,7 @@ for line in input:
     with open(config_path, "w") as out:
         print("total", sum(profile), file=out)
         for i, ab in enumerate(profile, start=1):
+            line = "sample" + str(i)
             if i in samples:
-                print("+", end="", file=out)
-            print("sample" + str(i), ab, file=out)
+                line = "+" + line
+            print(line, ab, file=out)
