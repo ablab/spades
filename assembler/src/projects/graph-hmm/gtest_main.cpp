13ae7a3ec6d598221fa08f06b529b2b0a7186404
Alexander Shlemov <shlemovalex@gmail.com>
2018-02-25 00:16:25 +0300

    Add unit-tests from the original graph-hmm repo

---
+++ assembler/src/projects/graph-hmm/gtest_main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <gtest/gtest.h>
+#include "utils/segfault_handler.hpp"
+
+GTEST_API_ int main(int argc, char **argv) {
+  utils::segfault_handler sh;
+  printf("Running main() from gtest_main.cpp\n");
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ assembler/src/projects/graph-hmm/test.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+
+#include "demo.hpp"
+
+
+
+double levenshtein_substring_score(const std::string &s, const std::string &query) {
+  auto fees = levenshtein_fees(query);
+  auto graph = Graph({s});
+  auto result = find_best_path(fees, graph.all());
+  return result[0].second;
+}
+
+
+TEST(LevenshteinZero, LEVENSHTEIN_SUBSTRING) {
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("A", "A"), 0);
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("AAAAA", "A"), 0);
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("AAAAACGTAAAAAAACGT", "AAA"), 0);
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("AAAAACGTAAAAAAACGT", ""), 0);
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("", ""), 0);
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("AAAAACGTAAAAAAACGT", "CGT"), 0);
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("AAAAACGTAAAAAAACGT", "AAAAACGTAAAAAAACGT"), 0);
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("TTTTTTTTTTTTTAAAAACGTAAAAAAACGTTTTTTTTTTTTCCCCT", "AAAAACGTAAAAAAACGT"), 0);
+}
+
+TEST(LevenshteinNonZero, LEVENSHTEIN_SUBSTRING) {
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("T", "A"), 1);
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("AA", "AT"), 1);
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("AA", "T"), 1);
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("AAAAAAAAACGGGGGGGCCCCCAAAAAAGGGGGGCCCCGGG", "T"), 1);
+
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("AT", "TA"), 1);
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("ATA", "TA"), 0);
+  EXPECT_FLOAT_EQ(levenshtein_substring_score("", "AA"), 2);
+}
