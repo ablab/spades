10c01f40060da9edda03695920eee21c82ac75ee
Dmitry Antipov <dmitrij.antipov@gmail.com>
2018-03-01 17:06:29 +0300

    bad workaround for stage id

--- assembler/src/projects/spades/chromosome_removal.hpp
+++ assembler/src/projects/spades/chromosome_removal.hpp
@@ -15,11 +15,13 @@ namespace debruijn_graph {
 class ChromosomeRemoval : public spades::AssemblyStage {
 public:
     ChromosomeRemoval(size_t ext_limit = 0)
-            : AssemblyStage("Chromosome Removal", (std::string("chromosome_removal") + (ext_limit == 0?std::string(""):std::to_string(ext_limit))).c_str()), long_component_(), long_vertex_component_(),deadends_count_(), ext_limit_(ext_limit) { }
+            : full_name_(std::string("chromosome_removal") + (ext_limit == 0?std::string(""):std::to_string(ext_limit))), AssemblyStage("Chromosome Removal", full_name_.c_str()), long_component_(), long_vertex_component_(),
+              deadends_count_(), ext_limit_(ext_limit) {}
 
     void run(conj_graph_pack &gp, const char *);
 
 private:
+    std::string full_name_;
     std::unordered_map <EdgeId, size_t> long_component_;
     std::unordered_map <VertexId, size_t> long_vertex_component_;
     std::unordered_map <EdgeId, size_t> deadends_count_;
