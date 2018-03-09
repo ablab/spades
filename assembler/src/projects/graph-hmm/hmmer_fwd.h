5ec2bcaddbded1f47eee9f589d43a56d0a9df9e8
Anton Korobeynikov <anton@korobeynikov.info>
2018-03-09 15:49:43 +0300

    Add separate header for hmmer fwd decls

---
+++ assembler/src/projects/graph-hmm/hmmer_fwd.h
@@ -0,0 +1,12 @@
+#pragma once
+
+extern "C" {
+    typedef struct esl_alphabet_s ESL_ALPHABET;
+    typedef struct p7_hmmfile_s   P7_HMMFILE;
+    typedef struct p7_hmm_s       P7_HMM;
+    typedef struct p7_tophits_s   P7_TOPHITS;
+    typedef struct p7_pipeline_s  P7_PIPELINE;
+    typedef struct p7_profile_s   P7_PROFILE;
+    typedef struct p7_oprofile_s  P7_OPROFILE;
+    typedef struct p7_bg_s        P7_BG;
+}
