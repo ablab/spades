8c41eacf3df657c1a59ed6606ca09a6ba4006577
Alexander Shlemov <shlemovalex@gmail.com>
2018-03-15 18:01:02 +0300

    demo.hpp includes cleanup

--- assembler/src/projects/graph-hmm/demo.hpp
+++ assembler/src/projects/graph-hmm/demo.hpp
@@ -1,11 +1,6 @@
 #pragma once
 
-#include "hmmer_fwd.h"
 
 #include <string>
-#include <iostream>
-#include <vector>
-#include <limits>
-#include <cstdlib>
 
 std::vector<std::string> read_fasta_edges(const std::string &filename, bool add_rc = false);
