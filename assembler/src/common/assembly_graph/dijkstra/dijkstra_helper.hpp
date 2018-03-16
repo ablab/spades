ab21e9a3352e3defe9dd6d192ed30c2c8eeb57e7
Anton Korobeynikov <anton@korobeynikov.info>
2018-03-16 18:11:27 +0300

    Add another dijkstra flavour – calculate and consider ending vertices of edges

--- assembler/src/common/assembly_graph/dijkstra/dijkstra_helper.hpp
+++ assembler/src/common/assembly_graph/dijkstra/dijkstra_helper.hpp
@@ -67,6 +67,26 @@ public:
                                             max_vertex_number);
     }
 
+    typedef ComposedDijkstraSettings<Graph,
+            LengthCalculator<Graph>,
+            BoundProcessChecker<Graph>,
+            VertexPutChecker<Graph>,
+            ForwardNeighbourIteratorFactory<Graph> > EdgeBoundedDijkstraSettings;
+
+    typedef Dijkstra<Graph, EdgeBoundedDijkstraSettings> EdgeBoundedDijkstra;
+
+    static EdgeBoundedDijkstra
+    CreateEdgeBoundedDijkstra(const Graph &graph, size_t length_bound,
+                              size_t max_vertex_number = -1ul){
+        return EdgeBoundedDijkstra(graph,
+                                   EdgeBoundedDijkstraSettings(
+                                       LengthCalculator<Graph>(graph),
+                                       BoundProcessChecker<Graph>(length_bound),
+                                       VertexPutChecker<Graph>(),
+                                       ForwardNeighbourIteratorFactory<Graph>(graph)),
+                                   max_vertex_number);
+    }
+
     //------------------------------
     // bounded backward dijkstra
     //------------------------------
@@ -91,7 +111,6 @@ public:
                                            BackwardNeighbourIteratorFactory<Graph>(graph)),
                                        max_vertex_number);
     }
-
 
     //------------------------------
     // Undirected bounded dijkstra
@@ -118,6 +137,26 @@ public:
                                        max_vertex_number);
     }
 
+
+    typedef ComposedDijkstraSettings<Graph,
+                                     LengthCalculator<Graph>,
+                                     BoundProcessChecker<Graph>,
+                                     VertexPutChecker<Graph>,
+                                     BackwardNeighbourIteratorFactory<Graph> > BackwardEdgeBoundedDijkstraSettings;
+
+    typedef Dijkstra<Graph, BackwardEdgeBoundedDijkstraSettings> BackwardEdgeBoundedDijkstra;
+
+    static BackwardEdgeBoundedDijkstra
+    CreateBackwardEdgeBoundedDijkstra(const Graph &graph,
+                                      size_t bound, size_t max_vertex_number = size_t(-1)){
+        return BackwardEdgeBoundedDijkstra(graph,
+                                           BackwardEdgeBoundedDijkstraSettings(
+                                               LengthCalculator<Graph>(graph),
+                                               BoundProcessChecker<Graph>(bound),
+                                               VertexPutChecker<Graph>(),
+                                               BackwardNeighbourIteratorFactory<Graph>(graph)),
+                                           max_vertex_number);
+    }
 
     //------------------------------
 
@@ -275,6 +314,18 @@ template<class Graph>
 typename DijkstraHelper<Graph>::BackwardBoundedDijkstra CreateBackwardBoundedDijkstra(const Graph &graph, size_t length_bound,
                                                                                       size_t max_vertex_number = -1ul) {
     return DijkstraHelper<Graph>::CreateBackwardBoundedDijkstra(graph, length_bound, max_vertex_number);
+}
+
+template<class Graph>
+typename DijkstraHelper<Graph>::EdgeBoundedDijkstra CreateEdgeBoundedDijkstra(const Graph &graph, size_t length_bound,
+                                                                              size_t max_vertex_number = -1ul) {
+    return DijkstraHelper<Graph>::CreateEdgeBoundedDijkstra(graph, length_bound, max_vertex_number);
+}
+
+template<class Graph>
+typename DijkstraHelper<Graph>::BackwardEdgeBoundedDijkstra CreateBackwardEdgeBoundedDijkstra(const Graph &graph, size_t length_bound,
+                                                                                              size_t max_vertex_number = -1ul) {
+    return DijkstraHelper<Graph>::CreateBackwardEdgeBoundedDijkstra(graph, length_bound, max_vertex_number);
 }
 
 }
