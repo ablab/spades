aad333cc6f004121fbb837d37b0fce24a08b0c94
Alexander Shlemov <shlemovalex@gmail.com>
2018-03-23 08:07:51 +0300

    Enable logging in tests

--- assembler/src/projects/graph-hmm/gtest_main.cpp
+++ assembler/src/projects/graph-hmm/gtest_main.cpp
@@ -1,9 +1,20 @@
 #include <gtest/gtest.h>
 #include <stdio.h>
 #include "utils/segfault_handler.hpp"
+#include "utils/logger/logger.hpp"
+#include "utils/logger/log_writers.hpp"
+
+void create_console_logger() {
+    using namespace logging;
+
+    logger *lg = create_logger("");
+    lg->add_writer(std::make_shared<console_writer>());
+    attach_logger(lg);
+}
 
 GTEST_API_ int main(int argc, char **argv) {
   utils::segfault_handler sh;
+  create_console_logger();
   printf("Running main() from gtest_main.cpp\n");
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
