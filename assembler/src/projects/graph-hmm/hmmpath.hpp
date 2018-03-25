400a84662910afef6ef88ab82d28ed2264cd76ec
Alexander Shlemov <shlemovalex@gmail.com>
2018-03-25 22:46:27 +0300

    Add Depth class

--- assembler/src/projects/graph-hmm/hmmpath.hpp
+++ assembler/src/projects/graph-hmm/hmmpath.hpp
@@ -97,10 +97,52 @@ class StateSet : public std::unordered_map<GraphCursor, PathLinkRef<GraphCursor>
 };
 
 template <typename GraphCursor>
+class Depth {
+ public:
+  double depth(const GraphCursor &cursor) {
+    assert(stack.size() == 0);
+    auto result = get_depth_(cursor);
+    assert(stack.size() == 0);
+    return result;
+  }
+
+ private:
+  std::unordered_map<GraphCursor, double> depth_;
+  std::unordered_set<GraphCursor> stack;
+
+  double get_depth_(const GraphCursor &cursor) {
+    if (depth_.count(cursor)) {
+      return depth_[cursor];
+    }
+
+    if (cursor.is_empty() || cursor.letter() == '*') {
+      return depth_[cursor] = 0;
+    }
+
+    if (stack.count(cursor)) {
+      return depth_[cursor] = std::numeric_limits<double>::infinity();
+    }
+
+    auto nexts = cursor.next();
+    stack.insert(cursor);
+    double max_child = 0;
+    for (const GraphCursor &n : nexts) {
+      max_child = std::max(max_child, get_depth_(n));
+    }
+    stack.erase(cursor);
+
+    return depth_[cursor] = 1 + max_child;
+  }
+};
+
+template <typename GraphCursor>
 PathSet<GraphCursor> find_best_path(const hmm::Fees &fees, const std::vector<GraphCursor> &initial) {
   const double absolute_threshold = 100.0;
   using StateSet = StateSet<GraphCursor>;
   const auto &code = fees.code;
+
+
+
 
   INFO("pHMM size: " << fees.M);
   if (!fees.check_i_loop(0)) {
