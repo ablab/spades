8d3dd50a3de4c777620924eacce00814e780eae7
Tatiana Dvorkina <tdvorkina@chihua.cab.spbu.ru>
2018-04-10 17:36:26 +0300

    Update stats scripts

--- assembler/src/test/debruijn/aligner_stats/bwa_hits_distance.py
+++ assembler/src/test/debruijn/aligner_stats/bwa_hits_distance.py
@@ -1,4 +1,8 @@
 import sys
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 
 def load_reads(filename):
@@ -29,7 +33,7 @@ def load_truepaths(filename):
                 path.append(x.split()[0])
             seq.append(x.split("[")[1].split(",")[0])
 
-        res[cur_read] = { "len": rlen, "path": path, "edgelen": [int(x) for x in edgelen.split(",")[:-1]], "mapped": mapped_seq, "seq": seq }
+        res[cur_read] = { "len": int(rlen), "path": path, "edgelen": [int(x) for x in edgelen.split(",")[:-1]], "mapped": mapped_seq, "seq": seq }
     fin.close()
     return res
 
@@ -37,7 +41,7 @@ def load_truepaths(filename):
 def load_alignments(filename):
     res = {}
     fin = open(filename, "r")
-    bwa_num = 0
+    bwa_num = []
     for ln in fin.readlines():
         #cur_read, seq_start, seq_end, rlen, path_dirty, edgelen, ed = ln.strip().split("\t")
         cur_read, seq_start, seq_end, rlen, path_dirty, edgelen, ss, ed = ln.strip().split("\t")
@@ -47,6 +51,7 @@ def load_alignments(filename):
         bwa_path = []
         seq = []
         seq_ends = []
+        bwa_num.append(0)
         for x in path_dirty.split("]")[:-1]:
             if x.startswith(","):
                 path.append(x.split()[1])
@@ -56,7 +61,7 @@ def load_alignments(filename):
                 edge_tag.append("in")
             else:
                 edge_tag.append("bwa")
-                bwa_num += 1
+                bwa_num[len(bwa_num) - 1] += 1
                 if x.startswith(","):
                     bwa_path.append(x.split()[1])
                 else:
@@ -64,10 +69,10 @@ def load_alignments(filename):
             seq.append(x.split("[")[1].split(",")[0])
             seq_ends.append(x.split("[")[1].split(",")[1])
         #if int(seq_end) - int(seq_start) > 1000:
-        res[cur_read] = { "len": rlen, "path": path, "bwa_path": bwa_path, "edgelen": edgelen.split(",")[:-1], "edge_tag": edge_tag, \
+        res[cur_read] = { "len": int(rlen), "path": path, "bwa_path": bwa_path, "edgelen": edgelen.split(",")[:-1], "edge_tag": edge_tag, \
                             "mapped_s":int(seq_start), "mapped_e":int(seq_end), "seq": seq , "seq_end": seq_ends }
     fin.close()
-    print "Number of edges detected by bwa:", bwa_num
+    print "Median number of bwa hits per read:", sorted(bwa_num)[len(bwa_num)/2]
     return res
 
 
@@ -97,15 +102,6 @@ def cnt_problempaths(truepaths, alignedpaths):
             res += 1
     return res
 
-def cover_same_subseq(a_start, a_end, b_start, b_end):
-    OVERLAP = 20
-    if ( ((b_end - a_start)*(b_start - a_start) < 0 and (b_end - a_start) < OVERLAP) or (b_end - a_start)*(b_start - a_start) > 0) \
-             and  ( ((b_end - a_end)*(b_start - a_end) < 0 and (a_end - b_start) < OVERLAP) or (b_end - a_end)*(b_start - a_end) > 0) \
-             and  ( ((a_end - b_start)*(a_start - b_start) < 0 and (a_end - b_start) < OVERLAP) or (a_end - b_start)*(a_start - b_start) > 0) \
-             and  ( ((a_end - b_end)*(a_start - b_end) < 0 and (b_end - a_start) < OVERLAP) or (a_end - b_end)*(a_start - b_end) > 0)  :
-        return false;
-    return true;
-
 def cnt_problembwa(truepaths, alignedpaths):
     res = 0
     total_path = 0
@@ -119,10 +115,6 @@ def cnt_problembwa(truepaths, alignedpaths):
                 j += 1
         if j < len(alignedpaths[r]["bwa_path"]):
             res += 1
-            # print r
-            # print truepaths[r]["path"]
-            # print alignedpaths[r]["bwa_path"] 
-            # print alignedpaths[r]["path"]
     return res
 
 def get_bwa_inds(path, bwapath):
@@ -145,6 +137,7 @@ def get_bwa_inds_aligned(path):
     ind.append(len(path))
     return ind
 
+
 def is_unique(path, subpath):
     ind = get_bwa_inds(path, subpath)
     if len(ind) < len(subpath) + 2:
@@ -154,109 +147,76 @@ def is_unique(path, subpath):
             return False
     return True
 
-
 def is_wrong_start(truepath, path, true_ind, ind, edgelen, K):
     empty = False
     if truepath[true_ind[0]: true_ind[1]] == path[ind[0]: ind[1]] :
         return [False, empty]
     else:
-        # s = 0
-        # for j in xrange(true_ind[0], true_ind[1] + 1):
-        #     if truepath[j: true_ind[1]] == path[ind[0]: ind[1]] and s < K + 1:
-        #         return [False, empty]
-        #     if j < len(edgelen):
-        #         s += edgelen[j]
         if len(path[ind[0]: ind[1]]) == 0:
             empty = True
+        #print truepath[true_ind[0]: true_ind[1]], path[ind[0]: ind[1]] 
         return [True, empty]
 
-def is_wrong_end(truepath, path, true_ind, ind, edgelen, K):
+def is_wrong_end(truepath, path, true_ind, ind):
     empty = False
     if truepath[true_ind[-2]: true_ind[-1]] == path[ind[-2]: ind[-1]] :
         return [False, empty]
     else:
-        # for j in xrange(true_ind[-2] + 1, true_ind[-1] + 1):
-        #     s = 0
-        #     for it in edgelen[j:]:
-        #         s += it
-        #     if truepath[true_ind[-2]: j] == path[ind[-2]: ind[-1]] and s < K + 1:
-        #         print "Found wrong end"
-        #         print truepath[true_ind[-2]: true_ind[-1]]
-        #         print path[ind[-2]: ind[-1]] 
-        #         return [False, empty]
-
         if len(path[ind[-2]: ind[-1]]) == 1:
             empty = True
         return [True, empty]
 
-def is_wrong_gap(truepath, path, true_ind, ind):
-    empty = False
-    has_wrong_gap = False
-    for j in xrange(2, len(true_ind) - 1):
-        if truepath[true_ind[j - 1]: true_ind[j]] != path[ind[j - 1]: ind[j]]:
-            if len(path[ind[j - 1]: ind[j]]) == 1:
-                empty = True
-            else:
-                has_wrong_gap = True
-    return [has_wrong_gap, empty]
 
-
-def is_wrong_path(truepath, path):
-    has_good_path = False
-    for j in xrange(len(truepath) - len(path) + 1):
-        if " ".join(truepath[j: j + len(path)]) == " ".join(path):
-            has_good_path = True
-    return has_good_path
-
-def cnt_wronglyclosedgaps(truepaths, alignedpaths, K):
-    res, res_start, res_end = 0, 0, 0
-    res_empty, res_start_empty, res_end_empty, res_good = 0, 0, 0, 0
+def cnt_median_alignment_length(tpaths, apaths, K):
     unknown = 0
-    for r in alignedpaths.keys():
-        if r not in truepaths:
+    res = []
+    res_prefix = []
+    res_suffix = []
+    res_plus = []
+    wrong_start = 0
+    wrong_end = 0
+    for r in apaths.keys():
+        if r not in tpaths:
             continue
-        if not is_unique(truepaths[r]["path"], alignedpaths[r]["bwa_path"]):
+        #print r
+        #print tpaths[r]["path"], apaths[r]["bwa_path"]
+        if not is_unique(tpaths[r]["path"], apaths[r]["bwa_path"]):
             unknown += 1
         else:
+            #print apaths[r]["mapped_e"], apaths[r]["mapped_s"], apaths[r]["len"]
             true_ind = get_bwa_inds(truepaths[r]["path"], alignedpaths[r]["bwa_path"])
             aligned_ind = get_bwa_inds_aligned(alignedpaths[r]["edge_tag"])
             has_wrong_start, empty = is_wrong_start(truepaths[r]["path"], alignedpaths[r]["path"], true_ind, aligned_ind, truepaths[r]["edgelen"], K)
             if has_wrong_start:
-                if empty:
-                    res_start_empty += 1
-                else:
-                    res_start += 1
+                wrong_start += 1
 
-            has_wrong_end, empty = is_wrong_end(truepaths[r]["path"], alignedpaths[r]["path"], true_ind, aligned_ind, truepaths[r]["edgelen"], K)
+            has_wrong_end, empty = is_wrong_end(truepaths[r]["path"], alignedpaths[r]["path"], true_ind, aligned_ind)
             if has_wrong_end:
-                if empty:
-                    res_end_empty += 1
-                else:
-                    res_end += 1
-            has_wrong_gap, empty = is_wrong_gap(truepaths[r]["path"], alignedpaths[r]["path"], true_ind, aligned_ind)
-            if has_wrong_gap:
-                res += 1
-            if empty:
-                res_empty += 1
-            # has_good_path = is_wrong_path(truepaths[r]["path"], alignedpaths[r]["path"])
-            # if not has_good_path:
-            #     print r
-            #     print truepaths[r]["path"]
-            #     print alignedpaths[r]["path"]
-            #     res_good += 1
-    return [res, res_empty, res_start, res_start_empty, res_end, res_end_empty, unknown]
+                wrong_end += 1
+            res.append( (apaths[r]["mapped_e"] + K - apaths[r]["mapped_s"])*1.0/apaths[r]["len"])
+            res_prefix.append( apaths[r]["mapped_s"] )
+            res_suffix.append( apaths[r]["len"] - apaths[r]["mapped_e"] - K)
+            if apaths[r]["len"] - apaths[r]["mapped_e"] - K + apaths[r]["mapped_s"] > 100:
+                res_plus.append( apaths[r]["len"] - apaths[r]["mapped_e"] - K + apaths[r]["mapped_s"])
+
+    print wrong_start, unknown
+    print sum(res)*1.0/len(res), sum(res_prefix)*1.0/len(res_prefix), sum(res_suffix)*1.0/len(res_suffix), sum(res_plus)*1.0/len(res_plus)
+
+    plt.figure()
+    plt.hist(res_plus, 80, facecolor='green')
+    plt.axis([0, 10000, 0, 12000])
+    plt.savefig('len_d.png')
+    return sorted(res)[len(res)/2], sorted(res_prefix)[len(res_prefix)/2], sorted(res_suffix)[len(res_suffix)/2], sorted(res_plus)[len(res_plus)/2] 
+
 
 reads = load_reads(sys.argv[1])
 truepaths = load_truepaths(sys.argv[2])
 alignedpaths = load_alignments(sys.argv[3])
 K = int(sys.argv[4])
 
-
 print "Total=", len(reads), " ideal=", len(reads) - cnt_badideal(reads, truepaths),  " notmapped=", cnt_notmapped(reads, truepaths, alignedpaths) 
-print "Paths with problems ", cnt_problempaths(truepaths, alignedpaths)
-print "BWA fail ", cnt_problembwa(truepaths, alignedpaths)
 print "Warning: works only without GrowEnds()"
-print "Gaps stage problems (in, prefix, suffix, unknown) ", cnt_wronglyclosedgaps(truepaths, alignedpaths, K) 
+print "Median proportion of length of alignment between two fathest bwa hits to read length", cnt_median_alignment_length(truepaths, alignedpaths, K) 
 
 
 
