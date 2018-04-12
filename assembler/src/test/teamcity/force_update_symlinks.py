3b869013267ae3a394d7fd91fa6b298d7a251efd
Andrey Prjibelski <andrewprzh@gmail.com>
2018-04-12 13:33:55 +0300

    allow to set branch when updating latest symliks in TC contig storage

--- assembler/src/test/teamcity/force_update_symlinks.py
+++ assembler/src/test/teamcity/force_update_symlinks.py
@@ -15,8 +15,8 @@ import sys
 import os
 import glob
 
-def force_symlinks_for_suffix(contig_dir, suffix, name):
-    contigs = sorted(glob.glob(os.path.join(contig_dir, "2*" + suffix + "_master.fasta")))
+def force_symlinks_for_suffix(contig_dir, suffix, name, branch_name):
+    contigs = sorted(glob.glob(os.path.join(contig_dir, "2*" + suffix + "_" + branch_name + ".fasta")))
     if len(contigs) == 0:
         return
     clink = os.path.join(contig_dir, name)
@@ -26,26 +26,27 @@ def force_symlinks_for_suffix(contig_dir, suffix, name):
     print("Forced symlink to " + contigs[-1] + " -> " + clink)
 
 
-def force_symlinks(contig_dir):
-    force_symlinks_for_suffix(contig_dir, "contigs", "latest_contigs.fasta")
-    force_symlinks_for_suffix(contig_dir, "scaffolds", "latest_scaffolds.fasta")
-    force_symlinks_for_suffix(contig_dir, "before_rr", "latest_before_rr.fasta")
-    force_symlinks_for_suffix(contig_dir, "preliminary", "latest_preliminary.fasta")
-    force_symlinks_for_suffix(contig_dir, "transcripts", "latest_transcripts.fasta")
+def force_symlinks(contig_dir, branch_name):
+    force_symlinks_for_suffix(contig_dir, "contigs", "latest_contigs.fasta", branch_name)
+    force_symlinks_for_suffix(contig_dir, "scaffolds", "latest_scaffolds.fasta", branch_name)
+    force_symlinks_for_suffix(contig_dir, "before_rr", "latest_before_rr.fasta", branch_name)
+    force_symlinks_for_suffix(contig_dir, "preliminary", "latest_preliminary.fasta", branch_name)
+    force_symlinks_for_suffix(contig_dir, "transcripts", "latest_transcripts.fasta", branch_name)
 
 
-def force_symlinks_recursive(storage_dir):
-    force_symlinks(storage_dir)
+def force_symlinks_recursive(storage_dir, branch_name):
+    force_symlinks(storage_dir, branch_name)
     files = sorted(glob.glob(os.path.join(storage_dir, "*")))
     for f in files:
         if os.path.isdir(f):
-            force_symlinks_recursive(f)
+            force_symlinks_recursive(f, branch_name)
 
 
-if len(sys.argv) != 2:
-    print("Run: " + sys.argv[0] + " <dir> ")
+if len(sys.argv) < 2 or len(sys.argv) > 3:
+    print("Run: " + sys.argv[0] + " <dir> [branch name]")
     exit(1)
 
-force_symlinks_recursive(sys.argv[1])
+branch_name = "master" if len(sys.argv) == 2 else sys.argv[2]
+force_symlinks_recursive(sys.argv[1], branch_name)
 
             
