994ab601a7faa4c289d7b019e17f4ed22e169341
Alexander Shlemov <shlemovalex@gmail.com>
2018-04-23 20:14:19 +0300

    Graph-hmm: factor out depth filter

--- assembler/src/projects/graph-hmm/hmmpath.hpp
+++ assembler/src/projects/graph-hmm/hmmpath.hpp
@@ -11,6 +11,9 @@
 #include <queue>
 #include <memory>
 #include <vector>
+#include <limits>
+
+#include "depth_filter.hpp"
 
 extern "C" {
 #include "hmmer.h"
@@ -94,54 +97,6 @@ class StateSet : public std::unordered_map<GraphCursor, PathLinkRef<GraphCursor>
     return this->filter(pred);
   }
 
-};
-
-template <typename GraphCursor>
-class Depth {
- public:
-  double depth(const GraphCursor &cursor) {
-    assert(stack_.size() == 0);
-    auto result = get_depth_(cursor);
-    assert(stack_.size() == 0);
-    return result;
-  }
-
- size_t max_stack_size() const { return max_stack_size_; }
-
- private:
-  std::unordered_map<GraphCursor, double> depth_;
-  std::unordered_set<GraphCursor> stack_;
-  size_t max_stack_size_ = 0;
-
-  double get_depth_(const GraphCursor &cursor) {
-    if (depth_.count(cursor)) {
-      return depth_[cursor];
-    }
-
-    if (cursor.is_empty()) {
-      return depth_[cursor] = std::numeric_limits<double>::infinity();
-    }
-
-    if (cursor.letter() == '*') {
-      // INFO("Empty depth " << cursor);
-      return depth_[cursor] = 0;
-    }
-
-    if (stack_.count(cursor)) {
-      return depth_[cursor] = std::numeric_limits<double>::infinity();
-    }
-
-    auto nexts = cursor.next();
-    stack_.insert(cursor);
-    max_stack_size_ = std::max(max_stack_size_, stack_.size());
-    double max_child = 0;
-    for (const GraphCursor &n : nexts) {
-      max_child = std::max(max_child, get_depth_(n));
-    }
-    stack_.erase(cursor);
-
-    return depth_[cursor] = 1 + max_child;
-  }
 };
 
 template <typename GraphCursor>
