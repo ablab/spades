16be6c3be0677f4368681ade6cd4a7c6207ba599
Alexander Shlemov <shlemovalex@gmail.com>
2018-05-05 15:17:35 +0300

    Graph-hmm: add DepthAtLeast test

--- assembler/src/projects/graph-hmm/test-stack-limit.cpp
+++ assembler/src/projects/graph-hmm/test-stack-limit.cpp
@@ -2,6 +2,16 @@
 #include "graph.hpp"
 #include "hmmpath.hpp"
 
+TEST(DepthAtLeast, Depth) {
+  for (size_t n = 10; n <= 10000000; n *= 10) {
+    std::string s(n, 'A');
+    auto graph = Graph({s});
+    impl::DepthAtLeast<Graph::GraphCursor> depthAtLeast;
+    auto cursor = graph.begins()[0];
+    EXPECT_TRUE(depthAtLeast.depth_at_least(cursor, std::min<int>(n, 9999)));
+  }
+}
+
 TEST(TestStackLimit, Depth) {
   for (size_t n = 10; n <= 10000000; n *= 10) {
     std::string s(n, 'A');
