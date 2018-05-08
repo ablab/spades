0155ac6f68c69ee04ce0443a8ee7a5aefc54b724
Alexander Shlemov <shlemovalex@gmail.com>
2018-05-08 17:37:30 +0300

    Graph-hmm: fix ObjectCounter

--- assembler/src/projects/graph-hmm/hmmpath.hpp
+++ assembler/src/projects/graph-hmm/hmmpath.hpp
@@ -562,6 +562,9 @@ PathSet<GraphCursor> find_best_path(const hmm::Fees &fees, const std::vector<Gra
   upd_terminal(I, fees.t[fees.M][p7H_DM]);  // Do we really need I at the end?
   upd_terminal(M, fees.t[fees.M][p7H_MM]);
 
+  INFO(terminal.object_count_current() << " pathlink objects");
+  INFO(terminal.object_count_max() << " pathlink objects maximum");
+  INFO(terminal.object_count_constructed() << " pathlink objects constructed");
   result.clip_tails_non_aggressive();
 
   return result;
