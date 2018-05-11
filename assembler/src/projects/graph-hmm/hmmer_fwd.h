c7206161d106d7b296f9eae730c752e7ae956b07
Anton Korobeynikov <anton@korobeynikov.info>
2018-05-11 12:39:26 +0300

    Add generic single-sequence HMM builder

--- assembler/src/projects/graph-hmm/hmmer_fwd.h
+++ assembler/src/projects/graph-hmm/hmmer_fwd.h
@@ -2,6 +2,7 @@
 
 extern "C" {
     typedef struct esl_alphabet_s ESL_ALPHABET;
+    typedef struct esl_sq_s       ESL_SQ;
     typedef struct p7_hmmfile_s   P7_HMMFILE;
     typedef struct p7_hmm_s       P7_HMM;
     typedef struct p7_tophits_s   P7_TOPHITS;
@@ -11,4 +12,5 @@ extern "C" {
     typedef struct p7_profile_s   P7_PROFILE;
     typedef struct p7_oprofile_s  P7_OPROFILE;
     typedef struct p7_bg_s        P7_BG;
+    typedef struct p7_builder_s   P7_BUILDER;
 }
--- assembler/src/projects/graph-hmm/hmmfile.cpp
+++ assembler/src/projects/graph-hmm/hmmfile.cpp
@@ -19,7 +19,7 @@ HMMFile::HMMFile(const std::string &hmmfile)
     //maelse if (status != eslOK)        p7_Fail("Unexpected error %d in opening HMM file %s.\n%s\n",               status, cfg.hmmfile.c_str(), errbuf);
 }
 
-HMM::HMM(P7_HMM *hmm, ESL_ALPHABET *abc) 
+HMM::HMM(P7_HMM *hmm, ESL_ALPHABET *abc)
         : hmm_(hmm, p7_hmm_Destroy),
           abc_(abc, esl_alphabet_Destroy)
 {}
@@ -44,12 +44,87 @@ static ReadErrc to_ReadErrc(int err) {
 ErrorOr<HMM> HMMFile::read() {
     ESL_ALPHABET *abc = NULL;
     P7_HMM *rhmm = NULL;
-    
+
     int res = p7_hmmfile_Read(hmmfile_.get(), &abc, &rhmm);
     if (res != eslOK)
         return to_ReadErrc(res);
 
     return HMM(rhmm, abc);
+}
+
+static int toESLAlphabet(Alphabet alph) {
+    switch (alph) {
+        case Alphabet::DNA:
+            return eslDNA;
+        case Alphabet::AMINO:
+            return eslAMINO;
+        default:
+            return eslUNKNOWN;
+    }
+
+    return eslUNKNOWN;
+}
+
+static const char *toScoreSystem(ScoreSystem score, Alphabet alph) {
+    switch (score) {
+        case ScoreSystem::Default:
+        default:
+            return (alph == Alphabet::DNA ? "DNA1" : "BLOSUM62");
+        case ScoreSystem::PAM30:
+            return "PAM30";
+        case ScoreSystem::PAM70:
+            return "PAM70";
+        case ScoreSystem::PAM120:
+            return "PAM120";
+        case ScoreSystem::PAM240:
+            return "PAM240";
+        case ScoreSystem::BLOSUM45:
+            return "BLOSUM45";
+        case ScoreSystem::BLOSUM50:
+            return "BLOSUM50";
+        case ScoreSystem::BLOSUM62:
+            return "BLOSUM62";
+        case ScoreSystem::BLOSUM80:
+            return "BLOSUM80";
+        case ScoreSystem::BLOSUM90:
+            return "BLOSUM90";
+    }
+    return (alph == Alphabet::DNA ? "DNA1" : "BLOSUM62");
+}
+
+HMMSequenceBuilder::HMMSequenceBuilder(Alphabet alph, ScoreSystem score,
+                                       double popen, double pext)
+        : abc_(NULL, esl_alphabet_Destroy),
+          bg_(NULL, p7_bg_Destroy),
+          bld_(NULL, p7_builder_Destroy) {
+    abc_.reset(esl_alphabet_Create(toESLAlphabet(alph)));
+    bg_.reset(p7_bg_Create(abc_.get()));
+    bld_.reset(p7_builder_Create(NULL, abc_.get()));
+
+    p7_builder_LoadScoreSystem(bld_.get(), toScoreSystem(score, alph), popen, pext, bg_.get());
+}
+
+HMM HMMSequenceBuilder::from_string(const char *name, const char *seq, const char *desc) const {
+    std::unique_ptr<ESL_SQ, void(*)(ESL_SQ*)> dbsq(esl_sq_CreateFrom(name, seq, desc, NULL, NULL),
+                                                   esl_sq_Destroy);
+
+    esl_sq_Digitize(abc_.get(), dbsq.get());
+
+    return from_string(dbsq.get());
+}
+
+HMM HMMSequenceBuilder::from_string(ESL_SQ *dbsq) const {
+    P7_HMM *hmm  = NULL;
+    char errbuf[eslERRBUFSIZE];
+
+    p7_SingleBuilder(bld_.get(), dbsq, bg_.get(), &hmm, NULL, NULL, NULL);
+
+    if (p7_hmm_Validate(hmm, errbuf, 1e-5f) != eslOK) esl_fatal("HMM validation failed: %s\n", errbuf);
+
+    ESL_ALPHABET *labc = esl_alphabet_Create(abc_->type);
+    hmm->abc = labc;
+
+    return { hmm, labc };
 }
 
 }  // namespace hmmer
@@ -73,7 +148,7 @@ std::string HMMReadErrCategory::message(int ev) const {
         case hmmer::ReadErrc::ABCIncompat:
             return "incompatible HMM alphabet";
         case hmmer::ReadErrc::EndOfFile:
-            
+
         default:
             return "(unknown error)";
     }
@@ -88,7 +163,3 @@ std::error_code make_error_code(ReadErrc e) {
   return {static_cast<int>(e), theHMMReadErrCategory};
 }
 }
-
-
-
-
