40ddf0b6b36a1ff6fb9c6f1b67718fba34fcd2e6
Alexander Shlemov <shlemovalex@gmail.com>
2018-05-13 11:01:33 +0300

    Graph-hmm: bye-bye, AbstractGraphCursor, we will miss you

--- assembler/src/projects/graph-hmm/cursor.hpp
+++ assembler/src/projects/graph-hmm/cursor.hpp
@@ -3,39 +3,6 @@
 #include <vector>
 #include <utility>
 
-class EmptyClass {};
-
-template <class GraphCursor, class Base = EmptyClass>
-class AbstractGraphCursor : public Base {
- public:
-  AbstractGraphCursor() = default;
-  AbstractGraphCursor(const Base &gc) : Base{gc} {}
-  AbstractGraphCursor(Base &&gc) : Base{gc} {}
-
-  bool is_divergent() const { return crtp_this()->next().size() > 1; }
-  bool is_convergent() const { return crtp_this()->prev().size() > 1; }
-
-  std::vector<std::pair<GraphCursor, char>> next_pairs() const {
-    std::vector<std::pair<GraphCursor, char>> result;
-    for (const auto cur : crtp_this()->next()) {
-      result.emplace_back(cur, cur.letter());
-    }
-    return result;
-  }
-
-  std::vector<std::pair<GraphCursor, char>> prev_pairs() const {
-    std::vector<std::pair<GraphCursor, char>> result;
-    for (const auto cur : crtp_this()->prev()) {
-      result.emplace_back(cur, cur.letter());
-    }
-    return result;
-  }
-
- private:
-  GraphCursor *crtp_this() { return static_cast<GraphCursor *>(this); }
-  const GraphCursor *crtp_this() const { return static_cast<const GraphCursor *>(this); }
-};
-
 template <class GraphCursor>
 class ReversalGraphCursor : public GraphCursor {
  public:
