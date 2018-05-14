9a286762da02b58a668809f6e75e648321020e7f
Alexander Shlemov <shlemovalex@gmail.com>
2018-05-15 01:42:57 +0300

    Graph-hmm: print log into file

--- assembler/src/common/utils/logger/log_writers.hpp
+++ assembler/src/common/utils/logger/log_writers.hpp
@@ -11,6 +11,7 @@
 #include "logger.hpp"
 
 #include <iostream>
+#include <fstream>
 
 #include "config.hpp"
 
@@ -36,6 +37,29 @@ struct console_writer : public writer {
                       << std::endl;
     }
 
+};
+
+class file_writer : public writer {
+public:
+    file_writer(const std::string &filename) : fout(filename) {}
+
+    void write_msg(double time, size_t cmem, size_t max_rss, level l, const char *file, size_t line_num,
+                   const char *source, const char *msg) {
+        if (cmem != -1ull)
+            fout << fmt::format("{:14s} {:>5s} / {:<5s} {:6.6s} {:24.24s} ({:26.26s}:{:4d})   {:s}",
+                                utils::human_readable_time(time), utils::human_readable_memory(cmem),
+                                utils::human_readable_memory(max_rss), logging::level_name(l),
+                                source, fs::filename(file), int(line_num), msg)
+            << std::endl;
+        else
+            fout << fmt::format("{:14s} {:^5s} {:6.6s} {:24.24s} ({:26.26s}:{:4d})   {:s}",
+                                utils::human_readable_time(time), utils::human_readable_memory(max_rss),
+                                logging::level_name(l), source, fs::filename(file), int(line_num), msg)
+                << std::endl;
+    }
+
+private:
+    std::ofstream fout;
 };
 
 class mutex_writer : public writer {
