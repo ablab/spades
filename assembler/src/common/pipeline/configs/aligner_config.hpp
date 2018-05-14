78ede4d85abcb80f8da57b18ec0f4fa8eda2049a
Anton Korobeynikov <anton@korobeynikov.info>
2018-05-14 14:35:46 +0300

    Tune default to match spades config

--- assembler/src/common/pipeline/configs/aligner_config.hpp
+++ assembler/src/common/pipeline/configs/aligner_config.hpp
@@ -11,7 +11,7 @@ namespace debruijn_graph {
 namespace config {
 
 struct pacbio_processor {
-    size_t bwa_length_cutoff      = 500;
+    size_t bwa_length_cutoff      = 200;
     double compression_cutoff     = 0.6;
     double path_limit_stretching  = 1.3;
     double path_limit_pressing    = 0.7;
