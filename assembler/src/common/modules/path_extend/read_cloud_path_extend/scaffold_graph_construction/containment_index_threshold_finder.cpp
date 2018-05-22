7bef38b2de59d4ed5304f9d8c3df594a5a1698cd
Itolstoganov <oockvay@gmail.com>
2018-05-22 18:49:58 +0300

    Estimate training assembly graph edge length.

--- assembler/src/common/modules/path_extend/read_cloud_path_extend/scaffold_graph_construction/containment_index_threshold_finder.cpp
+++ assembler/src/common/modules/path_extend/read_cloud_path_extend/scaffold_graph_construction/containment_index_threshold_finder.cpp
@@ -2,100 +2,21 @@
 #include "containment_index_threshold_finder.hpp"
 
 namespace path_extend {
-ScoreHistogram::ScoreHistogram(const map<double, size_t> &score_to_number_) : score_to_number_(score_to_number_) {}
-ScoreHistogram EdgePairScoreHistogramConstructor::ConstructScoreHistogram() const {
-    INFO("Getting score histogram");
-    std::multiset<double> scores;
-    size_t processed_edges = 0;
-    size_t block_size = scaffold_vertices_.size() / 10;
-    size_t threads = cfg::get().max_threads;
-    INFO(scaffold_vertices_.size() << " unique edges.");
-#pragma omp parallel for num_threads(threads)
-    for (size_t i = 0; i < scaffold_vertices_.size(); ++i) {
-        scaffold_graph::ScaffoldVertex first = scaffold_vertices_[i];
-        vector<double> current_scores;
-        for (const auto &second: scaffold_vertices_) {
-            if (first != second and first.getConjugateFromGraph(g_) != second) {
-                path_extend::scaffold_graph::ScaffoldGraph::ScaffoldEdge scaffold_edge(first, second);
-                current_scores.push_back(score_function_->GetScore(scaffold_edge));
-            }
-        }
-#pragma omp critical
-        {
-            std::copy(current_scores.begin(), current_scores.end(), std::inserter(scores, scores.begin()));
-            ++processed_edges;
-            if (processed_edges % block_size == 0) {
-                INFO("Processed " << processed_edges << " out of " << scaffold_vertices_.size());
-            }
-        };
-    }
-    return ConstructScoreHistogramFromMultiset(scores);
-}
-double UnlabeledDistributionThresholdEstimator::GetThreshold() const {
-    const double STEP = 0.001;
-    const double MIN = 0.0;
-    const double MAX = 1.0;
-    EdgePairScoreHistogramConstructor hist_constructor(STEP, MIN, MAX, g_, score_function_, scaffold_vertices_);
-    ScoreHistogram histogram = hist_constructor.ConstructScoreHistogram();
-    return FindPercentile(histogram, scaffold_vertices_);
-}
-UnlabeledDistributionThresholdEstimator::UnlabeledDistributionThresholdEstimator(
-        const Graph &g_,
-        const vector<ScaffoldVertex> &scaffold_vertices_,
-        const shared_ptr<ScaffoldEdgeScoreFunction> &score_function_,
-        double vertex_multiplier)
-    : g_(g_), scaffold_vertices_(scaffold_vertices_),
-      score_function_(score_function_), vertex_multiplier_(vertex_multiplier) {}
-double UnlabeledDistributionThresholdEstimator::FindFirstLocalMin(const ScoreHistogram &histogram) const {
-
-    for (auto prev = histogram.begin(), curr = std::next(prev), next = std::next(curr);
-         next != histogram.end();
-         ++prev, ++curr, ++next) {
-        if (curr->second < next->second and curr->second < prev->second) {
-            return curr->first;
-        }
-    }
-    return 0.0;
-}
-double UnlabeledDistributionThresholdEstimator::FindPercentile(const ScoreHistogram &histogram,
-                                                             const vector<ScaffoldVertex> &scaffold_vertices) const {
-    size_t number_of_vertices = scaffold_vertices.size();
-    size_t optimal_number_of_edges = static_cast<size_t>(static_cast<double>(number_of_vertices) * vertex_multiplier_);
-    PercentileGetter percentile_getter;
-    double percent = static_cast<double>(optimal_number_of_edges) / static_cast<double>(histogram.size());
-    return percentile_getter.GetPercentile(histogram, percent);
-}
-ScoreHistogram AbstractScoreHistogramConstructor::ConstructScoreHistogramFromMultiset(
-        const std::multiset<double>& scores) const {
-    vector<double> ticks;
-    for (double t = min_score_; math::le(t, max_score_); t += step_) {
-        ticks.push_back(t);
-    }
-    std::map<double, size_t> score_to_number;
-    size_t current = 0;
-    auto current_tick_it = ticks.begin();
+AbstractScoreHistogramConstructor::ScoreDistribution AbstractScoreHistogramConstructor::ConstructScoreDistributionFromMultiset(
+    const std::multiset<double> &scores) const {
+    ScoreDistribution result;
     for (const auto& score: scores) {
-        if (math::ge(score, *current_tick_it)) {
-            score_to_number.insert({*current_tick_it, current});
-            current = 0;
-            ++current_tick_it;
-        } else {
-            ++current;
-        }
+        result.add(score);
     }
-    size_t sum = 0;
-    for (const auto &entry: score_to_number) {
-        sum += entry.second;
-    }
-    ScoreHistogram result(score_to_number);
+    result.sort();
     return result;
 }
-ScoreHistogram LongEdgeScoreHistogramConstructor::ConstructScoreHistogram() const {
+AbstractScoreHistogramConstructor::ScoreDistribution LongEdgeScoreHistogramConstructor::ConstructScoreDistribution() const {
     VERIFY(interesting_edges_.size() > 0);
     auto distance_values = ConstructDistanceDistribution(min_distance_, max_distance_);
     size_t left_block_start_offset = max_distance_ + left_block_length_ + right_block_length_;
     size_t block_size = interesting_edges_.size() / 10;
-    const size_t TOTAL_SAMPLE_SIZE = 2000;
+    const size_t TOTAL_SAMPLE_SIZE = 200;
     //why constant?
     const size_t edge_sample_size = TOTAL_SAMPLE_SIZE / interesting_edges_.size();
     std::random_device rd;
@@ -127,8 +48,8 @@ ScoreHistogram LongEdgeScoreHistogramConstructor::ConstructScoreHistogram() cons
 #pragma omp critical
                 {
                     if (score.is_initialized()) {
+                        DEBUG("Inserting score: " << score.get());
                         scores.insert(score.get());
-                        DEBUG("Score: " << score.get());
                     }
                 };
             }
@@ -138,7 +59,7 @@ ScoreHistogram LongEdgeScoreHistogramConstructor::ConstructScoreHistogram() cons
         }
     }
     INFO(scores.size() << " score samples");
-    return ConstructScoreHistogramFromMultiset(scores);
+    return ConstructScoreDistributionFromMultiset(scores);
 }
 LongEdgeScoreHistogramConstructor::LongEdgeScoreHistogramConstructor(
         double tick_step,
@@ -171,18 +92,15 @@ vector<size_t> LongEdgeScoreHistogramConstructor::ConstructDistanceDistribution(
     }
     return result;
 }
-double PercentileGetter::GetPercentile(const ScoreHistogram &histogram, double percent) {
+double PercentileGetter::GetPercentile(cluster_model::SimpleDistribution<double> distribution, double percent) {
+    //fixme code duplication with PrimaryParametersExtractor
     VERIFY(math::ge(percent, 0.0));
     VERIFY(math::le(percent, 1.0));
-    size_t prefix_size = static_cast<size_t>(static_cast<double>(histogram.size()) * percent);
-    size_t current_sum = 0;
-    for (auto it = histogram.begin(); it != histogram.end(); ++it) {
-        current_sum += it->second;
-        if (current_sum > prefix_size) {
-            return it->first;
-        }
+    if (not distribution.is_sorted()) {
+        distribution.sort();
     }
-    return 0.0;
+    size_t index = static_cast<size_t>(static_cast<double>(distribution.size()) * percent);
+    return distribution.at(index);
 }
 LabeledDistributionThresholdEstimator::LabeledDistributionThresholdEstimator(
         const Graph &g,
@@ -222,7 +140,7 @@ double LabeledDistributionThresholdEstimator::GetThreshold() const {
                                                             long_edges, left_block_length_,
                                                             right_block_length_, min_distance_,
                                                             max_distance_, max_threads_);
-    auto score_histogram = histogram_constructor.ConstructScoreHistogram();
+    auto score_histogram = histogram_constructor.ConstructScoreDistribution();
     PercentileGetter percentile_getter;
     const double debug_percentile_step = 0.1;
     for (double i = 0.0; math::le(i, 1.0); i += debug_percentile_step) {
@@ -236,10 +154,10 @@ double LabeledDistributionThresholdEstimator::GetThreshold() const {
 SegmentBarcodeScoreFunction::SegmentBarcodeScoreFunction(shared_ptr<barcode_index::FrameBarcodeIndexInfoExtractor> barcode_extractor)
     : barcode_extractor_(barcode_extractor) {}
 optional<double> ContainmentIndexFunction::GetScoreFromTwoFragments(EdgeId edge,
-                                                                                size_t left_start,
-                                                                                size_t left_end,
-                                                                                size_t right_start,
-                                                                                size_t right_end) const {
+                                                                    size_t left_start,
+                                                                    size_t left_end,
+                                                                    size_t right_start,
+                                                                    size_t right_end) const {
     VERIFY(left_start < left_end);
     VERIFY(right_start < right_end);
     boost::optional<double> result;
@@ -248,6 +166,8 @@ optional<double> ContainmentIndexFunction::GetScoreFromTwoFragments(EdgeId edge,
     auto left_barcodes = barcode_extractor_->GetBarcodesFromRange(edge, LOCAL_COUNT_THRESHOLD, left_start, left_end);
     auto right_barcodes = barcode_extractor_->GetBarcodesFromRange(edge, LOCAL_COUNT_THRESHOLD, right_start, right_end);
 
+    DEBUG("Left barcodes: " << left_barcodes.size());
+    DEBUG("Right barcodes: " << right_barcodes.size());
     //fixme code duplication with NormalizedBarcodeScoreFunction
     if (left_barcodes.size() == 0 or right_barcodes.size() == 0) {
         return result;
@@ -255,6 +175,7 @@ optional<double> ContainmentIndexFunction::GetScoreFromTwoFragments(EdgeId edge,
     vector<barcode_index::BarcodeId> intersection;
     std::set_intersection(left_barcodes.begin(), left_barcodes.end(), right_barcodes.begin(), right_barcodes.end(),
                           std::back_inserter(intersection));
+    DEBUG("Intersection: " << intersection.size());
     double min_size = static_cast<double>(std::min(left_barcodes.size(), right_barcodes.size()));
     double containment_index = static_cast<double>(intersection.size()) / min_size;
     result = containment_index;
