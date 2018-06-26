9ce33775cb0d61fad3df8c97aab07779c7520b9b
Itolstoganov <oockvay@gmail.com>
2018-06-26 23:57:26 +0300

    Unfinalize gaps

--- assembler/src/common/modules/path_extend/pipeline/launcher.cpp
+++ assembler/src/common/modules/path_extend/pipeline/launcher.cpp
@@ -499,7 +499,7 @@ void PathExtendLauncher::PolishPaths(const PathContainer &paths, PathContainer &
             size_t tail_threshold = cfg::get().ts_res.scaff_con.path_scaffolder_tail_threshold;
             size_t count_threshold = cfg::get().ts_res.scaff_con.path_scaffolder_count_threshold;
             size_t length_threshold = cfg::get().ts_res.scaff_con.min_edge_length_for_barcode_collection;
-            
+
             //fixme move to configs
             const size_t scan_bound = 150;
             INFO("Tail threshold: " << tail_threshold);
