6b0d7f65e0bcb4bc5b86f57dcd9c8f95b9e3a186
Dmitry Antipov <dmitrij.antipov@gmail.com>
2018-08-08 20:02:46 +0300

    blast file parser rewriten

--- assembler/src/plasmid_utils/parse_blast_xml.py
+++ assembler/src/plasmid_utils/parse_blast_xml.py
@@ -3,30 +3,29 @@ import sys
 import argparse
 from glob import glob
 from joblib import Parallel, delayed
-significant_e_value = 0.001
 import os.path
 import pickle
 
-# Input - BLAST XML of scaffolds.fasta (?)
-# Output:
-# if no alignment at all - non-significant
-# if good alignment (e<0.001):
-# if "chrom"  in name - chrom
-# if "plasmid" in name:
-#    <3 matches and close length (0.9) - plasmids
-#    others - plasmids_bad
-# 
-# others - unclass 
+
+#hsps with larger e-value are discarded
+significant_e_value = 0.001
+
+#if query coverage fraction is smaller then alignment is discarded
+significant_query_fraction = 0.1
+
+#if best_hit_query_coverage * significant_ratio < other_hit_query_coverage && hits are of different types (i.e plasmid and chromosome then ambiguous)
+significant_ratio = 0.9
 
 def parse_args(args):
     ###### Command Line Argument Parser
     parser = argparse.ArgumentParser(description="Check the help flag")
-    parser.add_argument('-i', help='Path to folder with XML output')
+    parser.add_argument('-i', help='Path to file with XML output')
     parser.add_argument('-o', help='Path to output folder')  
     return parser.parse_args()
 
 
-    ###### Return total length of alignment with respect of significant_e_value (0.001) threshold
+###### Return total length of alignment with respect of significant_e_value (0.001) threshold
+#
 def get_total_len(alignment):
     global significant_e_value
     total_len = 0
@@ -34,6 +33,25 @@ def get_total_len(alignment):
         if hsp.expect < significant_e_value:
             total_len += hsp.align_length
     return total_len
+
+def get_query_coverage (alignment):
+    global significant_e_value
+    total_len = 0
+    coords = []
+    for hsp in alignment.hsps:
+        if hsp.expect < significant_e_value:
+            coords.append([hsp.query_start, 1])
+            coords.append([hsp.query_end, -1])
+    coords.sort()
+    if len(coords) > 0:
+        layers = coords[0][1]
+        for j in range (1, len(coords)):
+            if layers > 0:
+                total_len  += coords[j][0] - coords[j - 1][0]
+            layers += coords[j][1]
+           
+    return total_len
+    
 
 def get_identity(alignment):
     global significant_e_value
@@ -43,20 +61,49 @@ def get_identity(alignment):
             identity += hsp.identities
     return identity
 
+def get_hsp_count(alignment):
+    global significant_e_value
+    hsp_count = 0
+    for hsp in alignment.hsps:
+        if hsp.expect < significant_e_value:
+            hsp_count += 1
+    return hsp_count
+
+
+def parse_name(seq):
+    if (seq.find('hage') != -1 or seq.find('irus') != -1 ):
+        return "Virus"
+    elif seq.find('lasmid') != -1:
+        return "Plasmid"
+    elif (seq.find('hromosome') != -1 or seq.find("omplete genome") != -1):
+        return "Chromosome"
+    else:
+        return "Unknown"
+
+def report_file(file, query, alignment):
+    file.write(query + '\n')
+    file.write(alignment[1] + "\t query coverage: " + str(1.0 * alignment[0]/alignment[6]) + "\t identity: " + str(1.0 * alignment[4]/alignment[3]) + "\t hsp_num: " + str(alignment[5]))
+    file.write('\n')
+    return
+
 
 
 def parser(f, out_dir):
+    global significant_query_fraction
+    global significant_ratio
+
     scat_out={}
     xml_file = open(f,"r")
     name = os.path.basename(f)
     name = os.path.join(out_dir, name)
+    print(name)
     nosig = open(name[:-4]+"_no_significant.names", "w")
     chrom = open(name[:-4]+"_chromosome.names", "w")
     plasmids = open(name[:-4]+"_plasmid.names", "w")
-    plasmids_bad = open(name[:-4]+"_plasmids_bad.names", "w")
-    unclas = open(name[:-4]+"_unclassified.names", "w")
+    amb_file = open(name[:-4]+"_ambiguous.names", "w")
     records= NCBIXML.parse(xml_file)
     viral = open(name[:-4]+"_viruses.names", "w")
+    files = {"Virus": viral, "Plasmid": plasmids, "Chromosome": chrom}
     for item in records:
     #    print (item.query)
 	### We are taking query length from contig name provided by SPAdes. Weird, huh?
@@ -65,7 +112,7 @@ def parser(f, out_dir):
 	###### No alignment - put in non-significant
         if len(item.alignments) == 0:
             print ("No Significant")
-            nosig.write(item.query + '\n')
+            nosig.write(item.query + '\n\n')
             continue
 
 
@@ -73,121 +120,46 @@ def parser(f, out_dir):
         good_aln = []
         scat_good_aln = []
         #print item.alignments
-        for alignment in item.alignments:        
-            total_len = get_total_len(alignment)
-            total_identity = get_identity(alignment)            
-            if (len(alignment.hsps) <= 3 and total_len > 0.9 * pl_len and pl_len > 0.9 * total_len):
-                good_aln.append(alignment.title)
-                scat_good_aln.append([str(alignment.title), total_len, pl_len, float(total_len)/pl_len, total_identity, float(total_identity)/total_len])                
-        pl = 0
-        for seq in good_aln:
-            if (seq.find('hage') != -1 or seq.find('irus') != -1 ):
-                #total_len = get_total_len(item.seq)
-                #total_identity = get_identity(item.seq)
-                #viral.write(item.query + '\n')
-                viral.write(seq + '\n')
-                for i in scat_good_aln:
-                    if i[0]==seq:
-                        scat_out[str(item.query)] = i+["Virus"]
-                #scatter_data[item.query]=[total_len, pl_len, float(total_len)/pl_len, total_identity, float(total_identity)/total_len, "Virus"]
-                pl = 1
-                break
-        if pl == 0:
-            for seq in good_aln:
-                if ((seq.find('hromosome') != -1) or ((seq.find("omplete genome") != -1) and not (seq.find('lasmid') != -1))):
-                    ##total_len = get_total_len(item.seq)
-                    #total_identity = get_identity(item.seq)
-                    chrom.write(item.query + '\n')
-                    chrom.write(seq + '\n')
-                    for i in scat_good_aln:
-                      if i[0]==seq:
-                        scat_out[str(item.query)] = i+["Chromosome"]
-                    #scatter_data[item.query]=[total_len, pl_len, float(total_len)/pl_len, total_identity, float(total_identity)/total_len, "Chromosome"]
-                    pl = 1
+        alignments = []
+        for alignment in item.alignments:
+            seq_type = parse_name(alignment.title)
+            query_coverage = get_query_coverage(alignment)
+            if seq_type == "Unknown" or get_query_coverage(alignment) < significant_query_fraction * pl_len:
+                continue
+            alignments.append([get_query_coverage(alignment), alignment.title, parse_name(alignment.title), get_total_len(alignment), get_identity(alignment), get_hsp_count(alignment), pl_len])
+        if len(alignments)== 0:
+            print ("No Significant")
+            nosig.write(item.query + '\n')
+            al = item.alignments[0]
+            nosig.write(al.title + "\t" + "total query cov " + str(get_query_coverage(al)) + " of " + str(pl_len))
+            nosig.write('\n')
+        else:
+            alignments.sort()
+            alignments.reverse()
+            type = alignments[0][2]
+            best_query_cov = alignments[0][0]
+            ambigous = 0
+            for i in range (1, len(alignments)):
+                if alignments[i][0] < best_query_cov * significant_ratio:
                     break
-        if pl == 0:
-            for seq in good_aln:
-                if seq.find('lasmid') != -1:
-                    plasmids.write(item.query + '\n')
-                    plasmids.write(seq + '\n')
-                    for i in scat_good_aln:
-                      if i[0]==seq:
-                        scat_out[str(item.query)] = i+["Plasmid"]
-                    #scatter_data[item.query]=[total_len, pl_len, float(total_len)/pl_len, total_identity, float(total_identity)/total_len, "Plasmid"]
-                    pl = 1
+                if type !=  alignments[i][2]:
+                    ambigous = i
                     break
-
-
-        if pl == 0:
-            found = False
-            arr = []
-            for alignment in item.alignments:
-                seq = alignment.title
-                total_len = get_total_len(alignment)
-                total_identity = get_identity(alignment)            
-                arr.append(str(total_len))
-#alignment is too small to decide
-                if total_len < pl_len * 0.1:
-                    continue
-                if (seq.find('hage') != -1 or seq.find('irus') != -1 ):
-                    viral.write(item.query + '\n')
-                    viral.write(seq + '\n')
-                    scat_out[str(item.query)] = [alignment.title, total_len, pl_len, float(total_len)/pl_len, total_identity, float(total_identity)/total_len, "Virus"]
-                    
-                    #for i in scatter_data:
-                     # if i[1]==seq:
-                        #print i 
-                      #  scatter_out.append([i]+["Plasmid"])
-
-
-                    found = True
-
-                    break
-                if seq.find('lasmid') != -1:
-                    scat_out[str(item.query)] = [alignment.title, total_len, pl_len, float(total_len)/pl_len, total_identity, float(total_identity)/total_len, "Plasmid_bad"]
-                    plasmids_bad.write(item.query + '\n')
-                    plasmids_bad.write(seq + "\n" + "len " + str (alignment.length) +" alnments lenghts:" + "\n")
-                    for hsp in alignment.hsps:
-                        plasmids_bad.write( str (hsp.align_length )+ " ")
-                        plasmids_bad.write("\n")
-                    found = True
-                    break
-                if (seq.find('hromosome') != -1) or (seq.find("omplete genome") != -1):
-                    scat_out[str(item.query)] = [alignment.title, total_len, pl_len, float(total_len)/pl_len, total_identity, float(total_identity)/total_len, "Chromosome"]
-                    chrom.write(item.query + '\n')
-                    chrom.write(seq + '\n')                    
-                    found = True
-                    break
-            if not found:
-
-
-                scat_out[str(item.query)] = [alignment.title, total_len, pl_len, float(total_len)/pl_len, total_identity, float(total_identity)/total_len, "Unclassified"]
-                unclas.write(item.query + '\n')
-                unclas.write("Best alignment to " + item.alignments[0].title + '\n' + "Total lengths of all alignments:")
-                for s in arr:
-                    unclas.write(s + " ")
-                unclas.write('\n')
-
-    with open (name[:-4]+"_span_identity.txt","w") as span:
-        for a,b in scat_out.items():
-            span.write(a + "\t" + "\t".join((str(i) for i in b)) + "\n")
-#            print a + "\t" + "\t".join((str(i) for i in b))
-
-
-    #with open('scatter_data_dict.pkl', 'wb') as fid:
-     #   pickle.dump(scat_out, fid)    
-    #for i in scatter_data:
-     #   print str(i).strip('[]')  
-#        print str("".join(i[0]+i[2:]))
+            if ambigous != 0:
+                print "Ambiguous"
+                amb_file.write(item.query + '\n')
+                amb_file.write (alignments[0][1] + '\t' + str(best_query_cov) + '\t' + alignments[ambigous][1] + '\t' + str(alignments[ambigous][0]) + '\n')
+            else:
+                print type
+                report_file(files[type], item.query,alignments[0])
     return
-
 
 def main ():
     parsed_args = parse_args(sys.argv[1:])
     files = glob(str(parsed_args.i)+"/*.xml")
 #    print (files)
-
-    Parallel(n_jobs=30)(delayed(parser) (infile, parsed_args.o) for infile in files)
+    parser(parsed_args.i, parsed_args.o)    
+#    Parallel(n_jobs=30)(delayed(parser) (infile, parsed_args.o) for infile in files)
 #Parallel(n_jobs=30)(delayed(parser) (args) for args in files)
 
 #                print('score:', hsp.score)
