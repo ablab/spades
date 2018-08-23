54f7e50113caaebfaf8cea3db2561386cdc0d63e
Alexander Shlemov <shlemovalex@gmail.com>
2018-08-23 22:52:39 +0300

    Move LinkCleaner class into _extra include

--- assembler/src/common/assembly_graph/construction/early_simplification.hpp
+++ assembler/src/common/assembly_graph/construction/early_simplification.hpp
@@ -13,52 +13,6 @@
 
 namespace debruijn_graph {
 
-class LinkCleaner {
-private:
-    typedef utils::DeBruijnExtensionIndex<> Index;
-    typedef Index::KMer Kmer;
-    typedef Index::KeyWithHash KeyWithHash;
-    Index &index_;
-
-    void CleanForwardLinks(const KeyWithHash &kh, char ch) {
-        if (index_.CheckOutgoing(kh, ch)) {
-            KeyWithHash next_kh = index_.GetOutgoing(kh, ch);
-            if (!index_.CheckIncoming(next_kh, kh[0])) {
-                index_.DeleteOutgoing(kh, ch);
-            }
-        }
-    }
-
-    void CleanBackwardLinks(const KeyWithHash &kh, char ch) {
-        if (index_.CheckIncoming(kh, ch)) {
-            KeyWithHash prev_kh = index_.GetIncoming(kh, ch);
-            if (!index_.CheckOutgoing(prev_kh, kh[index_.k() - 1])) {
-                index_.DeleteIncoming(kh, ch);
-            }
-        }
-    }
-
-public:
-    LinkCleaner(Index &index) : index_(index) {}
-
-    //TODO make parallel
-    void CleanLinks() {
-        vector<Index::kmer_iterator> iters = index_.kmer_begin(10 * omp_get_max_threads());
-#pragma omp parallel for schedule(guided)
-        for (size_t i = 0; i < iters.size(); i++) {
-            for (Index::kmer_iterator &it = iters[i]; it.good(); ++it) {
-                const KeyWithHash kh = index_.ConstructKWH(RtSeq(index_.k(), *it));
-                if (kh.is_minimal()) {
-                    for (char ch = 0; ch < 4; ch++) {
-                        CleanForwardLinks(kh, ch);
-                        CleanBackwardLinks(kh, ch);
-                    }
-                }
-            }
-        }
-    }
-};
-
 class AlternativeEarlyTipClipper {
 public:
     typedef utils::DeBruijnExtensionIndex<> Index;
