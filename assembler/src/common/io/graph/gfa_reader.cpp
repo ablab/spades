0a1a310d1cb673f10e2801278ba83f02b5544db0
Alexander Shlemov <shlemovalex@gmail.com>
2018-10-10 18:30:52 +0300

    Add GFA paths to reader

--- assembler/src/common/io/graph/gfa_reader.cpp
+++ assembler/src/common/io/graph/gfa_reader.cpp
@@ -35,6 +35,12 @@ bool GFAReader::open(const std::string &filename) {
 uint32_t GFAReader::num_edges() const { return gfa_->n_seg; }
 uint64_t GFAReader::num_links() const { return gfa_->n_arc; }
 
+size_t GFAReader::num_paths() const { return paths_.size(); }
+GFAReader::path_iterator GFAReader::path_begin() const { return paths_.begin(); }
+GFAReader::path_iterator GFAReader::path_end() const { return paths_.end(); }
+std::pair<GFAReader::path_iterator, GFAReader::path_iterator>
+GFAReader::paths() const { return { path_begin(), path_end() }; }
+
 void GFAReader::to_graph(ConjugateDeBruijnGraph &g,
                          io::IdMapper<std::string> *id_mapper) {
     auto helper = g.GetConstructionHelper();
