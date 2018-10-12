c156d77b8ee3e310eeedc78156d966ee3febf136
Andrey Prjibelski <andrewprzh@gmail.com>
2018-10-12 12:32:05 +0300

    fix spades_holybox_compile.sh

--- assembler/src/tools/spades_holybox_compile.sh
+++ assembler/src/tools/spades_holybox_compile.sh
@@ -2,22 +2,16 @@
 
 # Script for compiling SPAdes using holy build box
 # To compile spades run
-# docker run -u $(id -u):$(id -g) -t -i -v <path to spades_compile.sh>:/spades/ --rm cab/spades-buildbox  bash /spades/src/tools/spades_holybox_compile.sh
+# docker run -u $(id -u):$(id -g) -t -i -v <path to SPAdes repository:/spades/ --rm cab/spades-buildbox6-64:2.0.0-a4  bash /spades/assembler/src/tools/spades_holybox_compile.sh
 
 set -e
 source /hbb_exe/activate
 
 set -x
 
-cd /spades
+cd /spades/assembler
 ./spades_compile.sh -DSPADES_STATIC_BUILD=1
 
 cd build_spades
 make package
 
-#ldd /gitrep/algorithmic-biology/assembler/bin/spades
-#ldd /gitrep/algorithmic-biology/assembler/bin/hammer
-#ldd /gitrep/algorithmic-biology/assembler/bin/bwa-spades
-#ldd /gitrep/algorithmic-biology/assembler/bin/corrector
-#ldd /gitrep/algorithmic-biology/assembler/bin/ionhammer
-#ldd /gitrep/algorithmic-biology/assembler/bin/scaffold_correction
