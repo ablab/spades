519ef859adb5ba4fa3840bdd0dc4693c801987c7
Tatiana Dvorkina <tdvorkina@chihua.cab.spbu.ru>
2018-11-19 11:41:50 +0300

    Remove unused files

--- assembler/src/projects/spaligner/aligner_stats/run_tests_celegans_updated.sh
+++
@@ -1,3 +0,0 @@
-#python2 ./src/projects/galigner/aligner_stats/truevsaligned_paths.py ./src/projects/galigner/aligner_stats/galigner_stats_celegans_sim_nano.yaml
-python2 ./src/projects/galigner/aligner_stats/truevsaligned_paths.py ./src/projects/galigner/aligner_stats/galigner_stats_celegans_sim_pacbio.yaml
-#python2 ./src/projects/galigner/aligner_stats/truevsaligned_paths.py ./src/projects/galigner/aligner_stats/galigner_stats_celegans_real_pacbio.yaml
