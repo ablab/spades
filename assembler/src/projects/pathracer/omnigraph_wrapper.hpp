1d15cc74b118e7dd4ef095bba47203558705854f
Anton Korobeynikov <anton@korobeynikov.info>
2019-01-26 13:47:46 +0300

    Introduce the context and reduce the size of the cursors

--- assembler/src/projects/pathracer/omnigraph_wrapper.hpp
+++ assembler/src/projects/pathracer/omnigraph_wrapper.hpp
@@ -13,11 +13,10 @@
 class DebruijnGraphCursor {
   public:
     DebruijnGraphCursor()
-            : g_{nullptr}, e_(), position_(-1) {}
+            : e_(), position_(-1) {}
 
-    DebruijnGraphCursor(const debruijn_graph::ConjugateDeBruijnGraph *graph,
-                        debruijn_graph::EdgeId e, size_t position)
-            : g_(graph), e_(e), position_(position) {}
+    DebruijnGraphCursor(debruijn_graph::EdgeId e, size_t position)
+            : e_(e), position_(position) {}
 
     DebruijnGraphCursor(const DebruijnGraphCursor &) = default;
     DebruijnGraphCursor(DebruijnGraphCursor &&) = default;
@@ -31,58 +30,69 @@ class DebruijnGraphCursor {
 
     using EdgeId = debruijn_graph::EdgeId;
     EdgeId edge() const { return e_; }
-
     size_t position() const { return position_; }
-
     bool is_empty() const { return !e_; }
 
-    char letter() const { return nucl(g_->EdgeNucls(e_)[position_]); }
+    char letter(const void *context) const {
+        return nucl(g(context).EdgeNucls(e_)[position_]); }
 
-    bool is_convergent() const {
-        return position_ == g_->k() &&
-                g_->IncomingEdgeCount(g_->EdgeStart(e_)) > 1;
+    bool is_convergent(const void *context) const {
+        const debruijn_graph::ConjugateDeBruijnGraph &g = this->g(context);
+
+        return position_ == g.k() &&
+                g.IncomingEdgeCount(g.EdgeStart(e_)) > 1;
         // return prev().size() > 1;
     }
 
-    bool is_divergent() const {
-        return position_ + 1 == g_->length(e_) + g_->k() &&
-                g_->OutgoingEdgeCount(g_->EdgeEnd(e_)) > 1;
+    bool is_divergent(const void *context) const {
+        const debruijn_graph::ConjugateDeBruijnGraph &g = this->g(context);
+
+        return position_ + 1 == g.length(e_) + g.k() &&
+                g.OutgoingEdgeCount(g.EdgeEnd(e_)) > 1;
         // return next().size() > 1;
     }
 
-    std::vector<DebruijnGraphCursor> prev() const;
-    std::vector<DebruijnGraphCursor> next() const;
+    std::vector<DebruijnGraphCursor> prev(const void*) const;
+    std::vector<DebruijnGraphCursor> next(const void*) const;
 
     static std::vector<DebruijnGraphCursor> get_cursors(const debruijn_graph::ConjugateDeBruijnGraph &g, const debruijn_graph::EdgeId &e, size_t pos) {
         // Unfortunately, several different cursors (actually different, having different prev's) may have the same edge & position
         // Therefore, it's impossible to design a correct get_cursor() method, we have to implement get_cursorS()
-        DebruijnGraphCursor cursor{&g, e, pos};
+        DebruijnGraphCursor cursor{e, pos};
         std::vector<DebruijnGraphCursor> result;
-        cursor.generate_normalized_cursors(result);
+        cursor.generate_normalized_cursors(result, &g);
         return result;
     }
 
     static std::vector<DebruijnGraphCursor> all(const debruijn_graph::ConjugateDeBruijnGraph &g);
 
   private:
-    void normalize_prefix_to_suffix() {
+    const debruijn_graph::ConjugateDeBruijnGraph &g(const void *context) const {
+        return *static_cast<const debruijn_graph::ConjugateDeBruijnGraph *>(context);
+    }
+    
+    void normalize_prefix_to_suffix(const void *context) {
+        const debruijn_graph::ConjugateDeBruijnGraph &g = this->g(context);
+
         // This method is used ONLY in all() generators for duplicates merging
         // normalization is not one-valued, but for duplicates deletion it's sufficient to normalize to any of normal variants
         // Also please not that in terms of next() all normalized versions are equivalent
         // Thus, for initial cursors we can (and should!) perform even more aggressive normalization "to brother":
         // if we are in vertex, replace the edge with the edge having the least id among all ingoing edges.
-        while (position_ < g_->k() && g_->IncomingEdgeCount(g_->EdgeStart(e_)) > 0) {
-            e_ = *g_->in_begin(g_->EdgeStart(e_));
-            position_ = g_->length(e_) + position_;
+        while (position_ < g.k() && g.IncomingEdgeCount(g.EdgeStart(e_)) > 0) {
+            e_ = *g.in_begin(g.EdgeStart(e_));
+            position_ = g.length(e_) + position_;
         }
     }
 
-    void generate_normalized_cursors(std::vector<DebruijnGraphCursor> &out) const {
-        if (position_ < g_->k() && g_->IncomingEdgeCount(g_->EdgeStart(e_)) > 0) {
-            for (EdgeId new_e : g_->IncomingEdges(g_->EdgeStart(e_))) {
-                size_t new_position = g_->length(new_e) + position_;
-                DebruijnGraphCursor cursor{g_, new_e, new_position};
-                cursor.generate_normalized_cursors(out);
+    void generate_normalized_cursors(std::vector<DebruijnGraphCursor> &out,
+                                     const void *context) const {
+        const debruijn_graph::ConjugateDeBruijnGraph &g = this->g(context);
+        if (position_ < g.k() && g.IncomingEdgeCount(g.EdgeStart(e_)) > 0) {
+            for (EdgeId new_e : g.IncomingEdges(g.EdgeStart(e_))) {
+                size_t new_position = g.length(new_e) + position_;
+                DebruijnGraphCursor cursor{new_e, new_position};
+                cursor.generate_normalized_cursors(out, context);
             }
         } else {
             out.push_back(*this);
@@ -92,7 +102,6 @@ class DebruijnGraphCursor {
     friend struct std::hash<DebruijnGraphCursor>;
     friend std::ostream &operator<<(std::ostream &os, const DebruijnGraphCursor &p);
 
-    const debruijn_graph::ConjugateDeBruijnGraph *g_;
     debruijn_graph::EdgeId e_;
     size_t position_;
 };
@@ -100,11 +109,10 @@ class DebruijnGraphCursor {
 class DebruijnComponentCursor {
   public:
     DebruijnComponentCursor()
-            : c_{nullptr}, e_(), position_(-1) {}
+            : e_(), position_(-1) {}
 
-    DebruijnComponentCursor(const omnigraph::GraphComponent<debruijn_graph::ConjugateDeBruijnGraph> *component,
-                            debruijn_graph::EdgeId e, size_t position)
-            : c_(component), e_(e), position_(position) {}
+    DebruijnComponentCursor(debruijn_graph::EdgeId e, size_t position)
+            : e_(e), position_(position) {}
 
     DebruijnComponentCursor(const DebruijnComponentCursor &) = default;
     DebruijnComponentCursor(DebruijnComponentCursor &&) = default;
@@ -122,39 +130,47 @@ class DebruijnComponentCursor {
 
     bool is_empty() const { return !e_; }
 
-    char letter() const { return nucl(c_->g().EdgeNucls(e_)[position_]); }
+    char letter(const void *context) const { return nucl(this->g(context).EdgeNucls(e_)[position_]); }
 
-    bool is_convergent() const {
-        const debruijn_graph::ConjugateDeBruijnGraph &g = c_->g();
+    bool is_convergent(const void *context) const {
+        const debruijn_graph::ConjugateDeBruijnGraph &g = this->g(context);
         return position_ == g.k() &&
-                (g.IncomingEdgeCount(g.EdgeStart(e_)) > 1 && !c_->IsBorder(g.EdgeStart(e_)));
+                (g.IncomingEdgeCount(g.EdgeStart(e_)) > 1 && !this->c(context).IsBorder(g.EdgeStart(e_)));
         // return prev().size() > 1;
     }
 
-    bool is_divergent() const {
-        const debruijn_graph::ConjugateDeBruijnGraph &g = c_->g();
+    bool is_divergent(const void *context) const {
+        const debruijn_graph::ConjugateDeBruijnGraph &g = this->g(context);
         return position_ + 1 == g.length(e_) + g.k() &&
-                (g.OutgoingEdgeCount(g.EdgeEnd(e_)) > 1 && !c_->IsBorder(g.EdgeEnd(e_)));
+                (g.OutgoingEdgeCount(g.EdgeEnd(e_)) > 1 && !this->c(context).IsBorder(g.EdgeEnd(e_)));
         // return next().size() > 1;
     }
 
-    std::vector<DebruijnComponentCursor> prev() const;
-    std::vector<DebruijnComponentCursor> next() const;
+    std::vector<DebruijnComponentCursor> prev(const void*) const;
+    std::vector<DebruijnComponentCursor> next(const void*) const;
 
-    static DebruijnComponentCursor get_cursor(omnigraph::GraphComponent<debruijn_graph::ConjugateDeBruijnGraph> &c,
+    static DebruijnComponentCursor get_cursor(const omnigraph::GraphComponent<debruijn_graph::ConjugateDeBruijnGraph> &c,
                                               const debruijn_graph::EdgeId &e, size_t pos) {
-        DebruijnComponentCursor cursor{&c, e, pos};
-        cursor.normalize_prefix_to_suffix();
+        DebruijnComponentCursor cursor{e, pos};
+        cursor.normalize_prefix_to_suffix(&c);
         return cursor;
     }
 
     static std::vector<DebruijnComponentCursor> all(const omnigraph::GraphComponent<debruijn_graph::ConjugateDeBruijnGraph> &c);
 
   private:
-    void normalize_prefix_to_suffix() {
-        const debruijn_graph::ConjugateDeBruijnGraph &g = c_->g();
+    const omnigraph::GraphComponent<debruijn_graph::ConjugateDeBruijnGraph> &c(const void *context) const {
+        return *static_cast<const omnigraph::GraphComponent<debruijn_graph::ConjugateDeBruijnGraph> *>(context);
+    }
+    
+    const debruijn_graph::ConjugateDeBruijnGraph &g(const void *context) const {
+        return this->c(context).g();
+    }
+
+    void normalize_prefix_to_suffix(const void *context) {
+        const debruijn_graph::ConjugateDeBruijnGraph &g = this->g(context);
         while (position_ < g.k() &&
-               (g.IncomingEdgeCount(g.EdgeStart(e_)) > 0 && !c_->IsBorder(g.EdgeStart(e_)))) {
+               (g.IncomingEdgeCount(g.EdgeStart(e_)) > 0 && !this->c(context).IsBorder(g.EdgeStart(e_)))) {
             e_ = *g.in_begin(g.EdgeStart(e_));
             position_ = g.length(e_) + position_;
         }
@@ -163,7 +179,6 @@ class DebruijnComponentCursor {
     friend struct std::hash<DebruijnComponentCursor>;
     friend std::ostream &operator<<(std::ostream &os, const DebruijnComponentCursor &p);
 
-    const omnigraph::GraphComponent<debruijn_graph::ConjugateDeBruijnGraph> *c_;
     debruijn_graph::EdgeId e_;
     size_t position_;
 };
@@ -207,17 +222,25 @@ struct Fees;
 };
 
 PathSet<DebruijnGraphCursor> find_best_path(const hmm::Fees &fees,
-                                            const std::vector<DebruijnGraphCursor> &initial);
+                                            const std::vector<DebruijnGraphCursor> &initial,
+                                            const debruijn_graph::ConjugateDeBruijnGraph &g);
 PathSet<DebruijnComponentCursor> find_best_path(const hmm::Fees &fees,
-                                                const std::vector<DebruijnComponentCursor> &initial);
+                                                const std::vector<DebruijnComponentCursor> &initial,
+                                                const omnigraph::GraphComponent<debruijn_graph::ConjugateDeBruijnGraph> &c);
 PathSet<ReversalGraphCursor<DebruijnGraphCursor>> find_best_path_rev(const hmm::Fees &fees,
-                                                                     const std::vector<ReversalGraphCursor<DebruijnGraphCursor>> &initial);
+                                                                     const std::vector<ReversalGraphCursor<DebruijnGraphCursor>> &initial,
+                                                                     const debruijn_graph::ConjugateDeBruijnGraph &g);
 PathSet<ReversalGraphCursor<DebruijnComponentCursor>> find_best_path_rev(const hmm::Fees &fees,
-                                                                         const std::vector<ReversalGraphCursor<DebruijnComponentCursor>> &initial);
+                                                                         const std::vector<ReversalGraphCursor<DebruijnComponentCursor>> &initial,
+                                                                         const omnigraph::GraphComponent<debruijn_graph::ConjugateDeBruijnGraph> &c);
 PathSet<AAGraphCursor<DebruijnComponentCursor>> find_best_path(const hmm::Fees &fees,
-                                                               const std::vector<AAGraphCursor<DebruijnComponentCursor>> &initial);
-PathSet<AAGraphCursor<DebruijnGraphCursor>> find_best_path(const hmm::Fees &fees, const std::vector<AAGraphCursor<DebruijnGraphCursor>> &initial);
+                                                               const std::vector<AAGraphCursor<DebruijnComponentCursor>> &initial,
+                                                               const omnigraph::GraphComponent<debruijn_graph::ConjugateDeBruijnGraph> &c);
+PathSet<AAGraphCursor<DebruijnGraphCursor>> find_best_path(const hmm::Fees &fees, const std::vector<AAGraphCursor<DebruijnGraphCursor>> &initial,
+                                                           const debruijn_graph::ConjugateDeBruijnGraph &g);
 PathSet<AAGraphCursor<RestrictedGraphCursor<DebruijnGraphCursor>>> find_best_path(const hmm::Fees &fees,
-                                                                                  const std::vector<AAGraphCursor<RestrictedGraphCursor<DebruijnGraphCursor>>> &initial);
+                                                                                  const std::vector<AAGraphCursor<RestrictedGraphCursor<DebruijnGraphCursor>>> &initial,
+                                                                                  const debruijn_graph::ConjugateDeBruijnGraph &g);
 PathSet<RestrictedGraphCursor<DebruijnGraphCursor>> find_best_path(const hmm::Fees &fees,
-                                                                   const std::vector<RestrictedGraphCursor<DebruijnGraphCursor>> &initial);
+                                                                   const std::vector<RestrictedGraphCursor<DebruijnGraphCursor>> &initial,
+                                                                   const debruijn_graph::ConjugateDeBruijnGraph &g);
