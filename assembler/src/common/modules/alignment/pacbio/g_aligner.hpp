b119d6723d7af9012ff5849101b918d5da1a34ca
Tatiana Dvorkina <tdvorkina@chihua.cab.spbu.ru>
2019-02-18 19:26:54 +0300

    Update copyright

--- assembler/src/common/modules/alignment/pacbio/g_aligner.hpp
+++ assembler/src/common/modules/alignment/pacbio/g_aligner.hpp
@@ -1,6 +1,5 @@
 //***************************************************************************
-//* Copyright (c) 2015-2018 Saint Petersburg State University
-//* Copyright (c) 2011-2014 Saint Petersburg Academic University
+//* Copyright (c) 2015-2019 Saint Petersburg State University
 //* All Rights Reserved
 //* See file LICENSE for details.
 //***************************************************************************
