bf27b55296f5bfc4afbbd95be202a3effe04f559
Alexander Shlemov <shlemovalex@gmail.com>
2019-02-21 16:31:05 +0300

    PRacer: Reorganize utils I

--- assembler/src/projects/pathracer/utils.hpp
+++ assembler/src/projects/pathracer/utils.hpp
@@ -9,6 +9,7 @@
 #include "utils/stl_utils.hpp"
 using namespace utils;
 
+namespace std {
 // template <typename T>
 // std::ostream &operator<<(std::ostream &os, const std::vector<T> &v);
 template <typename T>
@@ -26,13 +27,14 @@ std::ostream &operator<<(std::ostream &os, const std::unordered_set<T> &v);
 
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const std::unordered_set<T> &v) {
-  os << "{ ";
-  for (const auto &e : v) {
-    os << e << " ";
-  }
-  os << "}";
-  return os;
+    os << "{ ";
+    for (const auto &e : v) {
+        os << e << " ";
+    }
+    os << "}";
+    return os;
 }
+}  // namespace std
 
 template <typename T>
 void remove_duplicates(std::vector<T> &v) {
@@ -64,9 +66,18 @@ template <typename T>
 struct hash<std::vector<T>> {
     std::size_t operator()(const std::vector<T> &v) const {
         size_t result = 0xDEADBEEF;
-        for (const auto &entry :v)
-            result = hash_size_t_pair(result, std::hash<T>()(entry));
+        for (const auto &entry : v) result = hash_size_t_pair(result, std::hash<T>()(entry));
         return result;
     }
 };
+}  // namespace std
+
+template <typename Iter, typename Key>
+void sort_by(Iter b, Iter e, const Key &key) {
+    std::sort(b, e, [&key](const auto &a, const auto &b) -> bool { return key(a) < key(b); });
+}
+
+template <typename Iter, typename Key>
+void stable_sort_by(Iter b, Iter e, const Key &key) {
+    std::stable_sort(b, e, [&key](const auto &a, const auto &b) -> bool { return key(a) < key(b); });
 }
