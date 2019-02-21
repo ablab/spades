14431898988a7a937b1c396113e71f8b3f3ccbcf
Anton Korobeynikov <anton@korobeynikov.info>
2019-02-21 18:17:09 +0300

    Add possibility to clone graph and / or extract component by edges

--- assembler/src/common/assembly_graph/core/construction_helper.hpp
+++ assembler/src/common/assembly_graph/core/construction_helper.hpp
@@ -24,13 +24,52 @@ class ConstructionHelper {
 
     Graph &graph_;
 
-public:
 
+    void MaybeAddVertex(Graph &clone,
+                        const VertexData &data,
+                        VertexId id1, VertexId id2) const {
+        if (clone.contains(id1))
+            return;
+        TRACE("Vertex " << id1 << " ~ " << id2 << " .");
+        auto new_id = clone.AddVertex(data, id1, id2);
+        VERIFY(new_id == id1);
+        VERIFY(clone.conjugate(new_id) == id2);
+    }
+
+public:
     ConstructionHelper(Graph &graph)
             : graph_(graph) {}
 
     Graph &graph() {
         return graph_;
+    }
+
+    Graph clone() const {
+        return component(graph_.e_begin(), graph_.e_end());
+    }
+
+    template<class It>
+    Graph component(It start, It end) const {
+        Graph clone(graph_.master_);
+        clone.reserve(graph_.vreserved(), graph_.ereserved());
+
+        for (; start != end; ++start) {
+            EdgeId e = *start;
+            if (clone.contains(e))
+                continue;
+
+            VertexId v1 = graph_.EdgeStart(e), v2 = graph_.EdgeEnd(e);
+            VertexData vdata = graph_.data(v1);
+            MaybeAddVertex(clone, vdata, v2, v2);
+
+            EdgeId new_id = clone.AddEdge(v1, v2,
+                                          graph_.data(e),
+                                          e, graph_.conjugate(e));
+            VERIFY(new_id == e);
+            VERIFY(clone.conjugate(new_id) = graph_.conjugate(e));
+        }
+
+        return clone;
     }
 
     EdgeId AddEdge(const EdgeData &data, EdgeId id = 0) {
