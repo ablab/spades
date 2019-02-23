ca33263ca5b7e1b7b0d5e96aa03dcbe7a746ced8
Alexander Shlemov <shlemovalex@gmail.com>
2019-02-23 21:47:03 +0300

    PR: cached_cursor fix WARN

--- assembler/src/projects/pathracer/cached_cursor.hpp
+++ assembler/src/projects/pathracer/cached_cursor.hpp
@@ -77,7 +77,7 @@ public:
     CachedCursorContext(const std::vector<Cursor> &cursors, typename Cursor::Context context) {
         VERIFY(std::numeric_limits<Index>::max() > cursors.size());
         std::unordered_map<Cursor, Index> cursor2index;
-        for (size_t i = 0; i < cursors.size(); ++i) {
+        for (Index i = 0; i < cursors.size(); ++i) {
             cursor2index[cursors[i]] = i;
         }
 
