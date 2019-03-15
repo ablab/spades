97526a975579b10ec3995da5042513e37c1887ba
Alexander Shlemov <shlemovalex@gmail.com>
2019-03-15 18:02:07 +0300

    PathRacer: factor out HMMPathInfo class

--- assembler/src/projects/pathracer/main.cpp
+++ assembler/src/projects/pathracer/main.cpp
@@ -12,6 +12,7 @@
 #include "path_utils.hpp"
 #include "cached_cursor.hpp"
 #include "superpath_index.hpp"
+#include "hmm_path_info.hpp"
 #include "fasta_reader.hpp"
 
 #include "assembly_graph/core/graph.hpp"
@@ -602,83 +603,6 @@ void LoadGraph(debruijn_graph::ConjugateDeBruijnGraph &graph,
         gio.Load(filename, graph);
     }
 }
-
-class HMMPathInfo {
-private:
-    double rounded_score() const {
-        return round(score * 1000.0) / 1000.0;
-    }
-
-    auto key() const {
-        return std::make_tuple(rounded_score(), nuc_seq, label, path);
-    }
-
-    bool supported_by_original_path(const SuperpathIndex &paths) const {
-        if (label.size() == 0) {
-            return false;
-        }
-
-        const std::string edge_prefix = "edge_";
-        if (label.substr(0, edge_prefix.size()) == edge_prefix) {
-            return false;
-        }
-        size_t path_id = std::stoll(label);
-        size_t pos = superpath_index::find_subpath(path, paths[path_id]);
-        return pos != size_t(-1);
-    }
-
-    friend void unique_hmm_path_info(std::vector<HMMPathInfo> &infos, const SuperpathIndex &paths);
-
-public:
-    std::string hmmname;
-    double score;
-    std::string seq;
-    std::string nuc_seq;
-    std::vector<EdgeId> path;
-    std::string alignment;
-    std::string label;
-    size_t pos;
-
-    bool operator<(const HMMPathInfo &that) const {
-        return key() < that.key();
-    }
-
-    size_t trim_first_edges(const ConjugateDeBruijnGraph &graph) {
-        size_t edges_to_trim = 0;
-        for (;edges_to_trim < path.size() - 1; ++edges_to_trim) {  // We cannot remove the last edge
-            size_t edge_length_before_overlap = graph.length(path[edges_to_trim]);
-            if (pos < edge_length_before_overlap) {
-                break;
-            }
-            pos -= edge_length_before_overlap;
-        }
-
-        path.erase(path.begin(), path.begin() + edges_to_trim);
-        return edges_to_trim;
-    }
-
-    HMMPathInfo(std::string name, double sc, std::string s, std::string nuc_s, std::vector<EdgeId> p, std::string alignment,
-                std::string label, size_t pos)
-            : hmmname(std::move(name)), score(sc),
-              seq(std::move(s)), nuc_seq{std::move(nuc_s)},
-              path(std::move(p)), alignment(std::move(alignment)), label{std::move(label)}, pos{pos} {}
-};
-
-void unique_hmm_path_info(std::vector<HMMPathInfo> &infos, const SuperpathIndex &paths) {
-    auto key = [&paths](const auto &info) {
-        int supported = info.supported_by_original_path(paths) ? 0 : 1;
-        return std::make_tuple(-info.rounded_score(), info.nuc_seq, info.path, supported, info.label);
-    };
-    sort_by(infos.begin(), infos.end(), key);
-
-    auto eq = [](const auto &i1, const auto &i2) {
-        return i1.rounded_score() == i2.rounded_score() && i1.nuc_seq == i2.nuc_seq && i1.path == i2.path;
-    };
-
-    auto it = std::unique(infos.begin(), infos.end(), eq);
-    infos.erase(it, infos.end());
-}
-
 
 void SaveResults(const hmmer::HMM &hmm, const ConjugateDeBruijnGraph & /* graph */,
                  const PathracerConfig &cfg,
--- assembler/src/projects/pathracer/open_event_graph.cpp
+++ assembler/src/projects/pathracer/open_event_graph.cpp
@@ -27,55 +27,12 @@
 
 #include "path_utils.hpp"
 #include "pathtree.hpp"
+#include "hmm_path_info.hpp"
 
 using debruijn_graph::EdgeId;
 using debruijn_graph::VertexId;
 using debruijn_graph::ConjugateDeBruijnGraph;
 
-class HMMPathInfo {
-private:
-    double rounded_score() const {
-        return round(score * 1000.0) / 1000.0;
-    }
-
-    auto key() const {
-        return std::make_tuple(rounded_score(), nuc_seq, label, path);
-    }
-
-public:
-    std::string hmmname;
-    double score;
-    std::string seq;
-    std::string nuc_seq;
-    std::vector<EdgeId> path;
-    std::string alignment;
-    std::string label;
-    size_t pos;
-
-    bool operator<(const HMMPathInfo &that) const {
-        return key() < that.key();
-    }
-
-    size_t trim_first_edges(const ConjugateDeBruijnGraph &graph) {
-        size_t edges_to_trim = 0;
-        for (;edges_to_trim < path.size() - 1; ++edges_to_trim) {  // We cannot remove the last edge
-            size_t edge_length_before_overlap = graph.length(path[edges_to_trim]);
-            if (pos < edge_length_before_overlap) {
-                break;
-            }
-            pos -= edge_length_before_overlap;
-        }
-
-        path.erase(path.begin(), path.begin() + edges_to_trim);
-        return edges_to_trim;
-    }
-
-    HMMPathInfo(std::string name, double sc, std::string s, std::string nuc_s, std::vector<EdgeId> p, std::string alignment,
-                std::string label, size_t pos)
-            : hmmname(std::move(name)), score(sc),
-              seq(std::move(s)), nuc_seq{std::move(nuc_s)},
-              path(std::move(p)), alignment(std::move(alignment)), label{std::move(label)}, pos{pos} {}
-};
 void create_console_logger(const std::string &filename = "") {
     using namespace logging;
 
