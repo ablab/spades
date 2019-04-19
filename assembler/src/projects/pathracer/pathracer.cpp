129101ef783219e553f59960dec7ccc26792a0f0
Alexander Shlemov <shlemovalex@gmail.com>
2019-04-19 17:18:10 +0300

    PR: catch exceptions

--- assembler/src/projects/pathracer/pathracer.cpp
+++ assembler/src/projects/pathracer/pathracer.cpp
@@ -4,8 +4,18 @@
 //* See file LICENSE for details.
 //***************************************************************************
 
+#include <exception>
+#include <iostream>
 int pathracer_main(int argc, char* argv[]);
 
 int main(int argc, char* argv[]) {
-    return pathracer_main(argc, argv);
+    try {
+        return pathracer_main(argc, argv);
+    } catch (const std::exception& e) {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cerr << "Unknown object caught" << std::endl;
+        return 1;
+    }
 }
