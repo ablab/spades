578994b9fc822a8f0d1351457e03202176164757
Tatiana Dvorkina <tdvorkina@chihua.cab.spbu.ru>
2019-04-26 23:14:32 +0300

    Add start banner and cmd line

--- assembler/src/projects/spaligner/align_longsequences.cpp
+++ assembler/src/projects/spaligner/align_longsequences.cpp
@@ -264,6 +264,12 @@ int main(int argc, char **argv) {
     ("positional", "", cxxopts::value<string>(cfg));
 
     options.parse_positional("positional");
+    
+    std::string cmd_line = "";
+    for (int i = 0; i < argc; ++ i) {
+        cmd_line += std::string(argv[i]) + " ";
+    }
+
     options.parse(argc, argv);
     if (options.count("help")) {
         cout << options.help() << endl;
@@ -288,6 +294,9 @@ int main(int argc, char **argv) {
     }
 
     create_console_logger();
+    START_BANNER("SPAligner: long sequence to graph alignment");
+    INFO("Command line: " << cmd_line);
+
     INFO("Loading config from " << cfg)
     auto buf = llvm::MemoryBuffer::getFile(cfg);
     VERIFY_MSG(buf, "Failed to load config file " + cfg);
