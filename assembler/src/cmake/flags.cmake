0620a5aa979b2c9cb37d71a05a9ca98f48c42199
Anton Korobeynikov <anton@korobeynikov.info>
2019-04-30 15:25:45 +0300

    Use moder cmake way to set C++ standard

--- assembler/src/cmake/flags.cmake
+++ assembler/src/cmake/flags.cmake
@@ -15,7 +15,7 @@ else ()
   endif()
 endif()
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
+set(CMAKE_CXX_STANDARD 14)
 add_compile_options(-Wno-deprecated)
 
 # Use libc++ with clang due to C++11 mode
