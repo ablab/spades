cd4302aa6f88b4a061dad55fe906770748c6ef40
Anton Korobeynikov <anton@korobeynikov.info>
2019-09-02 20:01:32 +0300

    Allow empty annotation and ambitios symbols in bntseq

--- assembler/ext/src/bwa/bntseq.c
+++ assembler/ext/src/bwa/bntseq.c
@@ -211,11 +211,13 @@ void bns_destroy(bntseq_t *bns)
 	else {
 		int i;
 		if (bns->fp_pac) err_fclose(bns->fp_pac);
-		free(bns->ambs);
-		for (i = 0; i < bns->n_seqs; ++i) {
-			free(bns->anns[i].name);
-			free(bns->anns[i].anno);
-		}
+		if (bns->ambs) free(bns->ambs); /* SPADES_LOCAL */
+        if (bns->anns) { /* SPADES_LOCAL */
+            for (i = 0; i < bns->n_seqs; ++i) {
+                free(bns->anns[i].name);
+                free(bns->anns[i].anno);
+            }
+        }
 		free(bns->anns);
 		free(bns);
 	}
