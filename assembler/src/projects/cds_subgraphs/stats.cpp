65adfd6a21e17f46e63140b054b7d73c8c01b3cd
Sergey Nurk <sergeynurk@gmail.com>
2019-11-13 05:58:54 +0000

    executable for computing statistics from alignment of ground truth CDS

---
+++ assembler/src/projects/cds_subgraphs/stats.cpp
@@ -0,0 +1,207 @@
+//***************************************************************************
+//* Copyright (c) 2019 University of Warwick
+//* All Rights Reserved
+//* See file LICENSE for details.
+//***************************************************************************
+
+#include "toolchain/edge_label_helper.hpp"
+#include "toolchain/utils.hpp"
+#include "io/reads/file_reader.hpp"
+#include "io/binary/graph_pack.hpp"
+#include "modules/alignment/sequence_mapper.hpp"
+#include "utils/segfault_handler.hpp"
+
+#include "version.hpp"
+
+#include <clipp/clipp.h>
+#include <string>
+#include <set>
+#include <numeric>
+#include <sys/types.h>
+
+namespace debruijn_graph {
+
+static void MappingPathStats(const Graph &g, size_t query_kmer_len,
+                             const omnigraph::MappingPath<EdgeId> &mapping_path,
+                             io::EdgeNamingF<Graph> edge_naming_f) {
+    VERIFY(mapping_path.size() > 0);
+    INFO("Mapping path stats");
+    std::stringstream ss;
+    std::string delim = "";
+    ss << "MappingPath ( ";
+    for (size_t i = 0; i < mapping_path.size(); i++) {
+        ss << delim << edge_naming_f(g, mapping_path[i].first) << ": " << mapping_path[i].second;
+        delim = " ; ";
+    }
+    ss << " )";
+    INFO(ss.str());
+
+    size_t curr_pos = 0;
+    for (size_t i = 0; i < mapping_path.size(); i++) {
+        Range q_range = mapping_path[i].second.initial_range;
+        //Range e_range = mapping_path[i].second.mapped_range;
+        if (q_range.start_pos > curr_pos) {
+            INFO("GAP of length " << (q_range.start_pos - curr_pos) << " between " << curr_pos << " and " << q_range.start_pos);
+        }
+        curr_pos = q_range.end_pos;
+    }
+    if (query_kmer_len > curr_pos) {
+        INFO("GAP of length " << (query_kmer_len - curr_pos) << " between " << curr_pos << " and " << query_kmer_len);
+    }
+}
+
+static bool UsedOnBothSides(const Graph &g, EdgeId e, const std::multimap<EdgeId, std::string> &edge_usage) {
+    auto used_f = [&] (EdgeId a) { return edge_usage.count(a) > 0; };
+    return std::any_of(g.out_begin(g.EdgeEnd(e)), g.out_end(g.EdgeEnd(e)), used_f) &&
+            std::any_of(g.in_begin(g.EdgeStart(e)), g.in_end(g.EdgeStart(e)), used_f);
+}
+
+static void Run(const conj_graph_pack &gp, const std::string &contigs_file,
+                const std::string &paths_fn, const std::string &edge_info_fn,
+                const io::EdgeLabelHelper<Graph> &label_helper) {
+
+    io::CanonicalEdgeHelper<Graph> canonical_helper(gp.g, label_helper.edge_naming_f());
+    auto mapper = MapperInstance(gp);
+    ReadPathFinder<Graph> path_finder(gp.g, /*skip_unfixed*/false);
+
+    std::ofstream os;
+    if (!paths_fn.empty())
+        os.open(paths_fn);
+
+    std::multimap<EdgeId, std::string> edge_usage;
+
+    io::FileReadStream reader(contigs_file);
+    io::SingleRead read;
+    while (!reader.eof()) {
+        reader >> read;
+        if (!read.IsValid()) {
+            WARN("Non-ACGT symbols in the " << read.name() << ". Not computing stats");
+            continue;
+        }
+
+        INFO("Aligning " << read.name());
+        os << read.name() << "\n";
+
+        auto mapping_path = mapper->MapRead(read);
+        if (mapping_path.empty()) {
+            WARN("Failed to align " << read.name());
+            continue;
+        }
+
+        MappingPathStats(gp.g, read.sequence().size() - gp.g.k(), mapping_path, label_helper.edge_naming_f());
+        std::string delimeter = "";
+        auto fixed_path = path_finder.FindReadPath(mapping_path);
+        if (!CheckContiguous(gp.g, fixed_path)) {
+            INFO("Were not able to recover continuous path for " << read.name() << " even after fixing attempt");
+        }
+        for (const auto& e : fixed_path) {
+            edge_usage.insert(make_pair(canonical_helper.Canonical(e), read.name()));
+            os << delimeter << canonical_helper.EdgeOrientationString(e);
+            delimeter = ",";
+        }
+        os << "\n";
+    }
+
+    size_t suspicious_edge_cnt = 0;
+    size_t total_suspicious_kmer_len = 0;
+    std::stringstream suspicious_edges;
+    std::ofstream edge_info_os;
+    if (!edge_info_fn.empty())
+        edge_info_os.open(edge_info_fn);
+
+    for (EdgeId e : gp.g.canonical_edges()) {
+        edge_info_os << label_helper.label(e);
+        std::string delimeter = "\t";
+        if (edge_usage.count(e) == 0 && UsedOnBothSides(gp.g, e, edge_usage)) {
+            suspicious_edge_cnt++;
+            suspicious_edges << label_helper.label(e) << ",";
+            total_suspicious_kmer_len += gp.g.length(e);
+        }
+        for (const auto &usage : utils::get_all(edge_usage, e)) {
+            edge_info_os << delimeter << usage;
+        }
+        edge_info_os << "\n";
+    }
+
+    INFO("Suspicious edge cnt: " << suspicious_edge_cnt << "; " <<
+            "total (" << (gp.g.k() + 1) << "-mer) length: " << total_suspicious_kmer_len);
+    INFO("Suspicious edges: " << suspicious_edges.str());
+}
+
+}
+
+struct gcfg {
+    gcfg() : k(0),
+             workdir("./tmp"),
+             nthreads(omp_get_max_threads() / 2 + 1)
+    {}
+
+    unsigned k;
+    std::string graph_path;
+    std::string sequences_fn;
+    std::string paths_fn;
+    std::string edge_info_fn;
+    std::string workdir;
+    unsigned nthreads;
+};
+
+static void process_cmdline(int argc, char **argv, gcfg &cfg) {
+    using namespace clipp;
+
+    auto cli = (
+               (required("-k") & integer("value", cfg.k)) % "k-mer length to use",
+               (required("-g", "--graph") & value("graph", cfg.graph_path)) % "In GFA (ending with .gfa) or prefix to SPAdes graph pack",
+               (required("-q", "--cds-queries") & value("file", cfg.sequences_fn)) % "Path to FASTA file with ground truth CDS sequences",
+               (option("-p", "--paths") & value("file", cfg.paths_fn)) % "Destination for outputting paths corresponding to CDS sequences",
+               (option("-e", "--edge-info") & value("file", cfg.edge_info_fn)) % "Destination for outputting edge usage information",
+               (option("-t", "--threads") & integer("value", cfg.nthreads)) % "# of threads to use (default: max_threads / 2)",
+               (option("--workdir") & value("dir", cfg.workdir)) % "scratch directory to use (default: ./tmp)"
+    );
+
+    auto result = parse(argc, argv, cli);
+    if (!result) {
+        std::cout << make_man_page(cli, argv[0]);
+        exit(1);
+    }
+}
+
+int main(int argc, char** argv) {
+    utils::segfault_handler sh;
+    gcfg cfg;
+
+    process_cmdline(argc, argv, cfg);
+
+    toolchain::create_console_logger();
+    START_BANNER("Analyzing alignment of known CDS sequences to assembly graph or its subgraph");
+
+    try {
+        unsigned k = cfg.k;
+        INFO("K-mer length set to " << k);
+
+        unsigned nthreads = cfg.nthreads;
+        nthreads = std::min(nthreads, (unsigned) omp_get_max_threads());
+        // Inform OpenMP runtime about this :)
+        omp_set_num_threads((int) nthreads);
+        INFO("# of threads to use: " << nthreads);
+
+        fs::make_dirs(cfg.workdir);
+        debruijn_graph::conj_graph_pack gp(k, cfg.workdir, 0);
+
+        omnigraph::GraphElementFinder<Graph> element_finder(gp.g);
+        INFO("Loading de Bruijn graph from " << cfg.graph_path);
+        gp.kmer_mapper.Attach(); // TODO unnecessary
+        io::EdgeLabelHelper<Graph> label_helper(element_finder,
+                toolchain::LoadGraph(gp, cfg.graph_path));
+
+        gp.EnsureBasicMapping();
+
+        debruijn_graph::Run(gp, cfg.sequences_fn, cfg.paths_fn, cfg.edge_info_fn, label_helper);
+        INFO("Done");
+    } catch (const std::string &s) {
+        std::cerr << s << std::endl;
+        return EINTR;
+    } catch (const std::exception &e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        return EINTR;
+    }
+}
