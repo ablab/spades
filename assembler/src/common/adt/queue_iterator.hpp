9b0ac25d2487f4388543b03e7595c6df39ee67ba
Alexander Shlemov <shlemovalex@gmail.com>
2019-12-20 19:34:34 +0300

    SmartIterator: heap-based

--- assembler/src/common/adt/queue_iterator.hpp
+++ assembler/src/common/adt/queue_iterator.hpp
@@ -11,6 +11,9 @@
 #include "utils/verify.hpp"
 #include "btree/btree_set.h"
 #include <set>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
 
 namespace adt {
 
@@ -72,13 +75,147 @@ public:
 
 };
 
+
+template<typename T, typename Comparator>
+class indexed_heap_erasable_priority_queue {
+private:
+    std::vector<T> heap_;
+    std::unordered_map<T, size_t> map_;
+    Comparator cmp_;
+public:
+    /*
+     * Be careful! This constructor requires Comparator to have default constructor even if you call it with
+     * specified comparator. In this case just create default constructor with VERIFY(false) inside it.
+     */
+    indexed_heap_erasable_priority_queue(const Comparator &comparator = Comparator()) :
+        cmp_(comparator) {
+    }
+
+    template<typename InputIterator>
+    indexed_heap_erasable_priority_queue(InputIterator begin, InputIterator end,
+            const Comparator &comparator = Comparator()) :
+        cmp_(comparator) {
+        for (; begin != end; ++begin) {
+            if (map_.insert({*begin, size_t(-1)}).second) {
+                heap_.push(*begin);
+            }
+        }
+        auto rev_cmp = [this](const T &a, const T &b) -> bool { return cmp_(b, a); };
+        std::make_heap(heap_.begin(), heap_.end(), rev_cmp);
+        for (size_t i = 0; i < heap_.size(); ++i) {
+            map_[heap_[i]] = i;
+        }
+        VERIFY(heap_.size() == map_.size());
+    }
+
+    void pop() {
+        VERIFY(!heap_.empty());
+        heap_pop();
+        VERIFY(map_.size() == heap_.size());
+    }
+
+    const T& top() {
+        VERIFY(!heap_.empty());
+        return heap_top();
+    }
+
+    void push(const T &key) {
+        auto it_fl = map_.insert({key, size_t(-1)});
+        if (!it_fl.second) return;
+        it_fl.first->second = heap_.size();
+        heap_.push_back(key);
+        up(heap_.size() - 1);
+        VERIFY(map_.size() == heap_.size());
+    }
+
+    bool erase(const T &key) {
+        auto it = map_.find(key);
+        if (it == map_.end()) return false;
+        size_t i = it->second;
+        heap_[i] = heap_.back();
+        map_.find(heap_[i])->second = i;
+        map_.erase(it);
+        heap_.resize(heap_.size() - 1);
+        if (i == heap_.size()) return true;
+        if (up(i) == i) down(i);
+        VERIFY(map_.size() == heap_.size());
+        return true;
+    }
+
+    void clear() {
+        map_.clear();
+        heap_.clear();
+    }
+
+    bool empty() const {
+        return heap_.empty();
+    }
+
+    size_t size() const {
+        return heap_.size();
+    }
+
+    template <class InputIterator>
+    void insert(InputIterator first, InputIterator last) {
+        for (; first != last; ++first) {
+            push(*first);
+        }
+    }
+
+private:
+    size_t down(size_t i) {
+        T val = heap_[i];
+        while (2 * i + 1 < heap_.size()) {
+            size_t son = 2 * i + 1;
+            size_t other_son = 2 * i + 2;
+            if (other_son < heap_.size() && cmp_(heap_[other_son], heap_[son])) son = other_son;
+            if (cmp_(heap_[son], val)) {
+                heap_[i] = heap_[son];
+                map_[heap_[i]] = i;
+                i = son;
+            } else {
+                break;
+            }
+        }
+        heap_[i] = val;
+        map_[val] = i;
+        return i;
+    }
+
+    size_t up(size_t i) {
+        T val = heap_[i];
+        while (i > 0) {
+            size_t parent = (i - 1) / 2;
+            if (cmp_(val, heap_[parent])) {
+                heap_[i] = heap_[parent];
+                map_[heap_[i]] = i;
+                i = parent;
+            } else {
+                break;
+            }
+        }
+        heap_[i] = val;
+        map_[val] = i;
+        return i;
+    }
+
+    void heap_pop() {
+        erase(heap_top());
+    }
+
+    const T& heap_top() const {
+        return heap_[0];
+    }
+};
+
+
 template<typename T, typename Comparator = std::less<T>>
 class DynamicQueueIterator {
 
     bool current_actual_;
     bool current_deleted_;
     T current_;
-    erasable_priority_queue<T, Comparator> queue_;
+    indexed_heap_erasable_priority_queue<T, Comparator> queue_;
 
 public:
 
