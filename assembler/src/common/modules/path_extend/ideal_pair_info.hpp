24986fd48c5839d247c6b0ef917104e32acd9cbd
Anton Korobeynikov <anton@korobeynikov.info>
2020-05-18 13:51:18 +0300

    Refactor IdealPairInfo

--- assembler/src/common/modules/path_extend/ideal_pair_info.hpp
+++ assembler/src/common/modules/path_extend/ideal_pair_info.hpp
@@ -8,6 +8,9 @@
 #pragma once
 
 #include "assembly_graph/core/graph.hpp"
+#include "adt/flat_map.hpp"
+#include <parallel_hashmap/phmap.h>
+#include <utility>
 #include <vector>
 
 namespace path_extend {
@@ -19,41 +22,36 @@ class IdealPairInfoCounter {
 public:
     IdealPairInfoCounter(const Graph& g, int d_min, int d_max, size_t read_size,
                          const std::map<int, size_t>& is_distribution)
-            : g_(g),
+            : g_(g), k_(g_.k()),
               d_min_(d_min),
               d_max_(d_max),
               read_size_(read_size) {
         size_t sum = 0;
-        for (auto iter = is_distribution.begin(); iter != is_distribution.end();
-                ++iter) {
-            sum += iter->second;
-        }
-        for (auto iter = is_distribution.begin(); iter != is_distribution.end();
-                ++iter) {
-            insert_size_distrib_[iter->first] = (double) iter->second
-                    / (double) sum;
-        }
+        for (const auto &entry : is_distribution)
+            sum += entry.second;
+
+        auto left_bound = is_distribution.lower_bound(std::max(d_min_, 0));
+        auto right_bound = is_distribution.upper_bound(d_max_);
+
+        for (auto it = left_bound; it != right_bound; ++it)
+            insert_size_distrib_[it->first] = double(it->second) / double(sum);
+
         PreCalculateNotTotalReadsWeight();
     }
 
     double IdealPairedInfo(EdgeId e1, EdgeId e2, int dist, bool additive = false) const {
-        std::pair<size_t, size_t> lengths{g_.length(e1), g_.length(e2)};
-        if (pi_.find(lengths) == pi_.end()) {
-            pi_.insert({lengths, {}});
-        }
-        std::map<int, double> &weights = pi_[lengths];
-        if (weights.find(dist) == weights.end()) {
-            weights.emplace(dist, IdealPairedInfo(g_.length(e1), g_.length(e2), dist, additive));
-        }
-        return weights[dist];
+        auto &weights = pi_[std::make_pair(g_.length(e1), g_.length(e2))];
+        auto entry = weights.insert(std::make_pair(dist, 0.));
+        if (entry.second)
+            entry.first->second = IdealPairedInfo(g_.length(e1), g_.length(e2), dist, additive);
+        return entry.first->second;
     }
 
     double IdealPairedInfo(size_t len1, size_t len2, int dist, bool additive = false) const {
         double result = 0.0;
-        for (auto it = insert_size_distrib_.lower_bound(std::max(d_min_, 0));
-                it != insert_size_distrib_.upper_bound(d_max_);
-                ++it) {
-            result += it->second * (double) IdealReads(len1, len2, dist, it->first, additive);
+        for (const auto &entry : insert_size_distrib_) {
+            if (entry.second > 0)
+                result += entry.second * (double) IdealReads(len1, len2, dist, entry.first, additive);
         }
         return result;
     }
@@ -65,18 +63,16 @@ private:
         int len1 = (int) len1_1;
         int len2 = (int) len2_1;
         int is = (int) is_1;
-        int k = (int) g_.k();
+        int k = (int) k_;
         int rs = (int) read_size_;
-        double w = 0.0;
         if (dist == 0) {
             return len1 - is + 2 * rs - 2 - k + 1;
-        }
-        if (dist < 0) {
-            int tmp = len1;
-            len1 = len2;
-            len2 = tmp;
+        } else if (dist < 0) {
+            std::swap(len1, len2);
             dist = -dist;
         }
+
+        double w = 0.0;
         int gap_len = dist - len1;
         int right_long = is - rs - 1;
         int right_short = gap_len + len2 - 1;
@@ -85,12 +81,13 @@ private:
         int right = std::min(right_long, right_short);
         int left = std::max(left_short, left_long);
         int result = std::max(right - left + 1, 0);
-        int right_e2 = std::min(gap_len + len2 - rs + k, right_long);
-        int left_e2 = std::max(left_long, gap_len);
-        int right_not_full = std::max(right - right_e2, 0);
-        int left_not_full = std::max(left_e2 - left, 0);
         w = result;
-        if (additive){
+        if (additive) {
+            int right_e2 = std::min(gap_len + len2 - rs + k, right_long);
+            int left_e2 = std::max(left_long, gap_len);
+            int right_not_full = std::max(right - right_e2, 0);
+            int left_not_full = std::max(left_e2 - left, 0);
+
             w = w - not_total_weights_right_[right_not_full]- not_total_weights_left_[left_not_full];
         }
         return w > 0.0 ? w : 0.0;
@@ -99,8 +96,8 @@ private:
     void PreCalculateNotTotalReadsWeight() {
         not_total_weights_right_.push_back(0.0);
         not_total_weights_left_.push_back(0.0);
-        for (int i = 1; i < int(read_size_) - int(g_.k()) + 1; ++i) {
-            double right = (double(i) + double(g_.k()) /2.0) / (double) read_size_;
+        for (int i = 1; i < int(read_size_) - int(k_) + 1; ++i) {
+            double right = (double(i) + double(k_) /2.0) / (double) read_size_;
             double left = 1 - right;
             not_total_weights_right_.push_back(not_total_weights_right_[i-1] + right);
             not_total_weights_left_.push_back(not_total_weights_left_[i-1] + left);
@@ -108,14 +105,25 @@ private:
     }
 
     const Graph& g_;
-    int d_min_;
-    int d_max_;
+    size_t k_;
+    const int d_min_;
+    const int d_max_;
     size_t read_size_;
-    std::vector<double> weights_;
-    std::map<int, double> insert_size_distrib_;
-    mutable std::map<std::pair<size_t, size_t>, std::map<int, double> > pi_;
+
+    using InsertSizeMap = adt::flat_map<int, double>;
+    InsertSizeMap insert_size_distrib_;
     std::vector<double> not_total_weights_right_;
     std::vector<double> not_total_weights_left_;
+
+    struct PairHash {
+        size_t operator()(std::pair<size_t, size_t> pair) const {
+            return phmap::HashState().combine(0, pair.first, pair.second);
+        }
+    };
+
+    mutable phmap::parallel_node_hash_map<std::pair<size_t, size_t>,
+                                          phmap::flat_hash_map<int, double>,
+                                          PairHash> pi_;
 protected:
     DECL_LOGGER("PathExtendPI");
 };
