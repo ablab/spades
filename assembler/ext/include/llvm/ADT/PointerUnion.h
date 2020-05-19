aed0c8cbde12f100f7cd71ffe54ffb769545640c
Anton Korobeynikov <anton@korobeynikov.info>
2020-05-19 12:06:55 +0300

    Workaround GCC bug

--- assembler/ext/include/llvm/ADT/PointerUnion.h
+++ assembler/ext/include/llvm/ADT/PointerUnion.h
@@ -54,15 +54,30 @@ struct PointerUnionTypeSelectorReturn<
 };
 
 namespace pointer_union_detail {
+  constexpr int constexprMin(int a, int b) { return a < b ? a : b; }
+
   /// Determine the number of bits required to store integers with values < n.
   /// This is ceil(log2(n)).
   constexpr int bitsRequired(unsigned n) {
     return n > 1 ? 1 + bitsRequired((n + 1) / 2) : 0;
   }
 
+// GCC 6.3 is buggy compiling this in debug mode
+#if 0
   template <typename... Ts> constexpr int lowBitsAvailable() {
     return std::min<int>({PointerLikeTypeTraits<Ts>::NumLowBitsAvailable...});
   }
+#else
+  // FIXME: In C++14, replace this with
+  //   std::min({PointerLikeTypeTraits<Ts>::NumLowBitsAvailable...})
+  template <typename T> constexpr int lowBitsAvailable() {
+    return PointerLikeTypeTraits<T>::NumLowBitsAvailable;
+  }
+  template <typename T1, typename T2, typename... Ts>
+  constexpr int lowBitsAvailable() {
+    return constexprMin(lowBitsAvailable<T1>(), lowBitsAvailable<T2, Ts...>());
+  }
+#endif
 
   /// Find the index of a type in a list of types. TypeIndex<T, Us...>::Index
   /// is the index of T in Us, or sizeof...(Us) if T does not appear in the
