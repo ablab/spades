3a682c2020ac16b8addefa9c991b63e3cc68586a
Anton Korobeynikov <anton@korobeynikov.info>
2020-06-08 18:51:50 +0300

    Reimplement nucl* functions for speed

--- assembler/src/common/sequence/nucl.hpp
+++ assembler/src/common/sequence/nucl.hpp
@@ -36,18 +36,23 @@ static const char INVALID_NUCL = char(-1);
  * @param char c
  * @return true if c is 'A/a/0', 'C/c/1', 'G/g/2', 'T/t/3'.
  */
+
+#define likely(x)       __builtin_expect((x),1)
+#define unlikely(x)     __builtin_expect((x),0)
+
+// WARNING: These functions were carefully crafted for speed
+// Change only if you know what you're doing
 inline bool is_nucl(char c) {
+    if (unlikely(c>= 0 && c < 4))
+        return true;
+
     switch (c) {
-        case 0:
         case 'a':
         case 'A':
-        case 1:
         case 'c':
         case 'C':
-        case 2:
         case 'g':
         case 'G':
-        case 3:
         case 't':
         case 'T':
             return true;
@@ -62,27 +67,22 @@ inline bool is_nucl(char c) {
  * @return complement symbol, i.e. 'A/a/0' => 'T/t/3', 'C/c/1' => 'G/g/2', 'G/g/2' => 'C/c/1', 'T/t/3' => 'A/a/0', 'N' => 'N'
  */
 inline char nucl_complement(char c) {
+    if (unlikely(c>= 0 && c < 4))
+        return complement(c);
+
     switch (c) {
-        case 0:
-            return 3;
         case 'a':
             return 't';
         case 'A':
             return 'T';
-        case 1:
-            return 2;
         case 'c':
             return 'g';
         case 'C':
             return 'G';
-        case 2:
-            return 1;
         case 'g':
             return 'c';
         case 'G':
             return 'C';
-        case 3:
-            return 0;
         case 't':
             return 'a';
         case 'T':
@@ -103,27 +103,13 @@ inline char nucl_complement(char c) {
  * @return 'A/a/0' => 'A', 'C/c/1' => 'C', 'G/g/2' => 'G', 'T/t/3' => 'T'
  */
 inline char nucl(char c) {
-    switch (c) {
-        case 0:
-        case 'a':
-        case 'A':
-            return 'A';
-        case 1:
-        case 'c':
-        case 'C':
-            return 'C';
-        case 2:
-        case 'g':
-        case 'G':
-            return 'G';
-        case 3:
-        case 't':
-        case 'T':
-            return 'T';
-        default:
-            VERIFY_DEV(false);
-            return INVALID_NUCL;
-    }
+    if (likely(c >= 0 && c < 4)) {
+        const uint32_t nucl_map = 'A' + ('C' << 8) + ('G' << 16) + ('T' << 24);
+        return (nucl_map >> (8*c)) & 0xFF;
+    } else if ('A' <= c && c <= 'T')
+        return c;
+
+    return c - 'a' + 'A';
 }
 
 /**
@@ -132,27 +118,18 @@ inline char nucl(char c) {
  * @return A => 0, C => 1, G => 2, T => 3
  */
 inline char dignucl(char c) {
-    switch (c) {
-        case 0:
-        case 'a':
-        case 'A':
-            return 0;
-        case 1:
-        case 'c':
-        case 'C':
-            return 1;
-        case 2:
-        case 'g':
-        case 'G':
-            return 2;
-        case 3:
-        case 't':
-        case 'T':
-            return 3;
-        default:
-            VERIFY_DEV(false);
-            return INVALID_NUCL;
+    if (unlikely(c>= 0 && c < 4)) {
+        return c;
+    } else if (unlikely('a' <= c && c <= 't')) {
+        c = c - 'a' + 'A';
     }
+
+    return (c <= 'C' ?
+            (c == 'A' ? 0 : 1) :
+            (c == 'G' ? 2 : 3));
 }
+
+#undef likely
+#undef unlikely
 
 #endif /* NUCL_HPP_ */
