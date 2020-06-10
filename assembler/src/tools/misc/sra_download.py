41a156ec4de7d243b78422414ab263baf5571d33
Dmitry Antipov <dmitrij.antipov@gmail.com>
2020-06-11 00:49:09 +0300

    moved from aspera dowloader to prefetch-fastqdump

--- assembler/src/tools/misc/sra_download.py
+++ assembler/src/tools/misc/sra_download.py
@@ -22,11 +22,9 @@ from genericpath import isdir, exists
 
 from os.path import join
 
-import socket
-socket.setdefaulttimeout(20.0)
-from urllib2 import urlopen
-import xml.etree.ElementTree as ET
-import urllib
+#Do not use sra tools from chihua! it's outdated and bugged
+sra_tools = "/home/dantipov/other_tools/sratoolkit.2.10.7-ubuntu64/bin/"
+
 
 def download_fastq():
     dirs = os.listdir(main_dir)
@@ -39,6 +37,8 @@ def sra_id_to_sra_path(sra_id):
     res = "anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra/"+ sra_id[:3] +"/" + sra_id[:6] + "/"+ sra_id+ "/" + sra_id + ".sra"
     return res
 
+
+#OUTDATED
 def download_one_fastq(main_dir, sra_id):
     aspera_string = sra_id_to_sra_path(sra_id.split()[0].strip())
     if exists(join(main_dir, sra_id+"_1.fastq")):
@@ -52,23 +52,38 @@ def download_one_fastq(main_dir, sra_id):
     return
 
 
+def download_sample(main_dir,sra_id):
+    if exists(join(main_dir, sra_id+"_1.fastq")):
+        print sra_id + " already downloaded"
+        return
+    pr_line = join(sra_tools, "prefetch") +" --max-size 40000000 " + sra_id
+#    print pr_line
+    print ('Downloading ' + sra_id)   
+    os.system(pr_line)
+    fq_dump_line = join(sra_tools, "fastq-dump") + " --gzip --split-files " + sra_id + " -O " + main_dir
+    os.system(fq_dump_line)
+    return
+
 
 
 def download_sra_from_list(sra_id_list):
     download_dir = sys.argv[2]
     sra_file = open(sra_id_list, 'r')
     for line in sra_file:
-        download_one_fastq(download_dir, line.strip())
+        download_sample(download_dir, line.strip())
 
 
 def main():
-    if len(sys.argv) != 3:
-        print "Usage: %s <list with SRA ids> <dir to download>" % sys.argv[0]
+    if len(sys.argv) < 3:
+        print "Usage: %s <list with SRA ids> <dir to download> [path to sra_tools installation]" % sys.argv[0]
         sys.exit()
+    if len(sys.argv) >= 4:
+        sra_tools = sys.argv[3]
     sra_id_list = sys.argv[1]
     download_sra_from_list(sra_id_list)
 
 if __name__ == '__main__':
     main()
+
 
 
