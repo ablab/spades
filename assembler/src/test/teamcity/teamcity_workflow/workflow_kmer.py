bd1fe5c4066c32f99cb92f2e48c816e210bd38e9
Olga Kunyavskaya <Oche24912@yandex.ru>
2020-06-15 12:20:56 +0300

    check kmers

--- assembler/src/test/teamcity/teamcity_workflow/workflow_kmer.py
+++ assembler/src/test/teamcity/teamcity_workflow/workflow_kmer.py
@@ -10,10 +10,41 @@
 # provide a path to .yaml file with test description
 
 import workflow_base
+from workflow_base import log
+import os
+import yaml
+
+
+def get_kmer_list(path):
+    run_spades_yaml = os.path.join(path, "run_spades.yaml")
+    stages = yaml.load(open(run_spades_yaml))
+    kmers = []
+    for stage in stages:
+        stage_name = stage['STAGE']
+        if stage_name[0] == 'K' and stage_name[1:].isdigit():
+            kmers.append(stage_name)
+    return [0]
 
 
 def check_kmer_set(dataset_info, test, output_dir):
-    return 0
+    if 'etalon_saves' in dataset_info:
+        log.log("Checking the correctness of K")
+        etalon_folder = dataset_info["etalon_saves"]
+        if ("name" in test):
+            etalon_folder += test["name"]
+
+        out_klist = get_kmer_list(output_dir)
+        log.log("Output kmers: " + str(out_klist))
+
+        etalon_klist = get_kmer_list(os.path.join(etalon_folder))
+        log.log("Etalon kmers: " + str(etalon_klist))
+
+        if out_klist != etalon_klist:
+            log.err("Kmers in output(" + str(out_klist) + ") != kmers in etalon(" + str(etalon_klist) + ")")
+            return 12
+
+    log.err("Etalon folder wasn't set in test config!")
+    return 12
 
 
 workflow_base.main(check_test=check_kmer_set)
