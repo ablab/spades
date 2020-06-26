18cc4c6015518095cee5d606a1831d0a011b0e14
Andrey Prjibelski <andrewprzh@gmail.com>
2020-06-26 16:22:30 +0300

    improve manual conversion script, add version

--- assembler/src/tools/misc/markdown_to_github_html.py
+++ assembler/src/tools/misc/markdown_to_github_html.py
@@ -4,21 +4,23 @@ import codecs
 from traceback import print_exc
 
 # GitHub style CSS for code sections
-HEADER = '<html> \
-<head> \
-    <title>SPAdes 3.13.1 Manual</title> \
-    <style type="text/css"> \
-        .highlight  pre { \
-          background-color: #f0f2f4; \
-          border-radius: 2px; \
-          font-size: 100%; \
-          line-height: 1.45; \
-          overflow: auto; \
-          padding: 16px; \
-        } \
-    </style> \
-</head> \
-<body>'
+def get_header(version):
+    header = '<html> \
+    <head> \
+        <title>SPAdes ' + version + ' Manual</title> \
+        <style type="text/css"> \
+            .highlight  pre { \
+              background-color: #f0f2f4; \
+              border-radius: 2px; \
+              font-size: 100%; \
+              line-height: 1.45; \
+              overflow: auto; \
+              padding: 16px; \
+            } \
+        </style> \
+    </head> \
+    <body>' 
+    return header
 
 # Extra spaces in the end
 FOOTER = '<br/><br/><br/><br/><br/> \
@@ -42,19 +44,26 @@ def make_github_request(in_file_name):
     return x.text
 
 # Substitute conversion artifacts and dump to file (non-ascii)
-def write_html_to_file(txt, out_file_name):
-    final_html = HEADER + txt.replace("<br><br>", "<br>").replace('<a name="user-content-', '<a name="').replace('<a href="assembler/', '<a href="').replace('<p><strong>SPAdes', ' <meta charset="UTF-8"> <p><strong>SPAdes') + FOOTER
+def write_html_to_file(txt, out_file_name, version):
+    final_html = get_header(version) + txt.replace("<br><br>", "<br>").replace('<a name="user-content-', '<a name="').replace('<a href="assembler/', '<a href="').replace('<p><strong>SPAdes', ' <meta charset="UTF-8"> <p><strong>SPAdes') + FOOTER
     outf = codecs.open(sys.argv[2], 'w', encoding='utf-8')
     outf.write(final_html)
+    outf.write('\n')
     outf.close()
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: " + sys.argv[0] + " <input.md> <output.html>")
+    if len(sys.argv) < 3:
+        print("Usage: " + sys.argv[0] + " <input.md> <output.html> [version | read from ./VERSION if not set]")
         exit(0)
 
-    write_html_to_file(make_github_request(sys.argv[1]), sys.argv[2])
+    if len(sys.argv) == 4:
+        version = sys.argv[3]
+    else:
+        f = open("./VERSION")
+        version = f.readline().strip()
+
+    write_html_to_file(make_github_request(sys.argv[1]), sys.argv[2], version)
 
 
 if __name__ == "__main__":
