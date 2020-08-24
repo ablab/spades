77db01bdd0f1030f8dac725c0c201a5d5fb4b256
uncerso <yayayayaya38@gmail.com>
2020-08-24 14:59:51 +0000

    Fixed licence header

--- assembler/spades_compile.sh
+++ assembler/spades_compile.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ############################################################################
-# Copyright (c) 2015 Saint Petersburg State University
-# Copyright (c) 2011-2020 Saint Petersburg Academic University
+# Copyright (c) 2015-2020 Saint Petersburg State University
+# Copyright (c) 2011-2014 Saint Petersburg Academic University
 # All Rights Reserved
 # See file LICENSE for details.
 ############################################################################
