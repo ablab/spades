80c2671b2ac53fa737f2e2ee33e5640c8d97a8b2
Anton Korobeynikov <anton@korobeynikov.info>
2020-09-23 23:57:57 +0300

    Compute sizes in parallel

--- assembler/src/common/assembly_graph/index/edge_index_builders.hpp
+++ assembler/src/common/assembly_graph/index/edge_index_builders.hpp
@@ -166,12 +166,15 @@ public:
 
             INFO("buckets: " << buckets_.size());
 
+            sizes_.resize(buckets_.size());
+#           pragma omp parallel for
             for (size_t i = 0; i < buckets_.size(); ++i) {
                 size_t sz = std::distance(bucket_begin(i), bucket_end(i));
                 INFO("sz: " << sz);
-                sizes_.push_back(sz);
-                total_kmers_ += sz;
+                sizes_[i] = sz;
             }
+            for (size_t sz : sizes_)
+                total_kmers_ += sz;
         }
 
         unsigned k() const { return k_; }
