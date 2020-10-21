33b38496e02c756459bca8959b8318c9e99c88b9
Anton Korobeynikov <anton@korobeynikov.info>
2020-10-21 22:29:10 +0300

    Switch to gtest

--- assembler/src/test/adt/phm_test.cpp
+++ assembler/src/test/adt/phm_test.cpp
@@ -1,16 +1,18 @@
-#define BOOST_TEST_MODULE phm_test
-#include <boost/test/included/unit_test.hpp>
-
+#include "utils/logger/logger.hpp"
+#include "utils/logger/log_writers.hpp"
+#include "utils/stl_utils.hpp"
 #include "boomphf/BooPHF.h"
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include "utils/stl_utils.hpp"
 
 #define XXH_INLINE_ALL
 #include "xxh/xxhash.h"
 
-struct F {
+#include <gtest/gtest.h>
+
+struct PHMTest : public ::testing::Test {
     class Hasher {
       public:
         std::pair<uint64_t, uint64_t> operator()(uint64_t key) const {
@@ -21,46 +23,61 @@ struct F {
     
     using phm = boomphf::mphf<Hasher>;
     
-    F() {
-        BOOST_TEST_MESSAGE("setup fixture");
+    PHMTest() {
+        INFO("setup fixture");
 
-        for (size_t i = 0; i < 100500; ++i)
+        for (size_t i = 0; i < 10005000; ++i)
             vals_.push_back(i);
 
         phm_.init(vals_.size());
         phm_.build(boomphf::range(vals_.begin(), vals_.end()));
 
-        BOOST_TEST_MESSAGE("PHM built. Size: " << phm_.size() << ", mem size:" << phm_.mem_size() << ", collision prob: " << phm_.prob_collision());
+        INFO("PHM built. Size: " << phm_.size() << ", mem size:" << phm_.mem_size() << ", collision prob: " << phm_.prob_collision());
     }
-    ~F() {
-        BOOST_TEST_MESSAGE( "teardown fixture" );
+    ~PHMTest() {
+        INFO("teardown fixture");
     }
 
     phm phm_;
     std::vector<uint64_t> vals_;
 };
 
-BOOST_FIXTURE_TEST_CASE(empty_test, F) {
-  BOOST_CHECK(phm_.size() > 0);
+TEST_F(PHMTest, empty_test) {
+    ASSERT_GT(phm_.size(), 0);
 }
 
-BOOST_FIXTURE_TEST_CASE(id_test, F) {
+TEST_F(PHMTest, id_test) {
     for (uint64_t entry : vals_) {
         uint64_t idx = phm_.lookup(entry);
         uint64_t invalid = phm::NOT_FOUND;
-        BOOST_CHECK_NE(idx, invalid);
-        BOOST_CHECK_LT(idx, phm_.size());
+        EXPECT_NE(idx, invalid);
+        EXPECT_LT(idx, phm_.size());
     }
 }
 
-BOOST_FIXTURE_TEST_CASE(collision_test, F) {
+TEST_F(PHMTest, collision_test) {
     std::vector<bool> marks(vals_.size());
     for (uint64_t entry : vals_) {
         uint64_t idx = phm_.lookup(entry);
         uint64_t invalid = phm::NOT_FOUND;
-        BOOST_CHECK_NE(idx, invalid);
-        BOOST_CHECK_LT(idx, phm_.size());
-        BOOST_CHECK_NE(marks[idx], true);
+        EXPECT_NE(idx, invalid);
+        EXPECT_LT(idx, phm_.size());
+        EXPECT_NE(marks[idx], true);
         marks[idx] = true;
     }
+}
+
+void create_console_logger() {
+    using namespace logging;
+
+    logger *lg = create_logger("");
+    lg->add_writer(std::make_shared<console_writer>());
+    attach_logger(lg);
+}
+
+GTEST_API_ int main(int argc, char **argv) {
+  create_console_logger();
+
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
