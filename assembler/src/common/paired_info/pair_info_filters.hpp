18da13f6051149e0a62fe1b3ef6d47ef6f5e0085
Dmitry <dmitrij.antipov@gmail.com>
2021-02-08 14:02:54 +0300

    fix for no paired info

--- assembler/src/common/paired_info/pair_info_filters.hpp
+++ assembler/src/common/paired_info/pair_info_filters.hpp
@@ -239,7 +239,7 @@ public:
     {}
 
     void Filter(PairedInfoIndexT<Graph>& index) {
-        INFO("Start filtering; index size: " << index.size());
+        INFO("Start filtering; library index size: " << index.size());
         //We can't filter while traversing, because Remove may invalidate iterators
         //So let's save edge pairs first
         using EdgePair = std::pair<EdgeId, EdgeId>;
@@ -260,7 +260,7 @@ public:
                 index.Remove(pair.first, pair.second, point);
         }
 
-        INFO("Done filtering");
+        INFO("Done filtering; library index size: " << index.size());
     }
 };
 
