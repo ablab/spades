0f5c4611be0f2c0f12faaf1ef9e47bc375a61ff3
Anton Korobeynikov <anton@korobeynikov.info>
2021-03-15 15:59:04 +0300

    Add barebone bin refiner

---
+++ assembler/src/projects/bin_refine/binning.cpp
@@ -0,0 +1,111 @@
+//***************************************************************************
+//* Copyright (c) 2021 Saint Petersburg State University
+//* All Rights Reserved
+//* See file LICENSE for details.
+//***************************************************************************
+
+#include "binning.hpp"
+
+#include "modules/alignment/sequence_mapper.hpp"
+
+#include "io/reads/io_helper.hpp"
+#include "utils/filesystem/path_helper.hpp"
+
+using namespace debruijn_graph;
+using namespace bin_stats;
+
+const std::string BinStats::SCAFFOLD_NAME_PREFIX = "NODE_";
+const std::string BinStats::UNBINNED_ID = "0";
+
+
+void BinStats::ScaffoldsToEdges(const std::string& scaffolds_file,
+                                const debruijn_graph::GraphPack &gp) {
+  fs::CheckFileExistenceFATAL(scaffolds_file);
+
+  edges_binning_.clear();
+  unbinned_edges_.clear();
+  auto mapper = MapperInstance(gp);
+
+  io::SingleStream scaffolds_stream = io::EasyStream(scaffolds_file, false);
+  io::SingleRead scaffold;
+
+  while (!scaffolds_stream.eof()) {
+    scaffolds_stream >> scaffold;
+    const std::string scaffold_name = scaffold.name();
+    if (!scaffolds_binning_.count(scaffold_name)) {
+      DEBUG("No such scaffold " + scaffold_name + " with name " + scaffold_name);
+      continue;
+    }
+    
+    const bin_id_t bin_id = scaffolds_binning_[scaffold_name];
+    const auto scaffold_path = mapper->MapRead(scaffold);
+
+    for (size_t i = 0; i < scaffold_path.size(); ++i) {
+      const EdgeId edge = scaffold_path[i].first;
+      edges_binning_[edge].insert(bin_id);
+      edges_binning_[graph_.conjugate(edge)].insert(bin_id);
+    }
+  }
+
+  // find unbinned edges
+  for (EdgeId edge : graph_.edges()) {
+    if (edges_binning_.count(edge))
+      continue;
+
+    unbinned_edges_.insert(edge);
+    unbinned_edges_.insert(graph_.conjugate(edge));
+  }
+}
+
+void BinStats::LoadBinning(const std::string& binning_file, const std::string& scaffolds_file,
+                           const debruijn_graph::GraphPack &gp) {
+  fs::CheckFileExistenceFATAL(binning_file);
+
+  scaffolds_binning_.clear();
+  bins_.clear();
+  std::ifstream binning_reader(binning_file);
+
+  for (std::string line; std::getline(binning_reader, line, '\n');) {
+    std::string scaffold_name;
+    std::istringstream line_stream(line);
+    line_stream >> scaffold_name;
+    bin_id_t bin_id;
+    line_stream >> bin_id;
+    if (bin_id == UNBINNED_ID) { // unbinned scaffold, skip
+      continue;
+    }
+
+    bins_.insert(bin_id);
+
+    scaffolds_binning_[scaffold_name] = bin_id;
+  }
+
+  ScaffoldsToEdges(scaffolds_file, gp);
+}
+
+namespace bin_stats {
+std::ostream &operator<<(std::ostream &os, const BinStats &stats) {
+    const auto &graph = stats.graph();
+
+    os << "Total bins: " << stats.bins().size() << std::endl;
+    size_t sum_length = 0;
+    size_t unbinned_length = 0;
+    for (const EdgeId e : graph.edges()) {
+      const size_t length = graph.length(e);
+      sum_length += length;
+      if (stats.unbinned_edges().count(e)) {
+        unbinned_length += length;
+      }
+    }
+    
+    os << "Total edges: " << graph.e_size() << std::endl
+       << "Total edges length: " << sum_length << std::endl
+       << "Total binned edges: " << stats.edges_binning().size() << std::endl
+       << "Unbinned edges: " << stats.unbinned_edges().size() << std::endl
+       << "Unbinned edges total length: " << unbinned_length << std::endl
+       << "Unbinned edges number ratio: " << static_cast<double>(stats.unbinned_edges().size()) / static_cast<double>(graph.e_size()) << std::endl
+       << "Unbinned edges length ratio: " << static_cast<double>(unbinned_length) / static_cast<double>(sum_length) << std::endl;
+
+    return os;
+}
+}
---
+++ assembler/src/projects/bin_refine/binning.hpp
@@ -0,0 +1,58 @@
+//***************************************************************************
+//* Copyright (c) 2021 Saint Petersburg State University
+//* All Rights Reserved
+//* See file LICENSE for details.
+//***************************************************************************
+
+#include "assembly_graph/core/graph.hpp"
+#include "pipeline/graph_pack.hpp"
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+
+namespace debruijn_graph {
+class GraphPack;
+}
+
+namespace bin_stats {
+
+class BinStats {
+    using bin_id_t = std::string;
+    using scaffold_id_t = std::string;
+    using edge_binning_t = std::set<bin_id_t>;
+
+
+    void ScaffoldsToEdges(const std::string& scaffolds_file,
+                          const debruijn_graph::GraphPack &gp);
+
+    static const std::string SCAFFOLD_NAME_PREFIX;
+    static const std::string UNBINNED_ID;
+
+ public:
+    explicit BinStats(const debruijn_graph::Graph& g)
+            : graph_(g) {}
+
+    /// binning file in .tsv format (NODE_{scaffold_id}_* -> bin_id); scaffolds_file in .fasta format
+    /// FIXME: Scaffolds should not be necessary
+    void LoadBinning(const std::string& binning_file, const std::string& scaffolds_file,
+                     const debruijn_graph::GraphPack &gp);
+
+    const debruijn_graph::Graph& graph() const { return graph_;  }
+
+    const auto& edges_binning() const { return edges_binning_; }
+    auto& edges_binning() { return edges_binning_; }
+
+    const auto& unbinned_edges() const { return unbinned_edges_; }
+    auto& unbinned_edges() { return unbinned_edges_; }
+
+    const auto& bins() const { return bins_; }
+
+    friend std::ostream &operator<<(std::ostream &os, const BinStats &stats);
+private:
+    const debruijn_graph::Graph& graph_;
+    std::unordered_map<scaffold_id_t, bin_id_t> scaffolds_binning_{};
+    std::unordered_map<debruijn_graph::EdgeId, edge_binning_t> edges_binning_{};
+    std::unordered_set<debruijn_graph::EdgeId> unbinned_edges_{};
+    std::unordered_set<bin_id_t> bins_{};
+};
+}
