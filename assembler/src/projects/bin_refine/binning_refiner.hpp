5b1ee68704acc204e7f7041b94c70a03cd1a3fbf
Anton Korobeynikov <anton@korobeynikov.info>
2021-04-07 10:40:07 +0300

    Add virtual dtor

--- assembler/src/projects/bin_refine/binning_refiner.hpp
+++ assembler/src/projects/bin_refine/binning_refiner.hpp
@@ -13,6 +13,7 @@ namespace bin_stats {
 class BinningRefiner {
  public:
     explicit BinningRefiner(const debruijn_graph::Graph& g) : g_(g) {}
+    virtual ~BinningRefiner() = default;
 
     virtual SoftBinsAssignment RefineBinning(const BinStats& bin_stats) const = 0;
  protected:
