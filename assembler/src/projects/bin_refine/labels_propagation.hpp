44d5c7932e5f2684c7e301f4fdf9e16cfe1b2a64
Damtev <damtev@yandex.ru>
2021-04-16 16:31:37 +0300

    United corrector and propagator to one; removed matrices

--- assembler/src/projects/bin_refine/labels_propagation.hpp
+++ assembler/src/projects/bin_refine/labels_propagation.hpp
@@ -11,13 +11,23 @@
 
 #include "id_map.hpp"
 
+#define USE_LENGTH_AND_MULTIPLICITY 0
+
 namespace bin_stats {
+
+struct CorrectionParameters {
+    const double labeled_alpha;
+    const double unlabeled_alpha;
+
+    CorrectionParameters(const double labeled_alpha, const double unlabeled_alpha)
+        : labeled_alpha(labeled_alpha), unlabeled_alpha(unlabeled_alpha) {}
+};
 
 class LabelsPropagation : public BinningRefiner {
  public:
     using FinalIteration = bool;
 
-    LabelsPropagation(const debruijn_graph::Graph& g, double eps);
+    LabelsPropagation(const debruijn_graph::Graph& g, double eps, std::unique_ptr<CorrectionParameters> correction_parameters = nullptr);
 
     SoftBinsAssignment RefineBinning(const BinStats& bin_stats) const override;
 
@@ -28,11 +38,21 @@ class LabelsPropagation : public BinningRefiner {
 
     FinalIteration PropagationIteration(SoftBinsAssignment& new_state,
                                         const SoftBinsAssignment& cur_state,
+                                        const std::shared_ptr<SoftBinsAssignment>& origin_state,
+    #ifdef USE_LENGTH_AND_MULTIPLICITY
+                                        const BinStats& bin_stats,
+    #endif
                                         unsigned iteration_step) const;
 
     const double eps_;
 
     adt::id_map<double, debruijn_graph::EdgeId> rdeg_;
     adt::id_map<double, debruijn_graph::EdgeId> rweight_;
+
+    #ifdef USE_LENGTH_AND_MULTIPLICITY
+    size_t max_edge_length_;
+    #endif
+
+    std::unique_ptr<CorrectionParameters> correction_parameters_;
 };
 }
