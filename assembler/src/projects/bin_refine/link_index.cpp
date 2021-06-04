ae1c992be88c67d2ba6667ec268f683a6062f6aa
Anton Korobeynikov <anton@korobeynikov.info>
2021-06-04 15:19:12 +0300

    Add possibility to dump link index

--- assembler/src/projects/bin_refine/link_index.cpp
+++ assembler/src/projects/bin_refine/link_index.cpp
@@ -5,6 +5,7 @@
 //***************************************************************************
 
 #include "link_index.hpp"
+#include "io/utils/id_mapper.hpp"
 
 using namespace binning;
 
@@ -15,6 +16,19 @@ void GraphLinkIndex::Init(const debruijn_graph::Graph &g) {
 
         for (EdgeId i : g_.IncomingEdges(g.EdgeStart(e)))
             add(e, i);
+    }
+}
+
+void LinkIndex::dump(const std::string &output_path, const io::IdMapper<std::string> &edge_mapper) {
+    std::ofstream os(output_path);
+
+    os << "FirstId\tFirstLength\tFirstCov\tSecondId\tSecondLength\tSecondCov\tWeight\n";
+    for (const EdgeId &edge: g_.canonical_edges()) {
+        for (const auto &neighbour: data_.at(edge)) {
+            EdgeId neigh = neighbour.e;
+            os << edge_mapper[edge.int_id()] << "\t" << g_.length(edge) + g_.k() - 1 << "\t" << g_.coverage(edge) << "\t"
+               << edge_mapper[neigh.int_id()] << "\t" << g_.length(neigh) + g_.k() - 1 << "\t" << g_.coverage(neigh) << "\t" << neighbour.w << "\n";
+        }
     }
 }
 
