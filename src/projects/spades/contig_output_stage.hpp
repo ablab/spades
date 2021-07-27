b6dbdc11773f308e4a9b875d4c846a033ce49cfc
Olga Kunyavsksya <oche24912@yandex.ru>
2021-07-27 20:14:05 +0300

    constat for contig output stage

--- src/projects/spades/contig_output_stage.hpp
+++ src/projects/spades/contig_output_stage.hpp
@@ -33,6 +33,7 @@ public:
     void save(const graph_pack::GraphPack &, const std::filesystem::path &, const char *) const override { }
     void run(graph_pack::GraphPack &gp, const char *) override;
 
+    bool constant() const override { return true; }
 private:
     OutputList outputs_;
 };
