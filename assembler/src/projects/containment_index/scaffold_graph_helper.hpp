e6aadc2c607fa4325e6c9d5a978c5401e7071c25
Itolstoganov <itolstoganov@gmail.com>
2021-09-06 15:18:50 +0300

    Path storage extractor

--- assembler/src/projects/containment_index/scaffold_graph_helper.hpp
+++ assembler/src/projects/containment_index/scaffold_graph_helper.hpp
@@ -20,13 +20,23 @@ class LinkIndexGraphConstructor {
     using Graph = debruijn_graph::Graph;
     using BarcodeExtractorPtr = std::shared_ptr<barcode_index::FrameBarcodeIndexInfoExtractor>;
 
-    LinkIndexGraphConstructor(const Graph &g, BarcodeExtractorPtr barcode_extractor, size_t max_threads);
+    LinkIndexGraphConstructor(const Graph &g,
+                              BarcodeExtractorPtr barcode_extractor,
+                              const double graph_score_threshold,
+                              const size_t tail_threshold,
+                              const size_t length_threshold,
+                              const size_t count_threshold,
+                              size_t max_threads);
 
     scaffold_graph::ScaffoldGraph ConstructGraph() const;
 
   private:
     const debruijn_graph::Graph &g_;
     BarcodeExtractorPtr barcode_extractor_;
+    const double graph_score_threshold_;
+    const size_t tail_threshold_;
+    const size_t length_threshold_;
+    const size_t count_threshold_;
     size_t max_threads_;
 };
 
