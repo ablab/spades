1c5f7cf68a64a8fa97d6fff8fc498701042e40ed
Olga Kunyavsksya <oche24912@yandex.ru>
2021-10-12 12:23:08 +0300

    separate distEst

--- src/common/paired_info/distance_estimation_utils.hpp
+++ src/common/paired_info/distance_estimation_utils.hpp
@@ -10,6 +10,7 @@
 #include "distance_estimation.hpp"
 #include "paired_info.hpp"
 #include "pair_info_filters.hpp"
+#include "smoothing_distance_estimation.hpp"
 
 #include "library/library.hpp"
 #include "library/library_data.hpp"
@@ -17,30 +18,114 @@
 #include "configs/distance_estimation.hpp"
 
 namespace distance_estimation {
-using omnigraph::de::AbstractDistanceEstimator;
-using omnigraph::de::AbstractPairInfoChecker;
-using omnigraph::de::PairedInfoIndexT;
-using omnigraph::de::UnclusteredPairedInfoIndexT;
+    using omnigraph::de::AbstractDistanceEstimator;
+    using omnigraph::de::AbstractPairInfoChecker;
+    using omnigraph::de::PairedInfoIndexT;
+    using omnigraph::de::UnclusteredPairedInfoIndexT;
 
-void EstimateWithEstimator(PairedInfoIndexT<debruijn_graph::Graph> &clustered_index,
-                           const AbstractDistanceEstimator &estimator);
+    class AbstractDistanceEstimatorFabric {
+    public:
+        typedef AbstractPairInfoChecker<debruijn_graph::Graph> PairInfoChecker;
 
-void RefinePairedInfo(PairedInfoIndexT<debruijn_graph::Graph>& clustered_index,
-                      const debruijn_graph::Graph& graph);
+        virtual std::unique_ptr<omnigraph::de::DistanceEstimator> getDistanceEstimator(const debruijn_graph::Graph &graph,
+                                                                      const distance_estimation::UnclusteredPairedInfoIndexT<debruijn_graph::Graph> &index,
+                                                                      const omnigraph::de::GraphDistanceFinder &distance_finder,
+                                                                      const PairInfoChecker &checker,
+                                                                      size_t linkage_distance,
+                                                                      size_t max_distance) const = 0;
+    };
 
-void EstimateScaffoldingDistances(PairedInfoIndexT<debruijn_graph::Graph> &scaffolding_index,
-                                  const debruijn_graph::Graph &graph,
-                                  const io::SequencingLibrary<debruijn_graph::config::LibraryData> &lib,
-                                  const UnclusteredPairedInfoIndexT<debruijn_graph::Graph> &paired_index,
-                                  const debruijn_graph::config::smoothing_distance_estimator &ade,
-                                  const debruijn_graph::config::distance_estimator &de_config =
-                                  debruijn_graph::config::distance_estimator());
+    class DistanceEstimatorFabric : public AbstractDistanceEstimatorFabric {
+    public:
+        std::unique_ptr<omnigraph::de::DistanceEstimator> getDistanceEstimator(const debruijn_graph::Graph &graph,
+                                                              const distance_estimation::UnclusteredPairedInfoIndexT<debruijn_graph::Graph> &index,
+                                                              const omnigraph::de::GraphDistanceFinder &distance_finder,
+                                                              const PairInfoChecker &checker,
+                                                              size_t linkage_distance,
+                                                              size_t max_distance) const override {
+            return std::make_unique<omnigraph::de::DistanceEstimator>(graph, index, distance_finder, checker, linkage_distance,
+                                                                      max_distance);
+        }
+    };
 
-void EstimatePairedDistances(PairedInfoIndexT<debruijn_graph::Graph> &clustered_index,
-                             const debruijn_graph::Graph &graph,
-                             const io::SequencingLibrary<debruijn_graph::config::LibraryData> &lib,
-                             const UnclusteredPairedInfoIndexT<debruijn_graph::Graph> &paired_index,
-                             size_t max_repeat_length = std::numeric_limits<size_t>::max(),
-                             const debruijn_graph::config::distance_estimator &de_config =
-                             debruijn_graph::config::distance_estimator());
+    class AbstractScaffoldDistanceEstimatorFabric {
+    public:
+        typedef AbstractPairInfoChecker<debruijn_graph::Graph> PairInfoChecker;
+
+        virtual std::unique_ptr<omnigraph::de::DistanceEstimator> getDistanceEstimator(const debruijn_graph::Graph &graph,
+                                                                      const distance_estimation::UnclusteredPairedInfoIndexT<debruijn_graph::Graph> &histogram,
+                                                                      const omnigraph::de::GraphDistanceFinder &dist_finder,
+                                                                      const PairInfoChecker &checker,
+                                                                      std::function<double(int)> weight_f,
+                                                                      size_t linkage_distance, size_t max_distance, size_t threshold,
+                                                                      double range_coeff, double delta_coeff,
+                                                                      size_t cutoff,
+                                                                      size_t min_peak_points,
+                                                                      double percentage,
+                                                                      double derivative_threshold) const = 0;
+    };
+
+    class ScaffoldDistanceEstimatorFabric : public AbstractScaffoldDistanceEstimatorFabric {
+    public:
+        std::unique_ptr<omnigraph::de::DistanceEstimator> getDistanceEstimator(const debruijn_graph::Graph &graph,
+                                                              const distance_estimation::UnclusteredPairedInfoIndexT<debruijn_graph::Graph> &histogram,
+                                                              const omnigraph::de::GraphDistanceFinder &dist_finder,
+                                                              const PairInfoChecker &checker,
+                                                              std::function<double(int)> weight_f,
+                                                              size_t linkage_distance, size_t max_distance, size_t threshold,
+                                                              double range_coeff, double delta_coeff,
+                                                              size_t cutoff,
+                                                              size_t min_peak_points,
+                                                              double percentage,
+                                                              double derivative_threshold) const override {
+            return std::unique_ptr<omnigraph::de::DistanceEstimator>(
+                    new omnigraph::de::SmoothingDistanceEstimator(graph, histogram, dist_finder, checker, weight_f,
+                                                                  linkage_distance, max_distance, threshold,
+                                                                  range_coeff, delta_coeff, cutoff, min_peak_points,
+                                                                  percentage, derivative_threshold));
+        }
+    };
+
+    void EstimateWithEstimator(PairedInfoIndexT<debruijn_graph::Graph> &clustered_index,
+                               const AbstractDistanceEstimator &estimator,
+                               AbstractPairInfoChecker<debruijn_graph::Graph> &checker);
+
+    void RefinePairedInfo(PairedInfoIndexT<debruijn_graph::Graph> &clustered_index,
+                          const debruijn_graph::Graph &graph);
+
+    void EstimateScaffoldingDistancesInner(PairedInfoIndexT<debruijn_graph::Graph> &scaffolding_index,
+                                           const debruijn_graph::Graph &graph,
+                                           const io::SequencingLibrary<debruijn_graph::config::LibraryData> &lib,
+                                           const UnclusteredPairedInfoIndexT<debruijn_graph::Graph> &paired_index,
+                                           const debruijn_graph::config::smoothing_distance_estimator &ade,
+                                           const debruijn_graph::config::distance_estimator &de_config =
+                                           debruijn_graph::config::distance_estimator(),
+                                           const AbstractScaffoldDistanceEstimatorFabric& distance_estimator_fabric =
+                                                   ScaffoldDistanceEstimatorFabric());
+
+    void EstimatePairedDistancesInner(PairedInfoIndexT<debruijn_graph::Graph> &clustered_index,
+                                      const debruijn_graph::Graph &graph,
+                                      const io::SequencingLibrary<debruijn_graph::config::LibraryData> &lib,
+                                      const UnclusteredPairedInfoIndexT<debruijn_graph::Graph> &paired_index,
+                                      size_t max_repeat_length = std::numeric_limits<size_t>::max(),
+                                      const debruijn_graph::config::distance_estimator &de_config =
+                                      debruijn_graph::config::distance_estimator(),
+                                      const AbstractDistanceEstimatorFabric& distance_estimator_fabric =
+                                              DistanceEstimatorFabric());
+
+    void EstimateScaffoldingDistances(PairedInfoIndexT<debruijn_graph::Graph> &scaffolding_index,
+                                           const debruijn_graph::Graph &graph,
+                                           const io::SequencingLibrary<debruijn_graph::config::LibraryData> &lib,
+                                           const UnclusteredPairedInfoIndexT<debruijn_graph::Graph> &paired_index,
+                                           const debruijn_graph::config::smoothing_distance_estimator &ade,
+                                           const debruijn_graph::config::distance_estimator &de_config =
+                                           debruijn_graph::config::distance_estimator());
+
+    void EstimatePairedDistances(PairedInfoIndexT<debruijn_graph::Graph> &clustered_index,
+                                      const debruijn_graph::Graph &graph,
+                                      const io::SequencingLibrary<debruijn_graph::config::LibraryData> &lib,
+                                      const UnclusteredPairedInfoIndexT<debruijn_graph::Graph> &paired_index,
+                                      size_t max_repeat_length = std::numeric_limits<size_t>::max(),
+                                      const debruijn_graph::config::distance_estimator &de_config =
+                                      debruijn_graph::config::distance_estimator());
 }
---
+++ src/projects/hpcspades/common/paired_info/distance_estimation.cpp
@@ -0,0 +1,33 @@
+//***************************************************************************
+//* Copyright (c) 2021 Saint Petersburg State University
+//* All Rights Reserved
+//* See file LICENSE for details.
+//***************************************************************************
+
+#include "distance_estimation.hpp"
+
+namespace omnigraph {
+    namespace de {
+        using namespace debruijn_graph;
+
+        void DistanceEstimatorMPI::Estimate(OutPairedIndex &result, size_t nthreads) const {
+            this->Init();
+            const auto &index = this->index();
+
+            DEBUG("Collecting edge infos");
+            std::vector<EdgeId> edges;
+            for (EdgeId e : this->graph().edges())
+                edges.push_back(e);
+
+            partask::TaskRegistry treg;
+            auto dist_estimator_mpi = treg.add<DistanceEstimatorTask>(std::cref(index), std::cref(*dist_estimator_), std::ref(result));
+            treg.listen();
+
+            if (partask::master()) {
+                dist_estimator_mpi(edges, nthreads);
+            }
+            treg.stop_listening();
+            partask::broadcast(result);
+        }
+    }
+}
