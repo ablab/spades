26067c33cda386b61dba0455985a9d0bc7c263f3
Anton Korobeynikov <anton@korobeynikov.info>
2021-11-16 00:28:59 +0300

    Try to unbreak

--- assembler/ext/include/bamtools/api/algorithms/Sort.h
+++ assembler/ext/include/bamtools/api/algorithms/Sort.h
@@ -76,7 +76,7 @@ struct API_EXPORT Sort {
         { }
 
         // comparison function
-        bool operator()(const BamTools::BamAlignment& lhs, const BamTools::BamAlignment& rhs) {
+        bool operator()(const BamTools::BamAlignment& lhs, const BamTools::BamAlignment& rhs) const {
             return sort_helper(m_order, lhs.Name, rhs.Name);
         }
 
@@ -112,7 +112,7 @@ struct API_EXPORT Sort {
         { }
 
         // comparison function
-        bool operator()(const BamTools::BamAlignment& lhs, const BamTools::BamAlignment& rhs) {
+        bool operator()(const BamTools::BamAlignment& lhs, const BamTools::BamAlignment& rhs) const {
 
             // force unmapped aligmnents to end
             if ( lhs.RefID == -1 ) return false;
@@ -161,7 +161,7 @@ struct API_EXPORT Sort {
         { }
 
         // comparison function
-        bool operator()(const BamTools::BamAlignment& lhs, const BamTools::BamAlignment& rhs) {
+        bool operator()(const BamTools::BamAlignment& lhs, const BamTools::BamAlignment& rhs) const {
 
             // force alignments without tag to end
             T lhsTagValue;
@@ -196,7 +196,7 @@ struct API_EXPORT Sort {
     struct Unsorted : public AlignmentSortBase {
 
         // comparison function
-        inline bool operator()(const BamTools::BamAlignment&, const BamTools::BamAlignment&) {
+        inline bool operator()(const BamTools::BamAlignment&, const BamTools::BamAlignment&) const {
             return false;   // returning false tends to retain insertion order
         }
 
