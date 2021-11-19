37653931e5cd54630f3215646993d6d85f374702
Olga Kunyavsksya <oche24912@yandex.ru>
2021-11-19 15:01:04 +0300

    update distEst Arhitecture

--- src/projects/hpcspades/distance_estimation_mpi.hpp
+++ src/projects/hpcspades/distance_estimation_mpi.hpp
@@ -7,31 +7,11 @@
 
 #pragma once
 
-#include "pipeline/mpi_stage.hpp"
-#include "assembly_graph/core/graph.hpp"
-#include "paired_info/paired_info.hpp"
-#include "pipeline/stage.hpp"
-#include <projects/spades/distance_estimation.hpp>
+#include "common/pipeline/mpi_stage.hpp"
+#include "projects/spades/distance_estimation.hpp"
 
 namespace debruijn_graph {
-    class DistanceEstimationInnerMPI : public DistanceEstimationInner {
-    protected:
-        void runEstimatePairedDistances(omnigraph::de::PairedInfoIndexT<Graph> &clustered_index,
-                                               const Graph &graph,
-                                               const io::SequencingLibrary<debruijn_graph::config::LibraryData> &lib,
-                                               const omnigraph::de::UnclusteredPairedInfoIndexT<debruijn_graph::Graph> &paired_index,
-                                               size_t max_repeat_length,
-                                               const debruijn_graph::config::distance_estimator &de_config) override;
-
-        void runEstimateScaffoldingDistances(omnigraph::de::PairedInfoIndexT<debruijn_graph::Graph> &scaffolding_index,
-                                                    const Graph &graph,
-                                                    const io::SequencingLibrary<debruijn_graph::config::LibraryData> &lib,
-                                                    const omnigraph::de::UnclusteredPairedInfoIndexT<Graph> &paired_index,
-                                                    const debruijn_graph::config::smoothing_distance_estimator &ade,
-                                                    const debruijn_graph::config::distance_estimator &de_config) override;
-    };
-
-    class DistanceEstimationMPI : public spades::MPIAssemblyStage {
+    class DistanceEstimationMPI : public DistanceEstimationBase, public spades::MPIAssemblyStage {
     public:
         DistanceEstimationMPI(bool preliminary = false)
                 : MPIAssemblyStage(preliminary ? "Preliminary Distance Estimation" : "Distance Estimation",
@@ -40,3 +20,4 @@ namespace debruijn_graph {
         void run(graph_pack::GraphPack &gp, const char *) override;
     };
 }
+
