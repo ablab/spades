702cf7998dcdea39895bef0abbdc726d2a132a15
Olga Kunyavsksya <oche24912@yandex.ru>
2021-11-23 14:40:32 +0300

    GapCloserBase

--- src/projects/spades/gap_closer.hpp
+++ src/projects/spades/gap_closer.hpp
@@ -7,15 +7,35 @@
 //***************************************************************************
 
 #pragma once
+
+#include "alignment/sequence_mapper_notifier.hpp"
 #include "pipeline/mpi_stage.hpp"
 #include "pipeline/stage.hpp"
+#include "io/reads/io_helper.hpp"
 
 namespace debruijn_graph {
 
-class GapClosing : public spades::MPIAssemblyStage {
+class GapClosingBase {
+  protected:
+    size_t num_readers = 0;
+    virtual void processLibrary(SequenceMapperListener* listener, const SequenceMapper<Graph>& mapper, io::BinaryPairedStreams& paired_streams) = 0;
+  public:
+    void execute(graph_pack::GraphPack &gp, const char *);
+};
+
+class GapClosing : public GapClosingBase, public spades::MPIAssemblyStage {
+  protected:
+    void processLibrary(SequenceMapperListener* listener, const SequenceMapper<Graph>& mapper, io::BinaryPairedStreams& paired_streams) override {
+        SequenceMapperNotifierMPI notifier;
+        notifier.Subscribe(listener);
+        notifier.ProcessLibrary(paired_streams, mapper);
+    }
+
   public:
     GapClosing(const char* id)
-        : MPIAssemblyStage("Gap Closer (parmap)", id) {}
+            : MPIAssemblyStage("Gap Closer (parmap)", id) {
+        num_readers = partask::overall_num_threads();
+    }
 
     void run(graph_pack::GraphPack &gp, const char*) override;
 };
