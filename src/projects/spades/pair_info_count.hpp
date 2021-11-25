4c93e61dddde4cf4cc8f615a3199ae4fefadadcb
Olga Kunyavsksya <oche24912@yandex.ru>
2021-11-25 12:06:17 +0300

    separate PairInfoCount MPI

--- src/projects/spades/pair_info_count.hpp
+++ src/projects/spades/pair_info_count.hpp
@@ -8,15 +8,33 @@
 
 #pragma once
 
+#include "alignment/sequence_mapper_notifier.hpp"
 #include "pipeline/stage.hpp"
-#include "pipeline/mpi_stage.hpp"
 
 namespace debruijn_graph {
+class PairInfoCountBase {
+public:
+    typedef std::function<void(debruijn_graph::SequenceMapperListener *,
+                               const debruijn_graph::SequenceMapper<debruijn_graph::Graph> &,
+                               io::ReadStreamList<io::PairedReadSeq> &streams)> MapPairLibFuncT;
 
-class PairInfoCount : public spades::MPIAssemblyStage {
+    typedef std::function<void(const std::vector<debruijn_graph::SequenceMapperListener *>&,
+                               const debruijn_graph::SequenceMapper<debruijn_graph::Graph> &,
+                               io::ReadStreamList<io::SingleReadSeq> &streams)> MapSingleSeqLibFuncT;
+
+    typedef std::function<void(const std::vector<debruijn_graph::SequenceMapperListener *>&,
+                               const debruijn_graph::SequenceMapper<debruijn_graph::Graph> &,
+                               io::ReadStreamList<io::SingleRead> &streams)> MapSingleLibFuncT;
+
+    void execute(graph_pack::GraphPack &gp, const char *, const MapPairLibFuncT&,
+                 const MapSingleSeqLibFuncT&,
+                 const MapSingleLibFuncT&, size_t num_readers=0);
+};
+
+class PairInfoCount : public PairInfoCountBase, public spades::AssemblyStage {
  public:
     PairInfoCount(bool preliminary = false)
-        : MPIAssemblyStage(preliminary ? "Preliminary Paired Information Counting" : "Paired Information Counting",
+        : AssemblyStage(preliminary ? "Preliminary Paired Information Counting" : "Paired Information Counting",
                            preliminary ? "late_pair_info_count_preliminary" : "late_pair_info_count") {}
 
     void run(graph_pack::GraphPack &gp, const char*) override;
