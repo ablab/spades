65952a89ffebe2e2c7a7185ea722e1ad14eadc3c
Olga Kunyavsksya <oche24912@yandex.ru>
2021-11-30 22:05:54 +0300

    move mpi_kmer_index_builder to mpi dir

--- src/common/kmer_index/ph_map/perfect_hash_map_builder.hpp
+++ src/common/kmer_index/ph_map/perfect_hash_map_builder.hpp
@@ -11,7 +11,7 @@
 #include "kmer_maps.hpp"
 #include "cqf_hash_map.hpp"
 #include "kmer_index/kmer_mph/kmer_index_builder.hpp"
-#include "kmer_index/kmer_mph/kmer_index_builder_mpi.hpp"
+#include "projects/hpcspades/common/kmer_index/kmer_mph/kmer_index_builder_mpi.hpp"
 #include "utils/perf/timetracer.hpp"
 
 namespace kmers {
