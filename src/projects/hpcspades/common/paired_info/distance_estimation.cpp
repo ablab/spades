db9db375bda8109b8cfedbcb1f5d67cacbdf36cd
Olga Kunyavsksya <oche24912@yandex.ru>
2021-12-02 23:38:42 +0300

    namespace spaces

--- src/projects/hpcspades/common/paired_info/distance_estimation.cpp
+++ src/projects/hpcspades/common/paired_info/distance_estimation.cpp
@@ -7,27 +7,28 @@
 #include "distance_estimation.hpp"
 
 namespace omnigraph {
-    namespace de {
-        using namespace debruijn_graph;
+namespace de {
+using namespace debruijn_graph;
 
-        void DistanceEstimatorMPI::Estimate(OutPairedIndex &result, size_t nthreads) const {
-            this->Init();
-            const auto &index = this->index();
+void DistanceEstimatorMPI::Estimate(OutPairedIndex &result, size_t nthreads) const {
+    this->Init();
+    const auto &index = this->index();
 
-            DEBUG("Collecting edge infos");
-            std::vector<EdgeId> edges;
-            for (EdgeId e : this->graph().edges())
-                edges.push_back(e);
+    DEBUG("Collecting edge infos");
+    std::vector<EdgeId> edges;
+    for (EdgeId e : this->graph().edges())
+        edges.push_back(e);
 
-            partask::TaskRegistry treg;
-            auto dist_estimator_mpi = treg.add<DistanceEstimatorTask>(std::cref(index), std::cref(*dist_estimator_), std::ref(result));
-            treg.listen();
+    partask::TaskRegistry treg;
+    auto dist_estimator_mpi = treg.add<DistanceEstimatorTask>(std::cref(index), std::cref(*dist_estimator_),
+                                                              std::ref(result));
+    treg.listen();
 
-            if (partask::master()) {
-                dist_estimator_mpi(edges, nthreads);
-            }
-            treg.stop_listening();
-            partask::broadcast(result);
-        }
+    if (partask::master()) {
+        dist_estimator_mpi(edges, nthreads);
     }
+    treg.stop_listening();
+    partask::broadcast(result);
+}
+}
 }
