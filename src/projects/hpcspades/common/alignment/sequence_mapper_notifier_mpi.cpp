3cb8a973588b1a96b5399760676bed7ae8f49ce1
Olga Kunyavsksya <oche24912@yandex.ru>
2021-12-02 23:38:42 +0300

    namespace spaces

--- src/projects/hpcspades/common/alignment/sequence_mapper_notifier_mpi.cpp
+++ src/projects/hpcspades/common/alignment/sequence_mapper_notifier_mpi.cpp
@@ -9,26 +9,26 @@
 #include "io/reads/read_stream_vector.hpp"
 
 namespace debruijn_graph {
-    void SequenceMapperNotifierMPI::PyramidMergeMPI(SequenceMapperListener &listener) {
-        size_t mpi_size = partask::world_size();
-        size_t mpi_rank = partask::world_rank();
-        const size_t deadbeef = 0xDEADBEEF;
+void SequenceMapperNotifierMPI::PyramidMergeMPI(SequenceMapperListener &listener) {
+    size_t mpi_size = partask::world_size();
+    size_t mpi_rank = partask::world_rank();
+    const size_t deadbeef = 0xDEADBEEF;
 
-        for (size_t step = 1; step < mpi_size; step *= 2) {
-            if ((mpi_rank % (2*step) == 0) && (mpi_rank + step < mpi_size)) {
-                partask::InputMPIStream is(mpi_rank + step);
-                size_t sz;
-                io::binary::BinRead(is, sz);
-                VERIFY_MSG(sz == deadbeef, "Listener type: " << typeid(listener).name());
-                listener.MergeFromStream(is);
-                io::binary::BinRead(is, sz);
-                VERIFY_MSG(sz == deadbeef, "Listener type: " << typeid(listener).name());
-            } else if (mpi_rank % (2*step) == step) {
-                partask::OutputMPIStream os(mpi_rank - step);
-                io::binary::BinWrite(os, deadbeef);
-                listener.Serialize(os);
-                io::binary::BinWrite(os, deadbeef);
-            }
+    for (size_t step = 1; step < mpi_size; step *= 2) {
+        if ((mpi_rank % (2 * step) == 0) && (mpi_rank + step < mpi_size)) {
+            partask::InputMPIStream is(mpi_rank + step);
+            size_t sz;
+            io::binary::BinRead(is, sz);
+            VERIFY_MSG(sz == deadbeef, "Listener type: " << typeid(listener).name());
+            listener.MergeFromStream(is);
+            io::binary::BinRead(is, sz);
+            VERIFY_MSG(sz == deadbeef, "Listener type: " << typeid(listener).name());
+        } else if (mpi_rank % (2 * step) == step) {
+            partask::OutputMPIStream os(mpi_rank - step);
+            io::binary::BinWrite(os, deadbeef);
+            listener.Serialize(os);
+            io::binary::BinWrite(os, deadbeef);
         }
     }
+}
 } // namespace debruijn_graph
