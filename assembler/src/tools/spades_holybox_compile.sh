4dfc8abddf93fefd9f24e45756ac7f8e995b0c45
Andrey Prjibelski <andrewprzh@gmail.com>
2022-02-01 14:36:35 +0300

    update spades_holybox_compile.sh

--- assembler/src/tools/spades_holybox_compile.sh
+++ assembler/src/tools/spades_holybox_compile.sh
@@ -2,12 +2,8 @@
 
 # Script for compiling SPAdes using holy build box
 # To compile spades run once:
-# udocker load -i /home/akorobeynikov/spades-buildbox.tar
-# udocker create --name=buildbox cab/spades-buildbox6-64:2.0.0-a4
-# cd /tmp
-# curl -LO https://github.com/Kitware/CMake/releases/download/v3.19.2/cmake-3.19.2-Linux-x86_64.tar.gz
-# tar zxvf cmake-3.19.2-Linux-x86_64.tar.gz
-# udocker run  --user=$(id -u) --volume=/tmp/:/temp buildbox cp -rv /temp/cmake-3.19.2-Linux-x86_64 /hbb/share/cmake-3.19
+# udocker load -i /Sid/prjbel/ablab.holy-build-box-x64.latest.tar
+# udocker create --name=ablab-buildbox ghcr.io/ablab/holy-build-box-x64
 # Then run:
 # udocker run  --user=$(id -u) --volume=<path to SPAdes repository>:/spades --cpuset-cpus=16 buildbox bash /spades/assembler/src/tools/spades_holybox_compile.sh
 
@@ -18,7 +14,9 @@ export PATH=/hbb/share/cmake-3.19/bin/:$PATH
 set -x
 
 cd /spades/assembler
-./spades_compile.sh -DSPADES_STATIC_BUILD=1
-
+#./spades_compile.sh -DSPADES_STATIC_BUILD=1
+mkdir -p build_spades
 cd build_spades
+cmake ../src -DSPADES_STATIC_BUILD=ON -DZLIB_ROOT=/hbb_exe -DOpenMP_pthread_LIBRARY=“-lpthread” -DBZIP2_LIBRARIES=/hbb_exe/lib/libbz2.a -DBZIP2_INCLUDE_DIR=/hbb_exe/include
+make -j 8
 make package
