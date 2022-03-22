6441d03f8be7bf3338f54f10f38d8cdbece5279d
Olga Kunyavsksya <oche24912@yandex.ru>
2022-03-22 14:25:46 +0300

    PEP8 fixs

--- assembler/src/spades_pipeline/support.py
+++ assembler/src/spades_pipeline/support.py
@@ -949,13 +949,14 @@ def is_int(value):
 
 # shutil.copyfile does not copy any metadata (time and permission), so one
 # cannot expect preserve_mode = False and preserve_times = True to work.
-def copy_tree(src, dst, preserve_times = True, preserve_mode = True):
+def copy_tree(src, dst, preserve_times=True, preserve_mode=True):
     if sys.version.split()[0][0] == '2':
         from distutils import dir_util
+        dir_util._path_created = {}  # see http://stackoverflow.com/questions/9160227/dir-util-copy-tree-fails-after-shutil-rmtree
         dir_util.copy_tree(src, dst, preserve_times=preserve_times, preserve_mode=preserve_mode)
         return
 
-    if preserve_mode == False:
+    if not preserve_mode:
         copy_fn = shutil.copyfile
     else:
         copy_fn = shutil.copy2
@@ -966,8 +967,9 @@ def copy_tree(src, dst, preserve_times = True, preserve_mode = True):
     # shutil.copytree preserves the timestamp, so we must update it afterwards.
     shutil.copytree(src, dst, copy_function = copy_fn)
 
-    if preserve_times == False:
+    if not preserve_times:
         for dirpath, _, filenames in os.walk(dst):
             os.utime(dirpath, None)
             for file in filenames:
                 os.utime(os.path.join(dirpath, file), None)
+                
