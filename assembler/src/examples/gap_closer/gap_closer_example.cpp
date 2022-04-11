17c56f1aad3bff2b4a60548427bf7d810c54a866
aberdichevskaya <aberdichevskaya@chihua.cab.spbu.ru>
2022-04-11 17:44:36 +0300

    documentation

--- assembler/src/examples/gap_closer/gap_closer_example.cpp
+++ assembler/src/examples/gap_closer/gap_closer_example.cpp
@@ -10,6 +10,7 @@
 #include <clipp/clipp.h>
 #include <gtest/gtest.h>
 
+//namespace examples {
 
 void AssertEdges(const debruijn_graph::Graph& g1, const debruijn_graph::Graph& g2) {
     /*
@@ -36,12 +37,8 @@ void AssertEdges(const debruijn_graph::Graph& g1, const debruijn_graph::Graph& g
     }
 }
 
-void load_and_save_graph_pack(const std::filesystem::path& saves,
-                              const std::filesystem::path& work_dir = "tmp_dir",
-                              const std::filesystem::path& output_dir = "tmp") {
-    //инструкцию писать из расчета на то, что я хотела бы прочитать, когда начинала это всё
-
-
+void GraphPackBasedExample(const std::filesystem::path& saves, const std::filesystem::path& work_dir,
+                              const std::filesystem::path& output_dir) {
     /*
      * GraphPack is a data structure, which stores information about assembly de Bruijn graph. It is declared
      * in common/pipeline/graph_pack.hpp file.
@@ -52,117 +49,122 @@ void load_and_save_graph_pack(const std::filesystem::path& saves,
      * where T is a name of data structure in which this information is stored.
      *
      * GraphPack has 3 obligatory arguments: size_t k - the length of reads, std::filesystem::path work_dir -
-     * some directory, which GraphPack uses during the runtime, and size_t lib_count (which is what?).
+     * some directory, which GraphPack uses during the runtime, and size_t lib_count.
      *
-     * todo: it would be useful to write about option arguments of graph pack constructor
+     * TODO: it would be useful to write about option arguments of graph pack constructor
      */
-
-    std::filesystem::path load_from = saves / "K55/saves";
-    std::filesystem::path binary_reads_dir = saves / ".bin_reads";
-
     create_directories(work_dir);
     graph_pack::GraphPack gp(55, work_dir, 1); //std::vector<std::string>(0), 55, 0, 0, true);
-    INFO("created graph pack");
+    INFO("Graph pack instance is created");
 
     debruijn_graph::GapClosing gapClosingStage("gap_closer_example");
-    INFO("created gap closing stage");
+    INFO("Gap closing stage instance is created");
 
-/*
- * todo: write why it is necessary here to create binary_reads_dir
- */
+   /*
+    * It is necessary to create <saves>/.bin_reads directory in which converted binary reads will be written during
+    * the loading of the assembly graph. Without this directory binary reads would be converted, but would not be
+    * saved, because of what stage running would fall.
+    */
+    std::filesystem::path binary_reads_dir = saves / ".bin_reads";
     create_directory(binary_reads_dir);
+
+    std::filesystem::path load_from = saves / "K55/saves";
     gapClosingStage.load(gp, load_from, "ec_threshold_finder");
-    INFO("graph is loaded");
-    //вывести какую-то инфу (размер графа, зааттачены ли индексы, что индекс какой-нибудь не пустой)
+    INFO("Graph pack is loaded from saves");
+    //TODO: print some information about the graph (size, are indexes are attached, are indexes are empty)
 
     gapClosingStage.run(gp, "");
-    INFO("gap closing finished");
+    INFO("Gap closing stage is finished");
 
     remove_all(output_dir);
     create_directories(output_dir);
 
     gapClosingStage.save(gp, output_dir, "gap_closer");
-    INFO("graph is saved");
+    INFO("Graph is saved to " << output_dir / "gap_closer");
 
     const auto& g1 = gp.get<debruijn_graph::Graph>();
+
     gapClosingStage.load(gp, load_from, "early_gapcloser");
     const auto& g2 = gp.get<debruijn_graph::Graph>();
+    INFO("The reference graph is loaded");
+
     AssertEdges(g1, g2);
+    INFO("Edges of the graphs are equal");
 
     remove_all(work_dir);
-    INFO("edges of graphs are equal");
 }
+// TODO: GraphBasedExample - load and save the graph itself (from .grseq file)
 
-void load_and_save_graph() {
-    //загрузить и выгрузить граф (.grseq) проверить, что норм это происходит
-    //можно загружать/выгружать сам граф, а не графпак
-    //сделать ещё загрузку не через stage, а просто граф
-}
+//} // namespace examples
 
-void create_console_logger(const std::filesystem::path& log_fn) {
-/*
- * To track the process of program execution it is useful to create a console logger.
- * In SPAdes logger is declared in common/utils/logger/logger.hpp file and logger writer is declared
- * in common/utils/logger/log_writters.hpp. To use logger in the project common/utils/logger/log_writters.hpp
- * should be included (in this case common/utils/logger/logger.hpp is including transitive) and link
- * utils library in CMake file.
- *
- * Logs will be written in log_fn file. (а собственно где этот файл?). In case if log_fn is an empty path,
- * (а что собственно случится тогда?).
- *
- * After logger creation it is convenient to use TRACE, DEBUG, INFO, WARN and ERROR macro.
- */
+void CreateConsoleLogger(const std::filesystem::path& log_fn="") {
+   /*
+    * To track the process of program execution it is useful to create a console logger.
+    * In SPAdes logger is declared in common/utils/logger/logger.hpp file and logger writer is declared
+    * in common/utils/logger/log_writters.hpp. To use logger in the project common/utils/logger/log_writters.hpp
+    * should be included (in this case common/utils/logger/logger.hpp is including transitive) and link
+    * utils library in CMake file.
+    *
+    * After logger creation it is convenient to use TRACE, DEBUG, INFO, WARN and ERROR macro.
+    *
+    * TODO: add logger file
+    */
     using namespace logging;
     logger *lg = create_logger(exists(log_fn) ? log_fn : "");
     lg->add_writer(std::make_shared<console_writer>());
     attach_logger(lg);
 }
 
-void parse_command_line(int argc, char *argv[], std::filesystem::path& saves, std::filesystem::path& tmp_dir,
-                        std::filesystem::path& out_dir, std::filesystem::path& log) {
+void ParseCommandLine(int argc, char *argv[], std::filesystem::path& saves, std::filesystem::path& tmp_dir,
+                        std::filesystem::path& out_dir) {
     using namespace clipp;
-    std::string saves_, tmp_dir_, out_dir_, log_;
+    std::string saves_, tmp_dir_, out_dir_;
     auto cli = (
         option("-s", "--saves") & value("dir", saves_),
         option("--tmp_dir") & opt_value("dir", tmp_dir_),
-        option("-o", "--out_dir") & opt_value("dir", out_dir_),
-        option("-l", "--log") & opt_value("file", log_)
+        option("-o", "--out_dir") & opt_value("dir", out_dir_)
     );
     parse(argc, argv, cli);
 
     saves = saves_;
-    tmp_dir = tmp_dir_;
-    out_dir = out_dir_;
-    log = log_;
+    tmp_dir = tmp_dir_.empty() ? "tmp_dir" : tmp_dir_;
+    out_dir = out_dir_.empty() ? "tmp" : out_dir_;
 }
 
 int main(int argc, char *argv[]) {
-/*
- * This example shows how to use one of the SPAdes stages (in this case it is Gap Closer stage)
- * separately from the SPAdes pipeline.
- *
- * to do: write what command user must run before using this example, and why --checkpoints all is mandatory here
- * it is not necessary
- * ./spades.py --tmp-dir tmp_dir  --sc --checkpoints all --only-assembler -m 20 -k 55 -1 /Nancy/teamcity/build_configurations/pipeline_tests/data/SAVES_ECOLI_UCSD_L1/ecoli_mda_lane1_left.00.0_0.cor.fastq.gz -2 /Nancy/teamcity/build_configurations/pipeline_tests/data/SAVES_ECOLI_UCSD_L1/ecoli_mda_lane1_right.00.0_0.cor.fastq.gz -s /Nancy/teamcity/build_configurations/pipeline_tests/data/SAVES_ECOLI_UCSD_L1/ecoli_mda_lane1__unpaired.00.0_0.cor.fastq.gz -o tmp
- *
- * to do: describe what should be on argv
- */
+   /*
+    * This example shows how to use one of the SPAdes stages (in this case it is Gap Closer stage)
+    * separately from the SPAdes pipeline.
+    *
+    * Before starting of this example user should run SPAdes to have saved versions of assembly graph after
+    * every stage of assembling. For example, "./spades.py --tmp-dir <some temporary directory>  --sc
+    * --checkpoints all --only-assembler -m 20 -k 55 -1 <file with forward paired-end reads> -2
+    * <file with reverse paired-end reads> -s <file with unpaired reads> -o <output directory>".
+    *
+    * The "--checkpoints all" option guarantees that a state of the assembly graph after each of the SPAdes stages
+    * will be saved in the <output directory>/K55/saves directory.
+    *
+    * Usage of this example: gap_closer_example -s <output directory of the preliminary command> [--tmp_dir
+    * <temporary dir for graph pack> -o <output dir>].
+    */
 
-    std::filesystem::path saves, tmp_dir, out_dir, log;
-    parse_command_line(argc, argv, saves, tmp_dir, out_dir, log);
+    std::filesystem::path saves, tmp_dir, out_dir;
+    ParseCommandLine(argc, argv, saves, tmp_dir, out_dir);
 
-    create_console_logger(log);
+    CreateConsoleLogger();
     INFO("Start of the example");
 
     /*
-     * to do: write about configs, how they appeared, and why it is nessesary to load them (why exactly?)
+     * Config are obligatory to make an example work correctly. Config files, necessary for the SPAdes stage,
+     * are generated during the preliminary command run and written to
+     * <output directory of the preliminary command>/saves/K55/configs. Before the start of the example user should
+     * create an instance of the config based on these config files.
      */
     std::vector<std::filesystem::path> cfg_fns = {
             saves / "K55/configs/config.info", saves / "K55/configs/mda_mode.info"
     };
     cfg::create_instance(cfg_fns);
 
-    load_and_save_graph_pack(saves, tmp_dir, out_dir);
-
+    GraphPackBasedExample(saves, tmp_dir, out_dir);
     return 0;
 }
