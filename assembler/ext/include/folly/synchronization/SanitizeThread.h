d0ebf648fd0e8f4a905e58ef7962475f5d636d60
Anton Korobeynikov <anton@korobeynikov.info>
2023-01-10 11:16:25 +0000

    Unbreak debug build: explicitly disable tsan in folly

--- assembler/ext/include/folly/synchronization/SanitizeThread.h
+++ assembler/ext/include/folly/synchronization/SanitizeThread.h
@@ -27,43 +27,21 @@ enum class annotate_rwlock_level : long {
 
 namespace detail {
 
-using annotate_rwlock_cd_t = void(char const*, int, void const volatile*);
-using annotate_rwlock_ar_t = void(char const*, int, void const volatile*, long);
-using annotate_benign_race_sized_t =
-    void(char const*, int, void const volatile*, long, char const*);
-using annotate_ignore_t = void(char const*, int);
-
-extern annotate_rwlock_cd_t* const annotate_rwlock_create_v;
-extern annotate_rwlock_cd_t* const annotate_rwlock_create_static_v;
-extern annotate_rwlock_cd_t* const annotate_rwlock_destroy_v;
-extern annotate_rwlock_ar_t* const annotate_rwlock_acquired_v;
-extern annotate_rwlock_ar_t* const annotate_rwlock_released_v;
-extern annotate_benign_race_sized_t* const annotate_benign_race_sized_v;
-extern annotate_ignore_t* const annotate_ignore_reads_begin_v;
-extern annotate_ignore_t* const annotate_ignore_reads_end_v;
-extern annotate_ignore_t* const annotate_ignore_writes_begin_v;
-extern annotate_ignore_t* const annotate_ignore_writes_end_v;
-extern annotate_ignore_t* const annotate_ignore_sync_begin_v;
-extern annotate_ignore_t* const annotate_ignore_sync_end_v;
-
 } // namespace detail
 
 FOLLY_ALWAYS_INLINE static void annotate_rwlock_create(
     void const volatile* const addr, char const* const f, int const l) {
-  auto fun = detail::annotate_rwlock_create_v;
-  return kIsSanitizeThread && fun ? fun(f, l, addr) : void();
+  return void();
 }
 
 FOLLY_ALWAYS_INLINE static void annotate_rwlock_create_static(
     void const volatile* const addr, char const* const f, int const l) {
-  auto fun = detail::annotate_rwlock_create_static_v;
-  return kIsSanitizeThread && fun ? fun(f, l, addr) : void();
+  return void();
 }
 
 FOLLY_ALWAYS_INLINE static void annotate_rwlock_destroy(
     void const volatile* const addr, char const* const f, int const l) {
-  auto fun = detail::annotate_rwlock_destroy_v;
-  return kIsSanitizeThread && fun ? fun(f, l, addr) : void();
+  return void();
 }
 
 FOLLY_ALWAYS_INLINE static void annotate_rwlock_acquired(
@@ -71,8 +49,7 @@ FOLLY_ALWAYS_INLINE static void annotate_rwlock_acquired(
     annotate_rwlock_level const w,
     char const* const f,
     int const l) {
-  auto fun = detail::annotate_rwlock_acquired_v;
-  return kIsSanitizeThread && fun ? fun(f, l, addr, long(w)) : void();
+  return void();
 }
 
 FOLLY_ALWAYS_INLINE static void annotate_rwlock_try_acquired(
@@ -81,7 +58,7 @@ FOLLY_ALWAYS_INLINE static void annotate_rwlock_try_acquired(
     bool const result,
     char const* const f,
     int const l) {
-  return result ? annotate_rwlock_acquired(addr, w, f, l) : void();
+  return void();
 }
 
 FOLLY_ALWAYS_INLINE static void annotate_rwlock_released(
@@ -89,8 +66,7 @@ FOLLY_ALWAYS_INLINE static void annotate_rwlock_released(
     annotate_rwlock_level const w,
     char const* const f,
     int const l) {
-  auto fun = detail::annotate_rwlock_released_v;
-  return kIsSanitizeThread && fun ? fun(f, l, addr, long(w)) : void();
+  return void();
 }
 
 FOLLY_ALWAYS_INLINE static void annotate_benign_race_sized(
@@ -99,44 +75,37 @@ FOLLY_ALWAYS_INLINE static void annotate_benign_race_sized(
     char const* const desc,
     char const* const f,
     int const l) {
-  auto fun = detail::annotate_benign_race_sized_v;
-  return kIsSanitizeThread && fun ? fun(f, l, addr, size, desc) : void();
+  return void();
 }
 
 FOLLY_ALWAYS_INLINE static void annotate_ignore_reads_begin(
     char const* const f, int const l) {
-  auto fun = detail::annotate_ignore_reads_begin_v;
-  return kIsSanitizeThread && fun ? fun(f, l) : void();
+  return void();
 }
 
 FOLLY_ALWAYS_INLINE static void annotate_ignore_reads_end(
     char const* const f, int const l) {
-  auto fun = detail::annotate_ignore_reads_end_v;
-  return kIsSanitizeThread && fun ? fun(f, l) : void();
+  return void();
 }
 
 FOLLY_ALWAYS_INLINE static void annotate_ignore_writes_begin(
     char const* const f, int const l) {
-  auto fun = detail::annotate_ignore_writes_begin_v;
-  return kIsSanitizeThread && fun ? fun(f, l) : void();
+  return void();
 }
 
 FOLLY_ALWAYS_INLINE static void annotate_ignore_writes_end(
     char const* const f, int const l) {
-  auto fun = detail::annotate_ignore_writes_end_v;
-  return kIsSanitizeThread && fun ? fun(f, l) : void();
+  return void();
 }
 
 FOLLY_ALWAYS_INLINE static void annotate_ignore_sync_begin(
     char const* const f, int const l) {
-  auto fun = detail::annotate_ignore_sync_begin_v;
-  return kIsSanitizeThread && fun ? fun(f, l) : void();
+  return void();
 }
 
 FOLLY_ALWAYS_INLINE static void annotate_ignore_sync_end(
     char const* const f, int const l) {
-  auto fun = detail::annotate_ignore_sync_end_v;
-  return kIsSanitizeThread && fun ? fun(f, l) : void();
+  return void();
 }
 
 class annotate_ignore_thread_sanitizer_guard {
