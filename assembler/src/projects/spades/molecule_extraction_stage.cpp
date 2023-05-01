e93919a9ec053f0ef84e12fec27337028665092d
1dayac <meleshko.dmitrii@gmail.com>
2023-05-01 11:05:17 -0400

    Fix

--- assembler/src/projects/spades/molecule_extraction_stage.cpp
+++ assembler/src/projects/spades/molecule_extraction_stage.cpp
@@ -607,8 +607,9 @@ namespace debruijn_graph {
         long_reads.FilterPaths(path_extend::LengthPathCondition(300));
         INFO(long_reads.size() << " paths after filtering");
         path_extend::ContigWriter writer(graph_pack.g, std::make_shared<path_extend::BarcodeContigNameGenerator>());
+        path_extend::ContigWriter writer_short(graph_pack.g, std::make_shared<path_extend::DefaultContigNameGenerator>());
         writer.OutputPathsRNA(long_reads, cfg::get().output_dir + "/extracted.fasta");
-        writer.OutputPaths(long_reads, cfg::get().output_dir + "/extracted_short.fasta");
+        writer_short.OutputPathsRNA(long_reads, cfg::get().output_dir + "/extracted_short.fasta");
     }
 
     void MoleculeExtractionStage::run(debruijn_graph::conj_graph_pack &graph_pack, const char *) {
