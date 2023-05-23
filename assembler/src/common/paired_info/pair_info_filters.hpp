ee1c86a355ab48a1dcdd152ecf35d7808488476a
1dayac <meleshko.dmitrii@gmail.com>
2023-05-23 15:38:10 -0400

    Switch back filters

--- assembler/src/common/paired_info/pair_info_filters.hpp
+++ assembler/src/common/paired_info/pair_info_filters.hpp
@@ -239,7 +239,7 @@ public:
     {}
 
     void Filter(PairedInfoIndexT<Graph>& index) {
-        return;
+//        return;
         DEBUG("Start filtering; index size: " << index.size());
         //We can't filter while traversing, because Remove may invalidate iterators
         //So let's save edge pairs first
