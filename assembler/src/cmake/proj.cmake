6e2a27a6aa1fdc67f2d41decc65c358862a6c853
Anton Korobeynikov <anton@korobeynikov.info>
2023-09-18 15:43:35 -0700

    First cut of component-based build system

---
+++ assembler/src/cmake/proj.cmake
@@ -0,0 +1,95 @@
+############################################################################
+# Copyright (c) 2023 SPAdes authors
+# All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
+# -*- cmake -*-
+
+# Side-by-side subprojects layout: automatically set the
+# SPADES_EXTERNAL_${project}_SOURCE_DIR using SPADES_ALL_PROJECTS
+set(SPADES_ALL_PROJECTS "spades;hammer;ionhammer;corrector;spaligner")
+set(SPADES_EXTRA_PROJECTS "mts;online_vis")
+set(SPADES_KNOWN_PROJECTS "${SPADES_ALL_PROJECTS};${SPADES_EXTRA_PROJECTS}")
+set(SPADES_ENABLE_PROJECTS "" CACHE STRING
+    "Semicolon-separated list of projects to build (${SPADES_KNOWN_PROJECTS}), or \"all\".")
+# Make sure expansion happens first to not handle "all" in rest of the checks.
+if (SPADES_ENABLE_PROJECTS STREQUAL "all" )
+  set(SPADES_ENABLE_PROJECTS ${SPADES_ALL_PROJECTS})
+endif()
+
+foreach(proj ${SPADES_ENABLE_PROJECTS})
+  if (NOT proj STREQUAL "spades" AND NOT "${proj}" IN_LIST SPADES_KNOWN_PROJECTS)
+     MESSAGE(FATAL_ERROR "${proj} isn't a known project: ${SPADES_KNOWN_PROJECTS}.")
+  endif()
+endforeach()
+
+# Some inter-project dependencies
+if ("spades" IN_LIST SPADES_ENABLE_PROJECTS)
+  if (NOT "hammer" IN_LIST SPADES_ENABLE_PROJECTS)
+    message(STATUS "Enabling BayesHammer as a dependency to SPAdes")
+    list(APPEND SPADES_ENABLE_PROJECTS "hammer")
+  endif()
+
+  if (NOT "ionhammer" IN_LIST SPADES_ENABLE_PROJECTS)
+    message(STATUS "Enabling IonHammer as a dependency to SPAdes")
+    list(APPEND SPADES_ENABLE_PROJECTS "ionhammer")
+  endif()
+
+  if (NOT "corrector" IN_LIST SPADES_ENABLE_PROJECTS)
+    message(STATUS "Enabling Corrector as a dependency to SPAdes")
+    list(APPEND SPADES_ENABLE_PROJECTS "corrector")
+  endif()
+endif()
+
+# SPADES_ENABLE_PROJECTS_USED is `ON` if the user has ever used the
+# `SPADES_ENABLE_PROJECTS` CMake cache variable.  This exists for
+# several reasons:
+#  * As an indicator that the `SPADES_ENABLE_PROJECTS` list is now the single
+#    source of truth for which projects to build. This means we will ignore user
+#    supplied `SPADES_TOOL_<project>_BUILD` CMake cache variables and overwrite
+#    them.
+#
+#  * The case where the user previously had `SPADES_ENABLE_PROJECTS` set to a
+#    non-empty list but now the user wishes to disable building all other
+#    projects by setting `SPADES_ENABLE_PROJECTS` to an empty string. In that
+#    case we still need to set the `SPADES_TOOL_${upper_proj}_BUILD` variables
+#    so that we disable building all the projects that were previously enabled.
+set(SPADES_ENABLE_PROJECTS_USED OFF CACHE BOOL "")
+mark_as_advanced(SPADES_ENABLE_PROJECTS_USED)
+
+if (SPADES_ENABLE_PROJECTS_USED OR NOT SPADES_ENABLE_PROJECTS STREQUAL "")
+  set(SPADES_ENABLE_PROJECTS_USED ON CACHE BOOL "" FORCE)
+  foreach(proj ${SPADES_KNOWN_PROJECTS} ${SPADES_EXTERNAL_PROJECTS})
+    string(TOUPPER "${proj}" upper_proj)
+    string(REGEX REPLACE "-" "_" upper_proj ${upper_proj})
+    if ("${proj}" IN_LIST SPADES_ENABLE_PROJECTS)
+      message(STATUS "${proj} project is enabled")
+      set(SHOULD_ENABLE_PROJECT TRUE)
+      set(PROJ_DIR ${SPADES_MAIN_PROJ_DIR}/${proj})
+      if (NOT EXISTS "${PROJ_DIR}" OR NOT IS_DIRECTORY "${PROJ_DIR}")
+        message(FATAL_ERROR "SPADES_ENABLE_PROJECTS requests ${proj} but directory not found: ${PROJ_DIR}")
+      endif()
+      if (SPADES_EXTERNAL_${upper_proj}_SOURCE_DIR STREQUAL "")
+        set(SPADES_EXTERNAL_${upper_proj}_SOURCE_DIR "${PROJ_DIR}" CACHE PATH "" FORCE)
+      else()
+        set(SPADES_EXTERNAL_${upper_proj}_SOURCE_DIR "${PROJ_DIR}" CACHE PATH "")
+      endif()
+    elseif ("${proj}" IN_LIST SPADES_EXTERNAL_PROJECTS)
+      message(STATUS "${proj} project is enabled")
+      set(SHOULD_ENABLE_PROJECT TRUE)
+    else()
+      message(STATUS "${proj} project is disabled")
+      set(SHOULD_ENABLE_PROJECT FALSE)
+    endif()
+    # Force `SPADES_TOOL_${upper_proj}_BUILD` variables to have values that
+    # corresponds with `SPADES_ENABLE_PROJECTS`. This prevents the user setting
+    # `SPADES_TOOL_${upper_proj}_BUILD` variables externally.
+    set(SPADES_TOOL_${upper_proj}_BUILD
+      ${SHOULD_ENABLE_PROJECT}
+      CACHE
+      BOOL "Whether to build ${upper_proj} as part of SPAdes" FORCE
+    )
+  endforeach()
+endif()
+unset(SHOULD_ENABLE_PROJECT)
---
+++ assembler/src/cmake/utils.cmake
@@ -0,0 +1,120 @@
+############################################################################
+# Copyright (c) 2023 SPAdes authors
+# All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
+# -*- cmake -*-
+
+function(canonicalize_tool_name name output)
+  string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}/" "" nameStrip ${name})
+  string(REPLACE "-" "_" nameUNDERSCORE ${nameStrip})
+  string(TOUPPER ${nameUNDERSCORE} nameUPPER)
+  set(${output} "${nameUPPER}" PARENT_SCOPE)
+endfunction(canonicalize_tool_name)
+
+# Custom add_subdirectory wrapper
+# Takes in a project name (i.e. SPAdes), the subdirectory name, and an optional
+# path if it differs from the name.
+function(add_spades_subdirectory project type name)
+  set(add_spades_external_dir "${ARGN}")
+  if ("${add_spades_external_dir}" STREQUAL "")
+    set(add_spades_external_dir ${name})
+  endif()
+  canonicalize_tool_name(${name} nameUPPER)
+  set(canonical_full_name ${project}_${type}_${nameUPPER})
+  get_property(already_processed GLOBAL PROPERTY ${canonical_full_name}_PROCESSED)
+  if (already_processed)
+    return()
+  endif()
+  set_property(GLOBAL PROPERTY ${canonical_full_name}_PROCESSED YES)
+
+  if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${add_spades_external_dir}/CMakeLists.txt)
+    # Treat it as in-tree subproject.
+    option(${canonical_full_name}_BUILD
+           "Whether to build ${name} as part of ${project}" On)
+    mark_as_advanced(${project}_${type}_${name}_BUILD)
+    if(${canonical_full_name}_BUILD)
+      message("Building: ${canonical_full_name}")
+      add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/${add_spades_external_dir} ${add_spades_external_dir})
+    endif()
+  else()
+    set(SPADES_EXTERNAL_${nameUPPER}_SOURCE_DIR
+      "${SPADES_EXTERNAL_${nameUPPER}_SOURCE_DIR}"
+      CACHE PATH "Path to ${name} source directory")
+    set(${canonical_full_name}_BUILD_DEFAULT ON)
+    if (NOT SPADES_EXTERNAL_${nameUPPER}_SOURCE_DIR OR NOT EXISTS ${SPADES_EXTERNAL_${nameUPPER}_SOURCE_DIR})
+      set(${canonical_full_name}_BUILD_DEFAULT OFF)
+    endif()
+    if ("${SPADES_EXTERNAL_${nameUPPER}_BUILD}" STREQUAL "OFF")
+      set(${canonical_full_name}_BUILD_DEFAULT OFF)
+    endif()
+    option(${canonical_full_name}_BUILD
+      "Whether to build ${name} as part of SPAdes"
+      ${${canonical_full_name}_BUILD_DEFAULT})
+    if (${canonical_full_name}_BUILD)
+      if (EXISTS ${SPADES_EXTERNAL_${nameUPPER}_SOURCE_DIR})
+        add_subdirectory(${SPADES_EXTERNAL_${nameUPPER}_SOURCE_DIR} ${add_spades_external_dir})
+      elseif(NOT "${SPADES_EXTERNAL_${nameUPPER}_SOURCE_DIR}" STREQUAL "")
+        message(WARNING "Nonexistent directory for ${name}: ${SPADES_EXTERNAL_${nameUPPER}_SOURCE_DIR}")
+      endif()
+    endif()
+  endif()
+endfunction()
+
+# Add external project that may want to be built as part of SPAdes such as SPAligner,
+# Pathracer, and BinSPreader. This adds two options. One for the source directory of the
+# project, which defaults to ${CMAKE_CURRENT_SOURCE_DIR}/${name}. Another to
+# enable or disable building it with everything else.
+# Additional parameter can be specified as the name of directory.
+macro(add_spades_external_project name)
+  add_spades_subdirectory(SPADES TOOL ${name} ${ARGN})
+endmacro()
+
+macro(add_spades_tool_subdirectory name)
+  add_spades_external_project(${name})
+endmacro(add_spades_tool_subdirectory)
+
+function(create_subdirectory_options project type)
+  file(GLOB sub-dirs "${CMAKE_CURRENT_SOURCE_DIR}/*")
+  foreach(dir ${sub-dirs})
+    if (IS_DIRECTORY "${dir}" AND EXISTS "${dir}/CMakeLists.txt")
+      canonicalize_tool_name(${dir} name)
+      option(${project}_${type}_${name}_BUILD
+           "Whether to build ${name} as part of ${project}" On)
+      mark_as_advanced(${project}_${type}_${name}_BUILD)
+    endif()
+  endforeach()
+endfunction(create_subdirectory_options)
+
+function(create_spades_tool_options)
+  create_subdirectory_options(SPADES TOOL)
+endfunction(create_spades_tool_options)
+
+function(spades_add_implicit_projects project)
+  set(list_of_implicit_subdirs "")
+  file(GLOB sub-dirs "${CMAKE_CURRENT_SOURCE_DIR}/*")
+  foreach(dir ${sub-dirs})
+    if(IS_DIRECTORY "${dir}" AND EXISTS "${dir}/CMakeLists.txt")
+      canonicalize_tool_name(${dir} name)
+      # I don't like special casing things by order, but the spades-driver ends up
+      # linking the object libraries from all the tools that opt-in, so adding
+      # it separately at the end is probably the simplest case.
+      if("${name}" STREQUAL "SPADES_DRIVER")
+        continue()
+      endif()
+      if (${project}_TOOL_${name}_BUILD)
+        get_filename_component(fn "${dir}" NAME)
+        list(APPEND list_of_implicit_subdirs "${fn}")
+      endif()
+    endif()
+  endforeach()
+
+  foreach(external_proj ${list_of_implicit_subdirs})
+    add_spades_subdirectory(${project} TOOL "${external_proj}" ${ARGN})
+  endforeach()
+endfunction(spades_add_implicit_projects)
+
+function(add_spades_implicit_projects)
+  spades_add_implicit_projects(SPADES)
+endfunction(add_spades_implicit_projects)
