96c56783063fa786320330ee89379f4f08c6b1cf
Anton Korobeynikov <anton@korobeynikov.info>
2024-09-18 15:02:48 -0700

    Add spades.py bits

--- src/projects/spades/pipeline/spades_pipeline/executors/executor_save_mpi_sh.py
+++ src/projects/spades/pipeline/spades_pipeline/executors/executor_save_mpi_sh.py
@@ -2,7 +2,6 @@
 
 ############################################################################
 # Copyright (c) 2023-2024 SPAdes team
-# Copyright (c) 2019-2022 Saint Petersburg State University
 # All Rights Reserved
 # See file LICENSE for details.
 ############################################################################
@@ -19,14 +18,14 @@ class Executor(executors.ExecutorBase):
 
     def execute(self, commands):
         super(Executor, self).execute(commands)
-        commands_parser.write_commands_to_sh(commands, os.path.join(options_storage.args.output_dir, "run_spades.sh"))
+        commands_parser.write_commands_to_mpi_sh(commands, os.path.join(options_storage.args.output_dir, "run_spades.sh"))
         commands_parser.write_commands_to_yaml(commands,
                                                os.path.join(options_storage.args.output_dir,
                                                "run_spades.yaml"))
         return None
 
     def dump_commands(self, commands, outputfile):
-        commands_parser.write_commands_to_sh(commands, outputfile)
+        commands_parser.write_commands_to_mpi_sh(commands, outputfile)
 
     def join(self, job_name):
         assert (job_name is None)
