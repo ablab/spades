a3df15c57f47cf0ecdccf495c36e6421ca7cdd16
Itolstoganov <itolstoganov@gmail.com>
2026-05-25 16:42:03 +0200

    Add virtual destructors

--- src/common/auxiliary_graphs/contracted_graph/contracted_graph_helper.hpp
+++ src/common/auxiliary_graphs/contracted_graph/contracted_graph_helper.hpp
@@ -15,13 +15,8 @@ class ContractedGraphFactoryHelper {
  public:
     typedef debruijn_graph::VertexId VertexId;
     typedef debruijn_graph::Graph Graph;
-
-    explicit ContractedGraphFactoryHelper(const debruijn_graph::Graph &g) : g_(g) {}
-
+    
     std::shared_ptr<ContractedGraph> ExtractContractedSubgraph(const ContractedGraph &other,
                                                                const std::unordered_set<VertexId> &vertices) const;
-
-  private:
-    const Graph& g_;
 };
 }
